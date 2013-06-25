acd8e9a062066960cb0d2c2bafeaf6aeb36fcaa4
Dan Macumber <daniel.macumber@nrel.gov>
2013-06-25 20:28:45 +0000

    Add specific end uses for results
    [#52332931]

--- openstudiocore/src/sdd/MapGeometry.cpp
+++ openstudiocore/src/sdd/MapGeometry.cpp
@@ -528,6 +528,7 @@ namespace sdd {
         openstudio::model::Lights lights(lightsDefinition);
         lights.setName(name + " Regulated Lights");
         lights.setSpace(space);
+        lights.setEndUseSubcategory("Reg Ltg");
 
         if (!intLtgRegSchRefElement.isNull()){
           std::string scheduleName = escapeName(intLtgRegSchRefElement.text());
@@ -556,6 +557,7 @@ namespace sdd {
         openstudio::model::Lights lights(lightsDefinition);
         lights.setName(name + " Non-Regulated Lights");
         lights.setSpace(space);
+        lights.setEndUseSubcategory("NonReg Ltg");
 
         if (!intLPDNonRegSchRefElement.isNull()){
           std::string scheduleName = escapeName(intLPDNonRegSchRefElement.text());
@@ -574,9 +576,15 @@ namespace sdd {
 
       //<RecptPwrDens>2.53</RecptPwrDens> - W per ft2
       //<RecptSchRef>Office Plugs Sched</RecptSchRef>
+      //<RecptRadFrac>0.2</RecptRadFrac>
+      //<RecptLatFrac>0</RecptLatFrac>
+      //<RecptLostFrac>0</RecptLostFrac>
 
       QDomElement recptPwrDensElement = element.firstChildElement("RecptPwrDens");
       QDomElement recptPwrDensSchRefElement = element.firstChildElement("RecptSchRef");
+      QDomElement recptRadFracElement = element.firstChildElement("RecptRadFrac");
+      QDomElement recptLatFracElement = element.firstChildElement("RecptLatFrac");
+      QDomElement recptLostFracElement = element.firstChildElement("RecptLostFrac");
       if (!recptPwrDensElement.isNull() && (recptPwrDensElement.text().toDouble() > 0)){
 
         openstudio::Quantity electricalDensityIP(recptPwrDensElement.text().toDouble(), openstudio::createUnit("W/ft^2").get());
@@ -588,9 +596,20 @@ namespace sdd {
         electricEquipmentDefinition.setName(name + " Recepticle Loads Definition");
         electricEquipmentDefinition.setWattsperSpaceFloorArea(electricalDensitySI->value()); // W/m2
 
+        if (!recptRadFracElement.isNull()){
+          electricEquipmentDefinition.setFractionRadiant(recptRadFracElement.text().toDouble());
+        }
+        if (!recptLatFracElement.isNull()){
+          electricEquipmentDefinition.setFractionLatent(recptLatFracElement.text().toDouble());
+        }
+        if (!recptLostFracElement.isNull()){
+          electricEquipmentDefinition.setFractionLost(recptLostFracElement.text().toDouble());
+        }
+
         openstudio::model::ElectricEquipment electricEquipment(electricEquipmentDefinition);
         electricEquipment.setName(name + " Recepticle Loads");
         electricEquipment.setSpace(space);
+        electricEquipment.setEndUseSubcategory("Receptacle");
 
         if (!recptPwrDensSchRefElement.isNull()){
           std::string scheduleName = escapeName(recptPwrDensSchRefElement.text());
@@ -604,17 +623,71 @@ namespace sdd {
       }
     }
 
+    //***** Gas Equipment Loads *****
+    {
+
+      //<GasEqpPwrDens>17.5377</GasEqpPwrDens>
+      //<GasEqpSchRef>RestaurantReceptacle</GasEqpSchRef>
+      //<GasEqpRadFrac>0.2</GasEqpRadFrac>
+      //<GasEqpLatFrac>0.4</GasEqpLatFrac>
+      //<GasEqpLostFrac>0.2></GasEqpLostFrac>
+
+      QDomElement gasEqpPwrDensElement = element.firstChildElement("GasEqpPwrDens");
+      QDomElement gasEqpPwrDensSchRefElement = element.firstChildElement("GasEqpSchRef");
+      QDomElement gasEqpRadFracElement = element.firstChildElement("GasEqpRadFrac");
+      QDomElement gasEqpLatFracElement = element.firstChildElement("GasEqpLatFrac");
+      QDomElement gasEqpLostFracElement = element.firstChildElement("GasEqpLostFrac");
+      if (!gasEqpPwrDensElement.isNull() && (gasEqpPwrDensElement.text().toDouble() > 0)){
+
+        openstudio::Quantity gasDensityIP(gasEqpPwrDensElement.text().toDouble(), openstudio::createUnit("Btu/h*ft^2").get());
+        OptionalQuantity gasDensitySI = QuantityConverter::instance().convert(gasDensityIP, whSys);
+        BOOST_ASSERT(gasDensitySI);
+        BOOST_ASSERT(gasDensitySI->units() == WhUnit(WhExpnt(1,0,-2)));
+
+        openstudio::model::GasEquipmentDefinition gasEquipmentDefinition(model);
+        gasEquipmentDefinition.setName(name + " Gas Equipment Loads Definition");
+        gasEquipmentDefinition.setWattsperSpaceFloorArea(gasDensitySI->value()); // W/m2
+
+        if (!gasEqpRadFracElement.isNull()){
+          gasEquipmentDefinition.setFractionRadiant(gasEqpRadFracElement.text().toDouble());
+        }
+        if (!gasEqpLatFracElement.isNull()){
+          gasEquipmentDefinition.setFractionLatent(gasEqpLatFracElement.text().toDouble());
+        }
+        if (!gasEqpLostFracElement.isNull()){
+          gasEquipmentDefinition.setFractionLost(gasEqpLostFracElement.text().toDouble());
+        }
+
+        openstudio::model::GasEquipment gasEquipment(gasEquipmentDefinition);
+        gasEquipment.setName(name + " Gas Equipment Loads");
+        gasEquipment.setSpace(space);
+        gasEquipment.setEndUseSubcategory("Receptacle");
+
+        if (!gasEqpPwrDensSchRefElement.isNull()){
+          std::string scheduleName = escapeName(gasEqpPwrDensSchRefElement.text());
+          boost::optional<model::Schedule> schedule = model.getModelObjectByName<model::Schedule>(scheduleName);
+          if (schedule){
+            gasEquipment.setSchedule(*schedule);
+          }else{
+            LOG(Error, "Could not find schedule '" << scheduleName << "'");
+          }
+        }
+      }
+    }
+
     //***** Process Electricity Loads *****
     {
       //<ProcElecPwrDens>0</ProcElecPwrDens> - W per ft2
-      //<ProcElecSensHtRt>0</ProcElecSensHtRt> - W per ft2
-      //<ProcElecLatHtRt>0</ProcElecLatHtRt> - W per ft2
       //<ProcElecSchRef>Office Plugs Sched</ProcElecSchRef>
+      //<ProcElecRadFrac>0.2</ProcElecRadFrac>
+      //<ProcElecLatFrac>0</ProcElecLatFrac>
+      //<ProcElecLostFrac>0</ProcElecLostFrac>
 
       QDomElement procElecPwrDensElement = element.firstChildElement("ProcElecPwrDens");
-      QDomElement procElecSensHtRtElement = element.firstChildElement("ProcElecSensHtRt");
-      QDomElement procElecLatHtRtElement = element.firstChildElement("ProcElecLatHtRt");
       QDomElement procElecSchRefElement = element.firstChildElement("ProcElecSchRef");
+      QDomElement procElecRadFracElement = element.firstChildElement("ProcElecRadFrac");
+      QDomElement procElecLatFracElement = element.firstChildElement("ProcElecLatFrac");
+      QDomElement procElecLostFracElement = element.firstChildElement("ProcElecLostFrac");
       if (!procElecPwrDensElement.isNull() && (procElecPwrDensElement.text().toDouble() > 0)){
 
         openstudio::Quantity electricalDensityIP(procElecPwrDensElement.text().toDouble(), openstudio::createUnit("W/ft^2").get());
@@ -626,14 +699,20 @@ namespace sdd {
         electricEquipmentDefinition.setName(name + " Process Electric Loads Definition");
         electricEquipmentDefinition.setWattsperSpaceFloorArea(electricalDensitySI->value()); // W/m2
 
-        if (!procElecSensHtRtElement.isNull() && !procElecLatHtRtElement.isNull()){
-          double fractionLatent = procElecLatHtRtElement.text().toDouble() / (procElecSensHtRtElement.text().toDouble() + procElecLatHtRtElement.text().toDouble());
-          electricEquipmentDefinition.setFractionLatent(fractionLatent);
+        if (!procElecRadFracElement.isNull()){
+          electricEquipmentDefinition.setFractionRadiant(procElecRadFracElement.text().toDouble());
+        }
+        if (!procElecLatFracElement.isNull()){
+          electricEquipmentDefinition.setFractionLatent(procElecLatFracElement.text().toDouble());
+        }
+        if (!procElecLostFracElement.isNull()){
+          electricEquipmentDefinition.setFractionLost(procElecLostFracElement.text().toDouble());
         }
 
         openstudio::model::ElectricEquipment electricEquipment(electricEquipmentDefinition);
         electricEquipment.setName(name + " Process Electric Loads");
         electricEquipment.setSpace(space);
+        electricEquipment.setEndUseSubcategory("Process");
 
         if (!procElecSchRefElement.isNull()){
           std::string scheduleName = escapeName(procElecSchRefElement.text());
@@ -651,9 +730,15 @@ namespace sdd {
     {
       //<CommRfrgEPD>0.06</CommRfrgEPD> - W per ft2
       //<CommRfrgEqpSchRef>Office Plugs Sched</CommRfrgEqpSchRef>
+      //<CommRfrgRadFrac>0.1</CommRfrgRadFrac>
+      //<CommRfrgLatFrac>0.1</CommRfrgLatFrac>
+      //<CommRfrgLostFrac>0.6</CommRfrgLostFrac>
 
       QDomElement commRfrgEPDElement = element.firstChildElement("CommRfrgEPD");
       QDomElement commRfrgEqpSchRefElement = element.firstChildElement("CommRfrgEqpSchRef");
+      QDomElement commRfrgRadFracElement = element.firstChildElement("CommRfrgRadFrac");
+      QDomElement commRfrgLatFracElement = element.firstChildElement("CommRfrgLatFrac");
+      QDomElement commRfrgLostFracElement = element.firstChildElement("CommRfrgLostFrac");
       if (!commRfrgEPDElement.isNull() && (commRfrgEPDElement.text().toDouble() > 0)){
 
         openstudio::Quantity electricalDensityIP(commRfrgEPDElement.text().toDouble(), openstudio::createUnit("W/ft^2").get());
@@ -665,9 +750,20 @@ namespace sdd {
         electricEquipmentDefinition.setName(name + " Refrigeration Loads Definition");
         electricEquipmentDefinition.setWattsperSpaceFloorArea(electricalDensitySI->value()); // W/m2
 
+        if (!commRfrgRadFracElement.isNull()){
+          electricEquipmentDefinition.setFractionRadiant(commRfrgRadFracElement.text().toDouble());
+        }
+        if (!commRfrgLatFracElement.isNull()){
+          electricEquipmentDefinition.setFractionLatent(commRfrgLatFracElement.text().toDouble());
+        }
+        if (!commRfrgLostFracElement.isNull()){
+          electricEquipmentDefinition.setFractionLost(commRfrgLostFracElement.text().toDouble());
+        }
+
         openstudio::model::ElectricEquipment electricEquipment(electricEquipmentDefinition);
         electricEquipment.setName(name + " Refrigeration Loads");
         electricEquipment.setSpace(space);
+        electricEquipment.setEndUseSubcategory("Refrig");
 
         if (!commRfrgEqpSchRefElement.isNull()){
           std::string scheduleName = escapeName(commRfrgEqpSchRefElement.text());
@@ -702,6 +798,7 @@ namespace sdd {
         openstudio::model::ElectricEquipment electricEquipment(electricEquipmentDefinition);
         electricEquipment.setName(name + " Elevator and Escalator Loads");
         electricEquipment.setSpace(space);
+        electricEquipment.setEndUseSubcategory("Process");
 
         if (!elevEscalSchRefElement.isNull()){
           std::string scheduleName = escapeName(elevEscalSchRefElement.text());
@@ -719,9 +816,15 @@ namespace sdd {
     {
       //<ProcGasPwrDens>0.04</ProcGasPwrDens> - Btu per h ft2
       //<ProcGasSchRef>Office Plugs Sched</ProcGasSchRef>
+      //<ProcGasRadFrac>0.2</ProcGasRadFrac>
+      //<ProcGasLatFrac>0.4</ProcGasLatFrac>
+      //<ProcGasLostFrac>0.2</ProcGasLostFrac>
 
       QDomElement procGasPwrDensElement = element.firstChildElement("ProcGasPwrDens");
       QDomElement procGasSchRefElement = element.firstChildElement("ProcGasSchRef");
+      QDomElement procGasRadFracElement = element.firstChildElement("ProcGasRadFrac");
+      QDomElement procGasLatFracElement = element.firstChildElement("ProcGasLatFrac");
+      QDomElement procGasLostFracElement = element.firstChildElement("ProcGasLostFrac");
       if (!procGasPwrDensElement.isNull() && (procGasPwrDensElement.text().toDouble() > 0)){
 
         openstudio::Quantity gasDensityIP(procGasPwrDensElement.text().toDouble(), openstudio::createUnit("Btu/h*ft^2").get());
@@ -733,9 +836,20 @@ namespace sdd {
         gasEquipmentDefinition.setName(name + " Gas Loads Definition");
         gasEquipmentDefinition.setWattsperSpaceFloorArea(gasDensitySI->value()); // W/m2
 
+        if (!procGasRadFracElement.isNull()){
+          gasEquipmentDefinition.setFractionRadiant(procGasRadFracElement.text().toDouble());
+        }
+        if (!procGasLatFracElement.isNull()){
+          gasEquipmentDefinition.setFractionLatent(procGasLatFracElement.text().toDouble());
+        }
+        if (!procGasLostFracElement.isNull()){
+          gasEquipmentDefinition.setFractionLost(procGasLostFracElement.text().toDouble());
+        }
+
         openstudio::model::GasEquipment gasEquipment(gasEquipmentDefinition);
         gasEquipment.setName(name + " Gas Loads");
         gasEquipment.setSpace(space);
+        gasEquipment.setEndUseSubcategory("Process");
 
         if (!procGasSchRefElement.isNull()){
           std::string scheduleName = escapeName(procGasSchRefElement.text());
