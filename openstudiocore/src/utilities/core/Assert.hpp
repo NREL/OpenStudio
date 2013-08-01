db8a532505cb3c0879ea1a6ef3036fc1e91e874d
elainethale <elaine.hale@nrel.gov>
2013-08-01 16:51:19 -0600

    Bad guess on include file name. Issue #410. [#54484554]

--- openstudiocore/src/utilities/core/Assert.hpp
+++ openstudiocore/src/utilities/core/Assert.hpp
@@ -21,6 +21,15 @@
 #define UTILITIES_CORE_ASSERT_HPP
 
 /****************************************************************************
+!!! THIS FILE MUST BE INCLUDED BY ANY SOURCE FILE THAT USES Q_ASSERT!!!
+*****************************************************************************/
+
+// enable Q_ASSERTs in Release mode
+#ifdef QT_NO_DEBUG
+  #undef QT_NO_DEBUG
+#endif
+
+/****************************************************************************
 !!! THIS FILE MUST BE INCLUDED BY ANY SOURCE FILE THAT USES BOOST_ASSERT!!!
 *****************************************************************************/
 
