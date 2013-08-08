ebca86055d061d66a4017348ef8fb389ec24223a
Elaine Hale <elaine.hale@nrel.gov>
2013-08-08 09:16:07 -0600

    Suppressing more dashboard warning reports that are not our fault.
    [#0]

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -40,4 +40,8 @@ set(CTEST_CUSTOM_COVERAGE_EXCLUDE
 set(CTEST_CUSTOM_WARNING_EXCEPTION
     ${CTEST_CUSTOM_WARNING_EXCEPTION}
     ".*/VTK-prefix/src/VTK/.*"
+    ".*/src/libssh/.*"
+    ".*/src/sqlite/.*"
+    ".*/src/litesql/.*"
+    ".*warning: variable 'klass' set but not used.*"
 )
