2a0bd97fa989da5e16471082c35d4cb211fac720
Dan Macumber <dan.macumber@gmail.com>
2013-08-25 18:13:47 -0600

    Use defaults in forward translators for blind, screen, and shade
    [#55749402]

--- openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateScreen.cpp
+++ openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateScreen.cpp
@@ -38,75 +38,75 @@ boost::optional<IdfObject> ForwardTranslator::translateScreen( Screen & modelObj
 
   idfObject.setString(WindowMaterial_ScreenFields::Name, modelObject.name().get());
   
-  OptionalString s = modelObject.getString(OS_WindowMaterial_ScreenFields::ReflectedBeamTransmittanceAccountingMethod, false, true);
+  OptionalString s = modelObject.getString(OS_WindowMaterial_ScreenFields::ReflectedBeamTransmittanceAccountingMethod, true, true);
   if (s){
     idfObject.setString(WindowMaterial_ScreenFields::ReflectedBeamTransmittanceAccountingMethod, *s);
   }
 
-  OptionalDouble d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::DiffuseSolarReflectance, false);
+  OptionalDouble d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::DiffuseSolarReflectance, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::DiffuseSolarReflectance, *d);
   }else{
     LOG(Error, "Missing required input 'Site Diffuse Solar Radiation Rate per Area Reflectance' for WindowMaterial:Screen named '" << modelObject.name().get() << "'");
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::DiffuseVisibleReflectance, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::DiffuseVisibleReflectance, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::DiffuseVisibleReflectance, *d);
   }else{
     LOG(Error, "Missing required input 'Diffuse Visible Reflectance' for WindowMaterial:Screen named '" << modelObject.name().get() << "'");
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ThermalHemisphericalEmissivity, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ThermalHemisphericalEmissivity, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::ThermalHemisphericalEmissivity, *d);
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::Conductivity, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::Conductivity, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::Conductivity, *d);
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ScreenMaterialSpacing, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ScreenMaterialSpacing, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::ScreenMaterialSpacing, *d);
   }else{
     LOG(Error, "Missing required input 'Screen Material Spacing' for WindowMaterial:Screen named '" << modelObject.name().get() << "'");
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ScreenMaterialDiameter, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ScreenMaterialDiameter, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::ScreenMaterialDiameter, *d);
   }else{
     LOG(Error, "Missing required input 'Screen Material Diameter' for WindowMaterial:Screen named '" << modelObject.name().get() << "'");
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ScreentoGlassDistance, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::ScreentoGlassDistance, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::ScreentoGlassDistance, *d);
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::TopOpeningMultiplier, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::TopOpeningMultiplier, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::TopOpeningMultiplier, *d);
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::BottomOpeningMultiplier, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::BottomOpeningMultiplier, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::BottomOpeningMultiplier, *d);
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::LeftSideOpeningMultiplier, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::LeftSideOpeningMultiplier, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::LeftSideOpeningMultiplier, *d);
   }
 
-  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::RightSideOpeningMultiplier, false);
+  d = modelObject.getDouble(OS_WindowMaterial_ScreenFields::RightSideOpeningMultiplier, true);
   if (d){
     idfObject.setDouble(WindowMaterial_ScreenFields::RightSideOpeningMultiplier, *d);
   }
 
-  s = modelObject.getString(OS_WindowMaterial_ScreenFields::AngleofResolutionforScreenTransmittanceOutputMap, false, true);
+  s = modelObject.getString(OS_WindowMaterial_ScreenFields::AngleofResolutionforScreenTransmittanceOutputMap, true, true);
   if (s){
     idfObject.setString(WindowMaterial_ScreenFields::AngleofResolutionforScreenTransmittanceOutputMap, *s);
   }
