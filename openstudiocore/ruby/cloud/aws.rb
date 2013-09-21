4f7c3818dddab6818d104a75817379e785598be9
Nicholas Long <nicholas.long@nrel.gov>
2013-09-20 23:09:54 -0600

    updating amis

--- openstudiocore/ruby/cloud/aws.rb
+++ openstudiocore/ruby/cloud/aws.rb
@@ -66,8 +66,8 @@ AWS.config(
     :region => ARGV[2],
     :ssl_verify_peer => false
 )
-@server_image_id = 'ami-fb7b3092'
-@worker_image_id = 'ami-e77b308e'
+@server_image_id = 'ami-155e147c'
+@worker_image_id = 'ami-c1450fa8'
 
 if ARGV[3] == "EC2"
   @aws = AWS::EC2.new
