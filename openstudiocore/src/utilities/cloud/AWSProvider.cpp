d838268a4ed0821185ab5a1b23c1aba2c607bb8e
Alex Swindler <Alex.Swindler@nrel.gov>
2013-10-02 15:50:23 -0600

    Updated aws.rb so that CMake configures OPENSTUDIO_VERSION aws.rb and its associated files are now installed with packages All Ruby 2.0 packages have been updated with the latest aws-sdk gem to support the recent EC2 API change

--- openstudiocore/src/utilities/cloud/AWSProvider.cpp
+++ openstudiocore/src/utilities/cloud/AWSProvider.cpp
@@ -468,7 +468,6 @@ namespace openstudio{
       : CloudProvider_Impl(),
         m_awsSettings(),
         m_awsSession(toString(createUUID()),boost::none,std::vector<Url>()),
-        m_script(getOpenStudioRubyScriptsPath() / toPath("cloud/aws.rb")),
         m_checkInternetProcess(0),
         m_checkServiceProcess(0),
         m_checkValidateProcess(0),
@@ -507,6 +506,19 @@ namespace openstudio{
 #endif
       if (!boost::filesystem::exists(m_ruby)) {
         LOG_AND_THROW("Ruby 2.0 executable cannot be found.");
+      }
+
+      if (applicationIsRunningFromBuildDirectory())
+      {
+        m_script = getApplicationBuildDirectory() / openstudio::toPath("ruby/aws.rb");
+      } else {
+#ifdef Q_OS_LINUX
+        m_script = getApplicationInstallDirectory() / openstudio::toPath("share/openstudio/Ruby/cloud/aws.rb");
+#elif defined(Q_OS_MAC)
+        m_script = getApplicationRunDirectory().parent_path().parent_path().parent_path() / openstudio::toPath("Ruby/cloud/aws.rb");
+#else
+        m_script = getApplicationRunDirectory().parent_path() / openstudio::toPath("Ruby/cloud/aws.rb");
+#endif
       }
       if (!boost::filesystem::exists(m_script)) {
         LOG_AND_THROW("AWS script cannot be found.");
