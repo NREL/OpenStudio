23c2575cd9a6926d0bfc17520c4cb88ab6444484
Rob Guglielmetti <robert.guglielmetti@nrel.gov>
2013-10-11 15:29:59 -0600

    added a building-average daylight autonomy

--- openstudiocore/ruby/openstudio/radiance/DaylightMetrics.rb
+++ openstudiocore/ruby/openstudio/radiance/DaylightMetrics.rb
@@ -108,6 +108,9 @@ exteriorIlluminanceTimeseries = eplusoutFile.timeSeries("Run Period 1".upcase, "
 # summary report string
 summary_report = ""
 
+building_average_space = []
+building_average = ""
+
 # loop through all the spaces
 building = model.getBuilding
 building.spaces.each do |space|
@@ -366,8 +369,16 @@ building.spaces.each do |space|
   summary_report += "#{space_name}: UDI(#{daylightSetpoint}) Daylit and Occupied Hours, #{cda_daylit_occupied_sum}, #{cda_daylit_occupied_num}, #{annual_udi_daylit_occupied}\n"
 
   # now replace nil with 0 in each timeseries to radout.sql for plotting
+  
+  building_average_space << annual_da_daylit_occupied
+  
 end
 
+building_average_space_sum = 0
+building_average_space.each {|e| building_average_space_sum += e}
+building_average = building_average_space_sum / building_average_space.length
+
 File.open('./DaylightingMetrics.csv', 'w') do |file|
+  file.puts "Building average daylight autonomy: #{building_average}"
   file.puts summary_report
 end
