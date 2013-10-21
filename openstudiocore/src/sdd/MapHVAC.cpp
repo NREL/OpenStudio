2e64d96ffbfe5baac3f640e99b2ff5eccd5eb9e2
Kyle Benne <kyle.benne@nrel.gov>
2013-10-21 15:43:53 -0500

    SDD: Generate "offset" schedules for min vent
    When translating OASchMthd where the method is FollowAvailability, we
    clone the availability schedule and apply start and stop offsets.  In
    this way the ventilation follows the availability schedule of the system
    with a start and stop lag, useful during morning warmup for instance.
    
    One limitation of this implementation is that it is built around
    ScheduleYear -> ScheduleWeek -> ScheduleDay.  If the ScheduleWeek
    references the same ScheduleDay for several days of the week, this clone
    method is not sophisticated enough to recognize the shared ScheduleDay.
    The cloned ScheduleWeek will have unique schedules for each day of the
    week.

--- openstudiocore/src/sdd/MapHVAC.cpp
+++ openstudiocore/src/sdd/MapHVAC.cpp
@@ -47,6 +47,11 @@
 #include <model/Curve_Impl.hpp>
 #include <model/ScheduleRuleset.hpp>
 #include <model/ScheduleDay.hpp>
+#include <model/ScheduleDay_Impl.hpp>
+#include <model/ScheduleYear.hpp>
+#include <model/ScheduleYear_Impl.hpp>
+#include <model/ScheduleWeek.hpp>
+#include <model/ScheduleWeek_Impl.hpp>
 #include <model/SizingZone.hpp>
 #include <model/SizingZone_Impl.hpp>
 #include <model/Node.hpp>
@@ -141,6 +146,7 @@
 #include <utilities/units/WhUnit.hpp>
 #include <utilities/core/Assert.hpp>
 #include <utilities/time/Time.hpp>
+#include <utilities/time/Date.hpp>
 #include <utilities/units/UnitFactory.hpp>
 #include <utilities/math/FloatCompare.hpp>
 
@@ -155,6 +161,228 @@ namespace sdd {
 
 const double cpWater = 4180.0;
 const double densityWater = 1000.0;
+
+// Adjust scheduleDay by delaying the start and stop by the respective offset
+// This is used by the OA Controller ventilation schedule
+void adjustScheduleDay(model::ScheduleDay & scheduleDay, int startOffset, int endOffset)
+{
+  std::vector<double> values = scheduleDay.values();
+  std::vector<Time> times = scheduleDay.times();
+  
+  std::vector<double>::iterator valueIt;
+  std::vector<Time>::iterator timeIt;
+
+  timeIt = times.begin() + 1;
+  for( valueIt = values.begin() + 1; valueIt < values.end(); valueIt++ )
+  {
+    if( equal<double>(*valueIt,1.0,0.01) && equal<double>(*(valueIt - 1),0.0,0.01) )
+    {
+      Time newStartTime;
+
+      if( startOffset < 0 )
+      {
+        newStartTime = *(timeIt - 1) + Time(0,std::abs(startOffset));
+      }
+      else
+      {
+        newStartTime = *(timeIt - 1) - Time(0,startOffset);
+      }
+
+      scheduleDay.removeValue(*(timeIt - 1));
+
+      scheduleDay.addValue(newStartTime,0.0);
+    }
+
+    timeIt++;
+  }
+
+  timeIt = times.begin();
+  for( valueIt = values.begin(); valueIt < (values.end() - 1); valueIt++ )
+  {
+    if( equal<double>(*valueIt,0.0,0.01) && equal<double>(*(valueIt - 1),1.0,0.01) )
+    {
+      Time newEndTime;
+
+      if( endOffset < 0 )
+      {
+        newEndTime = *(timeIt - 1) - Time(0,std::abs(endOffset));
+      }
+      else
+      {
+        newEndTime = *(timeIt - 1) + Time(0,endOffset);
+      }
+
+      scheduleDay.removeValue(*(timeIt - 1));
+
+      scheduleDay.addValue(newEndTime,1.0);
+    }
+
+    timeIt++;
+  }
+}
+
+void adjustSchedule(model::ScheduleYear & scheduleYear, int startOffset, int endOffset)
+{
+  std::vector<model::ScheduleWeek> scheduleWeeks = scheduleYear.scheduleWeeks();
+
+  for( std::vector<model::ScheduleWeek>::iterator it = scheduleWeeks.begin();
+       it != scheduleWeeks.end();
+       it++ )
+  {
+    std::vector<boost::optional<model::ScheduleDay> > scheduleDays;
+
+    scheduleDays.push_back(it->sundaySchedule());
+
+    scheduleDays.push_back(it->mondaySchedule());
+
+    scheduleDays.push_back(it->tuesdaySchedule());
+
+    scheduleDays.push_back(it->wednesdaySchedule());
+
+    scheduleDays.push_back(it->thursdaySchedule());
+
+    scheduleDays.push_back(it->fridaySchedule());
+
+    scheduleDays.push_back(it->saturdaySchedule());
+
+    scheduleDays.push_back(it->holidaySchedule());
+
+    scheduleDays.push_back(it->summerDesignDaySchedule());
+
+    scheduleDays.push_back(it->winterDesignDaySchedule());
+
+    scheduleDays.push_back(it->customDay1Schedule());
+
+    scheduleDays.push_back(it->customDay2Schedule());
+
+    for( std::vector<boost::optional<model::ScheduleDay> >::iterator scheduleDayIt = scheduleDays.begin();
+         scheduleDayIt != scheduleDays.end();
+         scheduleDayIt++ )
+    {
+      if( *scheduleDayIt )
+      {
+        adjustScheduleDay( (*scheduleDayIt).get(), startOffset, endOffset );
+      }
+    }
+  }
+}
+
+// Make a deep copy into same model as scheduleYear
+model::ScheduleYear deepScheduleYearClone(const model::ScheduleYear & scheduleYear, const std::string & name)
+{
+  model::Model model = scheduleYear.model();
+
+  model::ScheduleYear scheduleYearClone = scheduleYear.clone(model).cast<model::ScheduleYear>();
+  scheduleYearClone.setName(name);
+  scheduleYearClone.clearScheduleWeeks();
+
+  std::vector<model::ScheduleWeek> scheduleWeeks = scheduleYear.scheduleWeeks();
+  std::vector<Date> dates = scheduleYear.dates();
+
+  std::vector<Date>::iterator dateIt = dates.begin();
+
+  int i = 1;
+
+  for( std::vector<model::ScheduleWeek>::iterator it = scheduleWeeks.begin();
+       it != scheduleWeeks.end();
+       it++ )
+  {
+    model::ScheduleWeek scheduleWeekClone = it->clone(model).cast<model::ScheduleWeek>();
+    scheduleWeekClone.setName(name + " Week " + QString::number(i).toStdString());
+    scheduleYearClone.addScheduleWeek(*dateIt,scheduleWeekClone);
+
+    boost::optional<model::ScheduleDay> scheduleDay;
+
+    if( (scheduleDay = it->sundaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Sunday");
+      scheduleWeekClone.setSundaySchedule(s);
+    }
+
+    if( (scheduleDay = it->mondaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Monday");
+      scheduleWeekClone.setMondaySchedule(s);
+    }
+
+    if( (scheduleDay = it->tuesdaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Tuesday");
+      scheduleWeekClone.setTuesdaySchedule(s);
+    }
+
+    if( (scheduleDay = it->wednesdaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Wednesday");
+      scheduleWeekClone.setWednesdaySchedule(s);
+    }
+
+    if( (scheduleDay = it->thursdaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Thursday");
+      scheduleWeekClone.setThursdaySchedule(s);
+    }
+
+    if( (scheduleDay = it->fridaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Friday");
+      scheduleWeekClone.setFridaySchedule(s);
+    }
+
+    if( (scheduleDay = it->saturdaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Saturday");
+      scheduleWeekClone.setSaturdaySchedule(s);
+    }
+
+    if( (scheduleDay = it->holidaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Holiday");
+      scheduleWeekClone.setHolidaySchedule(s);
+    }
+
+    if( (scheduleDay = it->summerDesignDaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Summer");
+      scheduleWeekClone.setSummerDesignDaySchedule(s);
+    }
+
+    if( (scheduleDay = it->winterDesignDaySchedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Winter");
+      scheduleWeekClone.setWinterDesignDaySchedule(s);
+    }
+
+    if( (scheduleDay = it->customDay1Schedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Custom 1");
+      scheduleWeekClone.setCustomDay1Schedule(s);
+    }
+
+    if( (scheduleDay = it->customDay2Schedule()) )
+    {
+      model::ScheduleDay s = scheduleDay->clone(model).cast<model::ScheduleDay>();
+      s.setName(name + " Week " + QString::number(i).toStdString() + " Custom 2");
+      scheduleWeekClone.setCustomDay2Schedule(s);
+    }
+
+    dateIt++;
+    i++;
+  }
+
+  return scheduleYearClone;
+}
 
 model::Schedule ReverseTranslator::defaultDeckTempSchedule(openstudio::model::Model& model)
 {
@@ -486,15 +714,35 @@ boost::optional<openstudio::model::ModelObject> ReverseTranslator::translateAirS
         model::Schedule schedule = alwaysOnSchedule(model);
         oaController.setMinimumOutdoorAirSchedule(schedule);
       }
-      else if( istringEqual(oaSchMthdElement.text().toStdString(),"FollowAvailability") )
+      else if( istringEqual(oaSchMthdElement.text().toStdString(),"FollowAvailability") && availabilitySchedule )
       {
-        if( availabilitySchedule )
+        if( boost::optional<model::ScheduleYear> availabilityScheduleYear = availabilitySchedule->optionalCast<model::ScheduleYear>() )
         {
-          model::Schedule schedule = availabilitySchedule->clone(model).cast<model::Schedule>();
+          model::ScheduleYear schedule = deepScheduleYearClone(availabilityScheduleYear.get(),nameElement.text().toStdString() + " Schedule");
 
-          schedule.setName(nameElement.text().toStdString() + " OA Schedule");
+          int startOffset = 0;
+          int endOffset = 0;
+          int offsetValue;
 
-          // TODO offset this schedule by AvailSchOffsetEnd and AvailSchOffsetStart
+          QDomElement availSchOffsetStartElement = airSystemOACtrlElement.firstChildElement("AvailSchOffsetStart");
+
+          offsetValue = availSchOffsetStartElement.text().toInt(&ok);
+      
+          if( ok )
+          {
+            startOffset = offsetValue;
+          }
+
+          QDomElement availSchOffsetEndElement = airSystemOACtrlElement.firstChildElement("AvailSchOffsetEnd");
+
+          offsetValue = availSchOffsetEndElement.text().toInt(&ok);
+      
+          if( ok )
+          {
+            endOffset = offsetValue;
+          }
+
+          adjustSchedule(schedule,startOffset,endOffset);
 
           oaController.setMinimumOutdoorAirSchedule(schedule);
         }
