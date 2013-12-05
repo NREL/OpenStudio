2b91c981765d8aa12bfcbd13277ea9068d1e89a2
Dan Macumber <dan.macumber@gmail.com>
2013-12-05 00:31:45 -0700

    Don't build from command line, just configure environment and launch VS

--- developer/msvc/64bit_MSVC2010.bat
+++ developer/msvc/64bit_MSVC2010.bat
@@ -4,7 +4,6 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-msbuild OpenStudio.sln /m /p:Configuration=Debug
+:: msbuild OpenStudio.sln /m /p:Configuration=Debug
 :: msbuild OpenStudio.sln /m /p:Configuration=Release
-:: vcexpress OpenStudio.sln /build Debug
-:: vcexpress OpenStudio.sln /build Release
+vcexpress OpenStudio.sln
