40fad0d6a2bb0190061bd155abda048f539c06ca
Elaine Hale <elaine.hale@nrel.gov>
2014-01-15 11:50:04 -0700

    Adding methods for easily retrieving infiltration information. Needed to cleanly report out metrics of interest in the "reduce infiltration" measure.
    [#59977728]

--- openstudiocore/src/model/ThermalZone.hpp
+++ openstudiocore/src/model/ThermalZone.hpp
@@ -252,6 +252,30 @@ class MODEL_API ThermalZone : public HVACComponent {
   /** Returns the gas equipment power per person (W/person) of this thermal zone. Does not include space multiplier. Does include equipment multiplier. */
   double gasEquipmentPowerPerPerson() const;
 
+  /** Returns the infiltration design flow rate (m^3/s) in this thermal zone. Ignores
+   *  SpaceInfiltrationEffectiveLeakageArea objects. Does not include space multiplier. */
+  double infiltrationDesignFlowRate() const;
+
+  /** Returns the infiltration design flow per space floor area (m^3/m^2*s) in this thermal zone.
+   *  Ignores SpaceInfiltrationEffectiveLeakageArea objects. Does not include space multiplier. */
+  double infiltrationDesignFlowPerSpaceFloorArea() const;
+
+  /** Returns the infiltration design flow per exterior surface area (m^3/m^2*s) in this thermal zone.
+   *  Ignores SpaceInfiltrationEffectiveLeakageArea objects. Does not include space multiplier. */
+  double infiltrationDesignFlowPerExteriorSurfaceArea() const;
+
+  /** Returns the infiltration design flow per exterior wall area (m^3/m^2*s) in this thermal zone.
+   *  Ignores SpaceInfiltrationEffectiveLeakageArea objects. Does not include space multiplier. */
+  double infiltrationDesignFlowPerExteriorWallArea() const;
+
+  /** Returns the infiltration design flow per space floor area (m^3/m^2*s) in this thermal zone.
+   *  Ignores SpaceInfiltrationEffectiveLeakageArea objects. Does not include space multiplier. */
+  double infiltrationDesignFlowPerSpaceFloorArea() const;
+
+  /** Returns the infiltration design air changes per hour (1/h) in this thermal zone.
+   *  Ignores SpaceInfiltrationEffectiveLeakageArea objects. Does not include space multiplier. */
+  double infiltrationDesignAirChangesPerHour() const;
+
   /** Determines if this zone is conditioned, based on the SqlFile output. Returns 'Yes' if zone is conditioned. */
   boost::optional<std::string> isConditioned() const;
 
