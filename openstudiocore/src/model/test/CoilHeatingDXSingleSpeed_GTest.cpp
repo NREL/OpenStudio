ed20b68211802ead828167e7496652fe619d4eb3
Mark Adams <adamsmb@ornl.gov>
2014-04-25 12:36:16 -0400

    Add missing unit test
    For some reason, CoilHeatingDXSingleSpeed unit test got
    excluded/deleted in my last commit. This adds it back to the repo.

---
+++ openstudiocore/src/model/test/CoilHeatingDXSingleSpeed_GTest.cpp
@@ -0,0 +1,97 @@
+/**********************************************************************
+*  Copyright (c) 2008-2014, Alliance for Sustainable Energy.
+*  All rights reserved.
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+**********************************************************************/
+
+#include <gtest/gtest.h>
+#include <model/test/ModelFixture.hpp>
+#include <model/AirLoopHVAC.hpp>
+#include <model/PlantLoop.hpp>
+#include <model/Model.hpp>
+#include <model/Node.hpp>
+#include <model/Node_Impl.hpp>
+#include <model/CoilHeatingDXSingleSpeed.hpp>
+#include <model/Schedule.hpp>
+#include <model/CurveBiquadratic.hpp>
+#include <model/CurveQuadratic.hpp>
+#include <model/AirLoopHVACZoneSplitter.hpp>
+
+using namespace openstudio::model;
+
+TEST_F(ModelFixture,CoilHeatingDXSingleSpeed_CoilHeatingDXSingleSpeed)
+{
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+  ASSERT_EXIT ( 
+  {  
+     Model m;
+     Schedule s = m.alwaysOnDiscreteSchedule();
+     CurveBiquadratic  totalHeatingCapacityFunctionofTemperatureCurve(m);
+     CurveQuadratic  totalHeatingCapacityFunctionofFlowFractionCurve(m);
+     CurveBiquadratic  energyInputRatioFunctionofTemperatureCurve(m);
+     CurveQuadratic  energyInputRatioFunctionofFlowFractionCurve(m);
+     CurveQuadratic  partLoadFractionCorrelationCurve(m);
+
+     CoilHeatingDXSingleSpeed coil(m, s,
+                                   totalHeatingCapacityFunctionofTemperatureCurve,
+                                   totalHeatingCapacityFunctionofFlowFractionCurve,
+                                   energyInputRatioFunctionofTemperatureCurve,
+                                   energyInputRatioFunctionofFlowFractionCurve,
+                                   partLoadFractionCorrelationCurve); 
+
+     exit(0); 
+  } ,
+    ::testing::ExitedWithCode(0), "" );
+}
+
+TEST_F(ModelFixture,CoilHeatingDXSingleSpeed_addToNode) {
+  Model m;
+  Schedule s = m.alwaysOnDiscreteSchedule();
+  CurveBiquadratic  totalHeatingCapacityFunctionofTemperatureCurve(m);
+  CurveQuadratic  totalHeatingCapacityFunctionofFlowFractionCurve(m);
+  CurveBiquadratic  energyInputRatioFunctionofTemperatureCurve(m);
+  CurveQuadratic  energyInputRatioFunctionofFlowFractionCurve(m);
+  CurveQuadratic  partLoadFractionCorrelationCurve(m);
+
+  CoilHeatingDXSingleSpeed testObject(m, s,
+                                      totalHeatingCapacityFunctionofTemperatureCurve,
+                                      totalHeatingCapacityFunctionofFlowFractionCurve,
+                                      energyInputRatioFunctionofTemperatureCurve,
+                                      energyInputRatioFunctionofFlowFractionCurve,
+                                      partLoadFractionCorrelationCurve); 
+
+  AirLoopHVAC airLoop(m);
+
+  Node supplyOutletNode = airLoop.supplyOutletNode();
+
+  EXPECT_TRUE(testObject.addToNode(supplyOutletNode));
+  EXPECT_EQ( (unsigned)3, airLoop.supplyComponents().size() );
+
+  Node inletNode = airLoop.zoneSplitter().lastOutletModelObject()->cast<Node>();
+
+  EXPECT_FALSE(testObject.addToNode(inletNode));
+  EXPECT_EQ((unsigned)5, airLoop.demandComponents().size());
+
+  PlantLoop plantLoop(m);
+  supplyOutletNode = plantLoop.supplyOutletNode();
+  EXPECT_FALSE(testObject.addToNode(supplyOutletNode));
+  EXPECT_EQ( (unsigned)5, plantLoop.supplyComponents().size() );
+
+  Node demandOutletNode = plantLoop.demandOutletNode();
+  EXPECT_FALSE(testObject.addToNode(demandOutletNode));
+  EXPECT_EQ( (unsigned)5, plantLoop.demandComponents().size() );
+}
