ca30afe133e0d3327bb5a2d640bab63250222002
Alex Swindler <Alex.Swindler@nrel.gov>
2014-05-09 17:37:16 -0600

    Updating test program for third-parties

--- developer/cmake/tpl.cmake
+++ developer/cmake/tpl.cmake
@@ -1,46 +1,48 @@
 # Boost ######################
-if(APPLE)
-  set(Boost_USE_STATIC_LIBS ON)
-endif()
-if(WIN32)
-  set(Boost_USE_STATIC_LIBS ON)
-endif()
+set(Boost_USE_STATIC_LIBS ON)
 
-set(Boost_ADDITIONAL_VERSIONS "1.40")
-find_package(Boost 1.40.0 REQUIRED COMPONENTS filesystem regex program_options system thread date_time serialization)
+find_package(Boost 1.55.0 COMPONENTS filesystem regex program_options system thread date_time serialization log)
 include_directories(${Boost_INCLUDE_DIR})
-mark_as_advanced(Boost_LIB_DIAGNOSTIC_DEFINITIONS)
+
+add_definitions(-DBOOST_UBLAS_CHECK_ENABLE)
+add_definitions(-DBOOST_UBLAS_TYPE_CHECK)
+add_definitions(-DBOOST_UBLAS_NO_STD_CERR)
+add_definitions(-DBOOST_CHRONO_HEADER_ONLY)
 ##############################
 
 # Qt #########################
-find_package(Qt4 4.6.0 COMPONENTS QtCore QtGui QtSql QtNetwork QtXml QtWebKit REQUIRED)
-include(${QT_USE_FILE})
-include_directories(${QT_INCLUDE_DIR})
-include_directories(${QT_QTWEBKIT_INCLUDE_DIR})
+cmake_policy(SET CMP0020 NEW)
 
-mark_as_advanced(QT_QMAKE_EXECUTABLE QT_QTMOTIF_INCLUDE_DIR QT_QTMOTIF_LIBRARY_DEBUG QT_QTMOTIF_LIBRARY_RELEASE)
+find_package(Qt5Widgets REQUIRED)
+find_package(Qt5Sql REQUIRED)
+find_package(Qt5Network REQUIRED)
+find_package(Qt5Xml REQUIRED)
+find_package(Qt5WebKit REQUIRED)
+find_package(Qt5WebKitWidgets REQUIRED)
+find_package(Qt5Concurrent REQUIRED)
 
-# This sets the windows build that will need the special winmain@16 call. Qt provides
-#  this for us in the qtmain.lib file. Using this cmake code will ensure we have it
-#  linked into our build. Not needed on Unix/OS X/Linux which is why we have the
-#  if(WIN32) conditional.
-if(WIN32)
-  set(QT_USE_QTMAIN TRUE)
-endif()
+mark_as_advanced(
+  Qt5Concurrent_DIR
+  Qt5Core_DIR
+  Qt5Gui_DIR
+  Qt5Gui_EGL_LIBRARY
+  Qt5Location_DIR
+  Qt5Multimedia_DIR
+  Qt5MultimediaWidgets_DIR
+  Qt5Network_DIR
+  Qt5OpenGL_DIR
+  Qt5Positioning_DIR
+  Qt5PrintSupport_DIR
+  Qt5Qml_DIR
+  Qt5Quick_DIR
+  Qt5Sensors_DIR
+  Qt5Sql_DIR
+  Qt5WebKit_DIR
+  Qt5WebKitWidgets_DIR
+  Qt5Widgets_DIR
+  Qt5Xml_DIR
+)
 
-list(APPEND QT_LIBS ${QT_QTMAIN_LIBRARY})
-list(APPEND QT_LIBS ${QT_QTCORE_LIBRARY})
-list(APPEND QT_LIBS ${QT_QTCORE_LIB_DEPENDENCIES})
-list(APPEND QT_LIBS ${QT_QTGUI_LIBRARY})
-list(APPEND QT_LIBS ${QT_QTGUI_LIB_DEPENDENCIES})
-list(APPEND QT_LIBS ${QT_QTSQL_LIBRARY})
-list(APPEND QT_LIBS ${QT_QTSQL_LIB_DEPENDENCIES})
-list(APPEND QT_LIBS ${QT_QTNETWORK_LIBRARY})
-list(APPEND QT_LIBS ${QT_QTNETWORK_LIB_DEPENDENCIES})
-list(APPEND QT_LIBS ${QT_QTXML_LIBRARY})
-list(APPEND QT_LIBS ${QT_QTXML_LIB_DEPENDENCIES})
-list(APPEND QT_LIBS ${QT_QTWEBKIT_LIBRARY})
-list(APPEND QT_LIBS ${QT_QTWEBKIT_LIB_DEPENDENCIES})
 ##############################
 
 # OpenStudio #################
@@ -50,11 +52,9 @@ add_definitions(-DBOOST_LOG_SETUP_NO_LIB) # disable autolinking
 add_definitions(-DBOOST_LOG_NO_COMPILER_TLS) # disable autolinking
 
 include_directories(${OPENSTUDIOCORE_INCLUDE_DIR})
-include_directories("${OPENSTUDIOCORE_INCLUDE_DIR}/dependencies/BoostLog")
 include_directories("${OPENSTUDIOCORE_BUILD_DIR}/src")
 
 link_directories(${OPENSTUDIOCORE_LIB_DIR})
-link_directories(${BOOSTLOG_LIB_DIR})
 
 macro(find_openstudiocore_library name)
 
@@ -145,37 +145,5 @@ find_openstudiocore_library("zkqwt")
 #        ${OPENSTUDIOCORE_LIB_DIR}Debug
 #  NO_DEFAULT_PATH
 #)
-
-find_library(boost_log_DEBUG_LIB
-  NAME boost_log
-  PATHS ${BoostLog_DIR}
-        ${BoostLog_DIR}Debug
-  NO_DEFAULT_PATH
-)
-
-find_library(boost_log_RELEASE_LIB
-  NAME boost_log
-  PATHS ${BoostLog_DIR}
-        ${BoostLog_DIR}Release
-  NO_DEFAULT_PATH
-)
-
-find_library(boost_log_RELWITHDEBINFO_LIB
-  NAME boost_log
-  PATHS ${BoostLog_DIR}
-        ${BoostLog_DIR}RelWithDebInfo
-  NO_DEFAULT_PATH
-)
-
-find_library(boost_log_MINSIZEREL_LIB
-  NAME boost_log
-  PATHS ${BoostLog_DIR}
-        ${BoostLog_DIR}MinSizeRel
-  NO_DEFAULT_PATH
-)
-
-#find_package(BoostLog REQUIRED)
-#include_directories(${BoostLog_INCLUDE_DIRS})
-#link_directories(${BoostLog_LIBRARY_DIRS})
 
 ##############################
