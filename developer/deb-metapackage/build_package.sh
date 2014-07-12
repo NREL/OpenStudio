707007bf14c6f186f3b187a630f346a5d19f54b1
Alex Swindler <Alex.Swindler@nrel.gov>
2014-07-11 22:11:50 -0600

    Removing Ruby 1.8 support Removed VS2010 support Removed SketchUp < 2014 support Cleaned out the developer directory Updated debian packaging Updated OpenSSL to 1.0.1h

--- developer/deb-metapackage/build_package.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dpkg-deb -b zerokit-build-tools zerokit-build-tools.deb
--- developer/msvc/buildQt_Shared_MSVC2008.bat
+++
@@ -1,7 +0,0 @@
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-nmake confclean
-configure -debug-and-release -opensource -shared -stl -qt-sql-sqlite -qt-sql-odbc -accessibility -nomake examples -nomake demos -platform win32-msvc2008 -mp
-nmake
-
-:: remove all '-Zc:wchar_t-' in mkspecs/win32-msvc*/qmake.conf
-:: remove all '-GL' in mkspecs/win32-msvc*/qmake.conf
