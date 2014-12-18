a4d7a55b6116a85f2c0bbbf9c54eabc926d749ed
Dan Macumber <dan.macumber@gmail.com>
2014-12-18 14:41:11 -0700

    Get rid of incorrect warning message about incorrect layers

--- openstudiocore/src/model/LayeredConstruction.cpp
+++ openstudiocore/src/model/LayeredConstruction.cpp
@@ -190,7 +190,7 @@ namespace detail {
       clearExtensibleGroups();
       for (const Material& material : materials) {
         OS_ASSERT(material.model() == model());
-        ModelExtensibleGroup group = pushExtensibleGroup(StringVector()).cast<ModelExtensibleGroup>();
+        ModelExtensibleGroup group = pushExtensibleGroup(StringVector(), false).cast<ModelExtensibleGroup>();
         OS_ASSERT(!group.empty());
         bool ok = group.setPointer(0,material.handle());
         OS_ASSERT(ok);
@@ -205,7 +205,7 @@ namespace detail {
 
     OS_ASSERT(modelPartitionMaterial.model() == model());
     clearExtensibleGroups();
-    ModelExtensibleGroup group = pushExtensibleGroup(StringVector()).cast<ModelExtensibleGroup>();
+    ModelExtensibleGroup group = pushExtensibleGroup(StringVector(), false).cast<ModelExtensibleGroup>();
     OS_ASSERT(!group.empty());
     bool ok = group.setPointer(0,modelPartitionMaterial.handle());
     OS_ASSERT(ok);
