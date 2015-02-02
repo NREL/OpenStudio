4ec76a262b71c604ac8802740f71a9afe5d09dc9
Kyle Benne <kyle.benne@nrel.gov>
2015-02-02 16:17:31 -0600

    Add unit tests around SpaceType::clone
    All of these tests appear to show expected behavior, however the issue
    described in #870 remains.  The fact is the behavior described in the
    issue is as designed even if it is not very desirable in this case.
    Consider a feature or change in behavior to address #870.
    
    Calling ResourceObject::clone from Model A to Model B will always result
    in the creation of a new object even if that object already exists in
    the target Model, and even if it is a ResourceObject.  In contrast, when
    a ModelObject that *references* ResourceObject instance(s) is cloned,
    duplicates of the ResourceObject instance(s) will not be made.
    
    This is all demonstrated more clearly than these words in the unit tests added
    here.
    
    [#69514744]

---
+++ openstudiocore/src/model/test/PeopleDefinition_GTest.cpp
@@ -0,0 +1,52 @@
+/**********************************************************************
+*  Copyright (c) 2008-2015, Alliance for Sustainable Energy.
+*  All rights reserved.
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+**********************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "ModelFixture.hpp"
+#include "../Model.hpp"
+#include "../Model_Impl.hpp"
+#include "../PeopleDefinition.hpp"
+#include "../PeopleDefinition_Impl.hpp"
+
+#include <utilities/idd/IddEnums.hxx>
+
+using namespace openstudio;
+using namespace openstudio::model;
+
+TEST_F(ModelFixture, PeopleDefinition_Clone)
+{
+  Model library;
+  Model model;
+
+  PeopleDefinition definition(library); // A ResourceObject
+
+  EXPECT_EQ(1u,library.modelObjects().size());
+
+  definition.clone(library);
+  EXPECT_EQ(2u,library.modelObjects().size());
+
+  definition.clone(model);
+  EXPECT_EQ(1u,model.modelObjects().size());
+
+  definition.clone(model);
+  EXPECT_EQ(2u,model.modelObjects().size());
+}
+
+
