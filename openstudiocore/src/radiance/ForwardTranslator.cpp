53cd7f626752dfbf4781e47a7dc373b2d2125cb7
Dan Macumber <dan.macumber@gmail.com>
2015-03-19 15:07:23 -0600

    Compiles :shipit:

--- openstudiocore/src/radiance/ForwardTranslator.cpp
+++ openstudiocore/src/radiance/ForwardTranslator.cpp
@@ -1456,7 +1456,7 @@ namespace radiance {
                                         formatString(exteriorVisibleReflectance, 3) + " " + \
                                         formatString(exteriorVisibleReflectance, 3) + " 0 0\n\n");
                   // write polygon
-                  m_radSpaces[space_name] += "refl_" + formatString(exteriorVisibleReflectance, 3) + " polygon reveal_" + subSurface_name + "\n";
+                  m_radSpaces[space_name] += "refl_" + formatString(exteriorVisibleReflectance, 3) + " polygon outside_reveal_" + subSurface_name + formatString(i, 0) + "\n";
                   m_radSpaces[space_name] += "0\n0\n" + formatString(4 * 3) + "\n";
                   m_radSpaces[space_name] += formatString(vertex1.x()) + " " + formatString(vertex1.y()) + " " + formatString(vertex1.z()) + "\n\n";
                   m_radSpaces[space_name] += formatString(vertex2.x()) + " " + formatString(vertex2.y()) + " " + formatString(vertex2.z()) + "\n\n";
@@ -1486,7 +1486,7 @@ namespace radiance {
                                         formatString(interiorVisibleReflectance, 3) + " " + \
                                         formatString(interiorVisibleReflectance, 3) + " 0 0\n\n");
                   // write polygon
-                  m_radSpaces[space_name] += "refl_" + formatString(interiorVisibleReflectance, 3) + " polygon " + subSurface_name + "\n";
+                  m_radSpaces[space_name] += "refl_" + formatString(interiorVisibleReflectance, 3) + " polygon inside_reveal_" + subSurface_name + formatString(i, 0) + "\n";
                   m_radSpaces[space_name] += "0\n0\n" + formatString(4 * 3) + "\n";
                   m_radSpaces[space_name] += formatString(vertex1.x()) + " " + formatString(vertex1.y()) + " " + formatString(vertex1.z()) + "\n\n";
                   m_radSpaces[space_name] += formatString(vertex2.x()) + " " + formatString(vertex2.y()) + " " + formatString(vertex2.z()) + "\n\n";
@@ -1515,7 +1515,7 @@ namespace radiance {
                                         formatString(interiorVisibleReflectance, 3) + " " + \
                                         formatString(interiorVisibleReflectance, 3) + " 0 0\n\n");
                   // write polygon
-                  m_radSpaces[space_name] += "refl_" + formatString(interiorVisibleReflectance, 3) + " polygon " + subSurface_name + "\n";
+                  m_radSpaces[space_name] += "refl_" + formatString(interiorVisibleReflectance, 3) + " polygon inside_sill_" + subSurface_name + formatString(i, 0) + "\n";
                   m_radSpaces[space_name] += "0\n0\n" + formatString(4 * 3) + "\n";
                   m_radSpaces[space_name] += formatString(vertex1.x()) + " " + formatString(vertex1.y()) + " " + formatString(vertex1.z()) + "\n\n";
                   m_radSpaces[space_name] += formatString(vertex2.x()) + " " + formatString(vertex2.y()) + " " + formatString(vertex2.z()) + "\n\n";
