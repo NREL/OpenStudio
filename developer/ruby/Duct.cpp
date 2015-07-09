b97c2050f75ea8a31a504d7dc4bea01480c3f4e4
Evan Weaver <evan.weaver@nrel.gov>
2015-07-09 09:29:43 -0600

    Let that be a lesson! Of course, the IDD needs 2 blank lines at its end for cxx generation to be successful.

---
+++ developer/ruby/Duct.cpp
@@ -0,0 +1,163 @@
+/**********************************************************************
+ *  Copyright (c) 2008-2015, Alliance for Sustainable Energy.
+ *  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ **********************************************************************/
+
+#include <model/Duct.hpp>
+#include <model/Duct_Impl.hpp>
+
+// TODO: Check the following class names against object getters and setters.
+#include <model/Connection.hpp>
+#include <model/Connection_Impl.hpp>
+#include <model/Connection.hpp>
+#include <model/Connection_Impl.hpp>
+
+#include <utilities/idd/OS_Duct_FieldEnums.hxx>
+
+#include <utilities/core/Assert.hpp>
+
+namespace openstudio {
+namespace model {
+
+namespace detail {
+
+  Duct_Impl::Duct_Impl(const IdfObject& idfObject,
+                       Model_Impl* model,
+                       bool keepHandle)
+    : StraightComponent_Impl(idfObject,model,keepHandle)
+  {
+    OS_ASSERT(idfObject.iddObject().type() == Duct::iddObjectType());
+  }
+
+  Duct_Impl::Duct_Impl(const openstudio::detail::WorkspaceObject_Impl& other,
+                       Model_Impl* model,
+                       bool keepHandle)
+    : StraightComponent_Impl(other,model,keepHandle)
+  {
+    OS_ASSERT(other.iddObject().type() == Duct::iddObjectType());
+  }
+
+  Duct_Impl::Duct_Impl(const Duct_Impl& other,
+                       Model_Impl* model,
+                       bool keepHandle)
+    : StraightComponent_Impl(other,model,keepHandle)
+  {}
+
+  const std::vector<std::string>& Duct_Impl::outputVariableNames() const
+  {
+    static std::vector<std::string> result;
+    if (result.empty()){
+    }
+    return result;
+  }
+
+  IddObjectType Duct_Impl::iddObjectType() const {
+    return Duct::iddObjectType();
+  }
+
+  boost::optional<Connection> Duct_Impl::inletNode() const {
+    return getObject<ModelObject>().getModelObjectTarget<Connection>(OS_DuctFields::InletNode);
+  }
+
+  boost::optional<Connection> Duct_Impl::outletNode() const {
+    return getObject<ModelObject>().getModelObjectTarget<Connection>(OS_DuctFields::OutletNode);
+  }
+
+  bool Duct_Impl::setInletNode(const boost::optional<Connection>& connection) {
+    bool result(false);
+    if (connection) {
+      result = setPointer(OS_DuctFields::InletNode, connection.get().handle());
+    }
+    else {
+      resetInletNode();
+      result = true;
+    }
+    return result;
+  }
+
+  void Duct_Impl::resetInletNode() {
+    bool result = setString(OS_DuctFields::InletNode, "");
+    OS_ASSERT(result);
+  }
+
+  bool Duct_Impl::setOutletNode(const boost::optional<Connection>& connection) {
+    bool result(false);
+    if (connection) {
+      result = setPointer(OS_DuctFields::OutletNode, connection.get().handle());
+    }
+    else {
+      resetOutletNode();
+      result = true;
+    }
+    return result;
+  }
+
+  void Duct_Impl::resetOutletNode() {
+    bool result = setString(OS_DuctFields::OutletNode, "");
+    OS_ASSERT(result);
+  }
+
+} // detail
+
+Duct::Duct(const Model& model)
+  : StraightComponent(Duct::iddObjectType(),model)
+{
+  OS_ASSERT(getImpl<detail::Duct_Impl>());
+
+  // TODO: Appropriately handle the following required object-list fields.
+  bool ok = true;
+  // ok = setHandle();
+  OS_ASSERT(ok);
+}
+
+IddObjectType Duct::iddObjectType() {
+  return IddObjectType(IddObjectType::OS_Duct);
+}
+
+boost::optional<Connection> Duct::inletNode() const {
+  return getImpl<detail::Duct_Impl>()->inletNode();
+}
+
+boost::optional<Connection> Duct::outletNode() const {
+  return getImpl<detail::Duct_Impl>()->outletNode();
+}
+
+bool Duct::setInletNode(const Connection& connection) {
+  return getImpl<detail::Duct_Impl>()->setInletNode(connection);
+}
+
+void Duct::resetInletNode() {
+  getImpl<detail::Duct_Impl>()->resetInletNode();
+}
+
+bool Duct::setOutletNode(const Connection& connection) {
+  return getImpl<detail::Duct_Impl>()->setOutletNode(connection);
+}
+
+void Duct::resetOutletNode() {
+  getImpl<detail::Duct_Impl>()->resetOutletNode();
+}
+
+/// @cond
+Duct::Duct(std::shared_ptr<detail::Duct_Impl> impl)
+  : StraightComponent(impl)
+{}
+/// @endcond
+
+} // model
+} // openstudio
+
---
+++ developer/ruby/Duct.hpp
@@ -0,0 +1,106 @@
+/**********************************************************************
+ *  Copyright (c) 2008-2015, Alliance for Sustainable Energy.
+ *  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ **********************************************************************/
+
+#ifndef MODEL_DUCT_HPP
+#define MODEL_DUCT_HPP
+
+#include <model/ModelAPI.hpp>
+#include <model/StraightComponent.hpp>
+
+namespace openstudio {
+namespace model {
+
+// TODO: Check the following class names against object getters and setters.
+class Connection;
+class Connection;
+
+namespace detail {
+
+  class Duct_Impl;
+
+} // detail
+
+/** Duct is a StraightComponent that wraps the OpenStudio IDD object 'OS:Duct'. */
+class MODEL_API Duct : public StraightComponent {
+ public:
+  /** @name Constructors and Destructors */
+  //@{
+
+  explicit Duct(const Model& model);
+
+  virtual ~Duct() {}
+
+  //@}
+
+  static IddObjectType iddObjectType();
+
+  /** @name Getters */
+  //@{
+
+  // TODO: Check return type. From object lists, some candidates are: Connection.
+  boost::optional<Connection> inletNode() const;
+
+  // TODO: Check return type. From object lists, some candidates are: Connection.
+  boost::optional<Connection> outletNode() const;
+
+  //@}
+  /** @name Setters */
+  //@{
+
+  // TODO: Check argument type. From object lists, some candidates are: Connection.
+  bool setInletNode(const Connection& connection);
+
+  void resetInletNode();
+
+  // TODO: Check argument type. From object lists, some candidates are: Connection.
+  bool setOutletNode(const Connection& connection);
+
+  void resetOutletNode();
+
+  //@}
+  /** @name Other */
+  //@{
+
+  //@}
+ protected:
+  /// @cond
+  typedef detail::Duct_Impl ImplType;
+
+  explicit Duct(std::shared_ptr<detail::Duct_Impl> impl);
+
+  friend class detail::Duct_Impl;
+  friend class Model;
+  friend class IdfObject;
+  friend class openstudio::detail::IdfObject_Impl;
+  /// @endcond
+ private:
+  REGISTER_LOGGER("openstudio.model.Duct");
+};
+
+/** \relates Duct*/
+typedef boost::optional<Duct> OptionalDuct;
+
+/** \relates Duct*/
+typedef std::vector<Duct> DuctVector;
+
+} // model
+} // openstudio
+
+#endif // MODEL_DUCT_HPP
+
