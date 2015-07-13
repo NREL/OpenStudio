a88499fcd7f557a6f1a79f02aa320ff16d832acb
Evan Weaver <evan.weaver@nrel.gov>
2015-07-13 10:34:32 -0600

    [#98597390] FluidCooler_SingleSpeed forward translator.

--- openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateFluidCoolerSingleSpeed.cpp
+++ openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateFluidCoolerSingleSpeed.cpp
@@ -19,8 +19,12 @@
 
 #include "../ForwardTranslator.hpp"
 #include "../../model/FluidCoolerSingleSpeed.hpp"
+#include "../../model/FluidCoolerSingleSpeed_Impl.hpp"
+#include "../../model/Node.hpp"
+#include "../../model/Node_Impl.hpp"
 #include "../../utilities/idd/IddEnums.hpp"
 #include <utilities/idd/IddEnums.hxx>
+#include <utilities/idd/FluidCooler_SingleSpeed_FieldEnums.hxx>
 
 using namespace openstudio::model;
 
@@ -28,11 +32,111 @@ namespace openstudio {
 
 namespace energyplus {
 
-  boost::optional<IdfObject> ForwardTranslator::translateFluidCoolerSingleSpeed(FluidCoolerSingleSpeed & modelObject)
+boost::optional<IdfObject> ForwardTranslator::translateFluidCoolerSingleSpeed(FluidCoolerSingleSpeed & modelObject)
 {
   IdfObject idfObject(openstudio::IddObjectType::FluidCooler_SingleSpeed);
 
   m_idfObjects.push_back(idfObject);
+
+  OptionalString s;
+  OptionalDouble d;
+  OptionalModelObject mo;
+
+  ///////////////////////////////////////////////////////////////////////////
+  s = modelObject.name();
+  if (s)
+  {
+    idfObject.setName(*s);
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  mo = modelObject.waterInletNode();
+  if (mo)
+  {
+    s = mo->name();
+    if (s)
+    {
+      idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::WaterInletNodeName, *s);
+    }
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  mo = modelObject.waterOutletNode();
+  if (mo)
+  {
+    s = mo->name();
+    if (s)
+    {
+      idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::WaterOutletNodeName, *s);
+    }
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::PerformanceInputMethod, modelObject.performanceInputMethod());
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  d = modelObject.designAirFlowRateUfactorTimesAreaValue();
+  if (d)
+  {
+    idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::DesignAirFlowRateUfactorTimesAreaValue, toString(*d));
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::NominalCapacity, toString(modelObject.nominalCapacity()));
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::DesignEnteringWaterTemperature, toString(modelObject.designEnteringWaterTemperature()));
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::DesignEnteringAirTemperature, toString(modelObject.designEnteringAirTemperature()));
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::DesignEnteringAirWetbulbTemperature, toString(modelObject.designEnteringAirWetbulbTemperature()));
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  d = modelObject.designWaterFlowRate();
+  if (d)
+  {
+    idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::DesignWaterFlowRate, toString(*d));
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  d = modelObject.designAirFlowRate();
+  if (d)
+  {
+    idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::DesignAirFlowRate, toString(*d));
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  d = modelObject.designAirFlowRateFanPower();
+  if (d)
+  {
+    idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::DesignAirFlowRateFanPower, toString(*d));
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  mo = modelObject.outdoorAirInletNode();
+  if (mo)
+  {
+    s = mo->name();
+    if (s)
+    {
+      idfObject.setString(openstudio::FluidCooler_SingleSpeedFields::OutdoorAirInletNodeName, *s);
+    }
+  }
+  ///////////////////////////////////////////////////////////////////////////
 
   return boost::optional<IdfObject>(idfObject);
 }
