18a683c451872045a169ad2c1093fdef0efc19e8
Evan Weaver <evan.weaver@nrel.gov>
2015-07-13 09:36:23 -0600

    [#98597390] Duct forward translator.

--- openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateDuct.cpp
+++ openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateDuct.cpp
@@ -19,8 +19,10 @@
 
 #include "../ForwardTranslator.hpp"
 #include "../../model/Duct.hpp"
+#include "../../model/Duct_Impl.hpp"
 #include "../../utilities/idd/IddEnums.hpp"
 #include <utilities/idd/IddEnums.hxx>
+#include <utilities/idd/Duct_FieldEnums.hxx>
 
 using namespace openstudio::model;
 
@@ -33,6 +35,42 @@ namespace energyplus {
   IdfObject idfObject(openstudio::IddObjectType::Duct);
 
   m_idfObjects.push_back(idfObject);
+
+  OptionalString s;
+  OptionalDouble d;
+  OptionalModelObject mo;
+
+  ///////////////////////////////////////////////////////////////////////////
+  s = modelObject.name();
+  if (s)
+  {
+    idfObject.setName(*s);
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  mo = modelObject.inletModelObject();
+  if (mo)
+  {
+    s = mo->name();
+    if (s)
+    {
+      idfObject.setString(openstudio::DuctFields::InletNodeName, *s);
+    }
+  }
+  ///////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////
+  mo = modelObject.outletModelObject();
+  if (mo)
+  {
+    s = mo->name();
+    if (s)
+    {
+      idfObject.setString(openstudio::DuctFields::OutletNodeName, *s);
+    }
+  }
+  ///////////////////////////////////////////////////////////////////////////
 
   return boost::optional<IdfObject>(idfObject);
 }
