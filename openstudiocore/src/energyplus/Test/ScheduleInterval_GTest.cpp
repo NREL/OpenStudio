277c4a17a24c6bf3d48582e5be3c3baa68c63b74
Jason W. DeGraw <jason.degraw@gmail.com>
2015-09-03 17:24:26 -0600

    Fixed interval schedules also have problems

--- openstudiocore/src/energyplus/Test/ScheduleInterval_GTest.cpp
+++ openstudiocore/src/energyplus/Test/ScheduleInterval_GTest.cpp
@@ -48,7 +48,6 @@ using namespace openstudio;
 
 TEST_F(EnergyPlusFixture,ForwardTranslator_ScheduleFixedInterval_Hourly)
 {
-  //Vector values = linspace(0, 100, 8760);
   Vector values = linspace(1, 8760, 8760);
 
   TimeSeries timeseries(DateTime(Date(MonthOfYear::Jan, 1), Time(0,1,0)), Time(0,1,0), values, "");
@@ -148,9 +147,112 @@ TEST_F(EnergyPlusFixture,ForwardTranslator_ScheduleFixedInterval_Hourly)
   EXPECT_EQ(8760, numUntils);
 }
 
+TEST_F(EnergyPlusFixture, ForwardTranslator_ScheduleFixedInterval_Hourly_Shifted)
+{
+  Vector values = linspace(1, 8760, 8760);
+
+  TimeSeries timeseries(DateTime(Date(MonthOfYear::Jan, 1)), Time(0, 1, 0), values, "");
+
+  Model model;
+
+  boost::optional<ScheduleInterval> scheduleInterval = ScheduleInterval::fromTimeSeries(timeseries, model);
+  ASSERT_TRUE(scheduleInterval);
+  EXPECT_TRUE(scheduleInterval->optionalCast<ScheduleFixedInterval>());
+
+  TimeSeries ts = scheduleInterval->timeSeries();
+  EXPECT_EQ(DateTime(Date(MonthOfYear::Jan, 1)), ts.firstReportDateTime());
+
+  ForwardTranslator ft;
+
+  Workspace workspace = ft.translateModel(model);
+
+  std::vector<WorkspaceObject> objects = workspace.getObjectsByType(IddObjectType::Schedule_Compact);
+  ASSERT_EQ(1u, objects.size());
+
+  boost::regex throughRegex("^Through:\\s*(.*)/\\s*(.*)\\s*");
+  boost::regex untilRegex("^Until:\\s*(.*):(.*)\\s*");
+
+  workspace.save(toPath("./ForwardTranslator_ScheduleFixedInterval_Hourly_Shifted.idf"), true);
+
+  unsigned N = objects[0].numFields();
+  boost::optional<Date> lastDateThrough;
+  bool until24Found = false;
+  bool nextValueShouldBeLast = false;
+  unsigned numUntils = 0;
+
+  int currentHour = 0;
+  for (unsigned i = 0; i < N; ++i) {
+    boost::optional<std::string> field = objects[0].getString(i, true, false);
+    ASSERT_TRUE(field);
+
+    if (nextValueShouldBeLast) {
+      double value = boost::lexical_cast<double>(*field);
+      EXPECT_EQ(8760.0, value);
+      nextValueShouldBeLast = false;
+    }
+
+    boost::smatch throughMatches;
+    if (boost::regex_search(*field, throughMatches, throughRegex)) {
+
+      currentHour = 1;
+
+      std::string monthText(throughMatches[1].first, throughMatches[1].second);
+      std::string dayText(throughMatches[2].first, throughMatches[2].second);
+
+      int month = boost::lexical_cast<int>(monthText);
+      int day = boost::lexical_cast<int>(dayText);
+
+      Date date(MonthOfYear(month), day);
+      if (lastDateThrough) {
+        // check that this date is greater than last date
+        EXPECT_TRUE(date > *lastDateThrough) << date << " <= " << *lastDateThrough;
+
+        // DLM: this schedule should not wrap around to 1/1, it should end on 12/31 at 24:00
+
+        // check that last date was closed at 24:00
+        EXPECT_TRUE(until24Found);
+      }
+      lastDateThrough = date;
+      until24Found = false;
+    }
+
+    boost::smatch untilMatches;
+    if (boost::regex_search(*field, untilMatches, untilRegex)) {
+
+      numUntils += 1;
+
+      std::string hrText(untilMatches[1].first, untilMatches[1].second);
+      std::string minText(untilMatches[2].first, untilMatches[2].second);
+
+      int hr = boost::lexical_cast<int>(hrText);
+      int min = boost::lexical_cast<int>(minText);
+      EXPECT_EQ(currentHour, hr);
+      ++currentHour;
+      EXPECT_EQ(0, min);
+
+      if ((hr == 24) && (min == 0)) {
+        until24Found = true;
+      }
+
+      // should NOT see Until: 00:00,
+      EXPECT_FALSE((hr == 0) && (min == 0));
+
+      if ((lastDateThrough == Date(MonthOfYear(12), 31)) && until24Found) {
+        nextValueShouldBeLast = true;
+      }
+    }
+  }
+  bool lastUntil24Found = until24Found;
+
+  // check last date was closed
+  EXPECT_TRUE(lastUntil24Found);
+
+  // check that there were 8760 untils
+  EXPECT_EQ(8760, numUntils);
+}
+
 TEST_F(EnergyPlusFixture,ForwardTranslator_ScheduleFixedInterval_20hours)
 {
-  //Vector values = linspace(0, 100, 8760);
   Vector values = linspace(20, 8760, 438);
 
   TimeSeries timeseries(DateTime(Date(MonthOfYear::Jan, 1),Time(0,20,0)), Time(0,20,0), values, "");
