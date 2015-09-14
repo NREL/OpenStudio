04cbe6716cb48baf3bb8cedea1e7bc522c8e4b5b
Rob Guglielmetti <rob.guglielmetti@nrel.gov>
2015-09-13 20:14:25 -0600

    OK, as far as OpenStudio's Radiance is concerned, the celestial hemisphere can subdivided into 145 neat, (non)little patches, and that's just the way it is.

--- openstudiocore/src/radiance/ForwardTranslator.cpp
+++ openstudiocore/src/radiance/ForwardTranslator.cpp
@@ -361,18 +361,16 @@ namespace radiance {
       if (tregopt.is_open()){
         outfiles.push_back(tregoptpath);
         tregopt << "-c " << (int)radianceParameters.klemsSamplingDensity() << " ";
-
-
-
-        if (radianceParameters.skyDiscretizationResolution() == "146"){
-          tregopt << "-e MF:1 -f tregenza.cal -b tbin -bn Ntbins";
-          //tregopt << "-e MF:1 -f reinhartb.cal -b tbin -bn Ntbins";
-        } else if (radianceParameters.skyDiscretizationResolution() == "578"){
-          tregopt << "-e MF:2 -f reinhart.cal -b rbin -bn Nrbins";
-        } else if (radianceParameters.skyDiscretizationResolution() == "2306"){
-          tregopt << "-e MF:4 -f reinhart.cal -b rbin -bn Nrbins";
-        }
-        // TODO: make these values into a pulldown choice, add support for out of bounds
+				tregopt << "-e MF:1 -f tregenza.cal -b tbin -bn Ntbins";
+				//restricted to Klems, user is warned when writing out window groups. 2015.09.13 RPG
+        //if (radianceParameters.skyDiscretizationResolution() == "146"){
+        //  tregopt << "-e MF:1 -f tregenza.cal -b tbin -bn Ntbins";
+        //} else if (radianceParameters.skyDiscretizationResolution() == "578"){
+        //  tregopt << "-e MF:2 -f reinhart.cal -b rbin -bn Nrbins";
+        //} else if (radianceParameters.skyDiscretizationResolution() == "2306"){
+        //  tregopt << "-e MF:4 -f reinhart.cal -b rbin -bn Nrbins";
+        //}
+        // TODO: make these values into a pulldown choice, add support for higher resolution sampling bases and out of bounds
       }else{
         LOG(Error, "Cannot open file '" << toString(tregoptpath) << "' for writing");
       }
@@ -384,14 +382,17 @@ namespace radiance {
         outfiles.push_back(dcskyfilepath);
         // ground first (P0)!
         skyfile << "#@rfluxmtx h=u u=Y\nvoid glow groundglow\n0\n0\n4\n1 1 1 0\n\ngroundglow source ground\n0\n0\n4\n0 0 -1 180\n";
-        if (radianceParameters.skyDiscretizationResolution() == "146"){
-          skyfile << "#@rfluxmtx h=r1 u=Y\n";
-        } else if (radianceParameters.skyDiscretizationResolution() == "578"){
-          skyfile << "#@rfluxmtx h=r2 u=Y\n";
-        } else if (radianceParameters.skyDiscretizationResolution() == "2306"){
-          skyfile << "#@rfluxmtx h=r4 u=Y\n";
-        }
-        skyfile << "void glow skyglow\n0\n0\n4\n1 1 1 0\n\nskyglow source sky\n0\n0\n4\n0 0 1 180\n";
+
+//         if (radianceParameters.skyDiscretizationResolution() == "146"){
+//           skyfile << "#@rfluxmtx h=r1 u=Y\n";
+//         } else if (radianceParameters.skyDiscretizationResolution() == "578"){
+//           skyfile << "#@rfluxmtx h=r2 u=Y\n";
+//         } else if (radianceParameters.skyDiscretizationResolution() == "2306"){
+//           skyfile << "#@rfluxmtx h=r4 u=Y\n";
+//         }
+//         skyfile << "void glow skyglow\n0\n0\n4\n1 1 1 0\n\nskyglow source sky\n0\n0\n4\n0 0 1 180\n";
+
+				skyfile << "#@rfluxmtx h=r1 u=Y\nvoid glow skyglow\n0\n0\n4\n1 1 1 0\n\nskyglow source sky\n0\n0\n4\n0 0 1 180\n";
 
       }else{
         LOG(Error, "Cannot open file '" << toString(dcskyfilepath) << "' for writing");
@@ -1494,15 +1495,15 @@ namespace radiance {
  							openstudio::model::RadianceParameters radianceParameters = m_model.getUniqueModelObject<openstudio::model::RadianceParameters>();
  							std::string tempSkyDivs = "kf";
  							    
-              if (radianceParameters.skyDiscretizationResolution() == "146"){
-              	LOG(Info, windowGroup_name + "using Klems sampling basis.");         	
-              } else if (radianceParameters.skyDiscretizationResolution() == "578"){
-              	LOG(Warn, windowGroup_name + "reset to Klems sampling basis.");          	
-              } else if (radianceParameters.skyDiscretizationResolution() == "2306"){	
-              	LOG(Warn, windowGroup_name + "reset to Klems sampling basis."); 
-              }          
-              m_radWindowGroupShades[windowGroup_name] += "#@rfluxmtx h=" + tempSkyDivs +  " u=" + winUpVector + " o=output/dc/" + windowGroup_name + ".vmx\n";	
-              m_radWindowGroupShades[windowGroup_name] += "\n# shade for SubSurface: " + subSurface_name + "\n";
+							if (radianceParameters.skyDiscretizationResolution() == "146"){
+								LOG(Info, windowGroup_name + " using Klems sampling basis.");
+							} else if (radianceParameters.skyDiscretizationResolution() == "578"){
+								LOG(Warn, windowGroup_name + " reset to Klems sampling basis.");
+							} else if (radianceParameters.skyDiscretizationResolution() == "2306"){
+								LOG(Warn, windowGroup_name + " reset to Klems sampling basis.");
+							}
+							m_radWindowGroupShades[windowGroup_name] += "#@rfluxmtx h=" + tempSkyDivs +  " u=" + winUpVector + " o=output/dc/" + windowGroup_name + ".vmx\n";	
+							m_radWindowGroupShades[windowGroup_name] += "\n# shade for SubSurface: " + subSurface_name + "\n";
 
               // write the polygon
               m_radWindowGroupShades[windowGroup_name] += windowGroup_name + "_SHADE" + " polygon " + windowGroup_name + "_SHADE_" + subSurface_name + "\n";
