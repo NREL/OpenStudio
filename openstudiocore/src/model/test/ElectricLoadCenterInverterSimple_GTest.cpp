a18b34ce8cdeaa6addaea3043b5443f12dd560cf
Evan Weaver <evan.weaver@nrel.gov>
2015-09-24 15:26:26 -0600

    Increase tests for ElectricLoadCenterInverterSimple.

--- openstudiocore/src/model/test/ElectricLoadCenterInverterSimple_GTest.cpp
+++ openstudiocore/src/model/test/ElectricLoadCenterInverterSimple_GTest.cpp
@@ -22,14 +22,43 @@
 #include "ModelFixture.hpp"
 
 #include "../ElectricLoadCenterInverterSimple.hpp"
-#include "../ElectricLoadCenterInverterSimple_Impl.hpp"
+#include "../Schedule.hpp"
+#include "../ScheduleCompact.hpp"
 
 using namespace openstudio;
 using namespace openstudio::model;
 
 TEST_F(ModelFixture, ElectricLoadCenterInverterSimple_Instantiate) {
+  auto result = false;
+  auto value = 0.1;
+  
   Model model;
-  // TODO: Check constructor.
+
   ElectricLoadCenterInverterSimple electricLoadCenterInverterSimple(model);
+
+  EXPECT_FALSE(electricLoadCenterInverterSimple.availabilitySchedule());
+  ScheduleCompact scheduleCompact(model);
+  result = electricLoadCenterInverterSimple.setAvailabilitySchedule(scheduleCompact);
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(electricLoadCenterInverterSimple.availabilitySchedule());
+  electricLoadCenterInverterSimple.resetAvailabilitySchedule();
+  EXPECT_FALSE(electricLoadCenterInverterSimple.availabilitySchedule());
+
+  EXPECT_FALSE(electricLoadCenterInverterSimple.radiativeFraction());
+  result = electricLoadCenterInverterSimple.setRadiativeFraction(value);
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(electricLoadCenterInverterSimple.radiativeFraction());
+  EXPECT_EQ(electricLoadCenterInverterSimple.radiativeFraction().get(), value);
+  electricLoadCenterInverterSimple.resetRadiativeFraction();
+  EXPECT_FALSE(electricLoadCenterInverterSimple.radiativeFraction());
+
+  EXPECT_FALSE(electricLoadCenterInverterSimple.inverterEfficiency());
+  result = electricLoadCenterInverterSimple.setInverterEfficiency(value);
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(electricLoadCenterInverterSimple.inverterEfficiency());
+  EXPECT_EQ(electricLoadCenterInverterSimple.inverterEfficiency().get(), value);
+  electricLoadCenterInverterSimple.resetInverterEfficiency();
+  EXPECT_FALSE(electricLoadCenterInverterSimple.inverterEfficiency());
+ 
 }
 
