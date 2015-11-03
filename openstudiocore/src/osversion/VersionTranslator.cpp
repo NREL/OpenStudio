5c16ad12b27e371c869d52ba73848c09953c5b27
Evan Weaver <evan.weaver@nrel.gov>
2015-11-03 09:54:42 -0700

    Updating version to 1.9.4

--- openstudiocore/src/osversion/VersionTranslator.cpp
+++ openstudiocore/src/osversion/VersionTranslator.cpp
@@ -99,6 +99,7 @@ VersionTranslator::VersionTranslator()
   m_updateMethods[VersionString("1.8.5")] = &VersionTranslator::update_1_8_4_to_1_8_5;
   m_updateMethods[VersionString("1.9.0")] = &VersionTranslator::update_1_8_5_to_1_9_0;
   m_updateMethods[VersionString("1.9.3")] = &VersionTranslator::update_1_9_2_to_1_9_3;
+  m_updateMethods[VersionString("1.9.4")] = &VersionTranslator::defaultUpdate;
 
   // List of previous versions that may be updated to this one.
   //   - To increment the translator, add an entry for the version just released (branched for
@@ -189,6 +190,7 @@ VersionTranslator::VersionTranslator()
   m_startVersions.push_back(VersionString("1.9.0"));
   m_startVersions.push_back(VersionString("1.9.1"));
   m_startVersions.push_back(VersionString("1.9.2"));
+  m_startVersions.push_back(VersionString("1.9.3"));
 }
 
 boost::optional<model::Model> VersionTranslator::loadModel(const openstudio::path& pathToOldOsm, 
