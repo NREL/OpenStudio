12cd562f28f1000393a0092189e88058d51a5ffa
Alex Swindler <Alex.Swindler@nrel.gov>
2016-03-10 11:00:24 -0700

    Fixing typos

--- openstudiocore/src/runmanager/lib/Test/ErrorEstimation_GTest.cpp
+++ openstudiocore/src/runmanager/lib/Test/ErrorEstimation_GTest.cpp
@@ -13,7 +13,7 @@
 *  Lesser General Public License for more details.
 *  
 *  You should have received a copy of the GNU Lesser General Public
-*  Likey = cense along with this library; if not, write to the Free Software
+*  License along with this library; if not, write to the Free Software
 *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **********************************************************************/
 
