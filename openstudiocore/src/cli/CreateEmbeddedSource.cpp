e2eb0c2c10b917a1991f11994313a2aeee86d52c
Kyle Benne <kyle.benne@nrel.gov>
2016-04-19 12:54:51 -0500

    Create compiled exe to generate embedded source
    This makes the task of embedding files way faster. Before this work
    was done with cmake, now it is done with a task specific C++ based
    executable.

---
+++ openstudiocore/src/cli/CreateEmbeddedSource.cpp
@@ -0,0 +1,50 @@
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  if( argc != 5 ) {
+    return 1;
+  }
+
+  auto infile = argv[1];
+  auto outfile = argv[2];
+  auto filenum = argv[3];
+  auto embeddedname = argv[4];
+
+  std::fstream instream(infile, std::fstream::in | std::fstream::binary);
+  std::fstream outstream(outfile, std::fstream::out | std::fstream::trunc);
+
+  if( outstream.is_open() ) {
+    std::cout << "Writing: " << outfile << std::endl;
+    outstream << "static const uint8_t embedded_file_" << filenum << "[] = {";
+    unsigned length = 0;
+    while( 1 ) {
+      auto chunk = instream.get();
+      if( ! instream.eof() ) {
+        if( length != 0 ) {
+          outstream << ",";
+        }
+        outstream << "0x" << std::hex << chunk;
+        ++length;
+      } else {
+        break;
+      }
+    }
+    outstream << "};";
+
+    outstream << "\n";
+    outstream << "static const char *embedded_file_name_" << filenum << " = \"" << embeddedname << "\";";
+    outstream << "\n";
+    outstream << "static const size_t embedded_file_len_" << filenum << " = " << std::dec << length << ";";
+    outstream << std::endl;
+
+    instream.close();
+    outstream.close();
+  }
+
+  return 0;
+}
+
