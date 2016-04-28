17fb80e8fdf8f1c77d132ce4df0be1bd6627dc1d
Dan Macumber <dan.macumber@gmail.com>
2016-04-28 12:44:12 -0600

    Removing declspec commands for static libraries

--- openstudiocore/src/isomodel/ISOModelAPI.hpp
+++ openstudiocore/src/isomodel/ISOModelAPI.hpp
@@ -1,15 +1,4 @@
 #ifndef ISOMODEL_ISOMODELAPI_HPP
-#define ISOMODEL_ISOMODELAPI_HPP
-
-#if _WIN32 || _MSC_VER
-
-#ifdef openstudio_isomodel_EXPORTS
-#define ISOMODEL_API __declspec(dllexport)
-#else
-#define ISOMODEL_API __declspec(dllimport)
-#endif
-#else
-#define ISOMODEL_API
-#endif
-
+  #define ISOMODEL_ISOMODELAPI_HPP
+  #define ISOMODEL_API
 #endif
--- openstudiocore/src/utilities/UtilitiesAPI.hpp
+++ openstudiocore/src/utilities/UtilitiesAPI.hpp
@@ -1,21 +1,20 @@
 #ifndef UTILITIES_UTILITIESAPI_HPP
-#define UTILITIES_UTILITIESAPI_HPP
+  #define UTILITIES_UTILITIESAPI_HPP
 
-#if (_WIN32 || _MSC_VER) && !OPENSTUDIO_DIRECT_INCLUDE
+  #if (_WIN32 || _MSC_VER) && !OPENSTUDIO_DIRECT_INCLUDE
 
-#ifdef openstudio_utilities_EXPORTS
-#define UTILITIES_API __declspec(dllexport)
-#define UTILITIES_TEMPLATE_EXT
-#else
-#define UTILITIES_API __declspec(dllimport)
-#define UTILITIES_TEMPLATE_EXT extern
-#endif
+    #define UTILITIES_API
+    #ifdef openstudio_utilities_EXPORTS
+      #define UTILITIES_TEMPLATE_EXT
+    #else
+      #define UTILITIES_TEMPLATE_EXT extern
+    #endif
 
-#else
+  #else
 
-#define UTILITIES_API
-#define UTILITIES_TEMPLATE_EXT 
+    #define UTILITIES_API
+    #define UTILITIES_TEMPLATE_EXT 
 
-#endif
+  #endif
 
 #endif
