6024474d09f29be0a9923b749f26f15fde3345bb
Kyle Benne <kyle.benne@nrel.gov>
2016-05-18 12:11:55 -0500

    Fix some include paths
    I thought these were fixed in another commit, but seem to have been lost
    in the "big merge"

--- openstudiocore/src/model/GeneratorPhotovoltaic.cpp
+++ openstudiocore/src/model/GeneratorPhotovoltaic.cpp
@@ -33,8 +33,8 @@
 #include "PhotovoltaicPerformanceEquivalentOneDiode.hpp"
 #include "Schedule.hpp"
 #include "Schedule_Impl.hpp"
-#include "../../model/ScheduleTypeLimits.hpp"
-#include "../../model/ScheduleTypeRegistry.hpp"
+#include "ScheduleTypeLimits.hpp"
+#include "ScheduleTypeRegistry.hpp"
 
 #include <utilities/idd/IddFactory.hxx>
 #include <utilities/idd/IddEnums.hxx>
