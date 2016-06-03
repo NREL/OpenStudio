48ec68a2f79506573d9519833325643ec80d46bc
Kyle Benne <kyle.benne@nrel.gov>
2016-06-03 16:45:42 -0500

    Make webrick work
    on Mac...

--- openstudiocore/src/cli/EmbeddedHelp.hpp
+++ openstudiocore/src/cli/EmbeddedHelp.hpp
@@ -1,5 +1,8 @@
 
 #include <iostream>
+#include <QCoreApplication>
+#include <mach-o/dyld.h> /* _NSGetExecutablePath */
+#include <limits.h> /* PATH_MAX */
 
 extern "C" {
   void Init_generator(void);
@@ -13,5 +16,14 @@ namespace embedded_help {
 
   inline void initJSONGenerator() {
     Init_generator();
+  }
+
+  inline std::string applicationFilePath() {
+    char path[PATH_MAX + 1];
+    uint32_t size = sizeof(path);
+    if (_NSGetExecutablePath(path, &size) == 0)
+        return std::string(path);
+    else
+        return std::string();
   }
 }
--- openstudiocore/src/cli/embedded_help.rb
+++ openstudiocore/src/cli/embedded_help.rb
@@ -1,10 +1,20 @@
+
+module RbConfig
+  def RbConfig.ruby
+    EmbeddedScripting::applicationFilePath;
+  end
+end
+
 BINDING = Kernel::binding()
+Encoding.default_external = Encoding::ASCII
 
 module Kernel
   # ":" is our root path to the embedded file system
   # make sure it is in the ruby load path
   $LOAD_PATH << ':'
   $LOAD_PATH << ':/ruby/2.2.0'
+  # TODO configure this in a better way
+  $LOAD_PATH << ':/ruby/2.2.0/x86_64-darwin13'
   $LOAD_PATH << ':/openstudio-workflow-1.0.0.alpha.0/lib'
   $LOADED = []
 
