fafa0bfc9ca75871a96edff8c2e932c1c1aeaef0
Dan Macumber <dan.macumber@gmail.com>
2016-07-20 12:01:57 -0600

    Adding GeographicLib to src

---
+++ openstudiocore/src/geographic_lib/matlab/geographiclib/defaultellipsoid.m
@@ -0,0 +1,16 @@
+function ellipsoid = defaultellipsoid
+%DEFAULTELLIPSOID  Return the WGS84 ellipsoid
+%
+%   ellipsoid = DEFAULTELLIPSOID
+%
+%   returns a vector of the equatorial radius and eccentricity for the
+%   WGS84 ellipsoid.  use ecc2flat and flat2ecc to convert between
+%   the eccentricity and the flattening.
+%
+%   See also ECC2FLAT, FLAT2ECC.
+
+  a = 6378137;
+  f = 1/298.257223563;
+  e = flat2ecc(f);
+  ellipsoid = [a, e];
+end
---
+++ openstudiocore/src/geographic_lib/python/geographiclib/__init__.py
@@ -0,0 +1,4 @@
+"""geographiclib: geodesic routines from GeographicLib"""
+
+__version_info__ = (1, 45, 0)
+__version__ = "1.45"
