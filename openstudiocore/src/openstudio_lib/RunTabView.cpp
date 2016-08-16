2fafb81e9467be264612e344df91fae9d4b5e8a6
Kyle Benne <kyle.benne@nrel.gov>
2016-08-16 14:27:30 -0500

    Remove cruft in run tab

--- openstudiocore/src/openstudio_lib/RunTabView.cpp
+++ openstudiocore/src/openstudio_lib/RunTabView.cpp
@@ -75,6 +75,7 @@
 #include <QTextEdit>
 #include <QProcess>
 #include <QStandardPaths>
+#include <QFileSystemWatcher>
 
 namespace openstudio {
 
@@ -131,6 +132,12 @@ RunView::RunView()
 
   m_runProcess = new QProcess(this);
   connect(m_runProcess, static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished), this, &RunView::onRunProcessFinished);
+
+  //m_simDirWatcher = new QFileSystemWatcher(this);
+  //connect(m_simDirWatcher, &QFileSystemWatcher::directoryChanged, this, &RunView::onSimDirChanged);
+  //connect(m_simDirWatcher, &QFileSystemWatcher::fileChanged, this, &RunView::onFileChanged);
+
+  m_eperrWatcher = new QFileSystemWatcher(this);
 }
 
 void RunView::onRunProcessFinished(int exitCode, QProcess::ExitStatus status)
@@ -138,6 +145,16 @@ void RunView::onRunProcessFinished(int exitCode, QProcess::ExitStatus status)
   LOG(Debug, "run finished");
   m_playButton->setChecked(false);
 }
+
+//void RunView::onSimDirChanged(const QString &path)
+//{
+//  LOG(Debug, "Sim Dir Changed: " << path.toStdString());
+//}
+//
+//void RunView::onFileChanged(const QString &path)
+//{
+//  LOG(Debug, "File Changed: " << path.toStdString());
+//}
 
 void RunView::playButtonClicked(bool t_checked)
 {
@@ -165,408 +182,29 @@ void RunView::playButtonClicked(bool t_checked)
     auto openstudioExePath = QStandardPaths::findExecutable("openstudio", paths);
 
     QStringList arguments;
-    //QString::fromStdString(osdocument->model().workflowJSON().oswPath()->string())
     auto savePath = toPath(osdocument->savePath());
     auto baseName = savePath.stem();
     auto parentPath = savePath.parent_path();
-    auto workflowPath = parentPath / baseName / "workflow.osw";
+    auto resourcePath = parentPath / baseName;
+    auto workflowPath = resourcePath / "workflow.osw";
     auto workflowJSONPath = QString::fromStdString(workflowPath.string());
     arguments << "run" << "-w" << workflowJSONPath;
 
     LOG(Debug, "run exe" << openstudioExePath.toStdString());
     LOG(Debug, "run arguments" << arguments.join(";").toStdString());
 
+    // Not a great idea
+    //m_simDirWatcher->addPath( QString::fromStdString((resourcePath / "run/").string()) );
+    //std::ofstream outfile((resourcePath / "run/stdout-energyplus").string(),std::ofstream::trunc);
+    //outfile.close();
+    //m_simDirWatcher->addPath( QString::fromStdString((resourcePath / "run/stdout-energyplus").string()) );
+
     m_runProcess->start(openstudioExePath, arguments);
   } else {
     // stop running
+    LOG(Debug, "Kill Simulation");
     m_runProcess->kill();
   }
 }
 
-//RunView::RunView(const model::Model & model,
-//                 const openstudio::path &t_modelPath,
-//                 const openstudio::path &t_tempFolder,
-//                 openstudio::runmanager::RunManager t_runManager)
-//  : m_model(model),
-//    m_modelPath(t_modelPath),
-//    m_tempFolder(t_tempFolder),
-//    m_canceling(false)
-//
-//{
-//  bool isConnected = t_runManager.connect(SIGNAL(statsChanged()), this, SLOT(runManagerStatsChanged()));
-//  OS_ASSERT(isConnected);
-//
-//  auto mainLayout = new QGridLayout();
-//  mainLayout->setContentsMargins(5,5,5,5);
-//  mainLayout->setSpacing(5);
-//  setLayout(mainLayout);
-//
-//  // Run / Play button area
-//
-//  m_playButton = new QToolButton();
-//  m_playButton->setText("     Run");
-//  m_playButton->setCheckable(true);
-//  m_playButton->setChecked(false);
-//  QIcon playbuttonicon(QPixmap(":/images/run_simulation_button.png"));
-//  playbuttonicon.addPixmap(QPixmap(":/images/run_simulation_button.png"), QIcon::Normal, QIcon::Off);
-//  playbuttonicon.addPixmap(QPixmap(":/images/cancel_simulation_button.png"), QIcon::Normal, QIcon::On);
-//  m_playButton->setStyleSheet("QToolButton { background:transparent; font: bold; }");
-//  m_playButton->setIconSize(QSize(35,35));
-//  m_playButton->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-//  m_playButton->setIcon(playbuttonicon);
-//  m_playButton->setLayoutDirection(Qt::RightToLeft);
-////  m_playButton->setStyleSheet("QAbstractButton:!hover { border: none; }");
-//
-//  mainLayout->addWidget(m_playButton, 0, 0);
-//  connect(m_playButton, &QToolButton::clicked, this, &RunView::playButtonClicked);
-//
-//  // Progress bar area
-//  m_progressBar = new QProgressBar();
-// 
-//  auto progressbarlayout = new QVBoxLayout();
-//  progressbarlayout->addWidget(m_progressBar);
-//  m_statusLabel = new QLabel("Ready");
-//  progressbarlayout->addWidget(m_statusLabel);
-//  mainLayout->addLayout(progressbarlayout, 0, 1);
-//  /*
-//  m_radianceGroup = new QButtonGroup(this);
-//
-//  connect(m_radianceGroup, static_cast<void (QButtonGroup::*)(int)>(&QButtonGroup::buttonClicked), this, &RunView::on_radianceGroupClicked);
-//
-//  int buttonCount = 0;
-//
-//  m_energyPlusButton = new QRadioButton("EnergyPlus");
-//  m_radianceGroup->addButton(m_energyPlusButton,buttonCount++);
-//
-//  m_radianceButton = new QRadioButton("Radiance");
-//  m_radianceGroup->addButton(m_radianceButton,buttonCount++);
-//
-//
-//  // "Radiance" Button Layout
-// 
-//  QLabel *radianceLabel = new QLabel("<b>Select Daylight Simulation Engine</b>");
-//
-//  auto radianceWidget = new QWidget();
-//  radianceWidget->setObjectName("RunStatusViewRadiance");
-//  auto radianceInteriorLayout = new QHBoxLayout();
-//
-//  radianceWidget->setLayout(radianceInteriorLayout);
-//  radianceInteriorLayout->addWidget(radianceLabel);
-//  radianceInteriorLayout->addStretch();
-//  radianceInteriorLayout->addWidget(m_energyPlusButton);
-//  radianceInteriorLayout->addStretch();
-//  radianceInteriorLayout->addWidget(m_radianceButton);
-//  */
-///*
-//  radianceHLayout->addSpacing(100);
-//  radianceHLayout->addWidget(radianceWidget, 3);
-//  radianceHLayout->addStretch(2);
-//  */
-//  //radianceWidget->setStyleSheet("QWidget#RunStatusViewRadiance {background: #DADADA; border: 1px solid #A5A5A5;}");
-//
-//
-//
-///*
-//
-//  m_radianceWarningsAndErrorsButton = new QPushButton();
-//  m_radianceWarningsAndErrorsButton->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Preferred);
-//  m_radianceWarningsAndErrorsButton->hide();
-//
-//  connect(m_radianceWarningsAndErrorsButton, &QPushButton::clicked, this, &RunView::on_radianceWarningsAndErrorsClicked);
-//
-//  QHBoxLayout * radianceHLayout = new QHBoxLayout();
-//  radianceHLayout->addWidget(m_radianceButton);
-//  radianceHLayout->addWidget(m_radianceWarningsAndErrorsButton);
-//  radianceHLayout->addStretch();
-//
-//  QVBoxLayout * radianceVLayout = new QVBoxLayout();
-//  radianceVLayout->addWidget(m_energyPlusButton);
-//  radianceVLayout->addLayout(radianceHLayout);
-//
-//  QGroupBox * groupBox = new QGroupBox("For Daylighting Calculation use");
-//  groupBox->setLayout(radianceVLayout);
-//*/
-//
-//  //mainLayout->addWidget(radianceWidget, 1, 1);
-//
-//  //if (usesRadianceForDaylightCalculations(t_runManager))
-//  //{
-//  //  m_radianceButton->setChecked(true);
-//  //} else {
-//  //  m_energyPlusButton->setChecked(true);
-//  //}
-//
-//  openstudio::runmanager::ToolVersion epversion = getRequiredEnergyPlusVersion();
-//  if( auto tag = epversion.getTag() ) {
-//    m_toolWarningLabel = new QLabel(openstudio::toQString("<b>Notice:</b> EnergyPlus " + 
-//      std::to_string(epversion.getMajor().get()) + "." +
-//      std::to_string(epversion.getMinor().get()) + "." +
-//      std::to_string(epversion.getBuild().get()) + " Build \"" +
-//      tag.get() + "\""
-//      " is required and not yet located."
-//      "  Run Preferences -> Scan For Tools to locate."));
-//  } else {
-//    m_toolWarningLabel = new QLabel(openstudio::toQString("<b>Notice:</b> EnergyPlus " + 
-//      std::to_string(epversion.getMajor().get()) + "." +
-//      std::to_string(epversion.getMinor().get()) + "." +
-//      std::to_string(epversion.getBuild().get()) +
-//      " is required and not yet located."
-//      "  Run Preferences -> Scan For Tools to locate."));
-//  }
-//  m_toolWarningLabel->hide();
-//
-//  mainLayout->addWidget(m_toolWarningLabel, 2, 1);
-//
-//  locateEnergyPlus();
-//
-//  m_warningsLabel = new QLabel("<b>Warnings:</b> 0");
-//  m_errorsLabel = new QLabel("<b>Errors:</b> 0");
-//  mainLayout->addWidget(m_warningsLabel, 3, 1);
-//  mainLayout->addWidget(m_errorsLabel, 4, 1);
-//  mainLayout->addWidget(new QLabel("Output"), 5, 1);
-//  m_outputWindow = new QPlainTextEdit();
-//  m_outputWindow->setReadOnly(true);
-//  mainLayout->addWidget(m_outputWindow, 6, 1);
-//
-//  updateRunManagerStats(t_runManager);
-//}
-//
-///*
-//void RunView::getRadiancePreRunWarningsAndErrors(std::vector<std::string> & warnings,
-//                                                 std::vector<std::string> & errors)
-//{
-//  openstudio::runmanager::RunManager rm = runManager();
-//  boost::optional<model::Model> model(m_model);
-//  openstudio::getRadiancePreRunWarningsAndErrors(warnings, errors, rm, model);
-//}
-//*/
-//void RunView::locateEnergyPlus()
-//{
-//  openstudio::runmanager::ConfigOptions co(true);
-//  openstudio::runmanager::ToolVersion epversion = getRequiredEnergyPlusVersion();
-//  bool energyplus_not_installed = co.getTools().getAllByName("energyplus").getAllByVersion(epversion).tools().size() == 0;
-//  
-//  if (energyplus_not_installed){
-//    m_toolWarningLabel->show();
-//  } else {
-//    m_toolWarningLabel->hide();
-//  }
-//}
-//
-//void RunView::updateToolsWarnings()
-//{
-//  LOG(Debug, "updateToolsWarnings called");
-//  
-//  //getRadiancePreRunWarningsAndErrors(m_radianceWarnings,m_radianceErrors);
-//
-//  //QString checkBoxText;
-//  //QString buttonText;
-//
-//  //if(m_radianceErrors.size() > 0){
-//  //  m_energyPlusButton->setChecked(true);
-//  //}
-//
-//  locateEnergyPlus();
-//}
-//
-//void RunView::outputDataAdded(const openstudio::UUID &, const std::string &t_data)
-//{
-//  QTextCursor cursor = m_outputWindow->textCursor();
-//  cursor.movePosition(QTextCursor::End);
-//  cursor.insertText(openstudio::toQString(t_data));
-//  m_outputWindow->ensureCursorVisible();
-//}
-//
-//void RunView::updateRunManagerStats(openstudio::runmanager::RunManager t_runManager)
-//{
-//  double numberofjobs = 0;
-//  double completedjobs = 0;
-//  double totalerrors = 0;
-//  double totalwarnings = 0;
-//
-//  std::map<std::string, double> stats = t_runManager.statistics();
-//  numberofjobs = stats["Number of Jobs"];
-//  completedjobs = stats["Completed Jobs"];
-//  totalerrors = stats["Total Errors"];
-//  totalwarnings = stats["Total Warnings"];
-//
-//  if (numberofjobs == 0) numberofjobs = 1;
-//
-//  m_progressBar->setRange(0, numberofjobs);
-//  m_progressBar->setValue(completedjobs);
-//
-//  m_warningsLabel->setText(openstudio::toQString("<b>Warnings:</b> " + boost::lexical_cast<std::string>(int(totalwarnings))));
-//  m_errorsLabel->setText(openstudio::toQString("<b>Errors:</b> " + boost::lexical_cast<std::string>(int(totalerrors))));
-//}
-//
-//void RunView::runManagerStatsChanged()
-//{
-//  updateRunManagerStats(runManager());
-//}
-//
-//void RunView::runFinished(const openstudio::path &t_sqlFile, const openstudio::path &t_radianceOutputPath)
-//{
-//  if (m_canceling)
-//  {
-//    m_statusLabel->setText("Canceled");
-//  }
-//
-//  std::shared_ptr<OSDocument> osdocument = OSAppBase::instance()->currentDocument();
-//
-//  // DLM: should we attach the sql file to the model here?
-//  // DLM: if model is re-opened with results they will not be added here, better to do this on results tab
-//  //if (exists(t_sqlFile)){
-//  //  SqlFile sqlFile(t_sqlFile);
-//  //  if (sqlFile.connectionOpen()){
-//  //    osdocument->model().setSqlFile(sqlFile);
-//  //  }
-//  //}
-//  
-//  m_canceling = false;
-//  LOG(Debug, "Emitting results generated for sqlfile: " << openstudio::toString(t_sqlFile) << " and radiance file " << openstudio::toString(t_radianceOutputPath));
-//  emit resultsGenerated(t_sqlFile, t_radianceOutputPath);
-//
-//  // needed so save of osm file does not trigger out of date and start running again
-//  runManager().setPaused(true);
-//
-//  m_playButton->setChecked(false);
-//  osdocument->enableTabsAfterRun();
-//}
-//
-//void RunView::treeChanged(const openstudio::UUID &t_uuid)
-//{
-//  std::string statusstr = "Ready";
-//
-//  try {
-//    openstudio::runmanager::Job j = runManager().getJob(t_uuid);
-//    while (j.parent())
-//    {
-//      j = j.parent().get();
-//    }
-//
-//    openstudio::runmanager::TreeStatusEnum status = j.treeStatus();
-//    LOG(Debug, "Tree finished, status is: " << status.valueName());
-//    statusstr = status.valueDescription();
-//
-//    openstudio::path sqlpath;
-//    openstudio::path radianceOutPath;
-//
-//    if (status == openstudio::runmanager::TreeStatusEnum::Finished
-//        || status == openstudio::runmanager::TreeStatusEnum::Failed
-//        || status == openstudio::runmanager::TreeStatusEnum::Canceled)
-//    {
-//      if (status == openstudio::runmanager::TreeStatusEnum::Failed && m_canceling)
-//      {
-//        statusstr = "Canceled";
-//      }
-//
-//      try {
-//        sqlpath = j.treeAllFiles().getLastByFilename("eplusout.sql").fullPath;
-//      } catch (const std::exception &e) {
-//        LOG(Debug, "Tree finished, error getting sql file: " << e.what());
-//      } catch (...) {
-//        LOG(Debug, "Tree finished, error getting sql file");
-//        // no sql file exists
-//      }
-//
-//      try {
-//        radianceOutPath = j.treeOutputFiles().getLastByFilename("radout.sql").fullPath;
-//      } catch (const std::exception &e) {
-//        LOG(Debug, "Tree finished, error getting radout.sql file: " << e.what());
-//      } catch (...) {
-//        LOG(Debug, "Tree finished, error getting radout.sql file");
-//        // no sql file exists
-//      }
-//
-//      runFinished(sqlpath, radianceOutPath);
-//    } else { 
-//      m_canceling = false;
-//    }
-//  } catch (const std::exception &e) {
-//    LOG(Debug, "Tree finished, error getting status: " << e.what());
-//    runFinished(openstudio::path(), openstudio::path());
-//
-//  } catch (...) {
-//    LOG(Debug, "Tree finished, error getting status");
-//    runFinished(openstudio::path(), openstudio::path());
-//    // no sql file exists
-//  }
-//
-//  m_statusLabel->setText(openstudio::toQString(statusstr));
-//}
-//
-//openstudio::runmanager::ToolVersion RunView::getRequiredEnergyPlusVersion()
-//{
-//  std::string sha = energyPlusBuildSHA();
-//  if( ! sha.empty() ) {
-//    return openstudio::runmanager::ToolVersion(energyPlusVersionMajor(),energyPlusVersionMinor(),energyPlusVersionPatch(),sha);
-//  } else {
-//    return openstudio::runmanager::ToolVersion(energyPlusVersionMajor(),energyPlusVersionMinor(),energyPlusVersionPatch());
-//  }
-//}
-//
-//
-//void RunView::requestStartRunManager()
-//{
-//  // we are starting the simulations
-//  std::shared_ptr<OSDocument> osdocument = OSAppBase::instance()->currentDocument();
-//  bool requireCalibrationReports = (osdocument->model().getConcreteModelObjects<model::UtilityBill>().size() > 0);
-//  openstudio::runmanager::RunManager rm = runManager();
-//  startRunManager(rm, m_modelPath, m_tempFolder, std::vector<double>(), requireCalibrationReports, this);
-//}
-//
-//openstudio::runmanager::RunManager RunView::runManager()
-//{
-//  return OSAppBase::instance()->project()->runManager();
-//}
-///*
-//void RunView::showRadianceWarningsAndErrors(const std::vector<std::string> & warnings,
-//                                            const std::vector<std::string> & errors)
-//{
-//  QString errorsAndWarnings;
-//  QString text;
-//  
-//  if(warnings.size()){
-//    errorsAndWarnings += "WARNINGS:\n";
-//    for (std::string warning : warnings){
-//      text = warning.c_str();
-//      errorsAndWarnings += text;
-//      errorsAndWarnings += '\n';
-//    }
-//    errorsAndWarnings += '\n';
-//  }
-//
-//  if(errors.size()){
-//    errorsAndWarnings += "ERRORS:\n";
-//    for (std::string error : errors){
-//      text = error.c_str();
-//      errorsAndWarnings += text;
-//      errorsAndWarnings += '\n';
-//    }
-//  }
-//
-//  QMessageBox::critical(this, "Radiance Warnings and Errors", errorsAndWarnings);
-//}
-//
-//void RunView::on_radianceWarningsAndErrorsClicked(bool checked)
-//{
-//  showRadianceWarningsAndErrors(m_radianceWarnings,m_radianceErrors);
-//}
-//
-//void RunView::on_radianceGroupClicked(int idx)
-//{
-//  QAbstractButton * button = m_radianceGroup->button(idx);
-//  OS_ASSERT(button);
-//  if(button == m_radianceButton){
-//    emit useRadianceStateChanged(true);
-//    updateToolsWarnings();
-//    if(m_radianceErrors.size()){
-//      showRadianceWarningsAndErrors(m_radianceWarnings,m_radianceErrors);
-//    }
-//  }
-//  else{
-//    emit useRadianceStateChanged(false);
-//  }
-//}
-//*/
 } // openstudio
--- openstudiocore/src/openstudio_lib/RunTabView.hpp
+++ openstudiocore/src/openstudio_lib/RunTabView.hpp
@@ -39,52 +39,11 @@ class QRadioButton;
 class QStackedWidget;
 class QToolButton;
 class QTextEdit;
+class QFileSystemWatcher;
 
 namespace openstudio {
 
   class RunView;
-
-  //namespace runmanager {
-  //  class JobStatusWidget;
-  //}
-
-  //class RunListModel : public QAbstractListModel
-  //{
-  //  public:
-
-  //    RunListModel( RunView * runView );
-
-  //    int rowCount( const QModelIndex & parent = QModelIndex() ) const override;
-
-  //    QVariant data( const QModelIndex & index, int role = Qt::DisplayRole ) const override;
-
-  //    bool removeRows(int row, int count);
-
-  //    void emitDataChanged();
-
-  //  private:
-
-  //    RunView * m_runView;
-  //};
-
-  //class RunItemView : public QWidget
-  //{
-  //  Q_OBJECT
-
-  //  public:
-
-  //    //RunItemView(const model::myModelClass & run);
-  //    RunItemView();
-
-  //    virtual ~RunItemView() {}
-
-  //    //model::myModelClass run();
-  //    void run();
-
-  //  private:
-
-  //    //model::myModelClass m_run;
-  //};
 
   class RunView : public QWidget
   {
@@ -102,81 +61,17 @@ namespace openstudio {
 
     void onRunProcessFinished(int exitCode, QProcess::ExitStatus status);
 
+    //void onSimDirChanged(const QString &path);
+
+    //void onFileChanged(const QString &path);
+
     QToolButton * m_playButton;
     QProgressBar * m_progressBar;
     QLabel * m_statusLabel;
     QTextEdit * m_textInfo;
     QProcess * m_runProcess;
-
-
-  //  public:
-
-  //    RunView(const model::Model & model,
-  //        const openstudio::path &t_modelPath,
-  //        const openstudio::path &t_tempFolder,
-  //        openstudio::runmanager::RunManager t_runManager);
-
-  //    virtual ~RunView() {}
-
-  //    QStackedWidget * runStack();
-
-  //    openstudio::runmanager::RunManager runManager();
-
-  //  signals:
-  //    void resultsGenerated(const openstudio::path &t_sqlFile, const openstudio::path &t_radianceResultsFile);
-  //    void toolsUpdated();
-  //    //void useRadianceStateChanged(bool);
-
-  //  public slots:
-  //    void updateToolsWarnings();
-
-  //  private slots:
-  //    void playButtonClicked(bool);
-  //    void runManagerStatsChanged();
-  //    void outputDataAdded(const openstudio::UUID &, const std::string &t_data);
-  //    void treeChanged(const openstudio::UUID &t_uuid);
-  //    //void on_radianceWarningsAndErrorsClicked(bool checked);
-  //    //void on_radianceGroupClicked(int idx);
-  //    void requestStartRunManager();
-
-  //  private:
-  //    REGISTER_LOGGER("openstudio::RunView");
-  //    void runFinished(const openstudio::path &t_sqlFile, const openstudio::path &t_radianceOutputFile);
-  //    static openstudio::runmanager::ToolVersion getRequiredEnergyPlusVersion();
-  //    //void getRadiancePreRunWarningsAndErrors(std::vector<std::string> & warnings,
-  //    //                                        std::vector<std::string> & errors);
-  //    //void showRadianceWarningsAndErrors(const std::vector<std::string> & warnings,
-  //    //                                   const std::vector<std::string> & errors);
-  //    void locateEnergyPlus();
-  //    void updateRunManagerStats(openstudio::runmanager::RunManager t_runManager);
-
-  //    //openstudio::path loadRmdbPath() const;
-  //    //openstudio::path simulationDir() const;
-  //    //openstudio::path modelPath() const;
-  //    //openstudio::path scriptsDir() const;
-
-  //    QWidget * m_runNavigatorWidget;
-  //    QToolButton * m_playButton;
-  //    QComboBox * m_chooser;
-  //    QStackedWidget * m_runStack;
-  //    QLabel * m_toolWarningLabel;
-  //    QLabel * m_warningsLabel;
-  //    QLabel * m_errorsLabel;
-  //    QLabel * m_statusLabel;
-  //    model::Model m_model;
-  //    QProgressBar * m_progressBar;
-  //    QPlainTextEdit * m_outputWindow;
-  //    //QButtonGroup * m_radianceGroup;
-  //    //QRadioButton * m_radianceButton;
-  //    //QRadioButton * m_energyPlusButton;
-  //    //std::vector<std::string> m_radianceWarnings;
-  //    //std::vector<std::string> m_radianceErrors;
-  //    std::shared_ptr<RunListModel> m_runListModel;
-  //    openstudio::path m_modelPath;
-  //    openstudio::path m_tempFolder;
-  //
-  //    bool m_canceling;
-  //    bool m_statsConnected;
+    //QFileSystemWatcher * m_simDirWatcher;
+    //QFileSystemWatcher * m_eperrWatcher;
   };
 
   class RunTabView : public MainTabView
