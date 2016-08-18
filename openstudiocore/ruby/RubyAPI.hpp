f61469073c3f0ce5be2d6a2991118ad6539bc696
Dan Macumber <dan.macumber@gmail.com>
2016-08-18 09:11:41 -0600

    Fix symbol name

--- openstudiocore/ruby/RubyAPI.hpp
+++ openstudiocore/ruby/RubyAPI.hpp
@@ -2,7 +2,7 @@
 #define RUBYAPI_HPP
 
   #if (_WIN32 || _MSC_VER)
-    #ifdef openstudiorb_EXPORTS
+    #ifdef openstudio_rb_EXPORTS
       #define RUBY_API __declspec(dllexport)
     #else
       #define RUBY_API __declspec(dllimport)
