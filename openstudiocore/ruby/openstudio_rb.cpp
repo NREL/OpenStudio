8cb0a7e5922dd00f58ced9ab5fd7174088a47966
Dan Macumber <dan.macumber@gmail.com>
2016-08-26 08:37:10 -0600

    Also init resources

--- openstudiocore/ruby/openstudio_rb.cpp
+++ openstudiocore/ruby/openstudio_rb.cpp
@@ -4,22 +4,26 @@
 #include <iostream>
 #include <ruby.h>
 
-//#ifdef _MSC_VER
-//__declspec(noinline) void importPlugins() {
-//#else
-//__attribute__((noinline)) void importPlugins() {
-//#endif
-  #ifdef QT_STATIC
-    Q_IMPORT_PLUGIN(QSQLiteDriverPlugin);
-    //Q_INIT_RESOURCE(openstudio);
-  #endif
-  
-  #if defined(Q_OS_OSX) && defined(QT_STATIC)
+// this uses the static initialization trick to import plugins
+#ifdef QT_STATIC
+  Q_IMPORT_PLUGIN(QSQLiteDriverPlugin);
+
+  #if defined(Q_OS_OSX)
     Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
-  #elif defined(Q_OS_WIN) && defined(QT_STATIC)
+  #elif defined(Q_OS_WIN)
     Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
   #endif
-//}
+
+#endif // QT_STATIC
+
+
+inline void initResources() { 
+
+  #ifdef QT_STATIC
+    Q_INIT_RESOURCE(modeleditorlib); 
+  #endif // QT_STATIC
+
+}
 
 extern "C" {
 
@@ -27,7 +31,8 @@ void Init_openstudiomodeleditor();
 void Init_openstudiolib();
 
 RUBY_API void Init_openstudio(void) {
-  //importPlugins();
+
+  initResources();
 
   init_openstudio_internal();
 
