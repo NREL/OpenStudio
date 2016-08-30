a7bd6ed57a30bcf3ae0543b73f72df8f2a9432f2
Brian Ball <Brian.Ball@nrel.gov>
2016-08-30 17:18:00 -0600

    Gtest TendVariable

---
+++ openstudiocore/src/model/test/EnergyManagementSystemTrendVariable_GTest.cpp
@@ -0,0 +1,49 @@
+/**********************************************************************
+*  Copyright (c) 2008-2016, Alliance for Sustainable Energy.
+*  All rights reserved.
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+**********************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "ModelFixture.hpp"
+#include "../EnergyManagementSystemTrendVariable.hpp"
+#include "../Model_Impl.hpp"
+
+#include "../../utilities/idd/IddEnums.hpp"
+#include "../../utilities/idf/ValidityReport.hpp"
+#include "../../utilities/idf/IdfObject.hpp"
+#include "../../utilities/data/TimeSeries.hpp"
+#include "../../utilities/core/Compare.hpp"
+#include "../../utilities/core/Optional.hpp"
+
+using namespace openstudio;
+using namespace openstudio::model;
+using std::string;
+
+TEST_F(ModelFixture, EMSTrendVariable_EMSTrendVariable)
+{
+  Model model;
+    
+  // add internal variable
+  EnergyManagementSystemTrendVariable var(model);
+  var.setEMSVariableName("TestName");
+  EXPECT_EQ("TestName", var.eMSVariableName());
+
+  var.setNumberofTimestepstobeLogged(2);
+  EXPECT_EQ(2, var.numberofTimestepstobeLogged());
+}
+
