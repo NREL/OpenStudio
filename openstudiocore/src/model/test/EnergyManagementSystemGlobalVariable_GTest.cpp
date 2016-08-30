dcf09aa2a58e5f05927eec1e22e50e8ab9325240
Brian Ball <Brian.Ball@nrel.gov>
2016-08-30 10:53:59 -0600

    global variable test

---
+++ openstudiocore/src/model/test/EnergyManagementSystemGlobalVariable_GTest.cpp
@@ -0,0 +1,71 @@
+/**********************************************************************
+*  Copyright (c) 2008-2016, Alliance for Sustainable Energy.
+*  All rights reserved.
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+**********************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "ModelFixture.hpp"
+#include "../Building.hpp"
+#include "../Building_Impl.hpp"
+#include "../ThermalZone.hpp"
+#include "../EnergyManagementSystemSensor.hpp"
+#include "../EnergyManagementSystemGlobalVariable.hpp"
+#include "../OutputVariable.hpp"
+#include "../OutputVariable_Impl.hpp"
+#include "../OutputMeter.hpp"
+#include "../OutputMeter_Impl.hpp"
+#include "../Model_Impl.hpp"
+
+#include "../../utilities/idd/IddEnums.hpp"
+#include "../../utilities/idf/ValidityReport.hpp"
+#include "../../utilities/idf/IdfObject.hpp"
+#include "../../utilities/data/TimeSeries.hpp"
+#include "../../utilities/core/Compare.hpp"
+#include "../../utilities/core/Optional.hpp"
+
+using namespace openstudio;
+using namespace openstudio::model;
+using std::string;
+
+TEST_F(ModelFixture, EMSGlobalVariable_EMSGlobalVariable)
+{
+  Model model;
+
+  Building building = model.getUniqueModelObject<Building>();
+
+  ThermalZone zone1(model);
+  ThermalZone zone2(model);
+
+  std::vector<std::string> vars;
+
+  // add global variable
+  EnergyManagementSystemGlobalVariable var(model);
+  // should be empty
+  EXPECT_EQ(0, var.getVariables().size());
+  // add variable
+  var.addVariable("variable one");
+  vars.push_back("variable one");
+
+  EXPECT_EQ(vars[0], var.getVariables()[0]);
+
+  
+
+ // EXPECT_EQ(siteOutdoorAirDrybulbTemperature, var.outputVariable());
+ // EXPECT_EQ("", var.keyName().get());
+}
+
