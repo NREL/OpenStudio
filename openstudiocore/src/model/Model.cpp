69fd66ca9c6439be0a891fd8adce890f507ea6d9
Merket <nmerket@nmerket-24363s.local>
2016-09-09 16:37:53 -0600

    it compiles!

--- openstudiocore/src/model/Model.cpp
+++ openstudiocore/src/model/Model.cpp
@@ -233,6 +233,7 @@ if (_className::iddObjectType() == typeToCreate) { \
     REGISTER_CONSTRUCTOR(BoilerSteam);
     REGISTER_CONSTRUCTOR(Building);
     REGISTER_CONSTRUCTOR(BuildingStory);
+    REGISTER_CONSTRUCTOR(BuildingUnit);
     // REGISTER_CONSTRUCTOR(CentralHeatPumpSystem);
     // REGISTER_CONSTRUCTOR(CentralHeatPumpSystemModule);
     REGISTER_CONSTRUCTOR(CFactorUndergroundWallConstruction);
@@ -644,6 +645,7 @@ if (_className::iddObjectType() == typeToCreate) { \
     REGISTER_COPYCONSTRUCTORS(BoilerSteam);
     REGISTER_COPYCONSTRUCTORS(Building);
     REGISTER_COPYCONSTRUCTORS(BuildingStory);
+    REGISTER_COPYCONSTRUCTORS(BuildingUnit);
     // REGISTER_COPYCONSTRUCTORS(CentralHeatPumpSystem);
     // REGISTER_COPYCONSTRUCTORS(CentralHeatPumpSystemModule);
     REGISTER_COPYCONSTRUCTORS(CFactorUndergroundWallConstruction);
