15e0b2b0ae29cc1a68e36cb36d02fc89923555c1
Brian Ball <Brian.Ball@nrel.gov>
2016-09-12 18:23:14 -0600

    forward translate subroutine uuid swap

--- openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateEnergyManagementSystemSubroutine.cpp
+++ openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslateEnergyManagementSystemSubroutine.cpp
@@ -30,7 +30,10 @@
 #include "../../model/EnergyManagementSystemSubroutine_Impl.hpp"
 
 #include "../../utilities/idf/IdfExtensibleGroup.hpp"
+#include "../../utilities/core/UUID.hpp"
+#include "../../utilities/core/StringHelpers.hpp"
 
+#include <utilities/idd/OS_EnergyManagementSystem_Subroutine_FieldEnums.hxx>
 #include <utilities/idd/EnergyManagementSystem_Subroutine_FieldEnums.hxx>
 #include "../../utilities/idd/IddEnums.hpp"
 #include <utilities/idd/IddEnums.hxx>
@@ -47,20 +50,66 @@ namespace energyplus {
 boost::optional<IdfObject> ForwardTranslator::translateEnergyManagementSystemSubroutine(EnergyManagementSystemSubroutine & modelObject)
 {
   boost::optional<std::string> s;
+  int found = 0;
 
-  //IdfObject idfObject(openstudio::IddObjectType::EnergyManagementSystem_Program);
-  //m_idfObjects.push_back(idfObject);
-  IdfObject program = createAndRegisterIdfObject(openstudio::IddObjectType::EnergyManagementSystem_Subroutine, modelObject);
+  IdfObject idfObject(openstudio::IddObjectType::EnergyManagementSystem_Program);
+  m_idfObjects.push_back(idfObject);
+  m_map.insert(std::make_pair(modelObject.handle(), idfObject));
+  //IdfObject program = createAndRegisterIdfObject(openstudio::IddObjectType::EnergyManagementSystem_Subroutine, modelObject);
   //Name
   s = modelObject.name();
   if (s) {
-    program.setName(*s);
+    idfObject.setName(*s);
   }
  
+
   for (const IdfExtensibleGroup& eg : modelObject.extensibleGroups()) {
-    program.pushExtensibleGroup(eg.fields());
+    IdfExtensibleGroup group = idfObject.pushExtensibleGroup();
+    OptionalString line = eg.getString(OS_EnergyManagementSystem_SubroutineExtensibleFields::ProgramLine);
+    if (line) {
+      //result = boost::regex_search(line.get(), matches, openstudio::uuidInString());
+      //boost::sregex_token_iterator iter(line.get().begin(), line.get().end(), openstudio::uuidInString(),0);
+
+      //split string on whitespaces to isolate possible uids
+      std::vector<std::string> results = splitString(line.get(), ' ');
+      for (size_t j = 0; j < results.size(); j++) {
+        if (results.at(j).size() == 38) {
+          //possible uid so set found to 1
+          found = 1;
+          //remove {} from uid string
+          std::string possible_uid = results.at(j).substr(1, results.at(j).size() - 2);
+          //look to see if uid is in the model and return the object
+          UUID uid = toUUID(possible_uid);
+          Model m = modelObject.model();
+          //TODO cant get below to work so try the harder way
+          //m.getModelObjects<model::ModelObject>(&uid);
+          std::vector<model::ModelObject> modelObjects = m.getModelObjects<model::ModelObject>();
+          if (modelObjects.size() > 0) {
+            for (size_t k = 0; k < modelObjects.size(); k++) {
+              if (modelObjects.at(k).handle() == uid) {
+                found++;
+                results.at(j) = modelObjects.at(k).nameString();
+              };
+            }
+          };
+        }
+      }
+      std::string final_line;
+      for (size_t z = 0; z < results.size(); z++) {
+        final_line += results.at(z) + ' ';
+      }
+      if (final_line.back() == ' ') {
+        boost::erase_last(final_line, " ");
+      }
+      group.setString(EnergyManagementSystem_SubroutineExtensibleFields::ProgramLine, final_line);
+
+    }
+    OptionalString comment = eg.fieldComment(OS_EnergyManagementSystem_SubroutineExtensibleFields::ProgramLine);
+    if (comment) {
+      group.setFieldComment(EnergyManagementSystem_SubroutineExtensibleFields::ProgramLine, comment.get());
+    }
   }
-  return program;
+  return idfObject;
 }
 
 } // energyplus
