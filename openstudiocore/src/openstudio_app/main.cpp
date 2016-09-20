eaf7928ec389776610a1d9b38cac282e0fbc54a4
Kyle Benne <kyle.benne@nrel.gov>
2016-09-20 14:27:59 -0500

    Add crude prototype of making webkit work in static build on Mac
    This works. Now to make it work on Windows. Also need to clean it up so
    the find libraries are less hacky.

--- openstudiocore/src/openstudio_app/main.cpp
+++ openstudiocore/src/openstudio_app/main.cpp
@@ -49,6 +49,22 @@
 //#include "../utilities/core/RubyInterpreter.hpp"
 //#include "../measure/EmbeddedRubyMeasureInfoGetter.hpp"
 
+
+#ifdef Q_OS_WIN
+#pragma warning(disable : 4930 )
+#pragma warning(disable : 4101 )
+#endif
+
+#ifdef QT_STATIC
+  //Q_IMPORT_PLUGIN(QSQLiteDriverPlugin);
+#endif
+
+#if defined(Q_OS_OSX) && defined(QT_STATIC)
+  //Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
+#elif defined(Q_OS_WIN) && defined(QT_STATIC)
+  //Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+#endif
+
 int main(int argc, char *argv[])
 {
   /*
@@ -59,23 +75,9 @@ int main(int argc, char *argv[])
   }
   */
 
-#ifdef Q_OS_WIN
-#pragma warning(disable : 4930 )
-#pragma warning(disable : 4101 )
-#endif
-
 #ifdef QT_STATIC
-  Q_IMPORT_PLUGIN(QSQLiteDriverPlugin);
   Q_INIT_RESOURCE(openstudio);
 #endif
-
-#if defined(Q_OS_OSX) && defined(QT_STATIC)
-  Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
-#elif defined(Q_OS_WIN) && defined(QT_STATIC)
-  Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
-#endif
-
-
 
 #if _DEBUG || (__GNUC__ && !NDEBUG)
 #ifdef _WIN32
