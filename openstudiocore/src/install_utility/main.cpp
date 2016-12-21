19c9e40e3777c40f739acd4dc538da44d8c417a8
Kyle Benne <kyle@confluencesoftware.com>
2016-12-20 22:44:57 -0600

    Create firewall exception from installer
    Also have the uninstall/remove exception in place and functioning. Fancy!

--- openstudiocore/src/install_utility/main.cpp
+++ openstudiocore/src/install_utility/main.cpp
@@ -26,8 +26,69 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **********************************************************************************************************************/
 
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <regex>
+#include <sstream>
+#if defined __APPLE__
+#include <mach-o/dyld.h> /* _NSGetExecutablePath */
+#include <limits.h> /* PATH_MAX */
+#elif defined _WIN32 
+#include <windows.h>
+#endif
+#include "../utilities/core/Filesystem.hpp"
+
+inline std::string applicationFilePath() {
+#ifdef __APPLE__
+  char path[PATH_MAX + 1];
+  uint32_t size = sizeof(path);
+  if (_NSGetExecutablePath(path, &size) == 0) {
+    return std::string(path);
+  }
+#elif defined _WIN32 
+ TCHAR szPath[MAX_PATH];
+ if( GetModuleFileName( nullptr, szPath, MAX_PATH ) ) {
+   return std::string(szPath);
+ }
+#endif
+  return std::string();
+}
+
 int main(int argc, char *argv[])
 {
+#if defined _WIN32
+	if (argc > 1u) {
+ 		if (std::string(argv[1]) == "Install"){
+			openstudio::filesystem::path appDir = openstudio::filesystem::path(applicationFilePath()).parent_path();
+			openstudio::filesystem::path cliPath = appDir / "openstudio.exe";
+			openstudio::filesystem::path mongoPath = appDir / "pat\\mongo\\bin\\mongod.exe";
+			
+			std::ostringstream oss;
+			oss << "netsh advfirewall firewall add rule name=\"Allow OpenStudio CLI\" dir=in program="\
+				<< "\"" << cliPath.string() << "\" "\
+				<< "action=allow";
+			std::system(oss.str().c_str());
+			
+			std::ostringstream oss2;
+			oss2 << "netsh advfirewall firewall add rule name=\"Allow OpenStudio Mongo\" dir=in program="\
+			<< "\"" << mongoPath.string() << "\" "\
+			<< "action=allow";
+			std::system(oss2.str().c_str());
+			
+		}
+		else if (std::string(argv[1]) == "Remove") {
+			std::ostringstream oss;
+			oss << "netsh advfirewall firewall delete rule name=\"Allow OpenStudio CLI\"";
+			std::system(oss.str().c_str());
+
+			std::ostringstream oss2;
+			oss2 << "netsh advfirewall firewall delete rule name=\"Allow OpenStudio Mongo\"";
+			std::system(oss2.str().c_str());
+		}
+	}
+#endif
+
   return 0;
 }
 
