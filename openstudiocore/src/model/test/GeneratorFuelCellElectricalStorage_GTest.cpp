8e331d1be5b6eceb24ceb03095ee64f79dd2fee3
Brian Ball <Brian.Ball@nrel.gov>
2017-01-19 14:43:37 -0700

    electrical storage tests

--- openstudiocore/src/model/test/GeneratorFuelCellElectricalStorage_GTest.cpp
+++ openstudiocore/src/model/test/GeneratorFuelCellElectricalStorage_GTest.cpp
@@ -35,4 +35,18 @@
 
 using namespace openstudio;
 using namespace openstudio::model;
+using std::string;
+
+TEST_F(ModelFixture, FuelCellElectricalStorage) {
+  Model model;
+
+  GeneratorFuelCellElectricalStorage elecStorage(model);
+  EXPECT_EQ("SimpleEfficiencyWithConstraints", elecStorage.choiceofModel());
+  EXPECT_EQ(1.0, elecStorage.nominalChargingEnergeticEfficiency());
+  EXPECT_EQ(1.0, elecStorage.nominalDischargingEnergeticEfficiency());
+  EXPECT_EQ(0, elecStorage.simpleMaximumCapacity());
+  EXPECT_EQ(0, elecStorage.simpleMaximumPowerDraw());
+  EXPECT_EQ(0, elecStorage.simpleMaximumPowerStore());
+  EXPECT_EQ(0, elecStorage.initialChargeState());
+}
 
