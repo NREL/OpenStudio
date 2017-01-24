4ed230dfc5b156d5460d831a85d95e22cca7dbb1
Dan Macumber <dan.macumber@gmail.com>
2017-01-24 13:23:07 -0700

    Dynamically add embedded gem dirs to load path so we can require embedded gems

--- openstudiocore/src/cli/embedded_help.rb
+++ openstudiocore/src/cli/embedded_help.rb
@@ -25,6 +25,20 @@ module Kernel
   $LOAD_PATH << EmbeddedScripting::findFirstFileByName('openstudio-workflow.rb').gsub('/openstudio-workflow.rb', '')
   $LOADED = []
 
+  # dynamically add gem paths to load path
+  # DLM: the better thing to do would be to append the embedded locations to GEM_PATH
+  extra_include_paths = []
+  EmbeddedScripting::allFileNamesAsString().split(';').each do |f|
+    if md = /:\/ruby\/2.2.0\/gems\/([^\/]*)\//.match(f)
+      extra_include_paths << ':/ruby/2.2.0/gems/' + md[1] + '/lib'
+    elsif md = /:\/ruby\/2.2.0\/bundler\/gems\/([^\/]*)\//.match(f)
+      extra_include_paths << ':/ruby/2.2.0/bundler/gems/' + md[1]  + '/lib'
+    end 
+  end
+  extra_include_paths.uniq.each do |f|
+    $LOAD_PATH << f
+  end 
+
   alias :original_require_relative :require_relative
   alias :original_require :require
 
