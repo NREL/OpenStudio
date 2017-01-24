f54dc91b6710ae2a6115cb2b8919d35d123462fc
Dan Macumber <dan.macumber@gmail.com>
2017-01-24 12:16:16 -0700

    Add minitest gem to embedded gems

--- dependencies/ruby/build_openstudio_gems.rb
+++ dependencies/ruby/build_openstudio_gems.rb
@@ -1,6 +1,12 @@
 require 'fileutils'
+require 'rbconfig'
 
 install_dir = ARGV[0]
+expected_ruby_version = ARGV[1]
+
+if RUBY_VERSION != expected_ruby_version
+  raise "Incorrect Ruby version ${RUBY_VERSION} used, expecting #{expected_ruby_version}"
+end
 
 if /win/.match(RUBY_PLATFORM) || /mingw/.match(RUBY_PLATFORM)
   ENV['PATH'] = "#{ENV['PATH']};C:\\Program Files\\Git\\cmd"
@@ -11,13 +17,26 @@ if File.exists?(install_dir)
 end
 
 ENV['BUNDLE_WITHOUT'] = 'test'
-system("bundle install --without=test --path='#{install_dir}'")
+bundle_exe = File.join(RbConfig::CONFIG['bindir'], 'bundle')
 
-FileUtils.rm_rf("#{install_dir}/ruby/2.0.0/cache")
+if !File.exists?(bundle_exe)
+  raise "Required bundle executable not found"
+end
+
+system("#{bundle_exe} install --without=test --path='#{install_dir}'")
+
+FileUtils.rm_rf("#{install_dir}/ruby/2.2.0/cache")
 
 standards_gem_dir = nil
 workflow_gem_dir = nil
-Dir.glob("#{install_dir}/ruby/2.0.0/bundler/gems/*").each do |f|
+Dir.glob("#{install_dir}/ruby/2.2.0/bundler/gems/*").each do |f|
+  if /openstudio-standards/i.match(f)
+    standards_gem_dir = f
+  elsif /openstudio-workflow/i.match(f)
+    workflow_gem_dir = f
+  end
+end
+Dir.glob("#{install_dir}/ruby/2.2.0/gems/*").each do |f|
   if /openstudio-standards/i.match(f)
     standards_gem_dir = f
   elsif /openstudio-workflow/i.match(f)
@@ -25,12 +44,15 @@ Dir.glob("#{install_dir}/ruby/2.0.0/bundler/gems/*").each do |f|
   end
 end
 
+puts "standards_gem_dir = #{standards_gem_dir}"
+puts "workflow_gem_dir = #{workflow_gem_dir}"
+
 # clean up standards gem
 FileUtils.rm_rf("#{standards_gem_dir}/.git")
 FileUtils.rm_rf("#{standards_gem_dir}/measures")
 FileUtils.rm_rf("#{standards_gem_dir}/openstudio-standards/test")
 FileUtils.rm_rf("#{standards_gem_dir}/openstudio-standards/docs")
-Dir.glob("#{standards_gem_dir}/openstudio-standards/data/weather/*").each do |f|
+Dir.glob("#{standards_gem_dir}/data/weather/*").each do |f|
   if /CAN_/.match(f)
     FileUtils.rm_f(f)
   end
