2d469141380f49084c059bfaafce848831dc87eb
Brian Ball <Brian.Ball@nrel.gov>
2017-02-07 10:47:56 -0700

    ExternalInterfaceActuator_GTest

--- openstudiocore/src/model/test/ExternalInterfaceActuator_GTest.cpp
+++ openstudiocore/src/model/test/ExternalInterfaceActuator_GTest.cpp
@@ -33,6 +33,84 @@
 #include "../ExternalInterfaceActuator.hpp"
 #include "../ExternalInterfaceActuator_Impl.hpp"
 
+#include "../Building.hpp"
+#include "../Building_Impl.hpp"
+#include "../ThermalZone.hpp"
+#include "../FanConstantVolume.hpp"
+#include "../FanConstantVolume_Impl.hpp"
+#include "../ElectricEquipment.hpp"
+#include "../ElectricEquipment_Impl.hpp"
+#include "../ElectricEquipmentDefinition.hpp"
+#include "../ElectricEquipmentDefinition_Impl.hpp"
+#include "../Schedule.hpp"
+#include "../EnergyManagementSystemActuator.hpp"
+#include "../OutputVariable.hpp"
+#include "../OutputVariable_Impl.hpp"
+#include "../Model_Impl.hpp"
+
 using namespace openstudio;
 using namespace openstudio::model;
+using std::string;
+
+TEST_F(ModelFixture, ExternalInterfaceActuator) {
+  Model model;
+
+  Building building = model.getUniqueModelObject<Building>();
+
+  ThermalZone zone1(model);
+  ThermalZone zone2(model);
+
+  // add fan
+  Schedule s = model.alwaysOnDiscreteSchedule();
+  FanConstantVolume fan(model, s);
+  FanConstantVolume fan2(model, s);
+
+  // add actuator
+  std::string fanControlType = "Fan Pressure Rise";
+  std::string ComponentType = "Fan";
+  ExternalInterfaceActuator fanActuator(fan, ComponentType, fanControlType);
+  EXPECT_EQ(fanControlType, fanActuator.actuatedComponentControlType());
+  EXPECT_EQ(ComponentType, fanActuator.actuatedComponentType());
+
+  std::string fanName = fan.name().get() + "Press Actuator";
+  fanActuator.setName(fanName);
+
+  fanActuator.setActuatedComponentControlType(fanControlType);
+
+  EXPECT_EQ(fan, fanActuator.actuatedComponentUnique());
+  EXPECT_EQ(fanControlType, fanActuator.actuatedComponentControlType());
+
+  fanActuator.setActuatedComponentType(ComponentType);
+  EXPECT_EQ(ComponentType, fanActuator.actuatedComponentType());
+
+  fanActuator.setActuatedComponentUnique(fan2);
+  EXPECT_EQ(fan2, fanActuator.actuatedComponentUnique());
+
+  EXPECT_FALSE(fanActuator.optionalInitialValue());
+  fanActuator.setOptionalInitialValue(1);
+  EXPECT_EQ(1.0, fanActuator.optionalInitialValue());
+  fanActuator.resetOptionalInitialValue();
+  EXPECT_FALSE(fanActuator.optionalInitialValue());
+
+  //add electric equipment actuator
+  ElectricEquipmentDefinition definition(model);
+  ElectricEquipment electricEquipment(definition);
+  ComponentType = "ElectricEquipment";
+  std::string equipControlType = "Electric Power Level";
+  ExternalInterfaceActuator equipActuator(electricEquipment, ComponentType, equipControlType);
+  EXPECT_EQ(equipControlType, equipActuator.actuatedComponentControlType());
+  EXPECT_EQ(ComponentType, equipActuator.actuatedComponentType());
+
+  std::string equipName = electricEquipment.name().get() + "power level Actuator";
+  equipActuator.setName(equipName);
+
+  equipActuator.setActuatedComponentControlType(equipControlType);
+
+  EXPECT_EQ(electricEquipment, equipActuator.actuatedComponentUnique());
+  EXPECT_EQ(equipControlType, equipActuator.actuatedComponentControlType());
+
+  equipActuator.setActuatedComponentType(ComponentType);
+  EXPECT_EQ(ComponentType, equipActuator.actuatedComponentType());
+
+}
 
