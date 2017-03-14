20012029ecab8bf6dd3e19529bef18dc172eee9b
David Goldwasser <david.f.goldwasser@gmail.com>
2017-03-13 22:49:00 -0600

    fix to template model measure test
    Found issue on model test template where I had bool arg that I wanted
    to set to false. It skipped over the if statement, but works with
    args_hash.has_key?

--- openstudiocore/src/utilities/bcl/templates/ModelMeasure/tests/model_measure_test.rb
+++ openstudiocore/src/utilities/bcl/templates/ModelMeasure/tests/model_measure_test.rb
@@ -46,7 +46,7 @@ class ModelMeasureName_Test < MiniTest::Unit::TestCase
     # populate argument with specified hash value if specified
     arguments.each do |arg|
       temp_arg_var = arg.clone
-      if args_hash[arg.name]
+      if args_hash.has_key?(arg.name)
         assert(temp_arg_var.setValue(args_hash[arg.name]))
       end
       argument_map[arg.name] = temp_arg_var
@@ -93,7 +93,7 @@ class ModelMeasureName_Test < MiniTest::Unit::TestCase
     # populate argument with specified hash value if specified
     arguments.each do |arg|
       temp_arg_var = arg.clone
-      if args_hash[arg.name]
+      if args_hash.has_key?(arg.name)
         assert(temp_arg_var.setValue(args_hash[arg.name]))
       end
       argument_map[arg.name] = temp_arg_var
