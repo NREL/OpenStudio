dac050bf720bb93b232f4fc27fdac74f484cd065
Julien Marrec <julien.marrec@gmail.com>
2017-06-01 18:04:13 +0200

    Fixed imports in openstudio.py

--- openstudiocore/python/openstudio.py
+++ openstudiocore/python/openstudio.py
@@ -1,34 +1,38 @@
 # imports all of the openstudio libraries into a friendly namespace
 
-from openstudioutilitiescore import *
-from openstudioutilitieseconomics import *
-from openstudioutilitiestime import *
-from openstudioutilitiesdata import *
-from openstudioutilitiesplot import *
-from openstudioutilitiesgeometry import *
-from openstudioutilitiessql import *
-from openstudioutilitiesbcl import *
-from openstudioutilitiesunits import *
-from openstudioutilitiesidd import *
-from openstudioutilitiesidf import *
-from openstudioutilitiesfiletypes import *
-from openstudioutilities import *
+import .openstudioairflow as airflow
+import .openstudioanalysis as analysis
+import .openstudioanalysisdriver as analysisdriver
+import .openstudioenergyplus as energyplus
+import .openstudiogbxml as gbxml
+import .openstudioisomodel as isomodel
+import .openstudiolib as lib
+import .openstudiomeasure as measure
+import .openstudiomodel as model
+# import openstudiomodelcore as modelcore
+import .openstudiomodeleditor as modeleditor
+# import openstudiomodelgenerators as modelgenerators
+# import openstudiomodelgeometry as modelgeometry
+# import openstudiomodelhvac as modelhvac
+# import openstudiomodelrefrigeration as modelrefrigeration
+# import openstudiomodelresources as modelresources
+# import openstudiomodelsimulation as modelsimulation
+import .openstudioosversion as osversion
+import .openstudioradiance as radiance
+import .openstudiosdd as sdd
 
-import openstudioenergyplus as energyplus
+from .openstudioutilities import *
+from .openstudioutilitiesbcl import *
+from .openstudioutilitiescore import *
+from .openstudioutilitiesdata import *
+from .openstudioutilitieseconomics import *
+from .openstudioutilitiesgeometry import *
+from .openstudioutilitiesidd import *
+from .openstudioutilitiesidf import *
+# from .openstudioutilitiesfiletypes import *
+# from .openstudioutilitiesplot import *
+from .openstudioutilitiessql import *
+from .openstudioutilitiestime import *
+from .openstudioutilitiesunits import *
 
-import openstudiomodel as model
-
-import openstudioradiance as radiance
-
-import openstudiogbxml as gbxml
-
-import openstudioosversion as osversion
-
-import openstudiomeasure as measure
-import openstudiorunmanager as runmanager
-import openstudioproject as project
-import openstudioanalysisdriver as analysisdriver
-import openstudiomodeleditor as modeleditor
-import openstudioanalysis as analysis
-import openstudiolib as lib
 
