ddf6616551a121feebbde8338644c26d5fa89d46
Julien Marrec <julien.marrec@gmail.com>
2017-08-02 13:56:24 +0200

    Update the ruby test. Noticed that the plant loops stay connected... will need to redo this

--- test_CentralHeatPump.rb
+++ test_CentralHeatPump.rb
@@ -104,12 +104,81 @@ class CentralHeatPump_Test < MiniTest::Unit::TestCase
     assert(central_hp.sourcePlantLoop)
     assert_equal(sourcePlantLoop, central_hp.sourcePlantLoop.get)
 
+    # tertiaryPlantLoop = Hot Water loop
     heatingPlantLoop = OpenStudio::Model::PlantLoop.new(model)
     assert(central_hp.addToTertiaryNode(heatingPlantLoop.supplyInletNode))
     assert(central_hp.tertiaryPlantLoop)
     assert_equal(heatingPlantLoop, central_hp.tertiaryPlantLoop.get)
     assert(central_hp.heatingPlantLoop)
     assert_equal(heatingPlantLoop, central_hp.heatingPlantLoop.get)
+
+  end
+
+  def test_clone_in_same_model
+
+    model = OpenStudio::Model::Model.new
+
+    central_hp = OpenStudio::Model::CentralHeatPumpSystem.new(model)
+
+    # PlantLoop = CoolingPlantLoop
+    coolingPlantLoop = OpenStudio::Model::PlantLoop.new(model)
+    central_hp.addToNode(coolingPlantLoop.supplyInletNode)
+    # secondaryPlantLoop = Source Plant Loop
+    sourcePlantLoop = OpenStudio::Model::PlantLoop.new(model)
+    central_hp.addToNode(sourcePlantLoop.demandInletNode)
+    # tertiaryPlantLoop = Hot Water loop
+    heatingPlantLoop = OpenStudio::Model::PlantLoop.new(model)
+    central_hp.addToTertiaryNode(heatingPlantLoop.supplyInletNode)
+
+    central_hpClone = central_hp.clone(model)
+    central_hpClone = central_hpClone.to_CentralHeatPumpSystem.get
+    assert_equal(2, model.getCentralHeatPumpSystems.size)
+
+    # Make sure it's isn't connected to the same loops
+    assert(central_hpClone.coolingPlantLoop.empty?)
+    assert(central_hpClone.sourcePlantLoop.empty?)
+    assert(central_hpClone.heatingPlantLoop.empty?)
+
+
+    mod = OpenStudio::Model::CentralHeatPumpSystemModule.new(model)
+    assert_equal(1, model.getCentralHeatPumpSystemModules.size)
+    assert_equal(1, model.getChillerHeaterPerformanceElectricEIRs.size)
+    assert(central_hp.addModule(mod))
+    assert_equal(1, central_hp.modules.size)
+    assert_equal(0, central_hpClone.modules.size)
+
+    # Clone again, this time the Modules and chiller will be cloned too
+    central_hpClone1 = central_hp.clone(model)
+    central_hpClone1 = central_hpClone1.to_CentralHeatPumpSystem.get
+    assert_equal(3, model.getCentralHeatPumpSystems.size)
+    assert_equal(2, model.getCentralHeatPumpSystemModules.size)
+    assert_equal(2, model.getChillerHeaterPerformanceElectricEIRs.size)
+    assert_equal(1, central_hpClone1.modules.size)
+    modClone = central_hpClone1.modules[0]
+    refute_equal(mod, modClone)
+
+    chiller = mod.chillerHeaterModulesPerformanceComponent
+    chillerClone = modClone.chillerHeaterModulesPerformanceComponent
+    refute_equal(chiller, chillerClone)
+
+  end
+
+  def test_clone_in_other_model
+
+    model = OpenStudio::Model::Model.new
+
+    central_hp = OpenStudio::Model::CentralHeatPumpSystem.new(model)
+    mod = OpenStudio::Model::CentralHeatPumpSystemModule.new(model)
+    central_hp.addModule(mod)
+
+    # Clone in other model
+    model2 = OpenStudio::Model::Model.new
+    # TODO: This throws warnings for curve objects having multiple parents
+    # https://github.com/NREL/OpenStudio/blob/72e4961a2d6547dcb40be48f722345b5691e3a74/openstudiocore/src/model/Curve.cpp#L59
+    central_hpClone1 = central_hp.clone(model2)
+    assert_equal(1, model2.getCentralHeatPumpSystems.size)
+    assert_equal(1, model2.getCentralHeatPumpSystemModules.size)
+    assert_equal(1, model2.getChillerHeaterPerformanceElectricEIRs.size)
 
   end
 
