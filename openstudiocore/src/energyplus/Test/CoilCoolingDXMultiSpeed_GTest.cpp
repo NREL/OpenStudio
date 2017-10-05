eacfce051f58b0685f5a46d55ba6d9fbbb7bc1af
Julien Marrec <julien.marrec@gmail.com>
2017-10-05 15:50:17 +0200

    Added Gtest

---
+++ openstudiocore/src/energyplus/Test/CoilCoolingDXMultiSpeed_GTest.cpp
@@ -0,0 +1,111 @@
+/***********************************************************************************************************************
+ *  OpenStudio(R), Copyright (c) 2008-2017, Alliance for Sustainable Energy, LLC. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ *  following conditions are met:
+ *
+ *  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ *  disclaimer.
+ *
+ *  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ *  following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ *  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote
+ *  products derived from this software without specific prior written permission from the respective party.
+ *
+ *  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative
+ *  works may not use the "OpenStudio" trademark, "OS", "os", or any other confusingly similar designation without
+ *  specific prior written permission from Alliance for Sustainable Energy, LLC.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **********************************************************************************************************************/
+
+#include <gtest/gtest.h>
+#include "EnergyPlusFixture.hpp"
+
+#include "../ForwardTranslator.hpp"
+// #include "../ReverseTranslator.hpp"
+
+#include "../../model/CoilCoolingDXMultiSpeed.hpp"
+#include "../../model/CoilCoolingDXMultiSpeedStageData.hpp"
+
+#include "../../model/Model.hpp"
+#include "../../model/AirLoopHVAC.hpp"
+#include "../../model/Node.hpp"
+
+#include "../../utilities/idf/Workspace.hpp"
+#include "../../utilities/idf/WorkspaceObject.hpp"
+#include "../../utilities/idf/WorkspaceExtensibleGroup.hpp"
+
+#include <utilities/idd/Coil_Cooling_DX_MultiSpeed_FieldEnums.hxx>
+#include "../../utilities/idf/IdfExtensibleGroup.hpp"
+
+#include <resources.hxx>
+#include <sstream>
+
+using namespace openstudio::energyplus;
+using namespace openstudio::model;
+using namespace openstudio;
+
+
+/* It should allow for up to 4 stages. Tests that issue #2812 is fixed and doesn't come back */
+TEST_F(EnergyPlusFixture, ForwardTranslator_ThermalZone_4Stages)
+{
+
+  Model m;
+  CoilCoolingDXMultiSpeed coil(m);
+
+  CoilCoolingDXMultiSpeedStageData stage1(model);
+  coil.addStage(stage1);
+
+  CoilCoolingDXMultiSpeedStageData stage2(model);
+  coil.addStage(stage2);
+
+  CoilCoolingDXMultiSpeedStageData stage3(model);
+  coil.addStage(stage3);
+
+  CoilCoolingDXMultiSpeedStageData stage4(model);
+  coil.addStage(stage4);
+
+  ASSERT_EQ(4u,coil.stages().size());
+
+
+  AirLoopHVAC airLoop(m);
+  Node supplyOutletNode = airLoop.supplyOutletNode();
+  coil.addToNode(supplyOutletNode);
+
+  //translate the model to EnergyPlus
+  ForwardTranslator ft;
+  Workspace workspace = ft.translateModel(model);
+
+
+  EXPECT_EQ(1u, workspace.getObjectsByType(IddObjectType::Coil_Cooling_DX_MultiSpeed).size());
+  WorkspaceObject idf_coil = workspace.getObjectsByType(IddObjectType::Coil_Cooling_DX_MultiSpeed)[0];
+
+  // Check that there are indeed 4 stages, both the NumberofSpeeds field (int) and the number of extensible groups
+  ASSERT_EQ(4u, idf_coil.getInt(Coil_Cooling_DX_MultiSpeedFields::NumberofSpeeds).get());
+  ASSERT_EQ(4u, idf_coil.extensibleGroups().size())
+
+  // WorkspaceExtensibleGroup eg = idf_coil.extensibleGroups()[0] or getExtensibleGroup(0);
+
+  ASSERT_EQ(idf_coil.getExtensibleGroup(0).getString(Coil_Cooling_DX_MultiSpeedExtensibleFields::SpeedTotalCoolingCapacityFunctionofTemperatureCurveName).get(),
+            stage1.totalCoolingCapacityFunctionofTemperatureCurve().name().get());
+
+  ASSERT_EQ(idf_coil.getExtensibleGroup(1).getString(Coil_Cooling_DX_MultiSpeedExtensibleFields::SpeedTotalCoolingCapacityFunctionofTemperatureCurveName).get(),
+            stage2.totalCoolingCapacityFunctionofTemperatureCurve().name().get());
+
+  ASSERT_EQ(idf_coil.getExtensibleGroup(2).getString(Coil_Cooling_DX_MultiSpeedExtensibleFields::SpeedTotalCoolingCapacityFunctionofTemperatureCurveName).get(),
+            stage3.totalCoolingCapacityFunctionofTemperatureCurve().name().get());
+
+  ASSERT_EQ(idf_coil.getExtensibleGroup(3).getString(Coil_Cooling_DX_MultiSpeedExtensibleFields::SpeedTotalCoolingCapacityFunctionofTemperatureCurveName).get(),
+            stage4.totalCoolingCapacityFunctionofTemperatureCurve().name().get());
+
+}
+
+
