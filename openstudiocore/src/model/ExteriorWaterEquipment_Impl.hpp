35af0cea951dbe57c19ecc79359cc6dedbce9f39
Julien Marrec <julien.marrec@gmail.com>
2017-12-21 14:39:05 +0100

    Make inherit in Impl

--- openstudiocore/src/model/ExteriorWaterEquipment_Impl.hpp
+++ openstudiocore/src/model/ExteriorWaterEquipment_Impl.hpp
@@ -30,7 +30,7 @@
 #define MODEL_EXTERIORWATEREQUIPMENT_IMPL_HPP
 
 #include "ModelAPI.hpp"
-#include "ModelObject_Impl.hpp"
+#include "ExteriorLoadInstance_Impl.hpp"
 
 namespace openstudio {
 namespace model {
@@ -41,8 +41,8 @@ class Facility;
 
 namespace detail {
 
-  /** ExteriorWaterEquipment_Impl is a ModelObject_Impl that is the implementation class for ExteriorWaterEquipment.*/
-  class MODEL_API ExteriorWaterEquipment_Impl : public ModelObject_Impl {
+  /** ExteriorWaterEquipment_Impl is a ExteriorLoadInstance_Impl that is the implementation class for ExteriorWaterEquipment.*/
+  class MODEL_API ExteriorWaterEquipment_Impl : public ExteriorLoadInstance_Impl {
 
    public:
     /** @name Constructors and Destructors */
@@ -72,6 +72,12 @@ namespace detail {
 
     virtual std::vector<ScheduleTypeKey> getScheduleTypeKeys(const Schedule& schedule) const override;
 
+    virtual double multiplier() const override;
+
+    virtual bool isMultiplierDefaulted() const override;
+
+    virtual bool setDefinition(const ExteriorLoadDefinition& definition) override;
+
     //@}
     /** @name Getters */
     //@{
@@ -79,10 +85,6 @@ namespace detail {
     ExteriorWaterEquipmentDefinition exteriorWaterEquipmentDefinition() const;
 
     Schedule schedule() const;
-
-    double multiplier() const;
-
-    bool isMultiplierDefaulted() const;
 
     std::string endUseSubcategory() const;
 
