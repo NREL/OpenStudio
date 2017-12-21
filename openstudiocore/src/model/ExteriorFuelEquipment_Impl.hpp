7eae06e40f4048f9b48ae988df3ddcbb542278ed
Julien Marrec <julien.marrec@gmail.com>
2017-12-21 14:39:05 +0100

    Make inherit in Impl

--- openstudiocore/src/model/ExteriorFuelEquipment_Impl.hpp
+++ openstudiocore/src/model/ExteriorFuelEquipment_Impl.hpp
@@ -30,7 +30,7 @@
 #define MODEL_EXTERIORFUELEQUIPMENT_IMPL_HPP
 
 #include "ModelAPI.hpp"
-#include "ModelObject_Impl.hpp"
+#include "ExteriorLoadInstance_Impl.hpp"
 
 namespace openstudio {
 namespace model {
@@ -41,8 +41,8 @@ class Facility;
 
 namespace detail {
 
-  /** ExteriorFuelEquipment_Impl is a ModelObject_Impl that is the implementation class for ExteriorFuelEquipment.*/
-  class MODEL_API ExteriorFuelEquipment_Impl : public ModelObject_Impl {
+  /** ExteriorFuelEquipment_Impl is a ExteriorLoadInstance_Impl that is the implementation class for ExteriorFuelEquipment.*/
+  class MODEL_API ExteriorFuelEquipment_Impl : public ExteriorLoadInstance_Impl {
 
    public:
     /** @name Constructors and Destructors */
@@ -72,6 +72,12 @@ namespace detail {
 
     virtual std::vector<ScheduleTypeKey> getScheduleTypeKeys(const Schedule& schedule) const override;
 
+    virtual double multiplier() const override;
+
+    virtual bool isMultiplierDefaulted() const override;
+
+    virtual bool setDefinition(const ExteriorLoadDefinition& definition) override;
+
     //@}
     /** @name Getters */
     //@{
@@ -81,10 +87,6 @@ namespace detail {
     Schedule schedule() const;
 
     std::string fuelType() const;
-
-    double multiplier() const;
-
-    bool isMultiplierDefaulted() const;
 
     std::string endUseSubcategory() const;
 
