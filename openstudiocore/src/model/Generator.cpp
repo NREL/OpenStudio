1c515c55ab31d13971cdf8c62f724ccbba93cfd4
Brian Ball <Brian.Ball@nrel.gov>
2018-01-25 11:29:10 -0700

    Generator emsActuator and IntVar ElectricLoadCenterStorageSimple emsAct and IntVar

--- openstudiocore/src/model/Generator.cpp
+++ openstudiocore/src/model/Generator.cpp
@@ -96,6 +96,16 @@ boost::optional<ElectricLoadCenterDistribution> Generator_Impl::electricLoadCent
   return boost::none;
 }
 
+std::vector<EMSActuatorNames> Generator_Impl::emsActuatorNames() const {
+  std::vector<EMSActuatorNames> actuators{{"On-Site Generator Control", "Requested Power"}};
+  return actuators;
+}
+
+std::vector<std::string> Generator_Impl::emsInternalVariableNames() const {
+  std::vector<std::string> types{"Generator Nominal Maximum Power",
+                                 "Generator Nominal Thermal To Electric Ratio"};
+  return types;
+}
 } // detail
 
 Generator::Generator(IddObjectType type,const Model& model)
