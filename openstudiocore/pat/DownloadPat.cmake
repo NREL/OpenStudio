2942d45f0c93af0a8740f4307599b11bc3905631
ljbrackney <larry.brackney@nrel.gov>
2018-01-31 07:26:21 -0800

    Updated DownloadPat.cmake for slow connections

--- openstudiocore/pat/DownloadPat.cmake
+++ openstudiocore/pat/DownloadPat.cmake
@@ -9,11 +9,7 @@ if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/OpenStudio-PAT-${PAT_SHA}.zip)
   # Make github public and use a direct github url please
   file(DOWNLOAD "https://github.com/NREL/OpenStudio-PAT/archive/${PAT_SHA}.zip"
     "${CMAKE_CURRENT_BINARY_DIR}/OpenStudio-PAT-${PAT_SHA}.zip"
-<<<<<<< Updated upstream
-    TIMEOUT 480 INACTIVITY_TIMEOUT 480
-=======
     TIMEOUT 980 INACTIVITY_TIMEOUT 980 
->>>>>>> Stashed changes
     SHOW_PROGRESS
     STATUS DOWNLOAD_STATUS
   )
