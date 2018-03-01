1af02b1d90c2743f1267f81102c3f886b3c1648f
Brian Ball <Brian.Ball@nrel.gov>
2018-03-01 09:00:45 -0700

    building and boiler outputVariableNames

--- openstudiocore/src/model/BuildingUnit.cpp
+++ openstudiocore/src/model/BuildingUnit.cpp
@@ -72,8 +72,6 @@ namespace detail {
   const std::vector<std::string>& BuildingUnit_Impl::outputVariableNames() const
   {
     static std::vector<std::string> result;
-    if (result.empty()){
-    }
     return result;
   }
 
