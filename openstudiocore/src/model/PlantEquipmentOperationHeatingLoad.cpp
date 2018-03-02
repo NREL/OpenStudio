53b7cf5b249ff656ebe3bc0ae258cbf7be06729d
Brian Ball <Brian.Ball@nrel.gov>
2018-03-02 10:08:17 -0700

    P's outputVariableNames

--- openstudiocore/src/model/PlantEquipmentOperationHeatingLoad.cpp
+++ openstudiocore/src/model/PlantEquipmentOperationHeatingLoad.cpp
@@ -62,8 +62,6 @@ namespace detail {
   const std::vector<std::string>& PlantEquipmentOperationHeatingLoad_Impl::outputVariableNames() const
   {
     static std::vector<std::string> result;
-    if (result.empty()){
-    }
     return result;
   }
 
--- openstudiocore/src/model/PortList.cpp
+++ openstudiocore/src/model/PortList.cpp
@@ -75,8 +75,6 @@ PortList_Impl::PortList_Impl(const PortList_Impl& other,
 const std::vector<std::string>& PortList_Impl::outputVariableNames() const
 {
   static std::vector<std::string> result;
-  if (result.empty()){
-  }
   return result;
 }
 
