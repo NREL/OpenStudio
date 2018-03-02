c2e6f4bca47e2c7b654ede08972ca8b1360094db
Brian Ball <Brian.Ball@nrel.gov>
2018-03-02 09:59:57 -0700

    upto O's outputVariableNames

--- openstudiocore/src/model/OutputVariable.cpp
+++ openstudiocore/src/model/OutputVariable.cpp
@@ -84,8 +84,6 @@ namespace detail {
   const std::vector<std::string>& OutputVariable_Impl::outputVariableNames() const
   {
     static std::vector<std::string> result;
-    if (result.empty()){
-    }
     return result;
   }
 
