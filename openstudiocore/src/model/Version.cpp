13c41aa12e1b8bb700ea128a155d3b75a1c9473c
Brian Ball <Brian.Ball@nrel.gov>
2018-03-02 11:02:39 -0700

    U-V's outputVariableNames

--- openstudiocore/src/model/Version.cpp
+++ openstudiocore/src/model/Version.cpp
@@ -96,8 +96,6 @@ namespace detail {
   const std::vector<std::string>& Version_Impl::outputVariableNames() const
   {
     static std::vector<std::string> result;
-    if (result.empty()){
-    }
     return result;
   }
 
