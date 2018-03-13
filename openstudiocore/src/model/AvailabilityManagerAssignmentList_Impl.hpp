76bda1f664acf30e3eeb6d96729ef8d9008ef7e3
Julien Marrec <julien.marrec@gmail.com>
2018-03-13 22:27:18 +0100

    Remove build warnings due to override

--- openstudiocore/src/model/AvailabilityManagerAssignmentList_Impl.hpp
+++ openstudiocore/src/model/AvailabilityManagerAssignmentList_Impl.hpp
@@ -70,7 +70,7 @@ namespace detail {
 
     virtual const std::vector<std::string>& outputVariableNames() const override;
 
-    virtual IddObjectType iddObjectType() const;
+    virtual IddObjectType iddObjectType() const override;
 
     virtual ModelObject clone(Model model) const override;
 
