c81fbd165e7b8c5425eb6e5fcab0b03fdcb1a6ac
Julien Marrec <julien.marrec@gmail.com>
2018-03-14 01:56:51 +0100

    Unfinished Gtest for ATU

--- openstudiocore/src/model/test/AirTerminalSingleDuctConstantVolumeFourPipeBeam_GTest.cpp
+++ openstudiocore/src/model/test/AirTerminalSingleDuctConstantVolumeFourPipeBeam_GTest.cpp
@@ -33,6 +33,130 @@
 #include "../AirTerminalSingleDuctConstantVolumeFourPipeBeam.hpp"
 #include "../AirTerminalSingleDuctConstantVolumeFourPipeBeam_Impl.hpp"
 
+#include "../CoilCoolingFourPipeBeam.hpp"
+#include "../CoilHeatingFourPipeBeam.hpp"
+#include "../PlantLoop.hpp"
+#include "../AirLoopHVAC.hpp"
+#include "../ScheduleConstant.hpp"
+#include "../Node.hpp"
+
+// Needed for getConcreteModelObjects
+#include "../CoilCoolingFourPipeBeam_Impl.hpp"
+#include "../CoilHeatingFourPipeBeam_Impl.hpp"
+
 using namespace openstudio;
 using namespace openstudio::model;
 
+
+TEST_F(ModelFixture, AirTerminalSingleDuctConstantVolumeFourPipeBeam_DefaultConstructors)
+{
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+  ASSERT_EXIT (
+  {
+    Model m;
+    AirTerminalSingleDuctConstantVolumeFourPipeBeam atu(m);
+
+    exit(0);
+  } ,
+    ::testing::ExitedWithCode(0), "" );
+}
+
+TEST_F(ModelFixture, AirTerminalSingleDuctConstantVolumeFourPipeBeam_OverloadedCtor)
+{
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+  ASSERT_EXIT (
+  {
+    Model m;
+    CoilCoolingFourPipeBeam cc(m);
+    CoilHeatingFourPipeBeam hc(m);
+    AirTerminalSingleDuctConstantVolumeFourPipeBeam atu(m, cc, hc);
+
+    exit(0);
+  } ,
+    ::testing::ExitedWithCode(0), "" );
+}
+
+TEST_F(ModelFixture, AirTerminalSingleDuctConstantVolumeFourPipeBeam_BasicGettersSetters)
+{
+  Model m;
+  AirTerminalSingleDuctConstantVolumeFourPipeBeam atu(m);
+
+}
+
+TEST_F(ModelFixture, AirTerminalSingleDuctConstantVolumeFourPipeBeam_Coils)
+{
+
+  Model m;
+  CoilCoolingFourPipeBeam cc(m);
+  CoilHeatingFourPipeBeam hc(m);
+  AirTerminalSingleDuctConstantVolumeFourPipeBeam atu(m, cc, hc);
+
+  ASSERT_TRUE(atu.coolingCoil());
+  EXPECT_EQ(cc.handle(), atu.coolingCoil()->handle());
+
+  ASSERT_TRUE(atu.heatingCoil());
+  EXPECT_EQ(hc.handle(), atu.heatingCoil()->handle());
+
+  CoilCoolingFourPipeBeam cc2(m);
+  EXPECT_TRUE(atu.setCoolingCoil(cc2));
+  EXPECT_EQ(cc2.handle(), atu.coolingCoil()->handle());
+
+  CoilHeatingFourPipeBeam hc2(m);
+  EXPECT_TRUE(atu.setHeatingCoil(hc2));
+  EXPECT_EQ(hc2.handle(), atu.heatingCoil()->handle());
+
+  PlantLoop hw_p(m);
+  PlantLoop chw_p(m);
+
+  EXPECT_TRUE(chw_p.addDemandBranchForComponent(cc2));
+  EXPECT_TRUE(hw_p.addDemandBranchForComponent(hc2));
+
+  /* Test the provided convenience functions */
+  ASSERT_TRUE(atu.chilledWaterPlantLoop());
+  EXPECT_EQ(chw_p.handle(), atu.chilledWaterPlantLoop()->handle());
+
+  ASSERT_TRUE(atu.chilledWaterInletNode());
+  ASSERT_TRUE(cc2.chilledWaterInletNode());
+  EXPECT_EQ(cc2.chilledWaterInletNode()->handle(), atu.chilledWaterInletNode()->handle());
+
+  ASSERT_TRUE(atu.chilledWaterOutletNode());
+  ASSERT_TRUE(cc2.chilledWaterOutletNode());
+  EXPECT_EQ(cc2.chilledWaterOutletNode()->handle(), atu.chilledWaterOutletNode()->handle());
+
+
+  ASSERT_TRUE(atu.hotWaterPlantLoop());
+  EXPECT_EQ(hw_p.handle(), atu.hotWaterPlantLoop()->handle());
+
+  ASSERT_TRUE(atu.hotWaterInletNode());
+  ASSERT_TRUE(hc2.hotWaterInletNode());
+  EXPECT_EQ(hc2.hotWaterInletNode()->handle(), atu.hotWaterInletNode()->handle());
+
+  ASSERT_TRUE(atu.hotWaterOutletNode());
+  ASSERT_TRUE(hc2.hotWaterOutletNode());
+  EXPECT_EQ(hc2.hotWaterOutletNode()->handle(), atu.hotWaterOutletNode()->handle());
+
+
+}
+
+
+TEST_F(ModelFixture, AirTerminalSingleDuctConstantVolumeFourPipeBeam_Clone)
+{
+
+  Model m;
+  CoilCoolingFourPipeBeam cc(m);
+  CoilHeatingFourPipeBeam hc(m);
+  AirTerminalSingleDuctConstantVolumeFourPipeBeam atu(m, cc, hc);
+
+  AirTerminalSingleDuctConstantVolumeFourPipeBeam atuClone = atu.clone(m).cast<AirTerminalSingleDuctConstantVolumeFourPipeBeam>();
+
+  // I expect the cooling/heating coils to have been cloned too
+  EXPECT_EQ(2u, m.getConcreteModelObjects<CoilCoolingFourPipeBeam>().size());
+  EXPECT_EQ(2u, m.getConcreteModelObjects<CoilHeatingFourPipeBeam>().size());
+
+  // And they should have been reassigned to the clone too
+  ASSERT_TRUE(atuClone.coolingCoil());
+  ASSERT_TRUE(atuClone.heatingCoil());
+
+}
