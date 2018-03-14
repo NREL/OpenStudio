d0523545ec3d9118d44f9af663a3932073018cac
Julien Marrec <julien.marrec@gmail.com>
2018-03-14 01:42:39 +0100

    More Gtest for coil heating

--- openstudiocore/src/model/test/CoilHeatingFourPipeBeam_GTest.cpp
+++ openstudiocore/src/model/test/CoilHeatingFourPipeBeam_GTest.cpp
@@ -34,6 +34,8 @@
 #include "../CoilHeatingFourPipeBeam_Impl.hpp"
 #include "../CurveLinear.hpp"
 #include "../TableMultiVariableLookup.hpp"
+#include "../PlantLoop.hpp"
+#include "../Node.hpp"
 
 using namespace openstudio;
 using namespace openstudio::model;
@@ -79,28 +81,70 @@ TEST_F(ModelFixture, CoilHeatingFourPipeBeam_Getters_Setters)
   EXPECT_TRUE(coil.isBeamRatedHotWaterVolumeFlowRateperBeamLengthDefaulted());
 
   // A5: Beam Heating Capacity Temperature Difference Modification Factor Curve Name (UniVariateCurves // UniVariateTables)
-  CurveLinear capModFuncOfTempDiff(model);
+  CurveLinear capModFuncOfTempDiff(m);
   EXPECT_TRUE(coil.setBeamHeatingCapacityTemperatureDifferenceModificationFactorCurve(capModFuncOfTempDiff));
   EXPECT_EQ(capModFuncOfTempDiff.handle(), coil.beamHeatingCapacityTemperatureDifferenceModificationFactorCurve().handle());
 
   // A6: Beam Heating Capacity Air Flow Modification Factor Curve Name (UniVariateCurves // UniVariateTables)
-  TableMultiVariableLookup heatCapModFuncOfSAFlow(model, 1);
+  TableMultiVariableLookup heatCapModFuncOfSAFlow(m, 1);
   EXPECT_TRUE(coil.setBeamHeatingCapacityAirFlowModificationFactorCurve(heatCapModFuncOfSAFlow));
   EXPECT_EQ(heatCapModFuncOfSAFlow.handle(), coil.beamHeatingCapacityAirFlowModificationFactorCurve().handle());
 
   // A7: Beam Heating Capacity Hot Water Flow Modification Factor Curve Name (UniVariateCurves // UniVariateTables)
-  TableMultiVariableLookup capModFuncOfWaterFlow(model, 1);
+  TableMultiVariableLookup capModFuncOfWaterFlow(m, 1);
   EXPECT_TRUE(coil.setBeamHeatingCapacityHotWaterFlowModificationFactorCurve(capModFuncOfWaterFlow));
   EXPECT_EQ(capModFuncOfWaterFlow.handle(), coil.beamHeatingCapacityHotWaterFlowModificationFactorCurve().handle());
-
 
 }
 
 
 TEST_F(ModelFixture, CoilHeatingFourPipeBeam_LoopConnections)
 {
+
+  Model m;
+  CoilHeatingFourPipeBeam coil(m);
+  PlantLoop p(m);
+
   // A3: Hot Water Inlet Node Name
-
-
   // A4: Hot Water Outlet Node Name
+
+  // Connection should only be allowed on the demand side of the plantLoop
+  EXPECT_TRUE(p.addDemandBranchForComponent(coil));
+  ASSERT_TRUE(coil.plantLoop());
+  EXPECT_EQ(p.handle(), coil.plantLoop()->handle());
+  EXPECT_EQ(7u, p.demandComponents().size());
+  EXPECT_TRUE(coil.hotWaterInletNode());
+  EXPECT_TRUE(coil.hotWaterOutletNode());
+
+  // Shouldn't work
+  EXPECT_FALSE(p.addSupplyBranchForComponent(coil));
+
+  // Disconnect the coil
+  coil.disconnect();
+  EXPECT_EQ(0u, p.demandComponents().size());
+  EXPECT_FALSE(coil.plantLoop());
+  EXPECT_FALSE(coil.hotWaterInletNode());
+  EXPECT_FALSE(coil.hotWaterOutletNode());
+
+}
+
+TEST_F(ModelFixture, CoilHeatingFourPipeBeam_Clone)
+{
+
+  Model m;
+  CoilHeatingFourPipeBeam coil(m);
+
+  CoilHeatingFourPipeBeam coilClone = coil.clone(m).cast<CoilHeatingFourPipeBeam>();
+
+
+  // Are curves shared now?
+  EXPECT_EQ(     coil.beamHeatingCapacityTemperatureDifferenceModificationFactorCurve(),
+            coilClone.beamHeatingCapacityTemperatureDifferenceModificationFactorCurve() );
+
+  EXPECT_EQ(     coil.beamHeatingCapacityAirFlowModificationFactorCurve(),
+            coilClone.beamHeatingCapacityAirFlowModificationFactorCurve() );
+
+  EXPECT_EQ(     coil.beamHeatingCapacityHotWaterFlowModificationFactorCurve(),
+            coilClone.beamHeatingCapacityHotWaterFlowModificationFactorCurve() );
+
 }
