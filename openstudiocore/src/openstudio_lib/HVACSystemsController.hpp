b0d2af5472afd57338e6eeb545c02499111d4870
Kyle Benne <kyle.benne@nrel.gov>
2018-03-25 22:32:28 -0500

    Revert "Update HVACSystemsController.hpp"
    This reverts commit a72f790d69ab036f2a37eb30a84c2fe1b6b505e9.

--- openstudiocore/src/openstudio_lib/HVACSystemsController.hpp
+++ openstudiocore/src/openstudio_lib/HVACSystemsController.hpp
@@ -239,9 +239,9 @@ class HVACControlsController : public QObject, public Nano::Observer
 
   QPointer<NoMechanicalVentilationView> m_noMechanicalVentilationView;
 
-  OSDropZone * m_systemAvailabilityDropZone;
+  QPointer<OSDropZone> m_systemAvailabilityDropZone;
 
-  OSDropZone * m_supplyAirTempScheduleDropZone;
+  QPointer<OSDropZone> m_supplyAirTempScheduleDropZone;
 
   QPointer<FollowOATempSPMView> m_followOATempSPMView;
 
@@ -251,7 +251,7 @@ class HVACControlsController : public QObject, public Nano::Observer
 
   QPointer<HVACSystemsController> m_hvacSystemsController;
 
-  OSDropZone * m_availabilityManagerDropZone;
+  QPointer<OSDropZone> m_availabilityManagerDropZone;
 
   bool m_dirty;
 };
