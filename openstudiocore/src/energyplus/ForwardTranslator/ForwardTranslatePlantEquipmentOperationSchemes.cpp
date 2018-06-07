f41bdf0473d2b62b5c1a9fd7f975749c5c7ab369
Kyle Benne <kyle.benne@nrel.gov>
2018-06-07 11:26:24 -0500

    Add default plant op scheme for Evap Fluid Cooler Two Speed

--- openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslatePlantEquipmentOperationSchemes.cpp
+++ openstudiocore/src/energyplus/ForwardTranslator/ForwardTranslatePlantEquipmentOperationSchemes.cpp
@@ -51,6 +51,8 @@
 #include "../../model/PlantEquipmentOperationScheme_Impl.hpp"
 #include "../../model/EvaporativeFluidCoolerSingleSpeed.hpp"
 #include "../../model/EvaporativeFluidCoolerSingleSpeed_Impl.hpp"
+#include "../../model/EvaporativeFluidCoolerTwoSpeed.hpp"
+#include "../../model/EvaporativeFluidCoolerTwoSpeed_Impl.hpp"
 #include "../../model/FluidCoolerSingleSpeed.hpp"
 #include "../../model/FluidCoolerSingleSpeed_Impl.hpp"
 #include "../../model/FluidCoolerTwoSpeed.hpp"
@@ -208,6 +210,12 @@ boost::optional<double> flowrate(const HVACComponent & component)
       result = mo.designWaterFlowRate();
       break;
     }
+    case openstudio::IddObjectType::OS_EvaporativeFluidCooler_TwoSpeed:
+    {
+      auto mo = component.cast<EvaporativeFluidCoolerTwoSpeed>();
+      result = mo.designWaterFlowRate();
+      break;
+    }
     case openstudio::IddObjectType::OS_FluidCooler_SingleSpeed:
     {
       auto mo = component.cast<FluidCoolerSingleSpeed>();
@@ -338,6 +346,10 @@ ComponentType componentType(const HVACComponent & component)
       return ComponentType::COOLING;
     }
     case openstudio::IddObjectType::OS_EvaporativeFluidCooler_SingleSpeed:
+    {
+      return ComponentType::COOLING;
+    }
+    case openstudio::IddObjectType::OS_EvaporativeFluidCooler_TwoSpeed:
     {
       return ComponentType::COOLING;
     }
