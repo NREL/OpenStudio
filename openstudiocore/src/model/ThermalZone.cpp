6aa0f6a141dba78be4452109106262c6ecdecf64
Julien Marrec <julien.marrec@gmail.com>
2018-08-29 12:39:17 +0200

    Message format (spaces)

--- openstudiocore/src/model/ThermalZone.cpp
+++ openstudiocore/src/model/ThermalZone.cpp
@@ -1578,8 +1578,8 @@ namespace detail {
 
       // don't override if user provided zone floor area
       if (isEmpty(OS_ThermalZoneFields::FloorArea)){
-        LOG(Info, "ThermalZone " << this->name().get() << " has spaces with mis-matched 'Part of Total Floor Area' flags."
-               << "Setting set the flag to 'Yes', but hard-coding the total floor area to only take into account the spaces"
+        LOG(Info, "ThermalZone '" << this->name().get() << "' has spaces with mis-matched 'Part of Total Floor Area' flags. "
+               << "Setting set the flag to 'Yes', but hard-coding the total floor area to only take into account the spaces "
                << "that are part of total Floor Area");
         this->setDouble(OS_ThermalZoneFields::FloorArea, totalFloorArea);
       } else {
