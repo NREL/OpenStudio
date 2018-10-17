9dbde2e79eb87aa01128b98de82650c61fcba3a1
Julien Marrec <julien.marrec@gmail.com>
2018-10-17 14:14:20 +0200

    Expose the reverseTranslate in GenerateClass.rb
    Probably needs cleanup, deal with output directory, etc

--- developer/ruby/GenerateClass.rb
+++ developer/ruby/GenerateClass.rb
@@ -44,6 +44,23 @@ require_relative 'SubProjectClassGenerators/ModelClassGenerator.rb'
 require_relative 'SubProjectClassGenerators/ProjectClassGenerator.rb'
 require_relative 'SubProjectClassGenerators/AnalysisClassGenerator.rb'
 require_relative 'SubProjectClassGenerators/FileHeader.rb'
+require_relative 'SubProjectClassGenerators/ReverseTranslatorGenerator.rb'
+
+
+# Reverse Translation
+def reverseTranslate(options)
+  if options [:reverseTranslator]
+    if options[:iddObjectType]
+      # Strip out the "OS:"
+      epIddName = options[:iddObjectType].to_s.sub(/^OS:/, '')
+      rtgen = ReverseTranslatorGenerator.new(epIddName)
+      rtgen.write_file
+    else
+      raise "WARNING: cannot reverse translate without iddObjectType"
+    end
+  end
+end
+
 
 # HANDLE INPUT ARGUMENTS
 
@@ -79,15 +96,27 @@ optparse = OptionParser.new do |opts|
     options [:iddObjectType] = iddObjectType
   end
 
+  opts.on( '-r', '--[no-]reverseTanslator', "Autogenerate the ReverseTranslator code for IddObjectType") do |rt|
+    options [:reverseTranslator] = rt
+  end
+
 end
 
 # parse the input parameters
 optparse.parse!
 
+
+puts options
+
 # check for required parameters
-if not options[:className] or not options[:sourceDirectory]
-  puts optparse
-  exit
+if (not options[:className] or not options[:sourceDirectory])
+  if options [:reverseTranslator]
+    reverseTranslate(options)
+    exit
+  else
+    puts optparse
+    exit
+  end
 end
 
 # get options out
@@ -123,34 +152,8 @@ end
 
 # START ALL FILES
 
-fileHeader = String.new
+# Get header from Subproject
 fileHeader = fileHeader()
-fileHeader << " *  OpenStudio(R), Copyright (c) 2008-2018, Alliance for Sustainable Energy, LLC. All rights reserved.\n"
-fileHeader << " *\n"
-fileHeader << " *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the\n"
-fileHeader << " *  following conditions are met:\n"
-fileHeader << " *\n"
-fileHeader << " *  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following\n"
-fileHeader << " *  disclaimer.\n"
-fileHeader << " *\n"
-fileHeader << " *  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the\n"
-fileHeader << " *  following disclaimer in the documentation and/or other materials provided with the distribution.\n"
-fileHeader << " *\n"
-fileHeader << " *  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote\n"
-fileHeader << " *  products derived from this software without specific prior written permission from the respective party.\n"
-fileHeader << " *\n"
-fileHeader << " *  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative\n"
-fileHeader << " *  works may not use the \"OpenStudio\" trademark, \"OS\", \"os\", or any other confusingly similar designation without\n"
-fileHeader << " *  specific prior written permission from Alliance for Sustainable Energy, LLC.\n"
-fileHeader << " *\n"
-fileHeader << " *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES,\n"
-fileHeader << " *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n"
-fileHeader << " *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR\n"
-fileHeader << " *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,\n"
-fileHeader << " *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED\n"
-fileHeader << " *  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"
-fileHeader << " *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
-fileHeader << " **********************************************************************************************************************/\n"
 
 hpp = String.new
 cpp = String.new
@@ -552,5 +555,13 @@ if saveAux
   File.open((outputDirectory + "/" + className + ".aux"),"w") do |file|
     file.write(aux)
   end
+end
+
+###############################################################################
+#                       Reverse And Forward Translation                       #
+###############################################################################
+
+if options[:reverseTranslator]
+  reverseTranslate(options)
 end
 
