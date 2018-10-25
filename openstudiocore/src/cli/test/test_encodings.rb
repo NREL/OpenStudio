f5669422af49636ced28b11ff8e7a71b3d05fdb7
Dan Macumber <dan.macumber@gmail.com>
2018-10-24 18:57:25 -0600

    Adding tests which pass now

--- openstudiocore/src/cli/test/test_encodings.rb
+++ openstudiocore/src/cli/test/test_encodings.rb
@@ -27,4 +27,27 @@ class Encoding_Test < Minitest::Test
     assert_equal("UTF-8", path.to_s.encoding.to_s)
   end   
   
+  def test_encoding2
+    test_string = "模型"
+    assert_equal("UTF-8", test_string.encoding.to_s)
+    
+    model = OpenStudio::Model::Model.new
+    space = OpenStudio::Model::Space.new(model)
+    
+    space.setName(test_string)
+    
+    name_string = space.nameString
+    assert_equal(test_string, name_string)
+    assert_equal("UTF-8", name_string.encoding.to_s)
+    
+    optional_string = space.name
+    assert(optional_string.is_initialized)
+    assert_equal(test_string, optional_string.get)
+    assert_equal("UTF-8", optional_string.get.encoding.to_s)
+    
+    path = OpenStudio::toPath(test_string)
+    assert_equal(test_string, path.to_s)
+    assert_equal("UTF-8", path.to_s.encoding.to_s)
+  end   
+  
 end
