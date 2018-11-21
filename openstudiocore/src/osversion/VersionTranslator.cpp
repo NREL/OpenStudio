122ea16d557376852386273324acf055c5881d07
Dan Macumber <dan.macumber@gmail.com>
2018-11-20 23:29:49 -0700

    Update develop to 2.7.2

--- openstudiocore/src/osversion/VersionTranslator.cpp
+++ openstudiocore/src/osversion/VersionTranslator.cpp
@@ -126,7 +126,7 @@ VersionTranslator::VersionTranslator()
   m_updateMethods[VersionString("2.6.2")] = &VersionTranslator::update_2_6_1_to_2_6_2;
   m_updateMethods[VersionString("2.7.0")] = &VersionTranslator::update_2_6_2_to_2_7_0;
   m_updateMethods[VersionString("2.7.1")] = &VersionTranslator::update_2_7_0_to_2_7_1;
-  // m_updateMethods[VersionString("2.7.2")] = &VersionTranslator::defaultUpdate;
+  m_updateMethods[VersionString("2.7.2")] = &VersionTranslator::defaultUpdate;
 
   // List of previous versions that may be updated to this one.
   //   - To increment the translator, add an entry for the version just released (branched for
@@ -271,6 +271,7 @@ VersionTranslator::VersionTranslator()
   m_startVersions.push_back(VersionString("2.6.1"));
   m_startVersions.push_back(VersionString("2.6.2"));
   m_startVersions.push_back(VersionString("2.7.0"));
+  m_startVersions.push_back(VersionString("2.7.1"));
 }
 
 boost::optional<model::Model> VersionTranslator::loadModel(const openstudio::path& pathToOldOsm,
