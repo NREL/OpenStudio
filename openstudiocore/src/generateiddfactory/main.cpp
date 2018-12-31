482a3873d083b2f7e50c7ae92bfcfb8a7117853b
Alex Swindler <Alex.Swindler@nrel.gov>
2018-12-31 12:01:14 -0700

    Updated copyrights

--- openstudiocore/src/generateiddfactory/main.cpp
+++ openstudiocore/src/generateiddfactory/main.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************************************************************
-*  OpenStudio(R), Copyright (c) 2008-2018, Alliance for Sustainable Energy, LLC. All rights reserved.
+*  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC. All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 *  following conditions are met:
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
     std::stringstream fileHeader;
     fileHeader
       << "/***********************************************************************************************************************" << std::endl
-      << " *  OpenStudio(R), Copyright (c) 2008-2018, Alliance for Sustainable Energy, LLC. All rights reserved." << std::endl
+      << " *  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC. All rights reserved." << std::endl
       << " *" << std::endl
       << " *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the" << std::endl
       << " *  following conditions are met:" << std::endl
