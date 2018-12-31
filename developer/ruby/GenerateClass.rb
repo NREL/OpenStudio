33b92572e26d09357d9ec28b4dd8f0cd9205ab33
Alex Swindler <Alex.Swindler@nrel.gov>
2018-12-31 16:48:40 -0700

    Consistent copyrights

--- developer/ruby/GenerateClass.rb
+++ developer/ruby/GenerateClass.rb
@@ -1,5 +1,5 @@
 ########################################################################################################################
-#  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC. All rights reserved.
+#  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 #  following conditions are met:
@@ -7,23 +7,24 @@
 #  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
 #  disclaimer.
 #
-#  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-#  following disclaimer in the documentation and/or other materials provided with the distribution.
+#  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials provided with the distribution.
 #
-#  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote
-#  products derived from this software without specific prior written permission from the respective party.
+#  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products
+#  derived from this software without specific prior written permission from the respective party.
 #
-#  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative
-#  works may not use the "OpenStudio" trademark, "OS", "os", or any other confusingly similar designation without
-#  specific prior written permission from Alliance for Sustainable Energy, LLC.
+#  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative works
+#  may not use the "OpenStudio" trademark, "OS", "os", or any other confusingly similar designation without specific prior
+#  written permission from Alliance for Sustainable Energy, LLC.
 #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 #  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR
-#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-#  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE UNITED STATES GOVERNMENT, OR THE UNITED
+#  STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+#  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+#  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ########################################################################################################################
 
 ######################################################################
@@ -124,32 +125,33 @@ end
 
 fileHeader = String.new
 fileHeader << "/***********************************************************************************************************************\n"
-fileHeader << " *  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC. All rights reserved.\n"
-fileHeader << " *\n"
-fileHeader << " *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the\n"
-fileHeader << " *  following conditions are met:\n"
-fileHeader << " *\n"
-fileHeader << " *  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following\n"
-fileHeader << " *  disclaimer.\n"
-fileHeader << " *\n"
-fileHeader << " *  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the\n"
-fileHeader << " *  following disclaimer in the documentation and/or other materials provided with the distribution.\n"
-fileHeader << " *\n"
-fileHeader << " *  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote\n"
-fileHeader << " *  products derived from this software without specific prior written permission from the respective party.\n"
-fileHeader << " *\n"
-fileHeader << " *  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative\n"
-fileHeader << " *  works may not use the \"OpenStudio\" trademark, \"OS\", \"os\", or any other confusingly similar designation without\n"
-fileHeader << " *  specific prior written permission from Alliance for Sustainable Energy, LLC.\n"
-fileHeader << " *\n"
-fileHeader << " *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES,\n"
-fileHeader << " *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n"
-fileHeader << " *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR\n"
-fileHeader << " *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,\n"
-fileHeader << " *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED\n"
-fileHeader << " *  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"
-fileHeader << " *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
-fileHeader << " **********************************************************************************************************************/\n"
+fileHeader << "*  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.\n"
+fileHeader << "*\n"
+fileHeader << "*  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the\n"
+fileHeader << "*  following conditions are met:\n"
+fileHeader << "*\n"
+fileHeader << "*  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following\n"
+fileHeader << "*  disclaimer.\n"
+fileHeader << "*\n"
+fileHeader << "*  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following\n"
+fileHeader << "*  disclaimer in the documentation and/or other materials provided with the distribution.\n"
+fileHeader << "*\n"
+fileHeader << "*  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products\n"
+fileHeader << "*  derived from this software without specific prior written permission from the respective party.\n"
+fileHeader << "*\n"
+fileHeader << "*  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative works\n"
+fileHeader << "*  may not use the \"OpenStudio\" trademark, \"OS\", \"os\", or any other confusingly similar designation without specific prior\n"
+fileHeader << "*  written permission from Alliance for Sustainable Energy, LLC.\n"
+fileHeader << "*\n"
+fileHeader << "*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES,\n"
+fileHeader << "*  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n"
+fileHeader << "*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE UNITED STATES GOVERNMENT, OR THE UNITED\n"
+fileHeader << "*  STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,\n"
+fileHeader << "*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF\n"
+fileHeader << "*  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,\n"
+fileHeader << "*  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF\n"
+fileHeader << "*  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
+fileHeader << "***********************************************************************************************************************/\n"
 
 hpp = String.new
 cpp = String.new
--- openstudiocore/src/model_editor/ModelEditorAPI.hpp
+++ openstudiocore/src/model_editor/ModelEditorAPI.hpp
@@ -1,5 +1,5 @@
 /***********************************************************************************************************************
-*  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC. All rights reserved.
+*  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 *  following conditions are met:
@@ -26,6 +26,7 @@
 *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************************************************************/
+
 #include "QMetaTypes.hpp"
 
 #ifndef MODELEDITOR_MODELEDITORAPI_HPP
