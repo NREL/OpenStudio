0d5731043e39421365086cc74b7558ad1aa7e77d
Julien Marrec <julien.marrec@gmail.com>
2019-02-25 13:15:17 +0100

    Update name of field "Cooler [Design] Effectiveness" in model for Evap Cooler, preserving backward compat

--- openstudiocore/src/model/EvaporativeCoolerDirectResearchSpecial.cpp
+++ openstudiocore/src/model/EvaporativeCoolerDirectResearchSpecial.cpp
@@ -172,14 +172,14 @@ namespace detail{
     return result;
   }
 
-  double EvaporativeCoolerDirectResearchSpecial_Impl::coolerEffectiveness() const
+  double EvaporativeCoolerDirectResearchSpecial_Impl::coolerDesignEffectiveness() const
   {
-    return this->getDouble(OS_EvaporativeCooler_Direct_ResearchSpecialFields::CoolerEffectiveness).get();
+    return this->getDouble(OS_EvaporativeCooler_Direct_ResearchSpecialFields::CoolerDesignEffectiveness).get();
   }
 
-  bool EvaporativeCoolerDirectResearchSpecial_Impl::setCoolerEffectiveness( double value )
+  bool EvaporativeCoolerDirectResearchSpecial_Impl::setCoolerDesignEffectiveness( double value )
   {
-    return this->setDouble(OS_EvaporativeCooler_Direct_ResearchSpecialFields::CoolerEffectiveness,value);
+    return this->setDouble(OS_EvaporativeCooler_Direct_ResearchSpecialFields::CoolerDesignEffectiveness,value);
   }
 
   boost::optional<double> EvaporativeCoolerDirectResearchSpecial_Impl::recirculatingWaterPumpPowerConsumption() const
@@ -432,14 +432,26 @@ bool EvaporativeCoolerDirectResearchSpecial::setAvailableSchedule(Schedule & sch
   return getImpl<detail::EvaporativeCoolerDirectResearchSpecial_Impl>()->setAvailabilitySchedule(schedule);
 }
 
+double EvaporativeCoolerDirectResearchSpecial::coolerDesignEffectiveness() const
+{
+  return getImpl<detail::EvaporativeCoolerDirectResearchSpecial_Impl>()->coolerDesignEffectiveness();
+}
+
 double EvaporativeCoolerDirectResearchSpecial::coolerEffectiveness() const
 {
-  return getImpl<detail::EvaporativeCoolerDirectResearchSpecial_Impl>()->coolerEffectiveness();
+  LOG(Info, "This method is deprecated, please replace it with coolerDesignEffectiveness.");
+  return coolerDesignEffectiveness();
+}
+
+bool EvaporativeCoolerDirectResearchSpecial::setCoolerDesignEffectiveness( double value )
+{
+  return getImpl<detail::EvaporativeCoolerDirectResearchSpecial_Impl>()->setCoolerDesignEffectiveness(value);
 }
 
 bool EvaporativeCoolerDirectResearchSpecial::setCoolerEffectiveness( double value )
 {
-  return getImpl<detail::EvaporativeCoolerDirectResearchSpecial_Impl>()->setCoolerEffectiveness(value);
+  LOG(Info, "This method is deprecated, please replace it with setCoolerDesignEffectiveness.");
+  return setCoolerDesignEffectiveness(value);
 }
 
 boost::optional<double> EvaporativeCoolerDirectResearchSpecial::recirculatingWaterPumpPowerConsumption() const
