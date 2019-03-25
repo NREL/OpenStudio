94b4cf070f46a59fdb19fc6362590aa7aa80e786
Julien Marrec <julien.marrec@gmail.com>
2019-03-25 14:59:59 +0100

    Modify script, and commit historical structure

--- switch_structure.sh
+++ switch_structure.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
 
-git clean -fd
+# To resolve potential problems you might have to use `git clean -fd`
+
+declare -a DEFAULT_OS_STRUCT=(
+"CMake"
+"CMakeLists.txt"
+"csharp"
+"CTestConfig.cmake"
+"doc"
+"Doxyfile.in"
+"embedded"
+"icons"
+"java"
+"openstudioapp.desktop"
+"OpenStudioCoreConfig.cmake.in"
+"OpenStudio.in"
+"OpenStudio.nuspec.in"
+"OpenStudio.targets"
+"osruby_config.h"
+"pat"
+"ProjectMacros.cmake"
+"python"
+"resources"
+"ruby"
+"SketchUpInstallName.rb"
+"src"
+"v8-node"
+"x-openstudio.xml"
+)
 
 OS_STRUCT_BAK=.oscore_struct.bak
+OS_STRUCT_FILE=.current_structure
 
-if [ -f "$OS_STRUCT_BAK" ]; then
-  echo "Going from OS App sep to old"
+if [ "$STRUCTURE" = "NEW" ]; then
+
+  echo "Going from 3.x structure to historical structure"
 
   # Remake the openstudiocore level
   mkdir openstudiocore
-  # Read openstudiocore structure to move everything back in there
-  while read -r line || [[ -n "$line" ]]; do
-    mv $line openstudiocore/
-  done < $OS_STRUCT_BAK
+
+  # Populate it
+
+  # If an actual structure backup exists, use that
+  if [ -f "$OS_STRUCT_BAK" ]; then
+    # Read openstudiocore structure to move everything back in there
+    while read -r line || [[ -n "$line" ]]; do
+      mv $line openstudiocore/
+    done < $OS_STRUCT_BAK
+  else
+    echo "Defaulting structure"
+    for direntry in "${DEFAULT_OS_STRUCT[@]}"; do
+      mv $direntry openstudiocore/
+    done
+  fi
 
   # Move the meta stuff to root, and delete meta/
   mv meta/* .
@@ -22,10 +62,12 @@ if [ -f "$OS_STRUCT_BAK" ]; then
   mv developer/doc .
 
   # Delete the backup file
-  rm $OS_STRUCT_BAK
+  # rm $OS_STRUCT_BAK
+
+  echo "STRUCTURE=OLD" > $OS_STRUCT_FILE
 
 else
-  echo "Going from old to OS App sep"
+  echo "Going from historical structure to 3.x structure"
 
   # Make the meta/ level and move the super build stuff in there
   mkdir meta
@@ -40,5 +82,7 @@ else
   # Move everything one level up and delete the openstudiocore level
   mv openstudiocore/* .
   rm -R openstudiocore
+
+  echo "STRUCTURE=NEW" > $OS_STRUCT_FILE
 
 fi
