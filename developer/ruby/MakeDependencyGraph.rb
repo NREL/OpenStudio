8dff716f46d638c5d2650052761460b25d958329
Julien Marrec <julien.marrec@gmail.com>
2019-04-16 17:57:48 +0200

    Reduce dependency chain, and update MakeDependencyGraph as a result

--- developer/ruby/MakeDependencyGraph.rb
+++ developer/ruby/MakeDependencyGraph.rb
@@ -1,5 +1,4 @@
 src_dir = File.join(File.dirname(__FILE__), "../../openstudiocore/src/model/")
-
 module_depends = {}
 module_depends['ModelCore.i'] = nil
 module_depends['ModelSimulation.i'] = 'ModelCore.i'
@@ -10,14 +9,14 @@ module_depends['ModelZoneHVAC.i'] = 'ModelHVAC.i'
 module_depends['ModelAvailabilityManager.i'] = 'ModelHVAC.i'
 module_depends['ModelPlantEquipmentOperationScheme.i'] = 'ModelHVAC.i'
 module_depends['ModelStraightComponent.i'] = 'ModelZoneHVAC.i'
-module_depends['ModelAirflow.i'] = 'ModelHVAC.i'
-module_depends['ModelRefrigeration.i'] = 'ModelZoneHVAC.i'
-module_depends['ModelGenerators.i'] = 'ModelHVAC.i'
+module_depends['ModelAirflow.i'] = 'ModelStraightComponent.i'
+module_depends['ModelRefrigeration.i'] = 'ModelStraightComponent.i'
+module_depends['ModelGenerators.i'] = 'ModelStraightComponent.i'
 
 module_depends_array = {}
 module_depends.each do |key, value|
   depends_array = []
-  while value 
+  while value
     depends_array << value
     value = module_depends[value]
   end
@@ -52,34 +51,34 @@ class_names.each_index do |i|
   class_name = class_names[i]
   module_name = class_to_module[class_name]
   depends_array = module_depends_array[module_name]
-  
+
   puts "#{i} out of #{n}, #{class_name}, #{module_name}"
 
   display_module_name = module_name.gsub('.i','') # names can't have '.' in graphviz
   display_class_name = class_name.gsub('Node', 'mNode') # Node is key word in graphviz
-  
+
   file_name = File.join(src_dir, "#{class_name}.hpp")
   if !File.exist?(file_name)
     puts "no #{class_name}"
-  else 
-  
+  else
+
     #dot_in += "  #{display_class_name} -> #{display_module_name};\n"
-    
+
     File.readlines(file_name).each do |line|
- 
+
       line = line.gsub(/\/\/.*$/,'').gsub(/\/\*.*$/,'').gsub(/\*.*$/,'')
-    
+
       class_names.each do |other_class_name|
-      
+
         other_module_name = class_to_module[other_class_name]
 
         display_other_module_name = other_module_name.gsub('.i','') # names can't have '.' in graphviz
         display_other_class_name = other_class_name.gsub('Node', 'mNode') # Node is key word in graphviz
-        
+
         next if class_name == other_class_name
         next if class_to_module[class_name] == class_to_module[other_class_name]
         next if depends_array.index(other_module_name)
-        
+
         if /\b#{other_class_name}\b/.match(line)
           dot_in << "  #{display_module_name} -> #{display_other_module_name};"
           report_out += "#{class_name} in module #{module_name} points to #{other_class_name} in module #{other_module_name}:\n"
@@ -88,7 +87,7 @@ class_names.each_index do |i|
       end
     end
   end
-  
+
   #if i > 5
   #  break
   #end
