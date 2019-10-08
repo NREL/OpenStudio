b93509b88c3fd0b5ef524d3a272d523dc5faf548
Julien Marrec <julien.marrec@gmail.com>
2019-10-08 14:38:02 +0200

    Optional: add a Warning when setting Fan Availability Schedule when its connected to an AirLoopHVAC (will be ignored/overriden)

--- openstudiocore/src/model/FanConstantVolume.cpp
+++ openstudiocore/src/model/FanConstantVolume.cpp
@@ -148,6 +148,10 @@ namespace detail {
   }
 
   bool FanConstantVolume_Impl::setAvailabilitySchedule(Schedule& schedule) {
+    if (boost::optional<AirLoopHVAC> _airLoopHVAC = airLoopHVAC()) {
+      LOG(Warn, briefDescription() << " is connected to an AirLoopHVAC '" << _airLoop->nameString()
+             << "', therefore its Availability Schedule will be overriden by the AirLoopHVAC's one.");
+    }
     bool result = setSchedule(OS_Fan_ConstantVolumeFields::AvailabilityScheduleName,
                               "FanConstantVolume",
                               "Availability",
--- openstudiocore/src/model/FanVariableVolume.cpp
+++ openstudiocore/src/model/FanVariableVolume.cpp
@@ -293,6 +293,10 @@ namespace detail {
   }
 
   bool FanVariableVolume_Impl::setAvailabilitySchedule(Schedule& schedule) {
+    if (boost::optional<AirLoopHVAC> _airLoopHVAC = airLoopHVAC()) {
+      LOG(Warn, briefDescription() << " is connected to an AirLoopHVAC '" << _airLoop->nameString()
+             << "', therefore its Availability Schedule will be overriden by the AirLoopHVAC's one.");
+    }
     bool result = setSchedule(OS_Fan_VariableVolumeFields::AvailabilityScheduleName,
                               "FanVariableVolume",
                               "Availability",
