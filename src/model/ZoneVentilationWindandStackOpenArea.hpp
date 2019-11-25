98135a4e1af6d1c593ef3992003405f0d506f969
Julien Marrec <julien.marrec@gmail.com>
2019-11-26 00:08:44 +0100

    Manual stubbing of headers (so close to ZoneVentilationDesignFlowRate that I did it manually while building after IDD change)

---
+++ src/model/ZoneVentilationWindandStackOpenArea.hpp
@@ -0,0 +1,175 @@
+/***********************************************************************************************************************
+*  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+*  following conditions are met:
+*
+*  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+*  disclaimer.
+*
+*  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+*  disclaimer in the documentation and/or other materials provided with the distribution.
+*
+*  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products
+*  derived from this software without specific prior written permission from the respective party.
+*
+*  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative works
+*  may not use the "OpenStudio" trademark, "OS", "os", or any other confusingly similar designation without specific prior
+*  written permission from Alliance for Sustainable Energy, LLC.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+*  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE UNITED STATES GOVERNMENT, OR THE UNITED
+*  STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+*  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+*  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+*  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***********************************************************************************************************************/
+
+#ifndef MODEL_ZONEVENTILATIONWINDANDSTACKOPENAREA_HPP
+#define MODEL_ZONEVENTILATIONWINDANDSTACKOPENAREA_HPP
+
+#include "ModelAPI.hpp"
+#include "ZoneHVACComponent.hpp"
+
+namespace openstudio {
+
+namespace model {
+
+class Schedule;
+
+namespace detail {
+
+  class ZoneVentilationWindandStackOpenArea_Impl;
+
+} // detail
+
+/** ZoneVentilationWindandStackOpenArea is a ZoneHVACComponent that wraps the OpenStudio IDD object 'OS:ZoneVentilation:WindandStackOpenArea'. */
+class MODEL_API ZoneVentilationWindandStackOpenArea : public ZoneHVACComponent {
+ public:
+  /** @name Constructors and Destructors */
+  //@{
+
+  explicit ZoneVentilationWindandStackOpenArea(const Model& model);
+
+  virtual ~ZoneVentilationWindandStackOpenArea() {}
+
+  //@}
+
+  static IddObjectType iddObjectType();
+
+  /** @name Getters */
+  //@{
+
+  double openingArea() const;
+
+  Schedule openingAreaFractionSchedule() const;
+
+  boost::optional<double> openingEffectiveness() const;
+  bool isOpeningEffectivenessAutocalculated() const;
+
+  double effectiveAngle() const;
+
+  double heightDifference() const;
+
+  boost::optional<double> dischargeCoefficientforOpening() const;
+  bool isDischargeCoefficientforOpeningAutocalculated() const;
+
+  double minimumIndoorTemperature() const;
+
+  boost::optional<Schedule> minimumIndoorTemperatureSchedule() const;
+
+  double maximumIndoorTemperature() const;
+
+  boost::optional<Schedule> maximumIndoorTemperatureSchedule() const;
+
+  double deltaTemperature() const;
+
+  boost::optional<Schedule> deltaTemperatureSchedule() const;
+
+  double minimumOutdoorTemperature() const;
+
+  boost::optional<Schedule> minimumOutdoorTemperatureSchedule() const;
+
+  double maximumOutdoorTemperature() const;
+
+  boost::optional<Schedule> maximumOutdoorTemperatureSchedule() const;
+
+  double maximumWindSpeed() const;
+
+  //@}
+  /** @name Setters */
+  //@{
+
+  bool setOpeningArea(double openingArea);
+
+  bool setOpeningAreaFractionSchedule(Schedule& schedule);
+
+  bool setOpeningEffectiveness(double openingEffectiveness);
+  void autocalculateOpeningEffectiveness();
+
+  bool setEffectiveAngle(double effectiveAngle);
+
+  bool setHeightDifference(double heightDifference);
+
+  bool setDischargeCoefficientforOpening(double dischargeCoefficientforOpening);
+  void autocalculateDischargeCoefficientforOpening();
+
+  bool setMinimumIndoorTemperature(double minimumIndoorTemperature);
+
+  bool setMinimumIndoorTemperatureSchedule(Schedule& schedule);
+  void resetMinimumIndoorTemperatureSchedule();
+
+  bool setMaximumIndoorTemperature(double maximumIndoorTemperature);
+
+  bool setMaximumIndoorTemperatureSchedule(Schedule& schedule);
+  void resetMaximumIndoorTemperatureSchedule();
+
+  bool setDeltaTemperature(double deltaTemperature);
+
+  bool setDeltaTemperatureSchedule(Schedule& schedule);
+  void resetDeltaTemperatureSchedule();
+
+  bool setMinimumOutdoorTemperature(double minimumOutdoorTemperature);
+
+  bool setMinimumOutdoorTemperatureSchedule(Schedule& schedule);
+  void resetMinimumOutdoorTemperatureSchedule();
+
+  bool setMaximumOutdoorTemperature(double maximumOutdoorTemperature);
+
+  bool setMaximumOutdoorTemperatureSchedule(Schedule& schedule);
+  void resetMaximumOutdoorTemperatureSchedule();
+
+  bool setMaximumWindSpeed(double maximumWindSpeed);
+
+  //@}
+  /** @name Other */
+  //@{
+
+  //@}
+ protected:
+  /// @cond
+  typedef detail::ZoneVentilationWindandStackOpenArea_Impl ImplType;
+
+  explicit ZoneVentilationWindandStackOpenArea(std::shared_ptr<detail::ZoneVentilationWindandStackOpenArea_Impl> impl);
+
+  friend class detail::ZoneVentilationWindandStackOpenArea_Impl;
+  friend class Model;
+  friend class IdfObject;
+  friend class openstudio::detail::IdfObject_Impl;
+  /// @endcond
+ private:
+  REGISTER_LOGGER("openstudio.model.ZoneVentilationWindandStackOpenArea");
+};
+
+/** \relates ZoneVentilationWindandStackOpenArea*/
+typedef boost::optional<ZoneVentilationWindandStackOpenArea> OptionalZoneVentilationWindandStackOpenArea;
+
+/** \relates ZoneVentilationWindandStackOpenArea*/
+typedef std::vector<ZoneVentilationWindandStackOpenArea> ZoneVentilationWindandStackOpenAreaVector;
+
+} // model
+} // openstudio
+
+#endif // MODEL_ZONEVENTILATIONWINDANDSTACKOPENAREA_HPP
---
+++ src/model/ZoneVentilationWindandStackOpenArea_Impl.hpp
@@ -0,0 +1,190 @@
+/***********************************************************************************************************************
+*  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+*  following conditions are met:
+*
+*  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+*  disclaimer.
+*
+*  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+*  disclaimer in the documentation and/or other materials provided with the distribution.
+*
+*  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products
+*  derived from this software without specific prior written permission from the respective party.
+*
+*  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative works
+*  may not use the "OpenStudio" trademark, "OS", "os", or any other confusingly similar designation without specific prior
+*  written permission from Alliance for Sustainable Energy, LLC.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+*  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE UNITED STATES GOVERNMENT, OR THE UNITED
+*  STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+*  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+*  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+*  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***********************************************************************************************************************/
+
+#ifndef MODEL_ZONEVENTILATIONWINDANDSTACKOPENAREA_IMPL_HPP
+#define MODEL_ZONEVENTILATIONWINDANDSTACKOPENAREA_IMPL_HPP
+
+#include "ModelAPI.hpp"
+#include "ZoneHVACComponent_Impl.hpp"
+
+namespace openstudio {
+namespace model {
+
+class Schedule;
+
+namespace detail {
+
+  /** ZoneVentilationWindandStackOpenArea_Impl is a ZoneHVACComponent_Impl that is the implementation class for ZoneVentilationWindandStackOpenArea.*/
+  class MODEL_API ZoneVentilationWindandStackOpenArea_Impl : public ZoneHVACComponent_Impl {
+   public:
+    /** @name Constructors and Destructors */
+    //@{
+
+    ZoneVentilationWindandStackOpenArea_Impl(const IdfObject& idfObject,
+                                       Model_Impl* model,
+                                       bool keepHandle);
+
+    ZoneVentilationWindandStackOpenArea_Impl(const openstudio::detail::WorkspaceObject_Impl& other,
+                                       Model_Impl* model,
+                                       bool keepHandle);
+
+    ZoneVentilationWindandStackOpenArea_Impl(const ZoneVentilationWindandStackOpenArea_Impl& other,
+                                       Model_Impl* model,
+                                       bool keepHandle);
+
+    virtual ~ZoneVentilationWindandStackOpenArea_Impl() {}
+
+    //@}
+    /** @name Virtual Methods */
+    //@{
+
+    virtual const std::vector<std::string>& outputVariableNames() const override;
+
+    virtual IddObjectType iddObjectType() const override;
+
+    virtual std::vector<ScheduleTypeKey> getScheduleTypeKeys(const Schedule& schedule) const override;
+
+    virtual unsigned inletPort() const override;
+
+    virtual unsigned outletPort() const override;
+
+    virtual boost::optional<ThermalZone> thermalZone() const override;
+
+    virtual bool addToThermalZone(ThermalZone & thermalZone) override;
+
+    virtual void removeFromThermalZone() override;
+
+    virtual std::vector<EMSActuatorNames> emsActuatorNames() const override;
+
+    virtual std::vector<std::string> emsInternalVariableNames() const override;
+
+    //@}
+    /** @name Getters */
+    //@{
+
+    double openingArea() const;
+
+    Schedule openingAreaFractionSchedule() const;
+
+    boost::optional<double> openingEffectiveness() const;
+    bool isOpeningEffectivenessAutocalculated() const;
+
+    double effectiveAngle() const;
+
+    double heightDifference() const;
+
+    boost::optional<double> dischargeCoefficientforOpening() const;
+    bool isDischargeCoefficientforOpeningAutocalculated() const;
+
+    double minimumIndoorTemperature() const;
+
+    boost::optional<Schedule> minimumIndoorTemperatureSchedule() const;
+
+    double maximumIndoorTemperature() const;
+
+    boost::optional<Schedule> maximumIndoorTemperatureSchedule() const;
+
+    double deltaTemperature() const;
+
+    boost::optional<Schedule> deltaTemperatureSchedule() const;
+
+    double minimumOutdoorTemperature() const;
+
+    boost::optional<Schedule> minimumOutdoorTemperatureSchedule() const;
+
+    double maximumOutdoorTemperature() const;
+
+    boost::optional<Schedule> maximumOutdoorTemperatureSchedule() const;
+
+    double maximumWindSpeed() const;
+
+    //@}
+    /** @name Setters */
+    //@{
+
+    bool setOpeningArea(double openingArea);
+
+    bool setOpeningAreaFractionSchedule(Schedule& schedule);
+
+    bool setOpeningEffectiveness(double openingEffectiveness);
+    void autocalculateOpeningEffectiveness();
+
+    bool setEffectiveAngle(double effectiveAngle);
+
+    bool setHeightDifference(double heightDifference);
+
+    bool setDischargeCoefficientforOpening(double dischargeCoefficientforOpening);
+    void autocalculateDischargeCoefficientforOpening();
+
+    bool setMinimumIndoorTemperature(double minimumIndoorTemperature);
+
+    bool setMinimumIndoorTemperatureSchedule(Schedule& schedule);
+    void resetMinimumIndoorTemperatureSchedule();
+
+    bool setMaximumIndoorTemperature(double maximumIndoorTemperature);
+
+    bool setMaximumIndoorTemperatureSchedule(Schedule& schedule);
+    void resetMaximumIndoorTemperatureSchedule();
+
+    bool setDeltaTemperature(double deltaTemperature);
+
+    bool setDeltaTemperatureSchedule(Schedule& schedule);
+    void resetDeltaTemperatureSchedule();
+
+    bool setMinimumOutdoorTemperature(double minimumOutdoorTemperature);
+
+    bool setMinimumOutdoorTemperatureSchedule(Schedule& schedule);
+    void resetMinimumOutdoorTemperatureSchedule();
+
+    bool setMaximumOutdoorTemperature(double maximumOutdoorTemperature);
+
+    bool setMaximumOutdoorTemperatureSchedule(Schedule& schedule);
+    void resetMaximumOutdoorTemperatureSchedule();
+
+    bool setMaximumWindSpeed(double maximumWindSpeed);
+
+    //@}
+    /** @name Other */
+    //@{
+
+
+    //@}
+   protected:
+   private:
+    REGISTER_LOGGER("openstudio.model.ZoneVentilationWindandStackOpenArea");
+
+    boost::optional<Schedule> optionalOpeningAreaFractionSchedule() const;
+  };
+
+} // detail
+
+} // model
+} // openstudio
+
+#endif // MODEL_ZONEVENTILATIONWINDANDSTACKOPENAREA_IMPL_HPP
