eb302c3f877e24747a50edf1d6957aef6546f22a
Julien Marrec <julien.marrec@gmail.com>
2019-12-17 11:29:14 +0100

    Forked websocketpp to apply patch that supports boost 1.70+
    cf https://github.com/bincrafters/conan-websocketpp/pull/5

--- ConanInstall.cmake
+++ ConanInstall.cmake
@@ -85,7 +85,7 @@ if(NOT CONAN_OPENSTUDIO_ALREADY_RUN)
     fmt/6.0.0
     sqlite3/3.30.1 # Or 3.29.0
     cpprestsdk/2.10.14@jmarrec/testing # TODO:Temp @bincrafters/stable (need bincrafters/testing, which isn't on bintray.)
-    websocketpp/0.8.1@bincrafters/stable
+    websocketpp/0.8.1@jmarrec/testing # TODO: temp, needed to fork it to allow support for boost 1.70+
     geographiclib/1.49@bincrafters/stable
     swig_installer/4.0.1@bincrafters/stable
     ${CONAN_GTEST}
