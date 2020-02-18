a049d0fdd5d3c5c55b3d4c379f2afbd6228b2668
Joe Robertson <joseph.robertson@nrel.gov>
2020-02-18 16:20:31 -0700

    Add 9-2-0 sql to regression tests.

--- src/utilities/sql/Test/SqlFile_GTest.cpp
+++ src/utilities/sql/Test/SqlFile_GTest.cpp
@@ -556,6 +556,7 @@ TEST_F(SqlFileFixture, Regressions) {
   regressionTestSqlFile("1ZoneEvapCooler-V8-2-0.sql", 43.28, 20, 20);
   regressionTestSqlFile("1ZoneEvapCooler-V8-3-0.sql", 43.28, 20, 20);
   regressionTestSqlFile("1ZoneEvapCooler-V8-4-0.sql", 43.28, 20, 20);
+  regressionTestSqlFile("1ZoneEvapCooler-V9-2-0.sql", 43.28, 20, 20);
 }
 
 TEST_F(SqlFileFixture, SqlFile_LeapYear)
