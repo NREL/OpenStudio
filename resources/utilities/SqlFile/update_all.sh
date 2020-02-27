f0131dca85f6f6d0068d67df7cdaec8bdcb2eeee
Julien Marrec <julien.marrec@gmail.com>
2020-02-27 18:44:23 +0100

    Handle downloading IDF file and making the required changes directly too

--- resources/utilities/SqlFile/update_all.sh
+++ resources/utilities/SqlFile/update_all.sh
@@ -1,5 +1,9 @@
 # Bash script to run a bunch of 1ZoneEvapCooler.idf throughout versions of E+...
 
+# Add your OAUTH token here (public access only)
+token=
+curl_token="-H \"Authorization: token $token\""
+
 # All versions you want to run
 declare -a all_versions=("8.5.0" "8.6.0" "8.7.0" "8.8.0" "8.9.0" "9.0.1" "9.1.0")
 
@@ -26,23 +30,33 @@ for ep_version in "${all_versions[@]}"; do
   if [ ! -f $idf_file ]; then
     wget https://raw.githubusercontent.com/NREL/EnergyPlus/v$ep_version/testfiles/1ZoneEvapCooler.idf
     mv 1ZoneEvapCooler.idf $idf_file
+
+    echo "  Output:SQLite," >> $idf_file
+    echo '    SimpleAndTabular;        !- Option Type' >> $idf_file
+    echo "" >> $idf_file
+
+    sed -i 's/Output:Variable,\*,Zone Mean Air Temperature,hourly;/Output:Variable,\*,Zone Mean Air Temperature,Annual;\n\n  Output:Variable,\*,Zone Mean Air Temperature,RunPeriod;\n\n  Output:Variable,\*,Zone Mean Air Temperature,Environment;\n\n  Output:Variable,\*,Zone Mean Air Temperature,Monthly;\n\n  Output:Variable,\*,Zone Mean Air Temperature,Daily;\n\n  Output:Variable,\*,Zone Mean Air Temperature,Hourly;\n\n  Output:Variable,\*,Zone Mean Air Temperature,Timestep;\n\n  Output:Variable,\*,Zone Mean Air Temperature,Detailed;\n/g' $idf_file
+
   fi;
 
 
-  echo "curl https://api.github.com/repos/NREL/EnergyPlus/releases/tags/v$ep_version | jq -r '.assets | .[] | select(.name | contains("Linux")) | select(.name | contains("tar.gz")) | .browser_download_url')"
-  tar_gz_link=$(curl https://api.github.com/repos/NREL/EnergyPlus/releases/tags/v$ep_version | jq -r '.assets | .[] | select(.name | contains("Linux")) | select(.name | contains("tar.gz")) | .browser_download_url')
+  echo "curl https://api.github.com/repos/NREL/EnergyPlus/releases/tags/v$ep_version | jq -r '.assets | .[] | select(.name | contains("Linux")) | select(.name | contains("tar.gz")) | .browser_download_url'"
+  tar_gz_link=$(curl -H "Authorization: token $token" https://api.github.com/repos/NREL/EnergyPlus/releases/tags/v$ep_version | jq -r '.assets | .[] | select(.name | contains("Linux")) | select(.name | contains("tar.gz")) | .browser_download_url')
   if [ -z "$tar_gz_link" ]; then
-    sh_link=$(curl https://api.github.com/repos/NREL/EnergyPlus/releases/tags/v$ep_version | jq -r '.assets | .[] | select(.name | contains("Linux")) | select(.name | contains("sh")) | .browser_download_url')
+    echo "curl https://api.github.com/repos/NREL/EnergyPlus/releases/tags/v$ep_version | jq -r '.assets | .[] | select(.name | contains("Linux")) | select(.name | contains("sh")) | .browser_download_url'"
+    sh_link=$(curl -H "Authorization: token $token" https://api.github.com/repos/NREL/EnergyPlus/releases/tags/v$ep_version | jq -r '.assets | .[] | select(.name | contains("Linux")) | select(.name | contains("sh")) | .browser_download_url')
     # sh_name=${sh_link##*/}
+    echo $sh_link
     sh_name=$(basename -- $sh_link)
 
     if [ ! -f $sh_name ]; then
       echo "Downloading $sh_link"
-      # wget $sh_link
+      wget $sh_link
     else
       echo "$sh_name already exists"
     fi
 
+    chmod +x $sh_name
 
   else
     #tar_gz_name=${tar_gz_link##*/}
