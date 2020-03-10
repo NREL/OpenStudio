c25d18b0d41eeb63b27ab1c9fcd85b2c9a8629ea
Julien Marrec <julien.marrec@gmail.com>
2020-03-10 13:45:13 +0100

    Extend RT gtest to handle exrtensible groups maping to extensible modelObjectLists with zones on it

--- src/energyplus/Test/ShadowCalculation_GTest.cpp
+++ src/energyplus/Test/ShadowCalculation_GTest.cpp
@@ -38,6 +38,7 @@
 #include "../../model/ShadowCalculation_Impl.hpp"
 
 #include "../../model/ThermalZone.hpp"
+#include "../../model/ThermalZone_Impl.hpp"
 
 #include "../../utilities/idf/IdfFile.hpp"
 #include "../../utilities/idf/Workspace.hpp"
@@ -227,17 +228,21 @@ TEST_F(EnergyPlusFixture, ReverseTranslator_ShadowCalculation) {
     ShadowCalculation sc = m.getUniqueModelObject<ShadowCalculation>();
 
     EXPECT_TRUE(sc.isShadingCalculationUpdateFrequencyMethodDefaulted());
-    EXPECT_TRUE(sc.isShadingCalculationUpdateFrequencyDefaulted());
-    EXPECT_TRUE(sc.isMaximumFiguresInShadowOverlapCalculationsDefaulted());
+
+    // These have isXXX defaulted, but historically the model object Ctor has been hardsetting them, so we do the same
+    EXPECT_FALSE(sc.isShadingCalculationUpdateFrequencyDefaulted());
+    EXPECT_EQ(20, sc.shadingCalculationUpdateFrequency());
+    EXPECT_FALSE(sc.isMaximumFiguresInShadowOverlapCalculationsDefaulted());
+    EXPECT_EQ(15000, sc.maximumFiguresInShadowOverlapCalculations());
 
     // Doesn't have a isXXXDefaulted...
-    EXPECT_EQ("ConvexWeilerAtherton", sc.polygonClippingAlgorithm());
+    EXPECT_EQ("SutherlandHodgman", sc.polygonClippingAlgorithm());
     EXPECT_TRUE(sc.isEmpty(OS_ShadowCalculationFields::PolygonClippingAlgorithm));
     EXPECT_EQ("SimpleSkyDiffuseModeling", sc.skyDiffuseModelingAlgorithm());
     EXPECT_TRUE(sc.isEmpty(OS_ShadowCalculationFields::SkyDiffuseModelingAlgorithm));
 
     // New fields: Harcoded via model Ctor
-    EXPECT_EQ("PolygonClipping,", sc.shadingCalculationMethod());
+    EXPECT_EQ("PolygonClipping", sc.shadingCalculationMethod());
     EXPECT_EQ(512, sc.pixelCountingResolution());
     EXPECT_EQ(false, sc.outputExternalShadingCalculationResults());
     EXPECT_EQ(false, sc.disableSelfShadingWithinShadingZoneGroups());
@@ -277,7 +282,7 @@ TEST_F(EnergyPlusFixture, ReverseTranslator_ShadowCalculation) {
   EXPECT_TRUE(_i_sc->setString(ShadowCalculationFields::SkyDiffuseModelingAlgorithm, "DetailedSkyDiffuseModeling"));
   EXPECT_TRUE(_i_sc->setString(ShadowCalculationFields::OutputExternalShadingCalculationResults, boolToString(true)));
   EXPECT_TRUE(_i_sc->setString(ShadowCalculationFields::DisableSelfShadingWithinShadingZoneGroups, boolToString(false)));
-  EXPECT_TRUE(_i_sc->setString(ShadowCalculationFields::DisableSelfShadingFromShadingZoneGroupstoOtherZones, boolToString(false)));
+  EXPECT_TRUE(_i_sc->setString(ShadowCalculationFields::DisableSelfShadingFromShadingZoneGroupstoOtherZones, boolToString(true)));
 
   {
     Model m = rt.translateWorkspace(w);
@@ -296,6 +301,87 @@ TEST_F(EnergyPlusFixture, ReverseTranslator_ShadowCalculation) {
     EXPECT_EQ(false, sc.disableSelfShadingWithinShadingZoneGroups());
     EXPECT_EQ(true, sc.disableSelfShadingFromShadingZoneGroupstoOtherZones());
     EXPECT_EQ(0u, sc.numberofShadingZoneGroups());
+  }
+
+
+  // Test for Shading Zone Groups
+
+  // First group with two zones
+  {
+    OptionalWorkspaceObject _i_z1 = w.addObject(IdfObject(IddObjectType::Zone));
+    ASSERT_TRUE(_i_z1);
+    EXPECT_TRUE(_i_z1->setName("Zone1"));
+    OptionalWorkspaceObject _i_z2 = w.addObject(IdfObject(IddObjectType::Zone));
+    ASSERT_TRUE(_i_z2);
+    EXPECT_TRUE(_i_z2->setName("Zone2"));
+
+    // Create a ZoneList, and put both zones on it
+    OptionalWorkspaceObject _i_zoneList1 = w.addObject(IdfObject(IddObjectType::ZoneList));
+    ASSERT_TRUE(_i_zoneList1);
+    EXPECT_TRUE(_i_zoneList1->setName("ZoneList1 for Zone1 and Zone2"));
+
+    WorkspaceExtensibleGroup eg1 = _i_zoneList1->pushExtensibleGroup().cast<WorkspaceExtensibleGroup>();
+    EXPECT_TRUE(eg1.setPointer(ZoneListExtensibleFields::ZoneName, _i_z1->handle()));
+    WorkspaceExtensibleGroup eg2 = _i_zoneList1->pushExtensibleGroup().cast<WorkspaceExtensibleGroup>();
+    EXPECT_TRUE(eg2.setPointer(ZoneListExtensibleFields::ZoneName, _i_z2->handle()));
+
+    // Push an extensible in ShadowCalculation for a new 'Shading Zone Group' and assign that zone List
+    WorkspaceExtensibleGroup s_eg = _i_sc->pushExtensibleGroup().cast<WorkspaceExtensibleGroup>();
+    EXPECT_TRUE(s_eg.setPointer(ShadowCalculationExtensibleFields::ShadingZoneGroupZoneListName, _i_zoneList1->handle()));
+
+  }
+
+  // Put the Zone directly onto a group (not a ZoneList)
+  {
+    OptionalWorkspaceObject _i_z3 = w.addObject(IdfObject(IddObjectType::Zone));
+    ASSERT_TRUE(_i_z3);
+    EXPECT_TRUE(_i_z3->setName("Zone3"));
+
+    // Push an extensible in ShadowCalculation for a new 'Shading Zone Group' and assign that zone
+    WorkspaceExtensibleGroup s_eg = _i_sc->pushExtensibleGroup().cast<WorkspaceExtensibleGroup>();
+    EXPECT_TRUE(s_eg.setPointer(ShadowCalculationExtensibleFields::ShadingZoneGroupZoneListName, _i_z3->handle()));
+  }
+
+  {
+    Model m = rt.translateWorkspace(w);
+
+    // Get the unique object
+    ShadowCalculation sc = m.getUniqueModelObject<ShadowCalculation>();
+
+    EXPECT_EQ("PixelCounting", sc.shadingCalculationMethod());
+    EXPECT_EQ("Timestep", sc.shadingCalculationUpdateFrequencyMethod());
+    EXPECT_EQ(1, sc.shadingCalculationUpdateFrequency());
+    EXPECT_EQ(200, sc.maximumFiguresInShadowOverlapCalculations());
+    EXPECT_EQ("ConvexWeilerAtherton", sc.polygonClippingAlgorithm());
+    EXPECT_EQ(499, sc.pixelCountingResolution());
+    EXPECT_EQ("DetailedSkyDiffuseModeling", sc.skyDiffuseModelingAlgorithm());
+    EXPECT_EQ(true, sc.outputExternalShadingCalculationResults());
+    EXPECT_EQ(false, sc.disableSelfShadingWithinShadingZoneGroups());
+    EXPECT_EQ(true, sc.disableSelfShadingFromShadingZoneGroupstoOtherZones());
+    EXPECT_EQ(2u, sc.numberofShadingZoneGroups());
+
+    EXPECT_EQ(3u, m.getConcreteModelObjects<ThermalZone>().size());
+
+    boost::optional<openstudio::model::ThermalZone> _z1 = m.getModelObjectByName<openstudio::model::ThermalZone>("Zone1 Thermal Zone");
+    ASSERT_TRUE(_z1);
+    boost::optional<openstudio::model::ThermalZone> _z2 = m.getModelObjectByName<openstudio::model::ThermalZone>("Zone2 Thermal Zone");
+    ASSERT_TRUE(_z2);
+    boost::optional<openstudio::model::ThermalZone> _z3 = m.getModelObjectByName<openstudio::model::ThermalZone>("Zone3 Thermal Zone");
+    ASSERT_TRUE(_z3);
+
+    {
+      std::vector<ThermalZone> thermalZones = sc.getShadingZoneGroup(0);
+      EXPECT_EQ(2u, thermalZones.size());
+      EXPECT_TRUE(std::find(thermalZones.begin(), thermalZones.end(), _z1.get()) != thermalZones.end());
+      EXPECT_TRUE(std::find(thermalZones.begin(), thermalZones.end(), _z2.get()) != thermalZones.end());
+    }
+
+    {
+      std::vector<ThermalZone> thermalZones = sc.getShadingZoneGroup(1);
+      ASSERT_EQ(1u, thermalZones.size());
+      EXPECT_EQ(_z3.get(), thermalZones[0]);
+    }
+
   }
 
 }
