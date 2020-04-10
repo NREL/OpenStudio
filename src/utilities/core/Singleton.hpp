a27b2fc782db8989f167cc3815218f6813d74b52
Jason Turner <jason@emptycrate.com>
2020-04-09 19:47:25 -0600

    Address crash related to fclose on Windows
     * Issue: ruby's rb_w32_fclose is chosen during ~fstream() shutdown
     * Cause: ruby's dll provides an fclose which is loaded before msvcrt
     * What Changed: We moved to full static linking of openstudio into the
       ruby modules
     * Fix: Make ruby module dynamically link to openstudiolib.dll
    
    NOTE: This is not a perfect fix!
    
    The fundamental problem (Ruby's terrible dll and ruby/win32.h's tendency
    to `#define` common functions) still exists and will always exist.
    
    This works because it tweaks the linking order of things.
    
    Issues:
    
     * less parallelization of building of libraries
     * we have to put openstudiolib.dll in some path that is findable by
       ruby
     * It's possible ruby tests don't currently run if paths aren't set up
       correctly for test execution
     * Still not known why NREL built ruby cannot load our library
    
    Fixes #3942

--- src/utilities/core/Singleton.hpp
+++ src/utilities/core/Singleton.hpp
@@ -32,55 +32,29 @@
 
 // Warning: If T's constructor throws, instance() will return a null reference.
 
-namespace openstudio{
+namespace openstudio {
 
   // Ripped from boost http://www.boost.org/doc/libs/1_42_0/libs/pool/doc/implementation/singleton.html
   // T must be: no-throw default constructible and no-throw destructible
   template <typename T>
   class Singleton
   {
-    private:
-      struct object_creator
-      {
-        // This constructor does nothing more than ensure that instance()
-        //  is called before main() begins, thus creating the static
-        //  T object before multithreading race issues can come up.
-        object_creator() { Singleton<T>::instance(); }
-        inline void do_nothing() const {  }
-      };
-      static object_creator create_object;
-
-      Singleton();
-
     public:
+    Singleton() = delete;
+    typedef T object_type;
 
-      typedef T object_type;
+    // If, at any point (in user code), Singleton<T>::instance()
+    //  is called, then the following function is instantiated.
+    static object_type& instance() {
+      static object_type obj;
+      return obj;
+    }   
+  }
+};
 
-      // If, at any point (in user code), Singleton<T>::instance()
-      //  is called, then the following function is instantiated.
-      static object_type & instance()
-      {
-        // This is the object that we return a reference to.
-        // It is guaranteed to be created before main() begins because of
-        //  the next line.
-        static object_type obj;
+}  // namespace openstudio
 
-        // The following line does nothing else than force the instantiation
-        //  of Singleton<T>::create_object, whose constructor is
-        //  called before main() begins.
-        create_object.do_nothing();
-
-        return obj;
-      }
-  };
-  template <typename T>
-  typename Singleton<T>::object_creator
-  Singleton<T>::create_object;
-
-}; // openstudio
-
-#endif // UTILITIES_CORE_SINGLETON_HPP
-
+#endif  // UTILITIES_CORE_SINGLETON_HPP
 
 /* EXAMPLE USAGE
 
