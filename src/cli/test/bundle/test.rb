b22bdf9ad84f5512bb7d1e435032a04913612e0e
Julien Marrec <julien.marrec@gmail.com>
2020-04-21 15:59:51 +0200

    Fix #3311: use workflow gem instead of standards to skin down disk usage

--- src/cli/test/bundle/test.rb
+++ src/cli/test/bundle/test.rb
@@ -15,7 +15,9 @@ puts Tilt::VERSION
 raise "Tilt version does not match" unless Tilt::VERSION == '2.0.8'
 
 # test a more complex / larger gem, that should override the existing version installed
+# openstudio-workflow 2.0.0.pre1 is compatible with bundler 2.1 (OS 3.x),
+# and already older than currently used at this time (2.0.0)
 require 'openstudio'
-require 'openstudio-standards'
-puts OpenstudioStandards::VERSION
-raise "OpenStudio Standards version does not match" unless OpenstudioStandards::VERSION == '0.2.0'
+require 'openstudio-workflow'
+puts OpenStudio::Workflow::VERSION
+raise "OpenStudio Workflow gem version does not match" unless OpenStudio::Workflow::VERSION == '2.0.0.pre1'
