ae255b1e097a3741a0cc5df2477c3acb372c6844
Julien Marrec <julien.marrec@gmail.com>
2020-04-28 15:36:58 +0200

    Try to consume the local nuget pkg (not working)

--- csharp/examples/DotnetConsoleExample/Program.cs
+++ csharp/examples/DotnetConsoleExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OpenStudio;
 
 namespace DotnetConsoleExample
 {
@@ -6,7 +7,17 @@ namespace DotnetConsoleExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+              OpenStudio.Model model = new OpenStudio.Model();
+              Console.WriteLine(model);
+            }
+            catch (Exception e)
+            {
+              Console.WriteLine("It appears that there was an error accessing the C# SWIG Bindings for OpenStudio. Note that the libraries installed in <installdir>/CSharp/openstudio need to be accessible to this application at runtime, either through the path or in the same directory as the exe. Nothing else will work properly have this point.\n\nError Text: " + e.InnerException.InnerException.Message, "Error loading libraries");
+            }
+
         }
     }
 }
