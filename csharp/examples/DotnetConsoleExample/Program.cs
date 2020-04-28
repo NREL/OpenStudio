a90ff400cc66d5620f63caff9a0d5aaf4e7a9031
Julien Marrec <julien.marrec@gmail.com>
2020-04-28 12:25:01 +0200

    Add the output of `dotnet new console`: an hello world console script. gitignore stuff

---
+++ csharp/examples/DotnetConsoleExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
