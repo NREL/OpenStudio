00be1a31ba4ff1024db7615f22281140cf7aba59
Joe Robertson <joseph.robertson@nrel.gov>
2020-08-27 13:07:52 -0700

    Combine zone hvac low temp radiant const flow ft tests into one.

--- src/energyplus/Test/ZoneHVACLowTemperatureRadiantConstFlow_GTest.cpp
+++ src/energyplus/Test/ZoneHVACLowTemperatureRadiantConstFlow_GTest.cpp
@@ -56,8 +56,12 @@
 #include "../../model/StandardOpaqueMaterial_Impl.hpp"
 #include "../../model/ThermalZone.hpp"
 #include "../../model/ThermalZone_Impl.hpp"
+#include "../../model/Space.hpp"
+#include "../../model/Space_Impl.hpp"
 
-#include <utilities/idd/ZoneHVAC_LowTemperatureRadiant_VariableFlow_FieldEnums.hxx>
+#include <utilities/idd/ZoneHVAC_LowTemperatureRadiant_ConstantFlow_FieldEnums.hxx>
+#include <utilities/idd/IddEnums.hxx>
+
 #include "../../utilities/idf/IdfExtensibleGroup.hpp"
 
 #include <resources.hxx>
@@ -141,6 +145,14 @@ TEST_F(EnergyPlusFixture,ZoneHVACLowTempRadiantConstFlow_Set_Flow_Fractions)
     for (ModelObject equipment : thermalZone.equipment()){
       if (equipment.optionalCast<ZoneHVACLowTempRadiantConstFlow>()){
         ZoneHVACLowTempRadiantConstFlow testRad = equipment.optionalCast<ZoneHVACLowTempRadiantConstFlow>().get();
+        EXPECT_TRUE(testRad.isFluidtoRadiantSurfaceHeatTransferModelDefaulted());
+        EXPECT_TRUE(testRad.isHydronicTubingInsideDiameterDefaulted());
+        EXPECT_TRUE(testRad.isHydronicTubingOutsideDiameterDefaulted());
+        EXPECT_TRUE(testRad.isHydronicTubingLengthAutosized());
+        EXPECT_TRUE(testRad.isHydronicTubingConductivityDefaulted());
+        EXPECT_TRUE(testRad.isTemperatureControlTypeDefaulted());
+        EXPECT_TRUE(testRad.isRunningMeanOutdoorDryBulbTemperatureWeightingFactorDefaulted());
+        EXPECT_TRUE(testRad.isFractionofMotorInefficienciestoFluidStreamDefaulted());
         for (IdfExtensibleGroup extGrp : testRad.extensibleGroups()){
           EXPECT_EQ(0.25,extGrp.getDouble(1,false));
         }
@@ -150,4 +162,66 @@ TEST_F(EnergyPlusFixture,ZoneHVACLowTempRadiantConstFlow_Set_Flow_Fractions)
 
 }
 
+TEST_F(EnergyPlusFixture, ZoneHVACLowTempRadiantConstFlow_Crash_no_constructions) {
 
+  ForwardTranslator ft;
+
+  Model m;
+
+  // make a space with some surfaces
+  Point3dVector points;
+  points.push_back(Point3d(0, 0, 0));
+  points.push_back(Point3d(0, 1, 0));
+  points.push_back(Point3d(1, 1, 0));
+  points.push_back(Point3d(1, 0, 0));
+
+  boost::optional<Space> _space1 = Space::fromFloorPrint(points, 3, m);
+  ASSERT_TRUE(_space1);
+
+  // make a zone, add the space
+  ThermalZone z(m);
+  _space1->setThermalZone(z);
+
+  // make some plant loops
+  PlantLoop clg_loop(m);
+  PlantLoop htg_loop(m);
+
+  ASSERT_NO_THROW(ft.translateModel(m));
+
+  // Make a radiant low temperature system
+  Schedule alwaysOn = m.alwaysOnDiscreteSchedule();
+
+  ScheduleConstant availabilitySched(m);
+  ScheduleConstant coolingHighWaterTempSched(m);
+  ScheduleConstant coolingLowWaterTempSched(m);
+  ScheduleConstant coolingHighControlTempSched(m);
+  ScheduleConstant coolingLowControlTempSched(m);
+  ScheduleConstant heatingHighWaterTempSched(m);
+  ScheduleConstant heatingLowWaterTempSched(m);
+  ScheduleConstant heatingHighControlTempSched(m);
+  ScheduleConstant heatingLowControlTempSched(m);
+
+  availabilitySched.setValue(1.0);
+  coolingHighWaterTempSched.setValue(15.0);
+  coolingLowWaterTempSched.setValue(10.0);
+  coolingHighControlTempSched.setValue(26.0);
+  coolingLowControlTempSched.setValue(22.0);
+  heatingHighWaterTempSched.setValue(50.0);
+  heatingLowWaterTempSched.setValue(30.0);
+  heatingHighControlTempSched.setValue(21.0);
+  heatingLowControlTempSched.setValue(15.0);
+
+  CoilCoolingLowTempRadiantConstFlow clg_coil(m,coolingHighWaterTempSched,coolingLowWaterTempSched,coolingHighControlTempSched,coolingLowControlTempSched);
+  CoilHeatingLowTempRadiantConstFlow htg_coil(m,heatingHighWaterTempSched,heatingLowWaterTempSched,heatingHighControlTempSched,heatingLowControlTempSched);
+
+  ZoneHVACLowTempRadiantConstFlow testRad(m, alwaysOn, htg_coil, clg_coil);
+  EXPECT_TRUE(testRad.addToThermalZone(z));
+
+  ASSERT_NO_THROW(ft.translateModel(m));
+
+  // Test for #2797 - When no surfaces, it shouldn't even be translated or E+ will crash
+  EXPECT_TRUE(testRad.surfaces().empty());
+  Workspace w = ft.translateModel(m);
+  WorkspaceObjectVector idf_rads(w.getObjectsByType(IddObjectType::ZoneHVAC_LowTemperatureRadiant_ConstantFlow));
+  EXPECT_EQ(0u, idf_rads.size());
+}
