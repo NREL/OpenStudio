6ea00c1b148270885638a849639dcf489a2573d2
Julien Marrec <julien.marrec@gmail.com>
2020-10-19 19:51:41 +0200

    Final adjustments to setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution
 from setuptools.command.install import install
+import os
 import platform
 
 try:
@@ -17,23 +18,30 @@ try:
         def get_tag(self):
             # Setting it up to build generic wheels.
             python, abi, plat = _bdist_wheel.get_tag(self)
-            print("Original: (python, abi, plat) = ({}, {}, {})".format(python, abi, plat))
+            print("Original: (python, abi, plat) = "
+                  "({}, {}, {})".format(python, abi, plat))
             if platform.system() != 'Windows':
-                # There is no ABI incompatibility on Unix
-                # On windows, there is... since we need to link to Python37.dll for eg
+                # There is no ABI incompatibility on Unix.
+                # On windows, there is... since we need to actually link
+                # to Python37.dll for eg
 
-                # Edit: I later discovered that building on python3.9 for eg will
-                # not be compatible with installing on 3.8.6:
-                #    ImportError: /home/julien/Virtualenvs/py38/lib/python3.8/site-packages/openstudio/_openstudioairflow.so: undefined symbol: PyCMethod_New
-                # Commit that added it, which was in 3.9.0 but not in 3.8.x (3.8.6 included): https://github.com/python/cpython/commit/e1becf46b4e3ba6d7d32ebf4bbd3e0804766a423
-                # I think there is a chance that building on 3.8 will work for install on 3.9 though
+                # Edit: I later discovered that building on python3.9 for eg
+                # will not be compatible with installing on 3.8.6:
+                #    ImportError: _openstudioairflow.so: undefined symbol: PyCMethod_New
+                # Commit that added it, which was in 3.9.0
+                # but not in 3.8.x (3.8.6 included):
+                # https://github.com/python/cpython/commit/e1becf46b4e3ba6d7d32ebf4bbd3e0804766a423
+
+                # I tested that building on 3.8 will work for install on 3.9
+                # on both Ubuntu and macOS though
                 python = 'py3'
                 if abi != 'cp39':
                     abi = 'none'
 
             # Our bindings won't be compatible with all distributions,
             # BUT pypi will refuse the upload if we do not replace
-            # Binary wheel 'openstudio-3.1.0rc3-py3-none-linux_x86_64.whl' has an unsupported platform tag 'linux_x86_64'
+            # "Binary wheel 'openstudio-3.1.0rc3-py3-none-linux_x86_64.whl'
+            #  has an unsupported platform tag 'linux_x86_64'"
             plat = plat.lower().replace('linux', 'manylinux1')
             plat = plat.lower().replace('darwin_x86_64', 'macosx_10_6_intel')
             if plat[:3] == 'mac':
@@ -60,28 +68,43 @@ class InstallPlatlib(install):
         self.install_lib = self.install_platlib
 
 
+with open(os.path.join('@PROJECT_SOURCE_DIR@', 'README.md'),
+          encoding='utf-8') as f:
+    long_description = f.read()
+
 dev_release = ""
-# TODO: TEMP
-dev_release = ".dev1"
 
 setup(
     name='openstudio',
     # eg 3.1.0rc3
     version='@OpenStudio_VERSION@'.replace('-', '') + dev_release,
     description='OpenStudio python bindings.',
-    long_description="""OpenStudio python bindings.
-    """,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/NREL/OpenStudio',
-    author='Julien Marrec',
-    author_email='julien@effibem.com',
+    author='Alliance for Sustainable Energy, LLC, and other contributors',
+    author_email='openstudio@nrel.gov',
+    maintainer='Julien Marrec',
+    maintainer_email='contact@effibem.com',
+    license='BSD3',
+    project_urls={
+        "Bug Tracker": "https://github.com/NREL/OpenStudio/issues",
+        "Documentation": "https://openstudio.net/",
+        "Source Code": "https://github.com/NREL/OpenStudio",
+    },
+    platforms="any",
+    python_requires=">=3.7.1",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: BSD License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords='openstudio py3 setuptools development',
     packages=find_packages(include=['openstudio']),
