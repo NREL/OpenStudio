04d1b2c2b33f066bf2ce45a7f2c4f413b7d30c1f
tijcolem <tjc.coleman@gmail.com>
2020-11-12 14:15:09 -0700

    Update notes and add in example global change command in ci/clang-format.sh

--- ci/clang-format.sh
+++ ci/clang-format.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# This script compares two branches (eg. pr-bugfix - <> develop )
-# and finds files of types cpp, hpp, c, h
+# This script compares two branches (eg. pr-bugfix - <> develop )# and finds files of types cpp, hpp, c, h
 # that have changed between branches and processes each of these files using clang-format.
-# The default behavior of clang-format is to auto change the file and format according to the style guide (.clang-format).  If these files change the CI will alert fail and alert the user to correct the changes. The user should run this script locally and accept the auto changes proposed by clang-format.
+# The default behavior of clang-format is to auto change the file and format according to the style guide (.clang-format). 
+# If these files change the CI will alert fail and alert the user to correct the changes. 
+# The user should run this script locally and accept the auto changes proposed by clang-format.
+#
+# You can also run the clang-format on the entire /src directory if desired. To do that can use the following: 
+# find -E ./src -regex '.*\.(cpp|hpp|c|h)$'  | xargs clang-format -style=file -i -fallback-style=none
+
 
 display_usage() {
   echo -e "\nUsage:\ PR_BRANCH_NAME TARGET_BRANCH_NAME \n"
