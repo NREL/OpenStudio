7e8441aa9d82ec1e4b02486fa079aa915a0b8df6
tijcolem <tjc.coleman@gmail.com>
2021-02-03 17:49:18 -0700

    Initial build using ruby 2.7.2 and linux openstudio-gems

--- src/cli/openstudio_cli.rb
+++ src/cli/openstudio_cli.rb
@@ -417,8 +417,8 @@ def parse_main_args(main_args)
     # bundle was requested but bundle_path was not provided
     $logger.warn "Bundle activated but ENV['BUNDLE_PATH'] is not set"
 
-    $logger.info "Setting BUNDLE_PATH to ':/ruby/2.5.0/'"
-    ENV['BUNDLE_PATH'] = ':/ruby/2.5.0/'
+    $logger.info "Setting BUNDLE_PATH to ':/ruby/2.7.0/'"
+    ENV['BUNDLE_PATH'] = ':/ruby/2.7.0/'
 
   end
 
@@ -447,51 +447,16 @@ def parse_main_args(main_args)
 
   end
 
-  Gem.paths.path << ':/ruby/2.5.0/gems/'
-  Gem.paths.path << ':/ruby/2.5.0/bundler/gems/'
+  Gem.paths.path << ':/ruby/2.7.0/gems/'
+  Gem.paths.path << ':/ruby/2.7.0/bundler/gems/'
   Gem::Deprecate.skip = true
 
   # find all the embedded gems
   original_embedded_gems = {}
-  begin
-    EmbeddedScripting::allFileNamesAsString().split(';').each do |f|
-      if md = /specifications\/.*\.gemspec$/.match(f) ||
-         md = /bundler\/.*\.gemspec$/.match(f)
-        begin
-          spec = EmbeddedScripting::getFileAsString(f)
-          s = eval(spec)
-          s.loaded_from = f
-          # This is shenanigans because otherwise rubygems will think extensions are missing
-          # But we are initing them manually so they are not missing
-          # Here the missing_extensions? method is redefined for only this instance "s"
-          class << s
-            define_method(:missing_extensions?) { false }
-          end
-          original_embedded_gems[s.name] = s
 
-          init_count = 0
-          Gem::Specification.each {|x| init_count += 1}
-
-          # if already have an equivalent spec this will be a no-op
-          Gem::Specification.add_spec(s)
-
-          post_count = 0
-          Gem::Specification.each {|x| post_count += 1}
-
-          if post_count == init_count
-            $logger.debug "Found system gem #{s.name} #{s.version}, overrides embedded gem"
-          end
-
-        rescue LoadError => e
-          safe_puts e.message
-        rescue => e
-          safe_puts e.message
-        end
-      end
-    end
-  rescue NameError => e
-    # EmbeddedScripting not available
-  end
+  # Add the gem spec paths. This filepath name gets appended with 'specification' 
+  # This will trigger Gem to reload all gems in these paths. 
+  Gem::Specification.dirs=( [":/ruby/2.7.0", ":/ruby/2.7.0/gems", ":/ruby/2.7.0/bundler/gems" ] )
 
   original_load_path = $LOAD_PATH.clone
   embedded_gems_to_activate = []
