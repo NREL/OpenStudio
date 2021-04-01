77bafb4d9ce7036b9810a83f2a378f465f91457c
Julien Marrec <julien.marrec@gmail.com>
2021-04-01 15:27:35 +0200

    Move typedef after template for MSVC?

--- src/model/ScheduleTypeRegistry.hpp
+++ src/model/ScheduleTypeRegistry.hpp
@@ -123,9 +123,6 @@ namespace model {
     bool m_enforceScheduleTypeLimits = true;
   };
 
-  /** \relates ScheduleTypeRegistrySingleton */
-  typedef openstudio::Singleton<ScheduleTypeRegistrySingleton> ScheduleTypeRegistry;
-
 #if _WIN32 || _MSC_VER
 
   /// Explicitly instantiate and export Singleton template instance
@@ -133,6 +130,9 @@ namespace model {
   MODEL_TEMPLATE_EXT template class MODEL_API openstudio::Singleton<ScheduleTypeRegistrySingleton>;
 
 #endif
+
+  /** \relates ScheduleTypeRegistrySingleton */
+  typedef openstudio::Singleton<ScheduleTypeRegistrySingleton> ScheduleTypeRegistry;
 
   /** Returns true if candidate is consistent with the ScheduleType that corresponds to className
  *  and scheduleRelationshipName. Throws if there is no such ScheduleType.
