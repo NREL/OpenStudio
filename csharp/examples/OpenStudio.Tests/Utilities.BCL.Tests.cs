c0e0a8cdd5e3bc263aecb8088b7d9095b1d4db4f
MingboPeng <mingo1214@gmail.com>
2021-04-13 23:27:02 -0400

    test(csharp): add csharp test for .Net Framework 4.5

--- csharp/examples/OpenStudio.Tests/Utilities.BCL.Tests.cs
+++ csharp/examples/OpenStudio.Tests/Utilities.BCL.Tests.cs
@@ -1,18 +1,22 @@
 using System;
-using Xunit;
-using OpenStudio;
+using NUnit.Framework;
+using System.IO;
 
-namespace OpenStudio.Tests
+namespace OpenStudioTests
 {
     public class BCLTest1
     {
-        [Fact]
+        [Test]
         public void MeasureTypeTest()
         {
             // In Test mode, the path appears evaluated not from this file but from the bin dir which is 3 extra levels down
-            var fullPath = System.IO.Path.GetFullPath("../../../../../resources/Examples/compact_osw/measures/IncreaseRoofRValue/");
-            // Console.WriteLine(fullPath);
-            Assert.True(System.IO.Directory.Exists(fullPath));
+            var testRoot  = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(this.GetType().Assembly.Location))));
+            var projectRoot = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(testRoot)));
+
+            Console.WriteLine(projectRoot);
+            var fullPath = Path.Combine(projectRoot, "resources/Examples/compact_osw/measures/IncreaseRoofRValue/");
+            Console.WriteLine(fullPath);
+            Assert.True(Directory.Exists(fullPath));
 
 
             // Test for #2795: Creating a WorkflowJSON and calling setMeasureSteps with a MeasureType");
