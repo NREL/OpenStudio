94e344147c77f78b44d809820b164c5c401258e2
Julien Marrec <julien.marrec@gmail.com>
2021-06-04 12:07:30 +0200

    Add a FT test for the complex scenario of having the DesiccantHX inside a CoilSystem inside a Unitary

--- src/energyplus/Test/HeatExchangerDesiccantBalancedFlow_GTest.cpp
+++ src/energyplus/Test/HeatExchangerDesiccantBalancedFlow_GTest.cpp
@@ -34,19 +34,26 @@
 
 #include "../../model/Model.hpp"
 #include "../../model/HeatExchangerDesiccantBalancedFlow.hpp"
-#include "../../model/HeatExchangerDesiccantBalancedFlow_Impl.hpp"
 #include "../../model/HeatExchangerDesiccantBalancedFlowPerformanceDataType1.hpp"
-#include "../../model/HeatExchangerDesiccantBalancedFlowPerformanceDataType1_Impl.hpp"
 
 #include "../../model/AirLoopHVAC.hpp"
 #include "../../model/AirLoopHVAC_Impl.hpp"
 #include "../../model/Node.hpp"
-#include "../../model/Node_Impl.hpp"
 #include "../../model/AirLoopHVACOutdoorAirSystem.hpp"
-#include "../../model/AirLoopHVACOutdoorAirSystem_Impl.hpp"
+#include "../../model/AirLoopHVACUnitarySystem.hpp"
+#include "../../model/CoilHeatingDXSingleSpeed.hpp"
+#include "../../model/CoilHeatingElectric.hpp"
+#include "../../model/CoilSystemCoolingDXHeatExchangerAssisted.hpp"
+#include "../../model/FanSystemModel.hpp"
 #include "../../model/HVACTemplates.hpp"
 
+#include "../../utilities/idd/IddObject.hpp"
+
 #include <utilities/idd/HeatExchanger_Desiccant_BalancedFlow_FieldEnums.hxx>
+#include <utilities/idd/Coil_Cooling_DX_SingleSpeed_FieldEnums.hxx>
+#include <utilities/idd/Coil_Heating_DX_SingleSpeed_FieldEnums.hxx>
+#include <utilities/idd/AirLoopHVAC_FieldEnums.hxx>
+
 #include <utilities/idd/IddEnums.hxx>
 
 using namespace openstudio::energyplus;
@@ -93,4 +100,67 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_HeatExchangerDesiccantBalancedFlow) 
     EXPECT_EQ(woPerformance->iddObject().type(), IddObjectType::HeatExchanger_Desiccant_BalancedFlow_PerformanceDataType1);
   }
   EXPECT_EQ("No", idfHX.getString(HeatExchanger_Desiccant_BalancedFlowFields::EconomizerLockout, false).get());
+}
+
+TEST_F(EnergyPlusFixture, ForwardTranslator_CoilSystemCoolingDXHeatExchangerAssisted_HeatExchangerDesiccantBalancedFlow) {
+
+  // Test that the nodes are written correctly in this very complex scenario: the AirLoopHVAC has a Unitary,
+  // which has a CoilSystemCoolingDXHeatExchangerAssisted as a Cooling Coil, and that has a HeatExchangerDesiccantBalancedFlow as a HX.
+
+  Model m;
+  AirLoopHVAC a(m);
+
+  HeatExchangerDesiccantBalancedFlow hx(m);
+  CoilSystemCoolingDXHeatExchangerAssisted coilSystem(m, hx);
+  coilSystem.setName("CoilSystemDXHX");
+  EXPECT_EQ(hx, coilSystem.heatExchanger());
+  coilSystem.coolingCoil().setName("CoilSystemDXHX CoolingCoil");
+
+  AirLoopHVACUnitarySystem unitary(m);
+  EXPECT_TRUE(unitary.setCoolingCoil(coilSystem));
+  CoilHeatingDXSingleSpeed dxHeating(m);
+  EXPECT_TRUE(unitary.setHeatingCoil(dxHeating));
+  CoilHeatingElectric supplHeating(m);
+  EXPECT_TRUE(unitary.setSupplementalHeatingCoil(supplHeating));
+
+  FanSystemModel fan(m);
+  EXPECT_TRUE(unitary.setSupplyFan(fan));
+
+  Node supplyInletNode = a.supplyInletNode();
+  EXPECT_TRUE(unitary.addToNode(supplyInletNode));
+
+  a.supplyInletNode().setName("AirLoop Supply Inlet Node");
+  a.supplyOutletNode().setName("AirLoop Supply Outlet Node");
+
+  ForwardTranslator ft;
+  Workspace w = ft.translateModel(m);
+
+  auto getFirst = [&w](auto iddObjectType) -> WorkspaceObject {
+    WorkspaceObjectVector objs(w.getObjectsByType(iddObjectType));
+    return objs[0];
+  };
+
+  auto idf_hx = getFirst(IddObjectType::HeatExchanger_Desiccant_BalancedFlow);
+  auto idf_dxHeating = getFirst(IddObjectType::Coil_Heating_DX_SingleSpeed);
+  auto idf_dxCoolingCoilSystem = getFirst(IddObjectType::Coil_Cooling_DX_SingleSpeed);
+  auto idf_a = getFirst(IddObjectType::AirLoopHVAC);
+
+  EXPECT_EQ(idf_a.getString(AirLoopHVACFields::SupplySideInletNodeName).get(),
+            idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::RegenerationAirInletNodeName).get());
+  EXPECT_EQ("AirLoop Supply Inlet Node", idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::RegenerationAirInletNodeName).get());
+
+  EXPECT_EQ(idf_dxCoolingCoilSystem.getString(Coil_Cooling_DX_SingleSpeedFields::AirInletNodeName).get(),
+            idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::RegenerationAirOutletNodeName).get());
+  EXPECT_EQ("CoilSystemDXHX HX Supply Air Outlet - Cooling Inlet Node",
+            idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::RegenerationAirOutletNodeName).get());
+
+  EXPECT_EQ(idf_dxCoolingCoilSystem.getString(Coil_Cooling_DX_SingleSpeedFields::AirOutletNodeName).get(),
+            idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::ProcessAirInletNodeName).get());
+  EXPECT_EQ("CoilSystemDXHX HX Exhaust Air Inlet - Cooling Outlet Node",
+            idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::ProcessAirInletNodeName).get());
+
+  EXPECT_EQ(idf_dxHeating.getString(Coil_Heating_DX_SingleSpeedFields::AirInletNodeName).get(),
+            idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::ProcessAirOutletNodeName).get());
+  EXPECT_EQ("Air Loop HVAC Unitary System 1 Cooling Coil - Heating Coil Node",
+            idf_hx.getString(HeatExchanger_Desiccant_BalancedFlowFields::ProcessAirOutletNodeName).get());
 }
