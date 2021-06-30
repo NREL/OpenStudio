d80b6029707062112bf08fedb35f7fdcab9d3812
Tim Coleman <tjc.coleman@gmail.com>
2021-06-30 14:40:24 -0600

    Add in a bat script to install, compile and test nupkg

--- csharp/nuget_test/test_nuget.cs
+++ csharp/nuget_test/test_nuget.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace test_nuget
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             var m = new OpenStudio.Model();
         }
     }
