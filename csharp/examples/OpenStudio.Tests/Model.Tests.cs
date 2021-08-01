442e123cebd059a5b4147ab2d76d17e579f5b19d
MingboPeng <mingo1214@gmail.com>
2021-08-01 01:18:24 -0400

    test(csharp): add a test for EnergyPlusReverseTranslator

--- csharp/examples/OpenStudio.Tests/Model.Tests.cs
+++ csharp/examples/OpenStudio.Tests/Model.Tests.cs
@@ -59,5 +59,30 @@ namespace OpenStudio.Tests
             Assert.True(objList.Count == 1);
             Assert.True(objList.First().nameString() == "A New Coil");
         }
+
+        [Test]
+        public void EnergyPlusReverseTranslator_Test()
+        {
+            var mIdf = @"
+Material:NoMass,
+R13LAYER,  ! Material Name
+ Rough,  ! Roughness
+   2.290965    ,  ! Resistance {M**2K/W}
+  0.9000000    ,   ! Thermal Absorptance
+  0.7500000    ,   ! Solar Absorptance
+  0.7500000    ;   ! Visible Absorptance
+";
+
+            var ws = new OpenStudio.Workspace();
+            var idfobj = OpenStudio.IdfObject.load(mIdf).get();
+            ws.addObject(idfobj);
+
+            var trans = new OpenStudio.EnergyPlusReverseTranslator();
+            var om = trans.translateWorkspace(ws);
+            var mat = om.getMasslessOpaqueMaterials().First();
+
+            Assert.IsTrue(mat.nameString() == "R13LAYER");
+            Assert.IsTrue(mat.thermalResistance() == 2.290965);
+        }
     }
 }
