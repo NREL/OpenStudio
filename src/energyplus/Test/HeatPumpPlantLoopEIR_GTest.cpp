c5747e6ade28055d80247a3784a7826d0006bcda
Joe Robertson <joseph.robertson@nrel.gov>
2021-08-03 10:24:27 -0700

    Update ft test for node name changes.

--- src/energyplus/Test/HeatPumpPlantLoopEIR_GTest.cpp
+++ src/energyplus/Test/HeatPumpPlantLoopEIR_GTest.cpp
@@ -54,7 +54,7 @@ using namespace openstudio::energyplus;
 using namespace openstudio::model;
 using namespace openstudio;
 
-TEST_F(EnergyPlusFixture, ForwardTranslator_HeatPumpPlantLoopEIR) {
+TEST_F(EnergyPlusFixture, ForwardTranslator_HeatPumpPlantLoopEIR_AirSource) {
 
   Model m;
 
@@ -109,10 +109,10 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_HeatPumpPlantLoopEIR) {
     EXPECT_EQ(1u, idf_ccs.size());
     WorkspaceObject idf_cc(idf_ccs[0]);
 
-    EXPECT_NE("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideInletNodeName, false).get());
-    EXPECT_NE("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideOutletNodeName, false).get());
-    EXPECT_NE("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::SourceSideInletNodeName, false).get());
-    EXPECT_NE("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::SourceSideOutletNodeName, false).get());
+    EXPECT_EQ("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideInletNodeName, false).get());
+    EXPECT_EQ("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideOutletNodeName, false).get());
+    EXPECT_EQ("Inlet Node For Source Side", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::SourceSideInletNodeName, false).get());
+    EXPECT_EQ("Outlet Node For Source Side", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::SourceSideOutletNodeName, false).get());
     EXPECT_EQ("AirSource", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::CondenserType, false).get());
     EXPECT_EQ(plhp_htg.nameString(), idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::CompanionHeatPumpName).get());
     EXPECT_EQ(1.0, idf_cc.getDouble(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideReferenceFlowRate, false).get());
@@ -138,11 +138,120 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_HeatPumpPlantLoopEIR) {
     EXPECT_EQ(1u, idf_hcs.size());
     WorkspaceObject idf_hc(idf_hcs[0]);
 
-    EXPECT_NE("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideInletNodeName, false).get());
-    EXPECT_NE("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideOutletNodeName, false).get());
-    EXPECT_NE("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideInletNodeName, false).get());
-    EXPECT_NE("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideOutletNodeName, false).get());
+    EXPECT_EQ("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideInletNodeName, false).get());
+    EXPECT_EQ("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideOutletNodeName, false).get());
+    EXPECT_EQ("Inlet Node For Source Side", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideInletNodeName, false).get());
+    EXPECT_EQ("Outlet Node For Source Side", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideOutletNodeName, false).get());
     EXPECT_EQ("AirSource", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::CondenserType, false).get());
+    EXPECT_EQ(plhp_clg.nameString(), idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::CompanionHeatPumpName).get());
+    EXPECT_EQ(1.0, idf_hc.getDouble(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideReferenceFlowRate, false).get());
+    EXPECT_EQ(2.0, idf_hc.getDouble(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideReferenceFlowRate, false).get());
+    EXPECT_EQ(3.0, idf_hc.getDouble(HeatPump_PlantLoop_EIR_HeatingFields::ReferenceCapacity, false).get());
+    EXPECT_EQ(4.0, idf_hc.getDouble(HeatPump_PlantLoop_EIR_HeatingFields::ReferenceCoefficientofPerformance, false).get());
+    EXPECT_EQ(5.0, idf_hc.getDouble(HeatPump_PlantLoop_EIR_HeatingFields::SizingFactor, false).get());
+    boost::optional<WorkspaceObject> woCurve1(idf_hc.getTarget(HeatPump_PlantLoop_EIR_HeatingFields::CapacityModifierFunctionofTemperatureCurveName));
+    EXPECT_TRUE(woCurve1);
+    EXPECT_EQ(woCurve1->iddObject().type(), IddObjectType::Curve_Biquadratic);
+    boost::optional<WorkspaceObject> woCurve2(
+      idf_hc.getTarget(HeatPump_PlantLoop_EIR_HeatingFields::ElectricInputtoOutputRatioModifierFunctionofTemperatureCurveName));
+    EXPECT_TRUE(woCurve2);
+    EXPECT_EQ(woCurve2->iddObject().type(), IddObjectType::Curve_Biquadratic);
+    boost::optional<WorkspaceObject> woCurve3(
+      idf_hc.getTarget(HeatPump_PlantLoop_EIR_HeatingFields::ElectricInputtoOutputRatioModifierFunctionofPartLoadRatioCurveName));
+    EXPECT_TRUE(woCurve3);
+    EXPECT_EQ(woCurve3->iddObject().type(), IddObjectType::Curve_Quadratic);
+  }
+}
+
+TEST_F(EnergyPlusFixture, ForwardTranslator_HeatPumpPlantLoopEIR_WaterSource) {
+
+  Model m;
+
+  PlantLoop plant_loop_cup_clg(m);
+  PlantLoop plant_loop_cup_htg(m);
+  PlantLoop plant_loop_plhp_clg(m);
+  PlantLoop plant_loop_plhp_htg(m);
+
+  HeatPumpPlantLoopEIRCooling plhp_clg(m);
+  EXPECT_TRUE(plhp_clg.setCondenserType("WaterSource"));
+  EXPECT_TRUE(plhp_clg.setReferenceLoadSideFlowRate(1.0));
+  EXPECT_TRUE(plhp_clg.setReferenceSourceSideFlowRate(2.0));
+  EXPECT_TRUE(plhp_clg.setReferenceCapacity(3.0));
+  EXPECT_TRUE(plhp_clg.setReferenceCoefficientofPerformance(4.0));
+  EXPECT_TRUE(plhp_clg.setSizingFactor(5.0));
+  CurveBiquadratic curve1(m);
+  EXPECT_TRUE(plhp_clg.setCapacityModifierFunctionofTemperatureCurve(curve1));
+  CurveBiquadratic curve2(m);
+  EXPECT_TRUE(plhp_clg.setElectricInputtoOutputRatioModifierFunctionofTemperatureCurve(curve2));
+  CurveQuadratic curve3(m);
+  EXPECT_TRUE(plhp_clg.setElectricInputtoOutputRatioModifierFunctionofPartLoadRatioCurve(curve3));
+
+  HeatPumpPlantLoopEIRHeating plhp_htg(m);
+  EXPECT_TRUE(plhp_htg.setCondenserType("WaterSource"));
+  EXPECT_TRUE(plhp_htg.setReferenceLoadSideFlowRate(1.0));
+  EXPECT_TRUE(plhp_htg.setReferenceSourceSideFlowRate(2.0));
+  EXPECT_TRUE(plhp_htg.setReferenceCapacity(3.0));
+  EXPECT_TRUE(plhp_htg.setReferenceCoefficientofPerformance(4.0));
+  EXPECT_TRUE(plhp_htg.setSizingFactor(5.0));
+  CurveBiquadratic curve4(m);
+  EXPECT_TRUE(plhp_htg.setCapacityModifierFunctionofTemperatureCurve(curve4));
+  CurveBiquadratic curve5(m);
+  EXPECT_TRUE(plhp_htg.setElectricInputtoOutputRatioModifierFunctionofTemperatureCurve(curve5));
+  CurveQuadratic curve6(m);
+  EXPECT_TRUE(plhp_htg.setElectricInputtoOutputRatioModifierFunctionofPartLoadRatioCurve(curve6));
+
+  EXPECT_TRUE(plhp_clg.setCompanionHeatingHeatPump(plhp_htg));
+  EXPECT_TRUE(plhp_htg.setCompanionCoolingHeatPump(plhp_clg));
+
+  EXPECT_TRUE(plant_loop_cup_clg.addDemandBranchForComponent(plhp_clg));
+  EXPECT_TRUE(plant_loop_plhp_clg.addSupplyBranchForComponent(plhp_clg));
+  EXPECT_TRUE(plant_loop_cup_htg.addDemandBranchForComponent(plhp_htg));
+  EXPECT_TRUE(plant_loop_plhp_htg.addSupplyBranchForComponent(plhp_htg));
+
+  openstudio::energyplus::ForwardTranslator ft;
+  Workspace w = ft.translateModel(m);
+
+  EXPECT_EQ(0u, ft.errors().size());
+
+  {
+    WorkspaceObjectVector idf_ccs(w.getObjectsByType(IddObjectType::HeatPump_PlantLoop_EIR_Cooling));
+    EXPECT_EQ(1u, idf_ccs.size());
+    WorkspaceObject idf_cc(idf_ccs[0]);
+
+    EXPECT_EQ("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideInletNodeName, false).get());
+    EXPECT_EQ("", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideOutletNodeName, false).get());
+    EXPECT_EQ("Inlet Node For Source Side", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::SourceSideInletNodeName, false).get());
+    EXPECT_EQ("Outlet Node For Source Side", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::SourceSideOutletNodeName, false).get());
+    EXPECT_EQ("WaterSource", idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::CondenserType, false).get());
+    EXPECT_EQ(plhp_htg.nameString(), idf_cc.getString(HeatPump_PlantLoop_EIR_CoolingFields::CompanionHeatPumpName).get());
+    EXPECT_EQ(1.0, idf_cc.getDouble(HeatPump_PlantLoop_EIR_CoolingFields::LoadSideReferenceFlowRate, false).get());
+    EXPECT_EQ(2.0, idf_cc.getDouble(HeatPump_PlantLoop_EIR_CoolingFields::SourceSideReferenceFlowRate, false).get());
+    EXPECT_EQ(3.0, idf_cc.getDouble(HeatPump_PlantLoop_EIR_CoolingFields::ReferenceCapacity, false).get());
+    EXPECT_EQ(4.0, idf_cc.getDouble(HeatPump_PlantLoop_EIR_CoolingFields::ReferenceCoefficientofPerformance, false).get());
+    EXPECT_EQ(5.0, idf_cc.getDouble(HeatPump_PlantLoop_EIR_CoolingFields::SizingFactor, false).get());
+    boost::optional<WorkspaceObject> woCurve1(idf_cc.getTarget(HeatPump_PlantLoop_EIR_CoolingFields::CapacityModifierFunctionofTemperatureCurveName));
+    EXPECT_TRUE(woCurve1);
+    EXPECT_EQ(woCurve1->iddObject().type(), IddObjectType::Curve_Biquadratic);
+    boost::optional<WorkspaceObject> woCurve2(
+      idf_cc.getTarget(HeatPump_PlantLoop_EIR_CoolingFields::ElectricInputtoOutputRatioModifierFunctionofTemperatureCurveName));
+    EXPECT_TRUE(woCurve2);
+    EXPECT_EQ(woCurve2->iddObject().type(), IddObjectType::Curve_Biquadratic);
+    boost::optional<WorkspaceObject> woCurve3(
+      idf_cc.getTarget(HeatPump_PlantLoop_EIR_CoolingFields::ElectricInputtoOutputRatioModifierFunctionofPartLoadRatioCurveName));
+    EXPECT_TRUE(woCurve3);
+    EXPECT_EQ(woCurve3->iddObject().type(), IddObjectType::Curve_Quadratic);
+  }
+
+  {
+    WorkspaceObjectVector idf_hcs(w.getObjectsByType(IddObjectType::HeatPump_PlantLoop_EIR_Heating));
+    EXPECT_EQ(1u, idf_hcs.size());
+    WorkspaceObject idf_hc(idf_hcs[0]);
+
+    EXPECT_EQ("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideInletNodeName, false).get());
+    EXPECT_EQ("", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideOutletNodeName, false).get());
+    EXPECT_EQ("Inlet Node For Source Side", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideInletNodeName, false).get());
+    EXPECT_EQ("Outlet Node For Source Side", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideOutletNodeName, false).get());
+    EXPECT_EQ("WaterSource", idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::CondenserType, false).get());
     EXPECT_EQ(plhp_clg.nameString(), idf_hc.getString(HeatPump_PlantLoop_EIR_HeatingFields::CompanionHeatPumpName).get());
     EXPECT_EQ(1.0, idf_hc.getDouble(HeatPump_PlantLoop_EIR_HeatingFields::LoadSideReferenceFlowRate, false).get());
     EXPECT_EQ(2.0, idf_hc.getDouble(HeatPump_PlantLoop_EIR_HeatingFields::SourceSideReferenceFlowRate, false).get());
