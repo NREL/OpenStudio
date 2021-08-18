a1a64f49b87f9ce1b7b76da17fdd953793e7e33d
Julien Marrec <julien.marrec@gmail.com>
2021-08-18 12:49:21 +0200

    Add Gtests for #4399

--- src/model/test/ShadingSurface_GTest.cpp
+++ src/model/test/ShadingSurface_GTest.cpp
@@ -54,3 +54,19 @@ TEST_F(ModelFixture, ShadingSurface) {
   ShadingSurface shadingSurface(points, model);
   EXPECT_FALSE(shadingSurface.shadingSurfaceGroup());
 }
+
+TEST_F(ModelFixture, ShadingSurface_isXXXAutocalculated_4399) {
+  // Test for #4399
+  Model m;
+  std::vector<Point3d> points{
+    Point3d{0, 1, 0},
+    Point3d{0, 0, 0},
+    Point3d{1, 0, 0},
+    Point3d{1, 1, 0},
+  };
+
+  ShadingSurface shadingSurface(points, m);
+
+  shadingSurface.autocalculateNumberofVertices();
+  EXPECT_TRUE(shadingSurface.isNumberofVerticesAutocalculated());
+}
