052105acac1aa6b3cdd9d586b800c8adb242e56d
Julien Marrec <julien.marrec@gmail.com>
2021-08-18 09:41:49 +0200

    Fix isAutocalculated methods for surfaces

--- src/model/SubSurface.cpp
+++ src/model/SubSurface.cpp
@@ -502,7 +502,7 @@ namespace model {
       bool result = false;
       boost::optional<std::string> value = getString(OS_SubSurfaceFields::ViewFactortoGround, true);
       if (value) {
-        result = openstudio::istringEqual(value.get(), "canAutocalculate");
+        result = openstudio::istringEqual(value.get(), "Autocalculate");
       }
       return result;
     }
@@ -579,7 +579,7 @@ namespace model {
       bool result = false;
       boost::optional<std::string> value = getString(OS_SubSurfaceFields::NumberofVertices, true);
       if (value) {
-        result = openstudio::istringEqual(value.get(), "canAutocalculate");
+        result = openstudio::istringEqual(value.get(), "Autocalculate");
       }
       return result;
     }
