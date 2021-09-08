2cf3bf33f8a8890aa2ad539cff6f3c32a391bb7d
Joe Robertson <joseph.robertson@nrel.gov>
2021-09-08 12:16:09 -0700

    Improve space ft testing.

--- src/energyplus/Test/Space_GTest.cpp
+++ src/energyplus/Test/Space_GTest.cpp
@@ -35,8 +35,17 @@
 
 #include "../../model/Model.hpp"
 #include "../../model/Space.hpp"
+#include "../../model/Space_Impl.hpp"
 #include "../../model/LifeCycleCost.hpp"
 #include "../../model/ThermalZone.hpp"
+#include "../../model/ThermalZone_Impl.hpp"
+#include "../../model/Surface.hpp"
+#include "../../model/Surface_Impl.hpp"
+#include "../../model/SpaceType.hpp"
+#include "../../model/SpaceType_Impl.hpp"
+
+#include <utilities/idd/Space_FieldEnums.hxx>
+#include <utilities/idd/Zone_FieldEnums.hxx>
 
 #include <utilities/idd/IddEnums.hxx>
 
@@ -65,6 +74,8 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_Space) {
   ThermalZone thermalZone(model);
   space.setThermalZone(thermalZone);
 
+  ASSERT_EQ(1u, thermalZone.spaces().size());
+
   workspace = ft.translateModel(model);
   EXPECT_EQ(1u, workspace.getObjectsByType(IddObjectType::Space).size());
   EXPECT_EQ(1u, workspace.getObjectsByType(IddObjectType::Zone).size());
@@ -74,11 +85,13 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_Space) {
   Space space2(model);
   space2.setThermalZone(thermalZone);
 
+  ASSERT_EQ(2u, thermalZone.spaces().size());
+
   workspace = ft.translateModel(model);
   EXPECT_EQ(1u, workspace.getObjectsByType(IddObjectType::Zone).size());
   EXPECT_EQ(0u, workspace.getObjectsByType(IddObjectType::ComponentCost_LineItem).size());
 
-  WorkspaceObjectVector idf_spaces(workspace.getObjectsByType(IddObjectType::Spaces));
+  WorkspaceObjectVector idf_spaces(workspace.getObjectsByType(IddObjectType::Space));
   EXPECT_EQ(2u, idf_spaces.size());
   WorkspaceObject idf_space1(idf_spaces[0]);
   WorkspaceObject idf_space2(idf_spaces[1]);
@@ -95,7 +108,99 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_Space) {
 
   EXPECT_EQ(0, idf_space1.getDouble(SpaceFields::FloorArea, false).get());
   EXPECT_EQ(0, idf_space2.getDouble(SpaceFields::FloorArea, false).get());
+}
 
-  EXPECT_EQ("", idf_space1.getString(SpaceFields::SpaceType, false).get());
-  EXPECT_EQ("", idf_space2.getString(SpaceFields::SpaceType, false).get());
+TEST_F(EnergyPlusFixture, ForwardTranslator_Space2) {
+  Model model;
+
+  Point3dVector points1;
+  points1.push_back(Point3d(0, 1, 0));
+  points1.push_back(Point3d(1, 1, 0));
+  points1.push_back(Point3d(1, 0, 0));
+  points1.push_back(Point3d(0, 0, 0));
+  Surface floor1(points1, model);
+
+  Point3dVector points2;
+  points2.push_back(Point3d(0, 2, 0));
+  points2.push_back(Point3d(2, 2, 0));
+  points2.push_back(Point3d(2, 0, 0));
+  points2.push_back(Point3d(0, 0, 0));
+  Surface floor2(points2, model);
+
+  Space space1(model);
+  Space space2(model);
+  floor1.setParent(space1);
+  floor2.setParent(space2);
+
+  EXPECT_NEAR(1, space1.floorArea(), 0.0001);
+  EXPECT_NEAR(4, space2.floorArea(), 0.0001);
+  EXPECT_TRUE(space1.partofTotalFloorArea());
+  EXPECT_TRUE(space2.partofTotalFloorArea());
+
+  SpaceType spaceType1(model);
+  SpaceType spaceType2(model);
+  space1.setSpaceType(spaceType1);
+  space2.setSpaceType(spaceType2);
+
+  ThermalZone thermalZone(model);
+  space1.setThermalZone(thermalZone);
+  space2.setThermalZone(thermalZone);
+
+  ForwardTranslator ft;
+  Workspace workspace = ft.translateModel(model);
+
+  WorkspaceObjectVector idf_spaces(workspace.getObjectsByType(IddObjectType::Space));
+  EXPECT_EQ(2u, idf_spaces.size());
+  WorkspaceObject idf_space1(idf_spaces[0]);
+  WorkspaceObject idf_space2(idf_spaces[1]);
+
+  WorkspaceObjectVector idf_zones(workspace.getObjectsByType(IddObjectType::Zone));
+  EXPECT_EQ(1u, idf_zones.size());
+  WorkspaceObject idf_zone(idf_zones[0]);
+
+  boost::optional<WorkspaceObject> idf_zone1(idf_space1.getTarget(SpaceFields::ZoneName));
+  ASSERT_TRUE(idf_zone1);
+  EXPECT_EQ(idf_zone1->iddObject().type(), IddObjectType::Zone);
+  EXPECT_EQ(thermalZone.nameString(), idf_zone1->name().get());
+  EXPECT_EQ(idf_zone1, idf_zone);
+
+  boost::optional<WorkspaceObject> idf_zone2(idf_space2.getTarget(SpaceFields::ZoneName));
+  ASSERT_TRUE(idf_zone2);
+  EXPECT_EQ(idf_zone2->iddObject().type(), IddObjectType::Zone);
+  EXPECT_EQ(thermalZone.nameString(), idf_zone2->name().get());
+  EXPECT_EQ(idf_zone2, idf_zone);
+
+  double space1_floorArea = idf_space1.getDouble(SpaceFields::FloorArea, false).get();
+  double space2_floorArea = idf_space2.getDouble(SpaceFields::FloorArea, false).get();
+  EXPECT_NEAR(space1.floorArea(), space1_floorArea, 0.0001);
+  EXPECT_NEAR(space2.floorArea(), space2_floorArea, 0.0001);
+  EXPECT_NE("", idf_space1.getString(SpaceFields::SpaceType).get());
+  EXPECT_NE("", idf_space2.getString(SpaceFields::SpaceType).get());
+
+  EXPECT_FALSE(idf_zone.getDouble(ZoneFields::FloorArea, false));
+
+  // Check floor area
+  EXPECT_TRUE(space1.setPartofTotalFloorArea(false));
+
+  workspace = ft.translateModel(model);
+
+  idf_zones = workspace.getObjectsByType(IddObjectType::Zone);
+  EXPECT_EQ(1u, idf_zones.size());
+  idf_zone = idf_zones[0];
+
+  EXPECT_NEAR(space2_floorArea, idf_zone.getDouble(ZoneFields::FloorArea, false).get(), 0.0001);
+
+  EXPECT_TRUE(space2.setPartofTotalFloorArea(false));
+
+  workspace = ft.translateModel(model);
+
+  idf_zones = workspace.getObjectsByType(IddObjectType::Zone);
+  EXPECT_EQ(1u, idf_zones.size());
+  idf_zone = idf_zones[0];
+
+  EXPECT_FALSE(idf_zone.getDouble(ZoneFields::FloorArea, false));
+}
+
+TEST_F(EnergyPlusFixture, ForwardTranslator_Space_SecondaryDaylightingControl) {
+  // TODO
 }
