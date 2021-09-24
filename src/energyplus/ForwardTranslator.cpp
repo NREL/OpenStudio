8c89b742cd0cb9a6173df7ea41728d550a58c8fb
Joe Robertson <joseph.robertson@nrel.gov>
2021-09-24 12:26:11 -0700

    Set space bool back to true.

--- src/energyplus/ForwardTranslator.cpp
+++ src/energyplus/ForwardTranslator.cpp
@@ -108,7 +108,7 @@ namespace energyplus {
     m_excludeSQliteOutputReport = false;
     m_excludeHTMLOutputReport = false;
     m_excludeVariableDictionary = false;
-    m_excludeSpaceTranslation = false;
+    m_excludeSpaceTranslation = true;
   }
 
   Workspace ForwardTranslator::translateModel(const Model& model, ProgressBar* progressBar) {
