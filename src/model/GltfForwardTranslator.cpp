5c6c582c2897a42195be15c27bdf1c34f029e587
Richard See (DA) <Brijendra.Singh@D-Alchemy.com>
2021-10-22 11:10:49 -0700

    first commit

---
+++ src/model/GltfForwardTranslator.cpp
@@ -0,0 +1,146 @@
+/***********************************************************************************************************************
+*  OpenStudio(R), Copyright (c) 2008-2021, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+*  following conditions are met:
+*
+*  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+*  disclaimer.
+*
+*  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+*  disclaimer in the documentation and/or other materials provided with the distribution.
+*
+*  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products
+*  derived from this software without specific prior written permission from the respective party.
+*
+*  (4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative works
+*  may not use the "OpenStudio" trademark, "OS", "os", or any other confusingly similar designation without specific prior
+*  written permission from Alliance for Sustainable Energy, LLC.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+*  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE UNITED STATES GOVERNMENT, OR THE UNITED
+*  STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+*  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+*  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+*  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***********************************************************************************************************************/
+
+#include "GltfForwardTranslator.hpp"
+
+#include "RenderingColor.hpp"
+#include "ConstructionBase.hpp"
+#include "ConstructionBase_Impl.hpp"
+#include "ConstructionAirBoundary.hpp"
+#include "ConstructionAirBoundary_Impl.hpp"
+#include "AirLoopHVAC.hpp"
+#include "AirLoopHVAC_Impl.hpp"
+#include "ThermalZone.hpp"
+#include "ThermalZone_Impl.hpp"
+#include "SpaceType.hpp"
+#include "SpaceType_Impl.hpp"
+#include "Building.hpp"
+#include "Building_Impl.hpp"
+#include "BuildingStory.hpp"
+#include "BuildingStory_Impl.hpp"
+#include "BuildingUnit.hpp"
+#include "BuildingUnit_Impl.hpp"
+#include "Surface.hpp"
+#include "Surface_Impl.hpp"
+#include "SubSurface.hpp"
+#include "SubSurface_Impl.hpp"
+#include "ShadingSurface.hpp"
+#include "ShadingSurface_Impl.hpp"
+#include "InteriorPartitionSurface.hpp"
+#include "InteriorPartitionSurface_Impl.hpp"
+#include "PlanarSurfaceGroup.hpp"
+#include "PlanarSurfaceGroup_Impl.hpp"
+#include "Space.hpp"
+#include "Space_Impl.hpp"
+#include "DefaultConstructionSet.hpp"
+#include "DefaultConstructionSet_Impl.hpp"
+#include "ShadingSurfaceGroup.hpp"
+#include "InteriorPartitionSurfaceGroup.hpp"
+
+#include "../utilities/core/Assert.hpp"
+#include "../utilities/core/Compare.hpp"
+#include "../utilities/geometry/Point3d.hpp"
+#include "../utilities/geometry/Plane.hpp"
+#include "../utilities/geometry/BoundingBox.hpp"
+#include "../utilities/geometry/Transformation.hpp"
+#include "../utilities/geometry/Geometry.hpp"
+#include "tiny_gltf.h"
+
+#include <thread>
+
+#include <cmath>
+
+namespace openstudio {
+namespace model {
+
+  bool GltfForwardTranslator::modelToGLTF(const Model& model, bool triangulateSurfaces) {
+    return modelToGLTF(model, triangulateSurfaces, [](double percentage) {});
+  }
+
+  bool GltfForwardTranslator::modelToGLTF(const Model& model, bool triangulateSurfaces, std::function<void(double)> updatePercentage) {
+    m_logSink.setThreadId(std::this_thread::get_id());
+    m_logSink.resetStringStream();
+
+    tinygltf::Model tinyGLTF;
+    //new test start
+
+    tinygltf::TinyGLTF loader;
+    tinygltf::Model gltf_Model;
+    std::string err;
+    std::string warning;
+    openstudio::path p;
+    //p = resourcesPath() / toPath("utilities/Geometry/minimal_GLTF_File.gltf");
+    std::string fileName = "t2/Scene.gltf";
+    bool ret = loader.LoadASCIIFromFile(&gltf_Model, &err, &warning, "");
+    
+
+    //new test end
+    updatePercentage(0.0);
+
+    updatePercentage(100.0);
+
+    return false;
+  }
+
+  bool toJSON() {
+    return false;
+  }
+
+  GltfForwardTranslator::GltfForwardTranslator() {
+    m_logSink.setLogLevel(Warn);
+    //m_logSink.setChannelRegex(boost::regex("openstudio\\.model\\.GltfForwardTranslator"));
+    m_logSink.setThreadId(std::this_thread::get_id());
+  }
+
+  std::vector<LogMessage> GltfForwardTranslator::warnings() const {
+    std::vector<LogMessage> result;
+
+    for (LogMessage logMessage : m_logSink.logMessages()) {
+      if (logMessage.logLevel() == Warn) {
+        result.push_back(logMessage);
+      }
+    }
+
+    return result;
+  }
+
+  std::vector<LogMessage> GltfForwardTranslator::errors() const {
+    std::vector<LogMessage> result;
+
+    for (LogMessage logMessage : m_logSink.logMessages()) {
+      if (logMessage.logLevel() > Warn) {
+        result.push_back(logMessage);
+      }
+    }
+
+    return result;
+  }
+
+}  // namespace model
+}  // namespace openstudio
