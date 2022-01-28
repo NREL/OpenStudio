d316b17c5f58888c0f2875fb302433968eae3e1b
Julien Marrec <julien.marrec@gmail.com>
2022-01-28 15:48:50 +0100

    two stories per intersect is empty

--- src/osversion/benchmark/VersionTranslation_Benchmark.cpp
+++ src/osversion/benchmark/VersionTranslation_Benchmark.cpp
@@ -10,7 +10,7 @@
 
 using namespace openstudio;
 
-static void BM_LoadIdfFile(benchmark::State& state, const std::string& testCase) {
+static void BM_VT(benchmark::State& state, const std::string& testCase) {
 
   path modelPath = resourcesPath() / toPath(testCase);
 
@@ -20,11 +20,10 @@ static void BM_LoadIdfFile(benchmark::State& state, const std::string& testCase)
   }
 }
 
-BENCHMARK_CAPTURE(BM_LoadIdfFile, example_1_13_4, std::string("osversion/1_13_4/example.osm"))->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(BM_LoadIdfFile, example_1_14_0, std::string("osversion/1_14_0/example.osm"))->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(BM_LoadIdfFile, Windows_Complete, std::string("model/7-7_Windows_Complete.osm"))->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(BM_LoadIdfFile, Model12, std::string("model/15023_Model12.osm"))->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(BM_LoadIdfFile, floorplan_school, std::string("model/floorplan_school.osm"))->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(BM_LoadIdfFile, two_stories_pre_intersect, std::string("model/two_stories_pre_intersect.osm"))->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(BM_LoadIdfFile, CONTAMTemplate, std::string("contam/CONTAMTemplate.osm"))->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(BM_LoadIdfFile, seb, std::string("Examples/compact_osw/files/seb.osm"))->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_VT, example_1_13_4, std::string("osversion/1_13_4/example.osm"))->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_VT, example_1_14_0, std::string("osversion/1_14_0/example.osm"))->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_VT, Windows_Complete, std::string("model/7-7_Windows_Complete.osm"))->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_VT, Model12, std::string("model/15023_Model12.osm"))->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_VT, floorplan_school, std::string("model/floorplan_school.osm"))->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_VT, CONTAMTemplate, std::string("contam/CONTAMTemplate.osm"))->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_VT, seb, std::string("Examples/compact_osw/files/seb.osm"))->Unit(benchmark::kMillisecond);
