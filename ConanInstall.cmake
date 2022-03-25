c57231ee68aef33f8f57c735f03de6e756d77f37
Conan Dependabot <conan-dependabot@users.noreply.github.com>
2022-03-25 17:16:38 +0000

    Automated checkin - update conan recipes

--- ConanInstall.cmake
+++ ConanInstall.cmake
@@ -72,7 +72,7 @@ if(NOT CONAN_OPENSTUDIO_ALREADY_RUN)
   list(APPEND CONAN_BUILD "missing")
 
   if(BUILD_TESTING)
-    set(CONAN_GTEST "gtest/1.11.0#d0dbd15442f5646c00fdecf832f6f3e3")
+    set(CONAN_GTEST "gtest/1.11.0#8aca975046f1b60c660ee9d066764d69")
   else()
     set(CONAN_GTEST "")
   endif()
@@ -93,18 +93,18 @@ if(NOT CONAN_OPENSTUDIO_ALREADY_RUN)
     ${CONAN_READLINE}
     ${CONAN_QT}
     ${CONAN_RUBY}
-    "openssl/1.1.1m#18c0b6de6e23449e8c862a20d8dc4e87" # force every package to align on the same as our conan-openstudio-ruby
-    "boost/1.78.0#c6275b1e5ecf6e51fe8baf1e322bb065"
-    "pugixml/1.11#a5f7cda7d93b35886bd4ea135e08f25b"
+    "openssl/1.1.1n#15565f1dcde351c64837550a2305577c" # force every package to align on the same as our conan-openstudio-ruby
+    "boost/1.78.0#277bce646044c183375481df48f9ba26"
+    "pugixml/1.12.1#5a39f82651eba3e7d6197903a3202e21"
     "jsoncpp/1.9.5#536d080aa154e5853332339bf576747c"
-    "minizip/1.2.11#0658b664480f2a0755b88502743d5d0d" # This depends on zlib/1.2.11, and basically patches it
-    "zlib/1.2.11#683857dbd5377d65f26795d4023858f9" # Also needed, so we can find zlib.h and co (+ pinning exactly is good)
-    "fmt/8.0.1#826bcee69ee04232ff9864766afa728c"
-    "sqlite3/3.37.1#990b9120900d8f9f45294397c0e152b0"
-    "cpprestsdk/2.10.18#990b9120900d8f9f45294397c0e152b0"
+    "minizip/1.2.11#6c5afba2e0aef90b007855f7ac27111d" # This depends on zlib/1.2.11, and basically patches it
+    "zlib/1.2.11#4b38406da00104befece594b529fd155" # Also needed, so we can find zlib.h and co (+ pinning exactly is good)
+    "fmt/8.1.1#b3e969f8561a85087bd0365c09bbf4fb"
+    "sqlite3/3.38.1#c7348c448cd1d9b52f9ca90b2b936c1d"
+    "cpprestsdk/2.10.18#003f5d46fb101c1d3bba72cf0ab781a2"
     "websocketpp/0.8.2#2ec4cd9d431005dd52ee012df204c362"
-    "geographiclib/1.52.0#f8aea2d67350aff0e107df2c2c98287e"
-    "swig/4.0.2#187cec21277aafbeae11dcc3b936ce74"
+    "geographiclib/1.52#76536a9315a003ef3511919310b2fe37"
+    "swig/4.0.2#9fcccb1e39eed9acd53a4363d8129be5"
     "tinygltf/2.5.0#c8b2aca9505e86312bb42aa0e1c639ec"
     ${CONAN_GTEST}
     ${CONAN_BENCHMARK}
