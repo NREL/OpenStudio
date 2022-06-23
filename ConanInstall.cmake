7b6288767e4ba4a258a90283e3198373c8628ed2
Julien Marrec <julien.marrec@gmail.com>
2022-06-23 10:24:48 +0200

    Use a new revision of websocketpp and cpprestsdk that uses boost/1.79.0 and openssl/1.1.1o like we do
    I patched websocketpp here: https://github.com/conan-io/conan-center-index/pull/11334

--- ConanInstall.cmake
+++ ConanInstall.cmake
@@ -157,8 +157,8 @@ if(NOT CONAN_OPENSTUDIO_ALREADY_RUN)
     "zlib/1.2.12#3b9e037ae1c615d045a06c67d88491ae" # Also needed, so we can find zlib.h and co (+ pinning exactly is good)
     "fmt/8.1.1#b3e969f8561a85087bd0365c09bbf4fb"
     "sqlite3/3.38.5#010911927ce1889b5cf824f45e7cd3d2"
-    "cpprestsdk/2.10.18#df2f6ac88e47cadd9c9e8e0971e00d89"
-    "websocketpp/0.8.2#3fd704c4c5388d9c08b11af86f79f616"
+    "cpprestsdk/2.10.18#ed9788e9d202d6eadd92581368ddfc2f"
+    "websocketpp/0.8.2#e497a462458d2a2f62690af2b96a73e5"
     "geographiclib/1.52#76536a9315a003ef3511919310b2fe37"
     "swig/4.0.2#9fcccb1e39eed9acd53a4363d8129be5"
     "tinygltf/2.5.0#c8b2aca9505e86312bb42aa0e1c639ec"
