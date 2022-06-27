1223f06a1840383a695ee41351bb6e08dac2df70
Julien Marrec <julien.marrec@gmail.com>
2022-06-27 14:49:23 +0200

    Write tests for the two new SPMs

--- src/model/test/SetpointManagerSystemNodeResetHumidity_GTest.cpp
+++ src/model/test/SetpointManagerSystemNodeResetHumidity_GTest.cpp
@@ -27,62 +27,180 @@
 *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************************************************************/
 
+#include <gtest/gtest.h>
+
 #include "ModelFixture.hpp"
 
 #include "../SetpointManagerSystemNodeResetHumidity.hpp"
 #include "../SetpointManagerSystemNodeResetHumidity_Impl.hpp"
 
-// TODO: Check the following class names against object getters and setters.
+#include "../Model.hpp"
 #include "../Node.hpp"
-#include "../Node_Impl.hpp"
-
-#include "../Node.hpp"
-#include "../Node_Impl.hpp"
+#include "../AirLoopHVAC.hpp"
+#include "../PlantLoop.hpp"
 
 using namespace openstudio;
 using namespace openstudio::model;
 
 TEST_F(ModelFixture, SetpointManagerSystemNodeResetHumidity_GettersSetters) {
   Model m;
-  // TODO: Check regular Ctor arguments
-  SetpointManagerSystemNodeResetHumidity setpointManagerSystemNodeResetHumidity(m);
-  // TODO: Or if a UniqueModelObject (and make sure _Impl is included)
-  // SetpointManagerSystemNodeResetHumidity setpointManagerSystemNodeResetHumidity = m.getUniqueModelObject<SetpointManagerSystemNodeResetHumidity>();
+  SetpointManagerSystemNodeResetHumidity spm(m);
 
-  setpointManagerSystemNodeResetHumidity.setName("My SetpointManagerSystemNodeResetHumidity");
+  spm.setName("My SetpointManagerSystemNodeResetHumidity");
 
   // Control Variable: Required String
-  EXPECT_TRUE(setpointManagerSystemNodeResetHumidity.setControlVariable("HumidityRatio"));
-  EXPECT_EQ("HumidityRatio", setpointManagerSystemNodeResetHumidity.controlVariable());
+  EXPECT_TRUE(spm.setControlVariable("HumidityRatio"));
+  EXPECT_EQ("HumidityRatio", spm.controlVariable());
   // Bad Value
-  EXPECT_FALSE(setpointManagerSystemNodeResetHumidity.setControlVariable("BADENUM"));
-  EXPECT_EQ("HumidityRatio", setpointManagerSystemNodeResetHumidity.controlVariable());
+  EXPECT_FALSE(spm.setControlVariable("BADENUM"));
+  EXPECT_EQ("HumidityRatio", spm.controlVariable());
 
   // Setpoint at Low Reference Humidity Ratio: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetHumidity.setSetpointatLowReferenceHumidityRatio(0.4));
-  EXPECT_EQ(0.4, setpointManagerSystemNodeResetHumidity.setpointatLowReferenceHumidityRatio());
+  EXPECT_TRUE(spm.setSetpointatLowReferenceHumidityRatio(0.4));
+  EXPECT_EQ(0.4, spm.setpointatLowReferenceHumidityRatio());
 
   // Setpoint at High Reference Humidity Ratio: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetHumidity.setSetpointatHighReferenceHumidityRatio(0.5));
-  EXPECT_EQ(0.5, setpointManagerSystemNodeResetHumidity.setpointatHighReferenceHumidityRatio());
+  EXPECT_TRUE(spm.setSetpointatHighReferenceHumidityRatio(0.5));
+  EXPECT_EQ(0.5, spm.setpointatHighReferenceHumidityRatio());
 
   // Low Reference Humidity Ratio: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetHumidity.setLowReferenceHumidityRatio(0.6));
-  EXPECT_EQ(0.6, setpointManagerSystemNodeResetHumidity.lowReferenceHumidityRatio());
+  EXPECT_TRUE(spm.setLowReferenceHumidityRatio(0.6));
+  EXPECT_EQ(0.6, spm.lowReferenceHumidityRatio());
 
   // High Reference Humidity Ratio: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetHumidity.setHighReferenceHumidityRatio(0.7));
-  EXPECT_EQ(0.7, setpointManagerSystemNodeResetHumidity.highReferenceHumidityRatio());
+  EXPECT_TRUE(spm.setHighReferenceHumidityRatio(0.7));
+  EXPECT_EQ(0.7, spm.highReferenceHumidityRatio());
+}
 
-  // Reference Node Name: Optional Object
-  boost::optional<Node> obj(m);
-  EXPECT_TRUE(setpointManagerSystemNodeResetHumidity.setReferenceNode(obj));
-  ASSERT_TRUE(setpointManagerSystemNodeResetHumidity.referenceNode());
-  EXPECT_EQ(obj, setpointManagerSystemNodeResetHumidity.referenceNode().get());
+TEST_F(ModelFixture, SetpointManagerSystemNodeResetHumidity_addToNode) {
 
-  // Setpoint Node or NodeList Name: Optional Object
-  boost::optional<Node> obj(m);
-  EXPECT_TRUE(setpointManagerSystemNodeResetHumidity.setSetpointNodeorNodeList(obj));
-  ASSERT_TRUE(setpointManagerSystemNodeResetHumidity.setpointNodeorNodeList());
-  EXPECT_EQ(obj, setpointManagerSystemNodeResetHumidity.setpointNodeorNodeList().get());
+  Model m;
+
+  // AirLoopHVAC
+  {
+    SetpointManagerSystemNodeResetHumidity spm(m);
+    AirLoopHVAC a(m);
+    Node supplyOutlet = a.supplyOutletNode();
+    Node supplyInlet = a.supplyInletNode();
+    Node demandInlet = a.demandInletNode();
+
+    EXPECT_FALSE(spm.setpointNode());
+    EXPECT_FALSE(spm.referenceNode());
+
+    EXPECT_TRUE(spm.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    EXPECT_FALSE(spm.referenceNode());
+
+    EXPECT_TRUE(spm.setReferenceNode(supplyInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+    EXPECT_FALSE(spm.addToNode(demandInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+    EXPECT_TRUE(spm.setReferenceNode(demandInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(demandInlet, spm.referenceNode().get());
+
+    EXPECT_TRUE(spm.setControlVariable("MinimumHumidityRatio"));
+    ASSERT_EQ(1, supplyOutlet.setpointManagers().size());
+    EXPECT_EQ(spm, supplyOutlet.setpointManagers().front());
+
+    // Add a SPM with the same control variable => the first one is removed
+    SetpointManagerSystemNodeResetHumidity spm2(m);
+    EXPECT_TRUE(spm2.setControlVariable("MinimumHumidityRatio"));
+    EXPECT_TRUE(spm2.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm2.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm2.setpointNode().get());
+    ASSERT_EQ(1, supplyOutlet.setpointManagers().size());
+    EXPECT_EQ(spm2, supplyOutlet.setpointManagers().front());
+
+    SetpointManagerSystemNodeResetHumidity spm3(m);
+    EXPECT_TRUE(spm3.setControlVariable("MaximumHumidityRatio"));
+    EXPECT_TRUE(spm3.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm3.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm3.setpointNode().get());
+    ASSERT_EQ(2, supplyOutlet.setpointManagers().size());
+  }
+
+  // PlantLoop: **NOT** allowed
+  {
+    SetpointManagerSystemNodeResetHumidity spm(m);
+    PlantLoop p(m);
+    Node supplyOutlet = p.supplyOutletNode();
+    Node supplyInlet = p.supplyInletNode();
+    Node demandInlet = p.demandInletNode();
+    std::vector<Node> testNodes{supplyOutlet, supplyInlet, demandInlet};
+
+    EXPECT_FALSE(spm.setpointNode());
+    EXPECT_FALSE(spm.referenceNode());
+
+    for (auto& node : testNodes) {
+      EXPECT_FALSE(spm.addToNode(node));
+      EXPECT_FALSE(spm.setReferenceNode(node));
+
+      EXPECT_FALSE(spm.setpointNode());
+      EXPECT_FALSE(spm.referenceNode());
+    }
+  }
+}
+
+TEST_F(ModelFixture, SetpointManagerSystemNodeResetHumidity_remove) {
+  Model m;
+  AirLoopHVAC a(m);
+  Node supplyOutlet = a.supplyOutletNode();
+
+  SetpointManagerSystemNodeResetHumidity spm(m);
+
+  EXPECT_TRUE(spm.setControlVariable("MinimumHumidityRatio"));
+
+  EXPECT_TRUE(spm.addToNode(supplyOutlet));
+
+  EXPECT_EQ(1, supplyOutlet.setpointManagers().size());
+  EXPECT_EQ(1, m.getModelObjects<SetpointManagerSystemNodeResetHumidity>().size());
+
+  spm.remove();
+
+  EXPECT_EQ(0, supplyOutlet.setpointManagers().size());
+  EXPECT_EQ(0, m.getModelObjects<SetpointManagerSystemNodeResetHumidity>().size());
+}
+
+TEST_F(ModelFixture, SetpointManagerSystemNodeResetHumidity_clone) {
+  Model m;
+  AirLoopHVAC a(m);
+  Node supplyOutlet = a.supplyOutletNode();
+  Node supplyInlet = a.supplyInletNode();
+
+  SetpointManagerSystemNodeResetHumidity spm(m);
+
+  EXPECT_TRUE(spm.setControlVariable("MinimumHumidityRatio"));
+  EXPECT_TRUE(spm.setSetpointatLowReferenceHumidityRatio(0.002));
+  EXPECT_TRUE(spm.setSetpointatHighReferenceHumidityRatio(0.001));
+  EXPECT_TRUE(spm.setLowReferenceHumidityRatio(0.004));
+  EXPECT_TRUE(spm.setHighReferenceHumidityRatio(0.003));
+
+  EXPECT_TRUE(spm.addToNode(supplyOutlet));
+  EXPECT_TRUE(spm.setReferenceNode(supplyInlet));
+  ASSERT_TRUE(spm.setpointNode());
+  EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+  ASSERT_TRUE(spm.referenceNode());
+  EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+  auto spmClone = spm.clone(m).cast<SetpointManagerSystemNodeResetHumidity>();
+  EXPECT_FALSE(spmClone.setpointNode());
+  EXPECT_FALSE(spmClone.referenceNode());
+
+  EXPECT_EQ(spm.controlVariable(), spmClone.controlVariable());
+  EXPECT_EQ(spm.setpointatLowReferenceHumidityRatio(), spmClone.setpointatLowReferenceHumidityRatio());
+  EXPECT_EQ(spm.setpointatHighReferenceHumidityRatio(), spmClone.setpointatHighReferenceHumidityRatio());
+  EXPECT_EQ(spm.lowReferenceHumidityRatio(), spmClone.lowReferenceHumidityRatio());
+  EXPECT_EQ(spm.highReferenceHumidityRatio(), spmClone.highReferenceHumidityRatio());
 }
--- src/model/test/SetpointManagerSystemNodeResetTemperature_GTest.cpp
+++ src/model/test/SetpointManagerSystemNodeResetTemperature_GTest.cpp
@@ -27,62 +27,222 @@
 *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************************************************************/
 
+#include <gtest/gtest.h>
+
 #include "ModelFixture.hpp"
 
 #include "../SetpointManagerSystemNodeResetTemperature.hpp"
 #include "../SetpointManagerSystemNodeResetTemperature_Impl.hpp"
 
-// TODO: Check the following class names against object getters and setters.
+#include "../Model.hpp"
 #include "../Node.hpp"
 #include "../Node_Impl.hpp"
+#include "../AirLoopHVAC.hpp"
+#include "../PlantLoop.hpp"
+#include "../CoilHeatingWater.hpp"
 
-#include "../Node.hpp"
-#include "../Node_Impl.hpp"
+#include "ModelFixture.hpp"
 
 using namespace openstudio;
 using namespace openstudio::model;
 
 TEST_F(ModelFixture, SetpointManagerSystemNodeResetTemperature_GettersSetters) {
   Model m;
-  // TODO: Check regular Ctor arguments
-  SetpointManagerSystemNodeResetTemperature setpointManagerSystemNodeResetTemperature(m);
+  SetpointManagerSystemNodeResetTemperature spm(m);
   // TODO: Or if a UniqueModelObject (and make sure _Impl is included)
-  // SetpointManagerSystemNodeResetTemperature setpointManagerSystemNodeResetTemperature = m.getUniqueModelObject<SetpointManagerSystemNodeResetTemperature>();
+  // SetpointManagerSystemNodeResetTemperature spm = m.getUniqueModelObject<SetpointManagerSystemNodeResetTemperature>();
 
-  setpointManagerSystemNodeResetTemperature.setName("My SetpointManagerSystemNodeResetTemperature");
+  spm.setName("My SetpointManagerSystemNodeResetTemperature");
 
   // Control Variable: Required String
-  EXPECT_TRUE(setpointManagerSystemNodeResetTemperature.setControlVariable("Temperature"));
-  EXPECT_EQ("Temperature", setpointManagerSystemNodeResetTemperature.controlVariable());
+  EXPECT_TRUE(spm.setControlVariable("Temperature"));
+  EXPECT_EQ("Temperature", spm.controlVariable());
   // Bad Value
-  EXPECT_FALSE(setpointManagerSystemNodeResetTemperature.setControlVariable("BADENUM"));
-  EXPECT_EQ("Temperature", setpointManagerSystemNodeResetTemperature.controlVariable());
+  EXPECT_FALSE(spm.setControlVariable("BADENUM"));
+  EXPECT_EQ("Temperature", spm.controlVariable());
 
   // Setpoint at Low Reference Temperature: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetTemperature.setSetpointatLowReferenceTemperature(0.4));
-  EXPECT_EQ(0.4, setpointManagerSystemNodeResetTemperature.setpointatLowReferenceTemperature());
+  EXPECT_TRUE(spm.setSetpointatLowReferenceTemperature(0.4));
+  EXPECT_EQ(0.4, spm.setpointatLowReferenceTemperature());
 
   // Setpoint at High Reference Temperature: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetTemperature.setSetpointatHighReferenceTemperature(0.5));
-  EXPECT_EQ(0.5, setpointManagerSystemNodeResetTemperature.setpointatHighReferenceTemperature());
+  EXPECT_TRUE(spm.setSetpointatHighReferenceTemperature(0.5));
+  EXPECT_EQ(0.5, spm.setpointatHighReferenceTemperature());
 
   // Low Reference Temperature: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetTemperature.setLowReferenceTemperature(0.6));
-  EXPECT_EQ(0.6, setpointManagerSystemNodeResetTemperature.lowReferenceTemperature());
+  EXPECT_TRUE(spm.setLowReferenceTemperature(0.6));
+  EXPECT_EQ(0.6, spm.lowReferenceTemperature());
 
   // High Reference Temperature: Required Double
-  EXPECT_TRUE(setpointManagerSystemNodeResetTemperature.setHighReferenceTemperature(0.7));
-  EXPECT_EQ(0.7, setpointManagerSystemNodeResetTemperature.highReferenceTemperature());
+  EXPECT_TRUE(spm.setHighReferenceTemperature(0.7));
+  EXPECT_EQ(0.7, spm.highReferenceTemperature());
+}
 
-  // Reference Node Name: Optional Object
-  boost::optional<Node> obj(m);
-  EXPECT_TRUE(setpointManagerSystemNodeResetTemperature.setReferenceNode(obj));
-  ASSERT_TRUE(setpointManagerSystemNodeResetTemperature.referenceNode());
-  EXPECT_EQ(obj, setpointManagerSystemNodeResetTemperature.referenceNode().get());
+TEST_F(ModelFixture, SetpointManagerSystemNodeResetTemperature_addToNode) {
 
-  // Setpoint Node or NodeList Name: Optional Object
-  boost::optional<Node> obj(m);
-  EXPECT_TRUE(setpointManagerSystemNodeResetTemperature.setSetpointNodeorNodeList(obj));
-  ASSERT_TRUE(setpointManagerSystemNodeResetTemperature.setpointNodeorNodeList());
-  EXPECT_EQ(obj, setpointManagerSystemNodeResetTemperature.setpointNodeorNodeList().get());
+  Model m;
+
+  // AirLoopHVAC
+  {
+    SetpointManagerSystemNodeResetTemperature spm(m);
+    AirLoopHVAC a(m);
+    Node supplyOutlet = a.supplyOutletNode();
+    Node supplyInlet = a.supplyInletNode();
+    Node demandInlet = a.demandInletNode();
+
+    EXPECT_FALSE(spm.setpointNode());
+    EXPECT_FALSE(spm.referenceNode());
+
+    EXPECT_TRUE(spm.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    EXPECT_FALSE(spm.referenceNode());
+
+    EXPECT_TRUE(spm.setReferenceNode(supplyInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+    EXPECT_FALSE(spm.addToNode(demandInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+    EXPECT_TRUE(spm.setReferenceNode(demandInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(demandInlet, spm.referenceNode().get());
+
+    spm.setControlVariable("MinimumTemperature");
+    ASSERT_EQ(1, supplyOutlet.setpointManagers().size());
+    EXPECT_EQ(spm, supplyOutlet.setpointManagers().front());
+
+    // Add a SPM with the same control variable => the first one is removed
+    SetpointManagerSystemNodeResetTemperature spm2(m);
+    spm2.setControlVariable("MinimumTemperature");
+    EXPECT_TRUE(spm2.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm2.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm2.setpointNode().get());
+    ASSERT_EQ(1, supplyOutlet.setpointManagers().size());
+    EXPECT_EQ(spm2, supplyOutlet.setpointManagers().front());
+
+    SetpointManagerSystemNodeResetTemperature spm3(m);
+    EXPECT_TRUE(spm3.setControlVariable("MaximumTemperature"));
+    EXPECT_TRUE(spm3.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm3.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm3.setpointNode().get());
+    ASSERT_EQ(2, supplyOutlet.setpointManagers().size());
+  }
+
+  // PlantLoop: allowed
+  {
+    SetpointManagerSystemNodeResetTemperature spm(m);
+    PlantLoop p(m);
+    Node supplyOutlet = p.supplyOutletNode();
+    Node supplyInlet = p.supplyInletNode();
+
+    // The SPM is allowed on the demand side if that node isn't on a branch
+    CoilHeatingWater c(m);
+    p.addDemandBranchForComponent(c);
+    Node demandInlet = c.waterInletModelObject()->cast<Node>();
+
+    EXPECT_FALSE(spm.setpointNode());
+    EXPECT_FALSE(spm.referenceNode());
+
+    EXPECT_TRUE(spm.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    EXPECT_FALSE(spm.referenceNode());
+
+    EXPECT_TRUE(spm.setReferenceNode(supplyInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+    EXPECT_FALSE(spm.addToNode(demandInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+    EXPECT_TRUE(spm.setReferenceNode(demandInlet));
+    ASSERT_TRUE(spm.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+    ASSERT_TRUE(spm.referenceNode());
+    EXPECT_EQ(demandInlet, spm.referenceNode().get());
+
+    spm.setControlVariable("MinimumTemperature");
+    ASSERT_EQ(1, supplyOutlet.setpointManagers().size());
+    EXPECT_EQ(spm, supplyOutlet.setpointManagers().front());
+
+    // Add a SPM with the same control variable => the first one is removed
+    SetpointManagerSystemNodeResetTemperature spm2(m);
+    spm2.setControlVariable("MinimumTemperature");
+    EXPECT_TRUE(spm2.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm2.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm2.setpointNode().get());
+    ASSERT_EQ(1, supplyOutlet.setpointManagers().size());
+    EXPECT_EQ(spm2, supplyOutlet.setpointManagers().front());
+
+    SetpointManagerSystemNodeResetTemperature spm3(m);
+    EXPECT_TRUE(spm3.setControlVariable("MaximumTemperature"));
+    EXPECT_TRUE(spm3.addToNode(supplyOutlet));
+    ASSERT_TRUE(spm3.setpointNode());
+    EXPECT_EQ(supplyOutlet, spm3.setpointNode().get());
+    ASSERT_EQ(2, supplyOutlet.setpointManagers().size());
+  }
+}
+
+TEST_F(ModelFixture, SetpointManagerSystemNodeResetTemperature_remove) {
+  Model m;
+  AirLoopHVAC a(m);
+  Node supplyOutlet = a.supplyOutletNode();
+
+  SetpointManagerSystemNodeResetTemperature spm(m);
+
+  EXPECT_TRUE(spm.addToNode(supplyOutlet));
+
+  EXPECT_EQ(1, supplyOutlet.setpointManagers().size());
+  EXPECT_EQ(1, m.getModelObjects<SetpointManagerSystemNodeResetTemperature>().size());
+
+  spm.remove();
+
+  EXPECT_EQ(0, supplyOutlet.setpointManagers().size());
+  EXPECT_EQ(0, m.getModelObjects<SetpointManagerSystemNodeResetTemperature>().size());
+}
+
+TEST_F(ModelFixture, SetpointManagerSystemNodeResetTemperature_clone) {
+  Model m;
+  AirLoopHVAC a(m);
+  Node supplyOutlet = a.supplyOutletNode();
+  Node supplyInlet = a.supplyInletNode();
+
+  SetpointManagerSystemNodeResetTemperature spm(m);
+
+  EXPECT_TRUE(spm.setControlVariable("MinimumTemperature"));
+  EXPECT_TRUE(spm.setSetpointatLowReferenceTemperature(80.0));
+  EXPECT_TRUE(spm.setSetpointatHighReferenceTemperature(60.0));
+  EXPECT_TRUE(spm.setLowReferenceTemperature(-10.0));
+  EXPECT_TRUE(spm.setHighReferenceTemperature(10.0));
+
+  EXPECT_TRUE(spm.addToNode(supplyOutlet));
+  EXPECT_TRUE(spm.setReferenceNode(supplyInlet));
+  ASSERT_TRUE(spm.setpointNode());
+  EXPECT_EQ(supplyOutlet, spm.setpointNode().get());
+  ASSERT_TRUE(spm.referenceNode());
+  EXPECT_EQ(supplyInlet, spm.referenceNode().get());
+
+  auto spmClone = spm.clone(m).cast<SetpointManagerSystemNodeResetTemperature>();
+  EXPECT_FALSE(spmClone.setpointNode());
+  EXPECT_FALSE(spmClone.referenceNode());
+
+  EXPECT_EQ(spm.controlVariable(), spmClone.controlVariable());
+  EXPECT_EQ(spm.setpointatLowReferenceTemperature(), spmClone.setpointatLowReferenceTemperature());
+  EXPECT_EQ(spm.setpointatHighReferenceTemperature(), spmClone.setpointatHighReferenceTemperature());
+  EXPECT_EQ(spm.lowReferenceTemperature(), spmClone.lowReferenceTemperature());
+  EXPECT_EQ(spm.highReferenceTemperature(), spmClone.highReferenceTemperature());
 }
