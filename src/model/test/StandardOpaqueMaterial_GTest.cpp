6e934db474ba50d92825e31aef34fae619eabae3
Joe Robertson <joseph.robertson@nrel.gov>
2022-07-11 11:41:38 -0700

    Fix up tests a bit.

--- src/model/test/StandardOpaqueMaterial_GTest.cpp
+++ src/model/test/StandardOpaqueMaterial_GTest.cpp
@@ -42,28 +42,46 @@ using namespace openstudio::model;
 TEST_F(ModelFixture, StandardOpaqueMaterial_StandardOpaqueMaterial) {
   Model model;
 
+  std::string roughness = "Smooth";
+  double thickness = 0.1;
+  double conductivity = 0.1;
+  double density = 0.1;
+  double specificHeat = 1400;
+
+  double conductance = conductivity / thickness;
+  double resistivity = 1.0 / conductivity;
+  double resistance = resistivity * thickness;
+
+  double thermalAbsorptance = 0.9;
+  double solarAbsorptance = 0.7;
+  double visibleAbsorptance = 0.7;
+
+  double thermalReflectance = 1.0 - thermalAbsorptance;
+  double solarReflectance = 1.0 - solarAbsorptance;
+  double visibleReflectance = 1.0 - visibleAbsorptance;
+
   StandardOpaqueMaterial material(model);
 
-  EXPECT_EQ(0.1, material.thermalConductivity());
-  EXPECT_EQ(0.1 / 0.1, material.thermalConductance());
-  EXPECT_EQ(1.0 / 0.1, material.thermalResistivity());
-  EXPECT_EQ((1.0 / 0.1) * 0.1, material.thermalResistance());
+  EXPECT_EQ(conductivity, material.thermalConductivity());
+  EXPECT_EQ(conductance, material.thermalConductance());
+  EXPECT_EQ(resistivity, material.thermalResistivity());
+  EXPECT_EQ(resistance, material.thermalResistance());
   ASSERT_TRUE(material.thermalReflectance());
-  EXPECT_EQ(0.1, material.thermalReflectance().get());
+  EXPECT_EQ(thermalReflectance, material.thermalReflectance().get());
   ASSERT_TRUE(material.solarReflectance());
-  EXPECT_EQ(0.3, material.solarReflectance().get());
+  EXPECT_EQ(solarReflectance, material.solarReflectance().get());
   ASSERT_TRUE(material.visibleReflectance());
-  EXPECT_EQ(0.3, material.visibleReflectance().get());
-  EXPECT_EQ("Smooth", material.roughness());
-  EXPECT_EQ(0.1, material.thickness());
-  EXPECT_EQ(0.1, material.conductivity());
-  EXPECT_EQ(0.1, material.density());
-  EXPECT_EQ(1400.0, material.specificHeat());
-  EXPECT_EQ(0.9, material.thermalAbsorptance());
+  EXPECT_EQ(visibleReflectance, material.visibleReflectance().get());
+  EXPECT_EQ(roughness, material.roughness());
+  EXPECT_EQ(thickness, material.thickness());
+  EXPECT_EQ(conductivity, material.conductivity());
+  EXPECT_EQ(density, material.density());
+  EXPECT_EQ(specificHeat, material.specificHeat());
+  EXPECT_EQ(thermalAbsorptance, material.thermalAbsorptance());
   EXPECT_TRUE(material.isThermalAbsorptanceDefaulted());
-  EXPECT_EQ(0.7, material.solarAbsorptance());
+  EXPECT_EQ(solarAbsorptance, material.solarAbsorptance());
   EXPECT_TRUE(material.isSolarAbsorptanceDefaulted());
-  EXPECT_EQ(0.7, material.visibleAbsorptance());
+  EXPECT_EQ(visibleAbsorptance, material.visibleAbsorptance());
   EXPECT_TRUE(material.isVisibleAbsorptanceDefaulted());
   EXPECT_FALSE(material.heatCapacity());
 }
@@ -71,99 +89,177 @@ TEST_F(ModelFixture, StandardOpaqueMaterial_StandardOpaqueMaterial) {
 TEST_F(ModelFixture, StandardOpaqueMaterial_Ctor) {
   Model model;
 
-  {
-    StandardOpaqueMaterial material(model, "VerySmooth", 1, 2, 3, 900);
+  std::string roughness = "VerySmooth";
+  double thickness = 1.0;
+  double conductivity = 2.0;
+  double density = 3.0;
+  double specificHeat = 900.0;
 
-    EXPECT_EQ(2, material.thermalConductivity());
-    EXPECT_EQ(2 / 1, material.thermalConductance());
-    EXPECT_EQ(1.0 / 2, material.thermalResistivity());
-    EXPECT_EQ((1.0 / 2) * 1, material.thermalResistance());
-    EXPECT_EQ("Smooth", material.roughness());
-    EXPECT_EQ(1, material.thickness());
-    EXPECT_EQ(2, material.conductivity());
-    EXPECT_EQ(3, material.density());
-    EXPECT_EQ(900, material.specificHeat());
-  }
+  double conductance = conductivity / thickness;
+  double resistivity = 1.0 / conductivity;
+  double resistance = resistivity * thickness;
+
+  StandardOpaqueMaterial material(model, roughness, thickness, conductivity, density, specificHeat);
+
+  EXPECT_EQ(conductivity, material.thermalConductivity());
+  EXPECT_EQ(conductance, material.thermalConductance());
+  EXPECT_EQ(resistivity, material.thermalResistivity());
+  EXPECT_EQ(resistance, material.thermalResistance());
+  EXPECT_EQ(roughness, material.roughness());
+  EXPECT_EQ(thickness, material.thickness());
+  EXPECT_EQ(conductivity, material.conductivity());
+  EXPECT_EQ(density, material.density());
+  EXPECT_EQ(specificHeat, material.specificHeat());
 }
 
 TEST_F(ModelFixture, StandardOpaqueMaterial_SetGetFields) {
   Model model;
 
+  std::string roughness = "Smooth";
+  double thickness = 0.1;
+  double conductivity = 0.1;
+  double density = 0.1;
+  double specificHeat = 1400;
+
+  double conductance = conductivity / thickness;
+  double resistivity = 1.0 / conductivity;
+  double resistance = resistivity * thickness;
+
+  double thermalAbsorptance = 0.9;
+  double solarAbsorptance = 0.7;
+  double visibleAbsorptance = 0.7;
+
+  double thermalReflectance = 1.0 - thermalAbsorptance;
+  double solarReflectance = 1.0 - solarAbsorptance;
+  double visibleReflectance = 1.0 - visibleAbsorptance;
+
   StandardOpaqueMaterial material(model);
 
-  EXPECT_TRUE(material.setThermalConductivity(3));
-  EXPECT_EQ(3.0, material.thermalConductivity());
-  EXPECT_EQ(3.0 / 0.1, material.thermalConductance());
-  EXPECT_EQ(1.0 / 3.0, material.thermalResistivity());
-  EXPECT_EQ((1.0 / 3.0) * 0.1, material.thermalResistance());
-  EXPECT_EQ(3.0, material.conductivity());
-  EXPECT_EQ(0.1, material.thickness());
+  conductivity = 3.0;
+  conductance = conductivity / thickness;
+  resistivity = 1.0 / conductivity;
+  resistance = resistivity * thickness;
 
-  EXPECT_TRUE(material.setThermalConductance(4));
-  EXPECT_EQ(3.0, material.thermalConductivity());
-  EXPECT_EQ(3.0 / 0.1, material.thermalConductance());
-  EXPECT_EQ(1.0 / 3.0, material.thermalResistivity());
-  EXPECT_EQ((1.0 / 3.0) * (3.0 / 4.0), material.thermalResistance());
-  EXPECT_EQ(3.0, material.conductivity());
-  EXPECT_EQ(3.0 / 4.0, material.thickness());
+  EXPECT_TRUE(material.setThermalConductivity(conductivity));
+  EXPECT_EQ(conductivity, material.thermalConductivity());
+  EXPECT_EQ(conductance, material.thermalConductance());
+  EXPECT_EQ(resistivity, material.thermalResistivity());
+  EXPECT_EQ(resistance, material.thermalResistance());
+  EXPECT_EQ(conductivity, material.conductivity());
+  EXPECT_EQ(thickness, material.thickness());
 
-  EXPECT_TRUE(material.setThermalResistivity(5));
-  EXPECT_EQ(1.0 / 5.0, material.thermalConductivity());
-  EXPECT_EQ((1.0 / 5.0) / (3.0 / 4.0), material.thermalConductance());
-  EXPECT_EQ(1.0 / (1.0 / 5), material.thermalResistivity());
-  EXPECT_EQ((1.0 / (1.0 / 5)) * (3.0 / 4.0), material.thermalResistance());
-  EXPECT_EQ(1.0 / 5.0, material.conductivity());
-  EXPECT_EQ(3.0 / 4.0, material.thickness());
+  conductance = 4.0;
+  thickness = conductivity / conductance;
+  conductance = conductivity / thickness;
+  resistivity = 1.0 / conductivity;
+  resistance = resistivity * thickness;
 
-  EXPECT_TRUE(material.setThermalResistance(6));
-  EXPECT_EQ(1.0 / 5.0, material.thermalConductivity());
-  EXPECT_EQ((1.0 / 5.0) / (6.0 / (1.0 / (1.0 / 5.0))), material.thermalConductance());
-  EXPECT_EQ(1.0 / (1.0 / 5.0), material.thermalResistivity());
-  EXPECT_EQ((1.0 / (1.0 / 5.0)) * (3.0 / 4.0), material.thermalResistance());
-  EXPECT_EQ(1.0 / 5.0, material.conductivity());
-  EXPECT_EQ(6.0 / (1.0 / (1.0 / 5.0)), material.thickness());
+  EXPECT_TRUE(material.setThermalConductance(conductance));
+  EXPECT_EQ(conductivity, material.thermalConductivity());
+  EXPECT_EQ(conductance, material.thermalConductance());
+  EXPECT_EQ(resistivity, material.thermalResistivity());
+  EXPECT_EQ(resistance, material.thermalResistance());
+  EXPECT_EQ(conductivity, material.conductivity());
+  EXPECT_EQ(thickness, material.thickness());
 
-  EXPECT_TRUE(material.setThermalAbsorptance(0.2));
+  resistivity = 5.0;
+  conductivity = 1.0 / resistivity;
+  conductance = conductivity / thickness;
+  resistivity = 1.0 / conductivity;
+  resistance = resistivity * thickness;
+
+  EXPECT_TRUE(material.setThermalResistivity(resistivity));
+  EXPECT_EQ(conductivity, material.thermalConductivity());
+  EXPECT_EQ(conductance, material.thermalConductance());
+  EXPECT_EQ(resistivity, material.thermalResistivity());
+  EXPECT_EQ(resistance, material.thermalResistance());
+  EXPECT_EQ(conductivity, material.conductivity());
+  EXPECT_EQ(thickness, material.thickness());
+
+  resistance = 6.0;
+  thickness = resistance / resistivity;
+  conductance = conductivity / thickness;
+  resistivity = 1.0 / conductivity;
+  resistance = resistivity * thickness;
+
+  EXPECT_TRUE(material.setThermalResistance(resistance));
+  EXPECT_EQ(conductivity, material.thermalConductivity());
+  EXPECT_EQ(conductance, material.thermalConductance());
+  EXPECT_EQ(resistivity, material.thermalResistivity());
+  EXPECT_EQ(resistance, material.thermalResistance());
+  EXPECT_EQ(conductivity, material.conductivity());
+  EXPECT_EQ(thickness, material.thickness());
+
+  thermalAbsorptance = 0.2;
+  thermalReflectance = 1.0 - thermalAbsorptance;
+
+  EXPECT_TRUE(material.setThermalAbsorptance(thermalAbsorptance));
   EXPECT_FALSE(material.isThermalAbsorptanceDefaulted());
-  EXPECT_EQ(0.2, material.thermalAbsorptance());
-  EXPECT_EQ(0.8, material.thermalReflectance());
+  EXPECT_EQ(thermalAbsorptance, material.thermalAbsorptance());
+  EXPECT_EQ(thermalReflectance, material.thermalReflectance());
 
-  EXPECT_TRUE(material.setThermalReflectance(0.3));
-  EXPECT_EQ(0.7, material.thermalAbsorptance());
-  EXPECT_EQ(0.3, material.thermalReflectance());
+  thermalReflectance = 0.3;
+  thermalAbsorptance = 1.0
+                       - thermalReflectance
 
-  EXPECT_TRUE(material.setSolarAbsorptance(0.4));
+                         EXPECT_TRUE(material.setThermalReflectance(thermalReflectance));
+  EXPECT_EQ(thermalAbsorptance, material.thermalAbsorptance());
+  EXPECT_EQ(thermalReflectance, material.thermalReflectance());
+
+  solarAbsorptance = 0.4;
+  solarReflectance = 1.0 - solarAbsorptance;
+
+  EXPECT_TRUE(material.setSolarAbsorptance(solarAbsorptance));
   EXPECT_FALSE(material.isSolarAbsorptanceDefaulted());
-  EXPECT_EQ(0.4, material.solarAbsorptance());
-  EXPECT_EQ(0.6, material.solarReflectance());
+  EXPECT_EQ(solarAbsorptance, material.solarAbsorptance());
+  EXPECT_EQ(solarReflectance, material.solarReflectance());
 
-  EXPECT_TRUE(material.setSolarReflectance(0.6));
-  EXPECT_EQ(0.4, material.solarAbsorptance());
-  EXPECT_EQ(0.6, material.solarReflectance());
+  solarReflectance = 0.6;
+  solarAbsorptance = 1.0 - solarReflectance;
 
-  EXPECT_TRUE(material.setVisibleAbsorptance(0.7));
+  EXPECT_TRUE(material.setSolarReflectance(solarReflectance));
+  EXPECT_EQ(solarAbsorptance, material.solarAbsorptance());
+  EXPECT_EQ(solarReflectance, material.solarReflectance());
+
+  visibleAbsorptance = 0.7;
+  visibleReflectance = 1.0 - visibleAbsorptance;
+
+  EXPECT_TRUE(material.setVisibleAbsorptance(visibleAbsorptance));
   EXPECT_FALSE(material.isVisibleAbsorptanceDefaulted());
-  EXPECT_EQ(0.7, material.visibleAbsorptance());
-  EXPECT_EQ(0.3, material.visibleReflectance());
+  EXPECT_EQ(visibleAbsorptance, material.visibleAbsorptance());
+  EXPECT_EQ(visibleReflectance, material.visibleReflectance());
 
-  EXPECT_TRUE(material.setVisibleReflectance(0.8));
-  EXPECT_EQ(0.2, material.visibleAbsorptance());
-  EXPECT_EQ(0.8, material.visibleReflectance());
+  visibleReflectance = 0.8;
+  visibleAbsorptance = 1.0 - visibleReflectance;
 
-  EXPECT_TRUE(material.setRoughness("Rough"));
-  EXPECT_EQ("Rough", material.roughness());
+  EXPECT_TRUE(material.setVisibleReflectance(visibleReflectance));
+  EXPECT_EQ(visibleAbsorptance, material.visibleAbsorptance());
+  EXPECT_EQ(visibleReflectance, material.visibleReflectance());
 
-  EXPECT_TRUE(material.setThickness(4));
-  EXPECT_EQ(4, material.thickness());
+  roughness = "Rough";
 
-  EXPECT_TRUE(material.setConductivity(5));
-  EXPECT_EQ(5, material.conductivity());
+  EXPECT_TRUE(material.setRoughness(roughness));
+  EXPECT_EQ(roughness, material.roughness());
 
-  EXPECT_TRUE(material.setDensity(6));
-  EXPECT_EQ(6, material.density());
+  thickness = 4.0;
 
-  EXPECT_TRUE(material.setSpecificHeat(700));
-  EXPECT_EQ(700, material.specificHeat());
+  EXPECT_TRUE(material.setThickness(thickness));
+  EXPECT_EQ(thickness, material.thickness());
+
+  conductivity = 5.0;
+
+  EXPECT_TRUE(material.setConductivity(conductivity));
+  EXPECT_EQ(conductivity, material.conductivity());
+
+  density = 6.0;
+
+  EXPECT_TRUE(material.setDensity(density));
+  EXPECT_EQ(density, material.density());
+
+  specificHeat = 700.0;
+
+  EXPECT_TRUE(material.setSpecificHeat(specificHeat));
+  EXPECT_EQ(specificHeat, material.specificHeat());
 
   material.resetThermalAbsorptance();
   EXPECT_TRUE(material.isThermalAbsorptanceDefaulted());
