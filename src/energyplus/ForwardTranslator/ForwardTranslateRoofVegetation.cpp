ee675d23ef88c082b8f6408f0dba9e7ea2a34b1b
Joe Robertson <joseph.robertson@nrel.gov>
2022-07-19 12:06:22 -0700

    More removes.

--- src/energyplus/ForwardTranslator/ForwardTranslateRoofVegetation.cpp
+++ src/energyplus/ForwardTranslator/ForwardTranslateRoofVegetation.cpp
@@ -29,12 +29,6 @@
 
 #include "../ForwardTranslator.hpp"
 #include "../../model/RoofVegetation.hpp"
-#include "../../model/MaterialPropertyMoisturePenetrationDepthSettings.hpp"
-#include "../../model/MaterialPropertyMoisturePenetrationDepthSettings_Impl.hpp"
-#include "../../model/MaterialPropertyPhaseChange.hpp"
-#include "../../model/MaterialPropertyPhaseChange_Impl.hpp"
-#include "../../model/MaterialPropertyPhaseChangeHysteresis.hpp"
-#include "../../model/MaterialPropertyPhaseChangeHysteresis_Impl.hpp"
 #include <utilities/idd/OS_Material_RoofVegetation_FieldEnums.hxx>
 
 #include <utilities/idd/Material_RoofVegetation_FieldEnums.hxx>
