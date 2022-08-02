0853a80c5a89f1cef62e50e63038d4b7cc4bf80f
Joe Robertson <joseph.robertson@nrel.gov>
2022-08-02 16:38:43 -0700

    Typo.

--- src/osversion/test/VersionTranslator_GTest.cpp
+++ src/osversion/test/VersionTranslator_GTest.cpp
@@ -1833,7 +1833,7 @@ TEST_F(OSVersionFixture, update_3_4_0_to_3_4_1_AirWallMaterial) {
   openstudio::path outPath = resourcesPath() / toPath("osversion/3_4_1/test_vt_AirWallMaterial_updated.osm");
   model->save(outPath, true);
 
-  ASSERT_EQ(1u, model->numbObjects());
+  ASSERT_EQ(1u, model->numObjects());
 
   std::vector<WorkspaceObject> constrs = model->getObjectsByType("OS:Construction");
   ASSERT_EQ(0u, constrs.size());
