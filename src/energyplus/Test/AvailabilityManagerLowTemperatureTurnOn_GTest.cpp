119b57626cdcafbe9e3c6e2c63396e873476c3bd
Julien Marrec <julien.marrec@gmail.com>
2022-09-01 20:27:41 +0200

    fix FT tests

--- src/energyplus/Test/AvailabilityManagerLowTemperatureTurnOn_GTest.cpp
+++ src/energyplus/Test/AvailabilityManagerLowTemperatureTurnOn_GTest.cpp
@@ -58,7 +58,7 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_AvailabilityManagerLowTemperatureTur
 
   // Assign it to a plant loop
   PlantLoop p(m);
-  p.setAvailabilityManager(avm);
+  p.addAvailabilityManager(avm);
 
   // ForwardTranslate
   ForwardTranslator forwardTranslator;
@@ -88,7 +88,7 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_AvailabilityManagerLowTemperatureTur
 
   // Assign it to a plant loop (otherwise it's purely not translated anyways...)
   PlantLoop p(m);
-  p.setAvailabilityManager(avm);
+  p.addAvailabilityManager(avm);
 
   // ForwardTranslate
   ForwardTranslator forwardTranslator;
@@ -105,7 +105,7 @@ TEST_F(EnergyPlusFixture, ForwardTranslator_AvailabilityManagerLowTemperatureTur
 
   // Assign it to a plant loop
   PlantLoop p(m);
-  ASSERT_TRUE(p.setAvailabilityManager(avm));
+  ASSERT_TRUE(p.addAvailabilityManager(avm));
 
   // Assign it to an Air loop
   AirLoopHVAC a(m);
