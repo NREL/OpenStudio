a8ad627faa5f573d0b9b5f527916913f0a62bd5c
Julien Marrec <julien.marrec@gmail.com>
2022-11-04 22:01:20 +0100

    Error: Was saving the IDF inside the StepType loop

--- src/workflow/OSWorkflow.cpp
+++ src/workflow/OSWorkflow.cpp
@@ -1,6 +1,4 @@
 #include "./OSWorkflow.hpp"
-#include "../utilities/filetypes/WorkflowStep.hpp"
-#include "../utilities/bcl/BCLMeasure.hpp"
 
 #include "../osversion/VersionTranslator.hpp"
 #include "../measure/OSMeasure.hpp"
@@ -11,10 +9,13 @@
 #include "../measure/OSRunner.hpp"
 #include "../model/Model.hpp"
 #include "../model/Model_Impl.hpp"
+#include "../utilities/filetypes/WorkflowStep.hpp"
+#include "../utilities/bcl/BCLMeasure.hpp"
 #include "../utilities/idf/Workspace.hpp"
 #include "../utilities/data/Variant.hpp"
+#include "../utilities/core/Filesystem.hpp"
+#include "../utilities/core/Logger.hpp"
 #include "../energyplus/ForwardTranslator.hpp"
-#include "utilities/core/Logger.hpp"
 
 #include <fmt/format.h>
 #include <fmt/ostream.h>
@@ -133,6 +134,8 @@ void OSWorkflow::run() {
   boost::optional<openstudio::Workspace> workspace_;
 
   auto runDir = workflowJSON.absoluteRunDir();
+  openstudio::filesystem::remove_all(runDir);
+  openstudio::filesystem::create_directory(runDir);
 
   // 2. determine ruby or python
   // 3. import measure.(py|rb)
@@ -147,10 +150,10 @@ void OSWorkflow::run() {
   // TODO: need to merge workflowJSON flags with flags from command line (eg: FT options)
   // TODO: need to modify utilities/RunOptions.hpp instead of duplicating some of that work in workflow
 
-  for (const openstudio::MeasureType stepType : {openstudio::MeasureType::ModelMeasure, openstudio::MeasureType::EnergyPlusMeasure}) {
-    const auto modelSteps = workflowJSON.getMeasureSteps(stepType);
+  for (const auto stepType : {MeasureType::ModelMeasure, MeasureType::EnergyPlusMeasure}) {
     if (stepType == MeasureType::EnergyPlusMeasure) {
-      model.save(runDir / "in.osm");
+      // Save final Model
+      model.save(runDir / "in.osm", true);
       openstudio::energyplus::ForwardTranslator ft;
       auto& ftOptions = runOptions.ft_options;
       ft.setKeepRunControlSpecialDays(ftOptions.runcontrolspecialdays);
@@ -162,6 +165,7 @@ void OSWorkflow::run() {
       workspace_ = ft.translateModel(model);
     }
 
+    const auto modelSteps = workflowJSON.getMeasureSteps(stepType);
     for (const auto& step : modelSteps) {
       unsigned stepIndex = workflowJSON.currentStepIndex();
       fmt::print("\n\nRunning step {}\n", stepIndex);
@@ -371,8 +375,9 @@ spec.loader.exec_module(module)
         }
       }
     }  // End for (const auto& step : modelSteps)
+  }    // End for StepType
 
-    workspace_->save(runDir / "in.idf");
-  }
+  // Save final IDF
+  workspace_->save(runDir / "in.idf");
 }
 }  // namespace openstudio
