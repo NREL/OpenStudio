e56cf1baea40164ba28f7a08fb25cc85fa9ae0d6
Julien Marrec <julien.marrec@gmail.com>
2022-11-08 20:58:25 +0100

    Update fmt to 9.1.0 (going to need some stuff from it, like chrono)

--- ConanInstall.cmake
+++ ConanInstall.cmake
@@ -172,7 +172,7 @@ if(NOT CONAN_OPENSTUDIO_ALREADY_RUN)
     "jsoncpp/1.9.5#536d080aa154e5853332339bf576747c"
     "minizip/1.2.12#0b5296887a2558500d0323c6c94c8d02" # This depends on zlib, and basically patches it
     "zlib/1.2.12#3b9e037ae1c615d045a06c67d88491ae" # Also needed, so we can find zlib.h and co (+ pinning exactly is good)
-    "fmt/8.1.1#b3e969f8561a85087bd0365c09bbf4fb"
+    "fmt/9.1.0#811e918ca4b4e0b9ddd6d5a2883efa82"
     "sqlite3/3.38.5#010911927ce1889b5cf824f45e7cd3d2"
     "cpprestsdk/2.10.18#df2f6ac88e47cadd9c9e8e0971e00d89"
     "websocketpp/0.8.2#3fd704c4c5388d9c08b11af86f79f616"
