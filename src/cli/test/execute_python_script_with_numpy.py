7fd28c896cba0117687bab675e5dc7882facaa79
Julien Marrec <julien.marrec@gmail.com>
2023-04-26 14:43:38 +0200

    Deal with Ubuntu CIs which have the numpy package in `/usr/local/lib/python3.8/dist-packages/`

--- src/cli/test/execute_python_script_with_numpy.py
+++ src/cli/test/execute_python_script_with_numpy.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 def test_import_numpy():
     """Common test function."""
+    print(f"{sys.path=}")
     import numpy as np
 
     a = np.array([0, 1, 2, 3])
@@ -14,8 +15,17 @@ def test_import_numpy():
 
 def test_with_sys_path_explicit(std_lib: Path):
     """Explicitly insert site-packages and lib-dynload in sys.path."""
-    sys.path.insert(0, str(std_lib / "site-packages"))
-    sys.path.insert(0, str(std_lib / "lib-dynload"))
+    site_pack = std_lib / "site-packages"
+    if site_pack.exists():
+        sys.path.insert(0, str(site_pack))
+    # Only for unix
+    dynload = std_lib / "lib-dynload"
+    if dynload.exists():
+        sys.path.insert(0, str(dynload))
+    # Only for debian
+    dist_pack = std_lib.parent.parent / 'local/lib/python3.8/dist-packages'
+    if dist_pack.exists():
+        sys.path.insert(0, str(dist_pack))
 
     test_import_numpy()
 
@@ -28,7 +38,7 @@ def test_with_pythonpath_pythonhome():
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         std_lib = Path(sys.argv[1])
-        print(std_lib)
+        print("Passed {std_lib=}")
 
         if std_lib.is_dir():
             test_with_sys_path_explicit(std_lib)
