bf73c82e3abba2518851cef6cb31cac35707f54e
Joe Robertson <joseph.robertson@nrel.gov>
2023-08-29 15:56:13 -0700

    Add new model test for load profile plant.

--- src/model/test/LoadProfilePlant_GTest.cpp
+++ src/model/test/LoadProfilePlant_GTest.cpp
@@ -12,3 +12,39 @@
 
 using namespace openstudio;
 using namespace openstudio::model;
+
+TEST_F(ModelFixture, LoadProfilePlant_LoadProfilePlant) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+  ASSERT_EXIT(
+    {
+      Model m;
+      LoadProfilePlant lpp(m);
+
+      exit(0);
+    },
+    ::testing::ExitedWithCode(0), "");
+
+  Model m;
+  LoadProfilePlant lpp(m);
+
+  EXPECT_EQ(0.002, lpp.peakFlowRate());
+  EXPECT_EQ("Water", lpp.plantLoopFluidType());
+  EXPECT_EQ(5.0, lpp.degreeofSubCooling());
+  EXPECT_EQ(20.0, lpp.degreeofLoopSubCooling());
+}
+
+TEST_F(ModelFixture, LoadProfilePlant_GettersSetters) {
+  Model m;
+  LoadProfilePlant lpp(m);
+
+  EXPECT_TRUE(lpp.setPeakFlowRate(0.005));
+  EXPECT_TRUE(lpp.setPlantLoopFluidType("Steam"));
+  EXPECT_TRUE(lpp.setDegreeofSubCooling(6.0));
+  EXPECT_TRUE(lpp.setDegreeofLoopSubCooling(21.0));
+
+  EXPECT_EQ(0.005, lpp.peakFlowRate());
+  EXPECT_EQ("Steam", lpp.plantLoopFluidType());
+  EXPECT_EQ(6.0, lpp.degreeofSubCooling());
+  EXPECT_EQ(21.0, lpp.degreeofLoopSubCooling());
+}
