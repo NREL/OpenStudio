e69621fb89eeb29e6188ac12a9786231127671d8
Joe Robertson <joseph.robertson@nrel.gov>
2024-02-23 13:05:06 -0700

    Formatting.

--- src/osversion/VersionTranslator.cpp
+++ src/osversion/VersionTranslator.cpp
@@ -180,7 +180,7 @@ namespace osversion {
       VersionString("2.7.1"),  VersionString("2.7.2"),  VersionString("2.8.0"),  VersionString("2.8.1"),  VersionString("2.9.0"),
       VersionString("2.9.1"),  VersionString("3.0.0"),  VersionString("3.0.1"),  VersionString("3.1.0"),  VersionString("3.2.0"),
       VersionString("3.2.1"),  VersionString("3.3.0"),  VersionString("3.4.0"),  VersionString("3.5.0"),  VersionString("3.5.1"),
-      VersionString("3.6.0"),  VersionString("3.6.1"), VersionString("3.7.0"),
+      VersionString("3.6.0"),  VersionString("3.6.1"),  VersionString("3.7.0"),
       // Note: do **not** include the **current** version in m_startVersions, stop at the previous release
       //VersionString("3.8.0"),
     };
