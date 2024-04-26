00eb71c9aa9e74299415de75a3730eab94c66313
Julien Marrec <julien.marrec@gmail.com>
2024-04-26 18:08:46 +0200

    Extend testing

---
+++ src/cli/test/minitest_ok.rb
@@ -0,0 +1,8 @@
+require 'minitest'
+require 'minitest/autorun'
+
+class MyTest < Minitest::Test
+  def test_should_pass
+    assert 0 == 0
+  end
+end
--- src/cli/test/run_minitest_fail.py
+++ src/cli/test/run_minitest_fail.py
@@ -1,9 +1,40 @@
 import argparse
 import subprocess
 from pathlib import Path
+from typing import Optional
 
 THIS_DIR = Path(__file__).parent.absolute()
-MINITEST_FILE = THIS_DIR / "minitest_fail.rb"
+MINITEST_FAIL_FILE = THIS_DIR / "minitest_fail.rb"
+MINITEST_OK_FILE = THIS_DIR / "minitest_ok.rb"
+AT_EXIT_FILE = THIS_DIR / "at_exit.rb"
+ALL_FILES = [MINITEST_FAIL_FILE, MINITEST_OK_FILE, AT_EXIT_FILE]
+
+
+def run_file(
+    ruby_or_cli_path: str,
+    filepath: Path,
+    expected_return_code: int,
+    expected_stdout_message: Optional[str],
+    verbose: bool = True,
+):
+
+    command = [str(ruby_or_cli_path), str(filepath)]
+
+    if verbose:
+        print(f"Running: `{' '.join(command)}`")
+
+    r = subprocess.run(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8", universal_newlines=True
+    )
+    ret_code = r.returncode
+    if verbose:
+        print(f"return code: {ret_code}")
+        print(f"stdout:\n```\n{r.stdout}\n```")
+        print(f"stderr:\n```\n{r.stderr}\n```")
+
+    assert ret_code == expected_return_code, f"Expected the return code to be {expected_return_code}, got {ret_code}"
+    if expected_stdout_message is not None:
+        assert expected_stdout_message in r.stdout, f"stdout appears to be missing the minitest output.\n\n{r.stdout}"
 
 
 if __name__ == "__main__":
@@ -11,19 +42,33 @@ if __name__ == "__main__":
     parser.add_argument("ruby_or_cli_path", type=str, help="Path to the OS CLI or the System Ruby")
     args = parser.parse_args()
 
-    if not MINITEST_FILE.is_file():
-        raise ValueError(f"{MINITEST_FILE} does not exist")
+    for fp in ALL_FILES:
+        if not fp.is_file():
+            raise ValueError(f"{fp} does not exist")
 
-    command = [str(args.ruby_or_cli_path), str(MINITEST_FILE)]
-
-    print(f"Running: `{' '.join(command)}`")
-    r = subprocess.run(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8", universal_newlines=True
+    run_file(
+        ruby_or_cli_path=args.ruby_or_cli_path,
+        filepath=AT_EXIT_FILE,
+        expected_return_code=12,
+        expected_stdout_message=None,
     )
-    ret_code = r.returncode
-    print(f"return code: {ret_code}")
-    print(f"stdout:\n```\n{r.stdout}\n```")
-    print(f"stderr:\n```\n{r.stderr}\n```")
 
-    assert ret_code == 1, f"Expected the return code to be 1, got {ret_code}"
-    assert "1 failures" in r.stdout, f"stdout appears to be missing the minitest output.\n\n{r.stdout}"
+    run_file(
+        ruby_or_cli_path=args.ruby_or_cli_path,
+        filepath=MINITEST_OK_FILE,
+        expected_return_code=0,
+        expected_stdout_message="0 failures",
+    )
+
+    run_file(
+        ruby_or_cli_path=args.ruby_or_cli_path,
+        filepath=MINITEST_FAIL_FILE,
+        expected_return_code=1,
+        expected_stdout_message="1 failures",
+    )
+
+    # r = subprocess.run(
+    #     [str(args.ruby_or_cli_path), "-e", "at exit { exit 12 }"],
+    #     stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8", universal_newlines=True
+    # )
+    # assert r.returncode == 12
