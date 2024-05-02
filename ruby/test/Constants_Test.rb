8154024ba63d268fe15b808f4ec72037fbffe4ee
Julien Marrec <julien.marrec@gmail.com>
2024-05-02 12:50:25 +0200

    Group all constants test under the same test. It's way too slow with the CLI otherwise.

--- ruby/test/Constants_Test.rb
+++ ruby/test/Constants_Test.rb
@@ -3,35 +3,28 @@
 #  See also https://openstudio.net/license
 ########################################################################################################################
 
-require 'openstudio'
-
 require 'minitest/autorun'
 
 class ConstantsTest < Minitest::Test
 
-  def test_ruby_version
-    assert_equal("3.2.2", RUBY_VERSION)
-  end
+  # Note: putting all of these tests under the same one because of the way we
+  # run the tests with the CLI (we run a CLI instance for each individual test,
+  # and it takes almost 20 seconds for each which is ridiculous)
+  def test_ruby_constants
 
-  def test_ruby_description
+    assert_equal("3.2.2", RUBY_VERSION)
+
     refute_empty(RUBY_DESCRIPTION)
     assert_match(/ruby 3.2.2 \(.*\) \[.*\]/, RUBY_DESCRIPTION)
-  end
 
-  def test_ruby_engine
     assert_equal("ruby", RUBY_ENGINE)
-  end
 
-  def test_ruby_platform
     refute_empty(RUBY_PLATFORM)
-  end
 
-  def test_ruby_release_date
     refute_empty(RUBY_RELEASE_DATE)
-  end
 
-  def test_ruby_copyright
     refute_empty(RUBY_COPYRIGHT)
+
   end
 
 end
