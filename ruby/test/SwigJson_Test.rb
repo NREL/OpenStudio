4cfd581e97d8d9efa96dcf3b461efbb4345a2a6a
Julien Marrec <julien.marrec@gmail.com>
2024-05-13 22:45:28 +0200

    ``` The following tests FAILED: 	3705 - RubyTest-SwigJson_Test-swig_json_special_chars (Failed) 	3870 - CLITest-SwigJson_Test-swig_json_special_chars (Failed) ```

--- ruby/test/SwigJson_Test.rb
+++ ruby/test/SwigJson_Test.rb
@@ -55,4 +55,36 @@ class SwigJSONTest < Minitest::Test
     assert_kind_of(String, val)
     assert_equal('handle1', val)
   end
+
+  def test_swig_json_special_chars
+    workflow = OpenStudio::WorkflowJSON.new
+    runner = OpenStudio::Measure::OSRunner.new(workflow)
+
+    step1 = OpenStudio::MeasureStep.new('MeasureName1')
+    step1.setArgument('Argument1', 100)
+
+    workflow_step_result1 = OpenStudio::WorkflowStepResult.new
+    workflow_step_result1.setMeasureName('measure_name_1')
+
+    name = "name_(|)'*$£*µ%é"
+    val = "val_(|)'*$£*µ%é"
+    stepValue1 = OpenStudio::WorkflowStepValue.new(name, val)
+    assert_equal(name, stepValue1.name)
+    assert_equal(val, stepValue1.valueAsString)
+    assert_equal(val, stepValue1.valueAsJSON)
+    workflow_step_result1.addStepValue(stepValue1)
+
+    workflow_step_result1.setStepResult('Success'.to_StepResult)
+    workflow_step_result1.setCompletedAt(OpenStudio::DateTime.nowUTC)
+    step1.setResult(workflow_step_result1)
+
+    workflow.setWorkflowSteps([step1])
+
+    # workflow.saveAs("OSRunner_getPastStepValues_special_chars.osw")
+
+    h = runner.getPastStepValuesForMeasure('MeasureName1')
+    assert_equal(1, h.size)
+    assert_equal(name.to_sym, h.keys.first)
+    assert_equal(val, h.values.first)
+  end
 end
