6e8ca6fef05960d306c08fc78dd0a009cb79ee9b
Julien Marrec <julien.marrec@gmail.com>
2024-10-01 15:56:28 +0200

    To avoid dropping python 3.8 support on pypi yet, just install requirements.txt when BUILD_CLI is on

--- python/SetupPython.cmake
+++ python/SetupPython.cmake
@@ -35,9 +35,11 @@ else()
   endif()
 endif()
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E env --unset=PIP_REQUIRE_VIRTUALENV ${Python_EXECUTABLE}
-  -m pip install --target=${ENERGYPLUS_DIR}/python_lib --upgrade -r ${PROJECT_SOURCE_DIR}/python/requirements.txt
-)
+if(BUILD_CLI)
+  execute_process(COMMAND ${CMAKE_COMMAND} -E env --unset=PIP_REQUIRE_VIRTUALENV ${Python_EXECUTABLE}
+    -m pip install --target=${ENERGYPLUS_DIR}/python_lib --upgrade -r ${PROJECT_SOURCE_DIR}/python/requirements.txt
+  )
+endif()
 
 get_filename_component(Python_PROGRAM_NAME ${Python_EXECUTABLE} NAME)
 
