662ddbb2e85b8651b92b99ad74634186ec8504cc
TShapinsky <tobiasshapinsky@gmail.com>
2024-10-11 10:08:28 -0600

    use simplified API header

--- src/alfalfa/AlfalfaAPI.hpp
+++ src/alfalfa/AlfalfaAPI.hpp
@@ -6,34 +6,14 @@
 #ifndef ALFALFA_ALFALFAAPI_HPP
 #define ALFALFA_ALFALFAAPI_HPP
 
-#if (_WIN32 || _MSC_VER) && !OPENSTUDIO_DIRECT_INCLUDE
-
-#  ifdef SHARED_OS_LIBS
-
-#    if defined(openstudio_alfalfa_EXPORTS) || defined(openstudio_EXPORTS)
-#      define ALFALFA_API __declspec(dllexport)
-#      define ALFALFA_TEMPLATE_EXT
-#    else
-#      define ALFALFA_API __declspec(dllimport)
-#      define ALFALFA_TEMPLATE_EXT extern
-#    endif
-
+#if (_WIN32 || _MSC_VER) && SHARED_OS_LIBS
+#  if defined(openstudio_alfalfa_EXPORTS) || defined(openstudio_EXPORTS)
+#    define ALFALFA_API __declspec(dllexport)
 #  else
-
-#    define ALFALFA_API
-#    if defined(openstudio_alfalfa_EXPORTS) || defined(openstudio_EXPORTS)
-#      define ALFALFA_TEMPLATE_EXT
-#    else
-#      define ALFALFA_TEMPLATE_EXT extern
-#    endif
-
+#    define ALFALFA_API __declspec(dllimport)
 #  endif
-
 #else
-
 #  define ALFALFA_API
-#  define ALFALFA_TEMPLATE_EXT
-
 #endif
 
 #endif
