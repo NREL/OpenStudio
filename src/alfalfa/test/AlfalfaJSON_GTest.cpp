d1f6cff87163724e96c39616707fbee3e164631a
TShapinsky <tobiasshapinsky@gmail.com>
2024-10-16 10:37:00 -0600

    try using runtime wrapping for make fmt happy on ubuntu 22.04

--- src/alfalfa/test/AlfalfaJSON_GTest.cpp
+++ src/alfalfa/test/AlfalfaJSON_GTest.cpp
@@ -1,3 +1,4 @@
+#include <fmt/core.h>
 #include <gtest/gtest.h>
 #include <vector>
 
@@ -47,7 +48,7 @@ TEST(AlfalfaJSON, basic_export) {
   // Check that point does not have an input
   EXPECT_FALSE(constant.input().is_initialized());
 
-  AlfalfaPoint meter = alfalfa.exposeMeter("Electricity:Facility").get();
+  const AlfalfaPoint meter = alfalfa.exposeMeter("Electricity:Facility").get();
   std::cout << "here" << std::endl;
 
   // Check that point has output of type "Meter"
@@ -56,7 +57,7 @@ TEST(AlfalfaJSON, basic_export) {
   // Check that point does not have an input
   EXPECT_FALSE(meter.input().is_initialized());
 
-  AlfalfaPoint actuator = alfalfa.exposeActuator("thing1", "thing2", "thing3").get();
+  const AlfalfaPoint actuator = alfalfa.exposeActuator("thing1", "thing2", "thing3").get();
 
   // Check that point has output of type "Actuator"
   ASSERT_TRUE(actuator.output().is_initialized());
@@ -65,7 +66,7 @@ TEST(AlfalfaJSON, basic_export) {
   ASSERT_TRUE(actuator.input().is_initialized());
   EXPECT_EQ(actuator.input().get().typeName(), "Actuator");
 
-  AlfalfaPoint output_variable = alfalfa.exposeOutputVariable("key", "name").get();
+  const AlfalfaPoint output_variable = alfalfa.exposeOutputVariable("key", "name").get();
 
   // Check that point has output of type "OutputVariable"
   ASSERT_TRUE(output_variable.output().is_initialized());
@@ -73,7 +74,7 @@ TEST(AlfalfaJSON, basic_export) {
   // Check that point does not have an input
   EXPECT_FALSE(output_variable.input().is_initialized());
 
-  AlfalfaPoint global_variable = alfalfa.exposeGlobalVariable("name").get();
+  const AlfalfaPoint global_variable = alfalfa.exposeGlobalVariable("name").get();
 
   // Check that point has output of type "GlobalVariable"
   ASSERT_TRUE(global_variable.output().is_initialized());
@@ -100,7 +101,7 @@ TEST(AlfalfaJSON, basic_export) {
 TEST(AlfalfaJSON, json_serialization) {
   AlfalfaJSON alfalfa;
 
-  std::string point_name = "example_name";
+  const std::string point_name = "example_name";
 
   Json::Value root_json;
   root_json["name"] = point_name;
@@ -108,8 +109,8 @@ TEST(AlfalfaJSON, json_serialization) {
   root_json["optional"] = true;
 
   // Test generation of JSON for constant components
-  float value = 10.0;
-  AlfalfaPoint constant = alfalfa.exposeConstant(value, point_name + "_constant").get();
+  const double value = 10.0;
+  const AlfalfaPoint constant = alfalfa.exposeConstant(value, point_name + "_constant").get();
 
   Json::Value constant_json = root_json;
   constant_json["name"] = point_name + "_constant";
@@ -120,11 +121,11 @@ TEST(AlfalfaJSON, json_serialization) {
   EXPECT_EQ(constant.toJSON(), constant_json);
 
   // Test generation of JSON for actuator components
-  std::string act_component_name = "example_component";
-  std::string act_component_type = "example_component_type";
-  std::string act_control_type = "example_control_type";
+  const std::string act_component_name = "example_component";
+  const std::string act_component_type = "example_component_type";
+  const std::string act_control_type = "example_control_type";
 
-  AlfalfaPoint actuator = alfalfa.exposeActuator(act_component_name, act_component_type, act_control_type, point_name + "_actuator").get();
+  const AlfalfaPoint actuator = alfalfa.exposeActuator(act_component_name, act_component_type, act_control_type, point_name + "_actuator").get();
 
   Json::Value actuator_json = root_json;
   actuator_json["name"] = point_name + "_actuator";
@@ -139,9 +140,9 @@ TEST(AlfalfaJSON, json_serialization) {
   EXPECT_EQ(actuator.toJSON(), actuator_json);
 
   // Test generation of JSON for global variable components
-  std::string global_variable_name = "example_global_variable_name";
+  const std::string global_variable_name = "example_global_variable_name";
 
-  AlfalfaPoint global_variable = alfalfa.exposeGlobalVariable(global_variable_name, point_name + "_global_variable").get();
+  const AlfalfaPoint global_variable = alfalfa.exposeGlobalVariable(global_variable_name, point_name + "_global_variable").get();
 
   Json::Value global_variable_json = root_json;
   global_variable_json["name"] = point_name + "_global_variable";
@@ -154,9 +155,9 @@ TEST(AlfalfaJSON, json_serialization) {
   EXPECT_EQ(global_variable.toJSON(), global_variable_json);
 
   // Test generation of JSON for meter components
-  std::string meter_name = "example_meter_name";
+  const std::string meter_name = "example_meter_name";
 
-  AlfalfaPoint meter = alfalfa.exposeMeter(meter_name, point_name + "_meter").get();
+  const AlfalfaPoint meter = alfalfa.exposeMeter(meter_name, point_name + "_meter").get();
 
   Json::Value meter_json = root_json;
   meter_json["name"] = point_name + "_meter";
@@ -167,10 +168,10 @@ TEST(AlfalfaJSON, json_serialization) {
   EXPECT_EQ(meter.toJSON(), meter_json);
 
   // Test generation of JSON for output variable components
-  std::string output_variable_key = "example_output_variable_key";
-  std::string output_variable_name = "example_output_variable_name";
+  const std::string output_variable_key = "example_output_variable_key";
+  const std::string output_variable_name = "example_output_variable_name";
 
-  AlfalfaPoint output_variable = alfalfa.exposeOutputVariable(output_variable_key, output_variable_name, point_name + "_output_variable").get();
+  const AlfalfaPoint output_variable = alfalfa.exposeOutputVariable(output_variable_key, output_variable_name, point_name + "_output_variable").get();
 
   Json::Value output_variable_json = root_json;
   output_variable_json["name"] = point_name + "_output_variable";
@@ -182,30 +183,30 @@ TEST(AlfalfaJSON, json_serialization) {
   EXPECT_EQ(output_variable.toJSON(), output_variable_json);
 
   // Test that the file written is the same as the one inside the AlfalfaJSON object
-  path out_file = tempDir() / "alfalfa.json";
+  const path out_file = tempDir() / "alfalfa.json";
   alfalfa.saveAs(out_file);
 
   Json::Value root;
   std::ifstream ifs(openstudio::toSystemFilename(out_file));
 
-  Json::CharReaderBuilder r_builder;
+  const Json::CharReaderBuilder r_builder;
   std::string formatted_errors;
-  bool parsing_success = Json::parseFromStream(r_builder, ifs, &root, &formatted_errors);
+  const bool parsing_success = Json::parseFromStream(r_builder, ifs, &root, &formatted_errors);
   EXPECT_TRUE(parsing_success);
   EXPECT_EQ(alfalfa.toJSON(), root);
-  for (AlfalfaPoint point : alfalfa.points()) {
+  for (const AlfalfaPoint& point : alfalfa.points()) {
     EXPECT_EQ(root[point.id().get()], point.toJSON());
   }
 }
 
 TEST(AlfalfaJSON, point_exceptions_logging) {
-  std::string ID_VALID_CHARS_MSG = "IDs can only contain letters, numbers, and the following special characters _-[]():";
-  std::string DISPLAY_NAME_VALID_CHARS_MSG = "Display name '{}' does not produce a valid point ID. Manually set a valid ID or export will fail.";
-  std::string LOG_CHANNEL = "openstudio.AlfalfaPoint";
+  const std::string ID_VALID_CHARS_MSG = "IDs can only contain letters, numbers, and the following special characters _-[]():";
+  const std::string DISPLAY_NAME_VALID_CHARS_MSG = "Display name '{}' does not produce a valid point ID. Manually set a valid ID or export will fail.";
+  const std::string LOG_CHANNEL = "openstudio.AlfalfaPoint";
   StringStreamLogSink ss;
   ss.setLogLevel(Warn);
 
-  AlfalfaPoint point("Point");
+  const AlfalfaPoint point("Point");
   ASSERT_EQ(0, ss.logMessages().size());
   point.id();
   ASSERT_EQ(0, ss.logMessages().size());
@@ -214,18 +215,18 @@ TEST(AlfalfaJSON, point_exceptions_logging) {
   AlfalfaPoint invalid_point("Point$$$");
   ASSERT_EQ(1, ss.logMessages().size());
   LogMessage invalid_id_msg = ss.logMessages().at(0);
-  EXPECT_EQ(invalid_id_msg.logMessage(), fmt::format(DISPLAY_NAME_VALID_CHARS_MSG, "Point$$$"));
+  EXPECT_EQ(invalid_id_msg.logMessage(), fmt::format(fmt::runtime(DISPLAY_NAME_VALID_CHARS_MSG), "Point$$$"));
   EXPECT_EQ(invalid_id_msg.logLevel(), Warn);
   EXPECT_EQ(invalid_id_msg.logChannel(), LOG_CHANNEL);
   ss.resetStringStream();
   ASSERT_EQ(ss.logMessages().size(), 0);
 
   // Test logging when getting id()
-  boost::optional<std::string> invalid_point_id = invalid_point.id();
+  const boost::optional<std::string> invalid_point_id = invalid_point.id();
   EXPECT_FALSE(invalid_point_id.is_initialized());
   ASSERT_EQ(ss.logMessages().size(), 1);
   invalid_id_msg = ss.logMessages().at(0);
-  EXPECT_EQ(invalid_id_msg.logMessage(), fmt::format(DISPLAY_NAME_VALID_CHARS_MSG, "Point$$$"));
+  EXPECT_EQ(invalid_id_msg.logMessage(), fmt::format(fmt::runtime(DISPLAY_NAME_VALID_CHARS_MSG), "Point$$$"));
   EXPECT_EQ(invalid_id_msg.logLevel(), Warn);
   EXPECT_EQ(invalid_id_msg.logChannel(), LOG_CHANNEL);
   ss.resetStringStream();
@@ -257,7 +258,7 @@ TEST(AlfalfaJSON, point_exceptions_logging) {
     std::runtime_error);
   ASSERT_EQ(ss.logMessages().size(), 1);
   invalid_id_msg = ss.logMessages().at(0);
-  EXPECT_EQ(invalid_id_msg.logMessage(), fmt::format(DISPLAY_NAME_VALID_CHARS_MSG, "Point$$$"));
+  EXPECT_EQ(invalid_id_msg.logMessage(), fmt::format(fmt::runtime(DISPLAY_NAME_VALID_CHARS_MSG), "Point$$$"));
   EXPECT_EQ(invalid_id_msg.logLevel(), Warn);
   EXPECT_EQ(invalid_id_msg.logChannel(), LOG_CHANNEL);
   ss.resetStringStream();
@@ -284,7 +285,7 @@ TEST(AlfalfaJSON, point_exceptions_logging) {
   ASSERT_EQ(ss.logMessages().size(), 0);
 
   // Test that setting a valid ID is sticky
-  std::string new_id = "Another_Valid_Point(123)";
+  const std::string new_id = "Another_Valid_Point(123)";
   valid_point.setId(new_id);
   ASSERT_EQ(ss.logMessages().size(), 0);
   ASSERT_TRUE(valid_point.id().is_initialized());
@@ -331,9 +332,9 @@ class InputComponent : public ComponentBase
 TEST(AlfalfaJSON, point_component_exceptions) {
   AlfalfaPoint point("Point Name");
 
-  AlfalfaConstant constant(12.5);
+  const AlfalfaConstant constant(12.5);
   // No Components can not be used as outputs so I create a fictional component to test that guard
-  InputComponent input_only;
+  const InputComponent input_only;
 
   EXPECT_FALSE(point.input().is_initialized());
   EXPECT_FALSE(point.output().is_initialized());
@@ -377,10 +378,10 @@ TEST(AlfalfaJSON, point_component_exceptions) {
 
 TEST(AlfalfaJSON, expose_meter) {
   AlfalfaJSON alfalfa;
-  Model model;
+  const Model model;
 
-  std::string display_name = "Electricity Facility Meter";
-  std::string meter_name = "Electricity:Facility";
+  const std::string display_name = "Electricity Facility Meter";
+  const std::string meter_name = "Electricity:Facility";
 
   OutputMeter os_meter(model);
   IdfObject idf_meter(IddObjectType::Output_Meter);
@@ -400,18 +401,18 @@ TEST(AlfalfaJSON, expose_meter) {
   idf_meter.setString(Output_MeterFields::KeyName, meter_name);
 
   // All Components should be the same
-  AlfalfaMeter str_component(meter_name);
-  AlfalfaMeter os_component(os_meter);
-  AlfalfaMeter idf_component(idf_meter);
+  const AlfalfaMeter str_component(meter_name);
+  const AlfalfaMeter os_component(os_meter);
+  const AlfalfaMeter idf_component(idf_meter);
 
   EXPECT_EQ(str_component, os_component);
   EXPECT_EQ(str_component, idf_component);
 
   // All points should be the same
 
-  AlfalfaPoint str_point = alfalfa.exposeMeter(meter_name, display_name).get();
-  AlfalfaPoint os_point = alfalfa.exposeFromObject(os_meter).get();
-  AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_meter, display_name).get();
+  const AlfalfaPoint str_point = alfalfa.exposeMeter(meter_name, display_name).get();
+  const AlfalfaPoint os_point = alfalfa.exposeFromObject(os_meter).get();
+  const AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_meter, display_name).get();
 
   EXPECT_EQ(os_point, str_point);
   EXPECT_EQ(os_point, idf_point);
@@ -423,11 +424,11 @@ TEST(AlfalfaJSON, expose_meter) {
 
 TEST(AlfalfaJSON, expose_output_variable) {
   AlfalfaJSON alfalfa;
-  Model model;
+  const Model model;
 
-  std::string variable_name = "my_var";
-  std::string variable_key = "EMS";
-  std::string display_name = "My Output Variable";
+  const std::string variable_name = "my_var";
+  const std::string variable_key = "EMS";
+  const std::string display_name = "My Output Variable";
 
   OutputVariable os_output_variable(variable_name, model);
   EnergyManagementSystemOutputVariable os_ems_output_variable(model, "");
@@ -461,11 +462,11 @@ TEST(AlfalfaJSON, expose_output_variable) {
   idf_ems_output_variable.setString(EnergyManagementSystem_OutputVariableFields::Name, display_name);
 
   // All components created should be equal
-  AlfalfaOutputVariable str_component(variable_key, variable_name);
-  AlfalfaOutputVariable os_component(os_output_variable);
-  AlfalfaOutputVariable os_ems_component(os_ems_output_variable);
-  AlfalfaOutputVariable idf_component(idf_output_variable);
-  AlfalfaOutputVariable idf_ems_component(idf_ems_output_variable);
+  const AlfalfaOutputVariable str_component(variable_key, variable_name);
+  const AlfalfaOutputVariable os_component(os_output_variable);
+  const AlfalfaOutputVariable os_ems_component(os_ems_output_variable);
+  const AlfalfaOutputVariable idf_component(idf_output_variable);
+  const AlfalfaOutputVariable idf_ems_component(idf_ems_output_variable);
 
   EXPECT_EQ(str_component, os_component);
   EXPECT_EQ(str_component, os_ems_component);
@@ -475,11 +476,11 @@ TEST(AlfalfaJSON, expose_output_variable) {
   // All points created should be the same
   os_output_variable.setDisplayName(display_name);
 
-  AlfalfaPoint str_point = alfalfa.exposeOutputVariable(variable_key, variable_name, display_name).get();
-  AlfalfaPoint os_point = alfalfa.exposeFromObject(os_output_variable).get();
-  AlfalfaPoint os_ems_point = alfalfa.exposeFromObject(os_ems_output_variable).get();
-  AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_output_variable, display_name).get();
-  AlfalfaPoint idf_ems_point = alfalfa.exposeFromObject(idf_ems_output_variable).get();
+  const AlfalfaPoint str_point = alfalfa.exposeOutputVariable(variable_key, variable_name, display_name).get();
+  const AlfalfaPoint os_point = alfalfa.exposeFromObject(os_output_variable).get();
+  const AlfalfaPoint os_ems_point = alfalfa.exposeFromObject(os_ems_output_variable).get();
+  const AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_output_variable, display_name).get();
+  const AlfalfaPoint idf_ems_point = alfalfa.exposeFromObject(idf_ems_output_variable).get();
 
   EXPECT_EQ(str_point, os_point);
   EXPECT_EQ(str_point, os_ems_point);
@@ -493,10 +494,10 @@ TEST(AlfalfaJSON, expose_output_variable) {
 
 TEST(AlfalfaJSON, expose_global_variable) {
   AlfalfaJSON alfalfa;
-  Model model;
+  const Model model;
 
-  std::string variable_name = "Variable_Name";
-  std::string display_name = "Variable Name";
+  const std::string variable_name = "Variable_Name";
+  const std::string display_name = "Variable Name";
 
   EnergyManagementSystemGlobalVariable os_global_variable(model, variable_name);
   IdfObject idf_global_variable(IddObjectType::EnergyManagementSystem_GlobalVariable);
@@ -511,17 +512,17 @@ TEST(AlfalfaJSON, expose_global_variable) {
   idf_global_variable.setString(EnergyManagementSystem_GlobalVariableExtensibleFields::ErlVariableName, variable_name);
 
   // All Components created should be equal
-  AlfalfaGlobalVariable str_component(variable_name);
-  AlfalfaGlobalVariable os_component(os_global_variable);
-  AlfalfaGlobalVariable idf_component(idf_global_variable);
+  const AlfalfaGlobalVariable str_component(variable_name);
+  const AlfalfaGlobalVariable os_component(os_global_variable);
+  const AlfalfaGlobalVariable idf_component(idf_global_variable);
 
   EXPECT_EQ(str_component, os_component);
   EXPECT_EQ(str_component, idf_component);
 
   // All points created should be equal
-  AlfalfaPoint str_point = alfalfa.exposeGlobalVariable(variable_name, display_name).get();
-  AlfalfaPoint os_point = alfalfa.exposeFromObject(os_global_variable).get();
-  AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_global_variable, display_name).get();
+  const AlfalfaPoint str_point = alfalfa.exposeGlobalVariable(variable_name, display_name).get();
+  const AlfalfaPoint os_point = alfalfa.exposeFromObject(os_global_variable).get();
+  const AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_global_variable, display_name).get();
 
   EXPECT_EQ(str_point, os_point);
   EXPECT_EQ(str_point.toJSON(), idf_point.toJSON());
@@ -533,12 +534,12 @@ TEST(AlfalfaJSON, expose_global_variable) {
 
 TEST(AlfalfaJSON, expose_actuator) {
   AlfalfaJSON alfalfa;
-  Model model;
+  const Model model;
 
-  std::string component_name = "component_name";
-  std::string component_type = "component_type";
-  std::string control_type = "control_type";
-  std::string display_name = "My Actuator";
+  const std::string component_name = "component_name";
+  const std::string component_type = "component_type";
+  const std::string control_type = "control_type";
+  const std::string display_name = "My Actuator";
 
   ThermalZone actuated_zone(model);
   EnergyManagementSystemActuator os_actuator(actuated_zone, "", "");
@@ -560,17 +561,17 @@ TEST(AlfalfaJSON, expose_actuator) {
   idf_actuator.setString(EnergyManagementSystem_ActuatorFields::ActuatedComponentControlType, control_type);
 
   // All components should be the same
-  AlfalfaActuator str_component(component_name, component_type, control_type);
-  AlfalfaActuator os_component(os_actuator);
-  AlfalfaActuator idf_component(idf_actuator);
+  const AlfalfaActuator str_component(component_name, component_type, control_type);
+  const AlfalfaActuator os_component(os_actuator);
+  const AlfalfaActuator idf_component(idf_actuator);
 
   EXPECT_EQ(str_component, os_component);
   EXPECT_EQ(str_component, idf_component);
 
   // All point should be the same
-  AlfalfaPoint str_point = alfalfa.exposeActuator(component_name, component_type, control_type, display_name).get();
-  AlfalfaPoint os_point = alfalfa.exposeFromObject(os_actuator).get();
-  AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_actuator, display_name).get();
+  const AlfalfaPoint str_point = alfalfa.exposeActuator(component_name, component_type, control_type, display_name).get();
+  const AlfalfaPoint os_point = alfalfa.exposeFromObject(os_actuator).get();
+  const AlfalfaPoint idf_point = alfalfa.exposeFromObject(idf_actuator, display_name).get();
 
   EXPECT_EQ(str_point, os_point);
   EXPECT_EQ(str_point, idf_point);
