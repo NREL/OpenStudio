c96fd711f4c0d225ecd8293e9bfb9d304976bca5
Joe Robertson <joseph.robertson@nrel.gov>
2026-09-01 14:30:35 -0700

    Add model and translation tests for new ZoneMRTCalculation object.

---
+++ src/energyplus/Test/ZoneMRTCalculation_GTest.cpp
@@ -0,0 +1,117 @@
+/***********************************************************************************************************************
+*  OpenStudio(R), Copyright (c) Alliance for Energy Innovation, LLC.
+*  See also https://openstudio.net/license
+***********************************************************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "EnergyPlusFixture.hpp"
+
+#include "../ForwardTranslator.hpp"
+#include "../ReverseTranslator.hpp"
+
+#include "../../model/Model.hpp"
+#include "../../model/People.hpp"
+#include "../../model/PeopleDefinition.hpp"
+#include "../../model/Space.hpp"
+#include "../../model/ThermalZone.hpp"
+#include "../../model/ZoneMRTCalculation.hpp"
+
+#include "../../utilities/idf/IdfExtensibleGroup.hpp"
+#include "../../utilities/idf/WorkspaceExtensibleGroup.hpp"
+#include "../../utilities/idf/WorkspaceObject.hpp"
+
+#include <utilities/idd/IddEnums.hxx>
+#include <utilities/idd/People_FieldEnums.hxx>
+#include <utilities/idd/ZoneMRTCalculation_FieldEnums.hxx>
+
+using namespace openstudio::energyplus;
+using namespace openstudio::model;
+using namespace openstudio;
+
+TEST_F(EnergyPlusFixture, ForwardTranslator_ZoneMRTCalculation) {
+  Model model;
+  ThermalZone zone(model);
+  zone.setName("Thermal Zone 1");
+
+  Space space(model);
+  space.setName("Space 1");
+  EXPECT_TRUE(space.setThermalZone(zone));
+
+  PeopleDefinition definition(model);
+  EXPECT_TRUE(definition.setNumberofPeople(1.0));
+  EXPECT_TRUE(definition.setThermalComfortModelType(0, "Fanger"));
+
+  People people1(definition);
+  people1.setName("People 1");
+  EXPECT_TRUE(people1.setSpace(space));
+
+  People people2(definition);
+  people2.setName("People 2");
+  EXPECT_TRUE(people2.setSpace(space));
+
+  ZoneMRTCalculation zoneMRTCalculation = zone.getZoneMRTCalculation();
+  EXPECT_TRUE(zoneMRTCalculation.addMRTWeightingFactor(people1, 0.25));
+  EXPECT_TRUE(zoneMRTCalculation.addMRTWeightingFactor(people2, 0.75));
+
+  ForwardTranslator ft;
+  Workspace workspace = ft.translateModel(model);
+
+  std::vector<WorkspaceObject> idfZoneMRTCalculations = workspace.getObjectsByType(IddObjectType::ZoneMRTCalculation);
+  ASSERT_EQ(1u, idfZoneMRTCalculations.size());
+
+  WorkspaceObject idfZoneMRTCalculation = idfZoneMRTCalculations[0];
+  EXPECT_EQ("Thermal Zone 1", idfZoneMRTCalculation.getString(ZoneMRTCalculationFields::ZoneName).get());
+  ASSERT_EQ(2u, idfZoneMRTCalculation.numExtensibleGroups());
+
+  auto group0 = idfZoneMRTCalculation.extensibleGroups()[0].cast<WorkspaceExtensibleGroup>();
+  EXPECT_EQ("People 1", group0.getString(ZoneMRTCalculationExtensibleFields::PeopleName).get());
+  EXPECT_DOUBLE_EQ(0.25, group0.getDouble(ZoneMRTCalculationExtensibleFields::MRTWeightingFactor).get());
+
+  auto group1 = idfZoneMRTCalculation.extensibleGroups()[1].cast<WorkspaceExtensibleGroup>();
+  EXPECT_EQ("People 2", group1.getString(ZoneMRTCalculationExtensibleFields::PeopleName).get());
+  EXPECT_DOUBLE_EQ(0.75, group1.getDouble(ZoneMRTCalculationExtensibleFields::MRTWeightingFactor).get());
+}
+
+TEST_F(EnergyPlusFixture, ReverseTranslator_ZoneMRTCalculation) {
+  Workspace workspace(StrictnessLevel::Minimal, IddFileType::EnergyPlus);
+
+  OptionalWorkspaceObject zone = workspace.addObject(IdfObject(IddObjectType::Zone));
+  ASSERT_TRUE(zone);
+  zone->setName("Thermal Zone 1");
+
+  OptionalWorkspaceObject people = workspace.addObject(IdfObject(IddObjectType::People));
+  ASSERT_TRUE(people);
+  people->setName("People 1");
+  EXPECT_TRUE(people->setPointer(PeopleFields::ZoneorZoneListorSpaceorSpaceListName, zone->handle()));
+  EXPECT_TRUE(people->setString(PeopleFields::NumberofPeopleCalculationMethod, "People"));
+  EXPECT_TRUE(people->setDouble(PeopleFields::NumberofPeople, 1.0));
+  EXPECT_TRUE(people->setString(PeopleFields::ThermalComfortModel1Type, "Fanger"));
+
+  IdfObject idfZoneMRTCalculation(IddObjectType::ZoneMRTCalculation);
+  idfZoneMRTCalculation.setString(ZoneMRTCalculationFields::ZoneName, "Thermal Zone 1");
+  IdfExtensibleGroup group = idfZoneMRTCalculation.pushExtensibleGroup();
+  group.setString(ZoneMRTCalculationExtensibleFields::PeopleName, "People 1");
+  group.setDouble(ZoneMRTCalculationExtensibleFields::MRTWeightingFactor, 0.5);
+  OptionalWorkspaceObject epZoneMRTCalculation = workspace.addObject(idfZoneMRTCalculation);
+  ASSERT_TRUE(epZoneMRTCalculation);
+
+  ReverseTranslator rt;
+  Model model = rt.translateWorkspace(workspace);
+
+  std::vector<ThermalZone> thermalZones = model.getConcreteModelObjects<ThermalZone>();
+  ASSERT_EQ(1u, thermalZones.size());
+  ThermalZone thermalZone = thermalZones[0];
+  EXPECT_EQ("Thermal Zone 1", thermalZone.nameString());
+
+  std::vector<People> modelPeople = model.getConcreteModelObjects<People>();
+  ASSERT_EQ(1u, modelPeople.size());
+  EXPECT_EQ("People 1", modelPeople[0].nameString());
+
+  ZoneMRTCalculation zoneMRTCalculation = thermalZone.getZoneMRTCalculation();
+  EXPECT_EQ(1u, zoneMRTCalculation.numberofMRTWeightingFactors());
+  std::vector<MRTWeightingFactor> mrtWeightingFactors = zoneMRTCalculation.mrtWeightingFactors();
+  ASSERT_EQ(1u, mrtWeightingFactors.size());
+  EXPECT_EQ("People 1", mrtWeightingFactors[0].people().nameString());
+  EXPECT_DOUBLE_EQ(0.5, mrtWeightingFactors[0].mrtWeightingFactor());
+}
---
+++ src/model/test/ZoneMRTCalculation_GTest.cpp
@@ -0,0 +1,98 @@
+/***********************************************************************************************************************
+*  OpenStudio(R), Copyright (c) Alliance for Energy Innovation, LLC.
+*  See also https://openstudio.net/license
+***********************************************************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "ModelFixture.hpp"
+
+#include "../Model.hpp"
+#include "../People.hpp"
+#include "../PeopleDefinition.hpp"
+#include "../Space.hpp"
+#include "../ThermalZone.hpp"
+#include "../ZoneMRTCalculation.hpp"
+#include "../ZoneMRTCalculation_Impl.hpp"
+
+using namespace openstudio;
+using namespace openstudio::model;
+
+TEST_F(ModelFixture, ZoneMRTCalculation_MRTWeightingFactor) {
+  Model model;
+  PeopleDefinition definition(model);
+  People people(definition);
+
+  EXPECT_NO_THROW(MRTWeightingFactor(people, 0.5));
+  EXPECT_THROW(MRTWeightingFactor(people, -0.1), openstudio::Exception);
+  EXPECT_THROW(MRTWeightingFactor(people, 1.1), openstudio::Exception);
+}
+
+TEST_F(ModelFixture, ZoneMRTCalculation_Uniqueness) {
+  Model model;
+  ThermalZone thermalZone(model);
+  auto size = model.modelObjects().size();
+
+  ZoneMRTCalculation zoneMRTCalculation1 = thermalZone.getZoneMRTCalculation();
+  EXPECT_EQ(thermalZone.handle(), zoneMRTCalculation1.thermalZone().handle());
+  EXPECT_EQ(size + 1, model.modelObjects().size());
+
+  ZoneMRTCalculation zoneMRTCalculation2 = thermalZone.getZoneMRTCalculation();
+  EXPECT_EQ(size + 1, model.modelObjects().size());
+  EXPECT_EQ(zoneMRTCalculation1, zoneMRTCalculation2);
+
+  EXPECT_EQ(0u, zoneMRTCalculation2.numberofMRTWeightingFactors());
+  EXPECT_TRUE(zoneMRTCalculation2.mrtWeightingFactors().empty());
+
+  EXPECT_THROW((ZoneMRTCalculation(thermalZone)), openstudio::Exception);
+  EXPECT_EQ(size + 1, model.modelObjects().size());
+
+  EXPECT_THROW(zoneMRTCalculation1.clone(model), openstudio::Exception);
+  EXPECT_EQ(size + 1, model.modelObjects().size());
+}
+
+TEST_F(ModelFixture, ZoneMRTCalculation_AddAndRemoveMRTWeightingFactors) {
+  Model model;
+  ThermalZone thermalZone(model);
+  ZoneMRTCalculation zoneMRTCalculation = thermalZone.getZoneMRTCalculation();
+
+  Space space(model);
+  PeopleDefinition definition(model);
+  definition.setNumberofPeople(1.0);
+  EXPECT_TRUE(definition.setThermalComfortModelType(0, "Fanger"));
+
+  People people(definition);
+  people.setName("People 1");
+
+  EXPECT_FALSE(zoneMRTCalculation.addMRTWeightingFactor(people, 0.5));
+  EXPECT_EQ(0u, zoneMRTCalculation.numberofMRTWeightingFactors());
+
+  space.setThermalZone(thermalZone);
+  EXPECT_TRUE(people.setSpace(space));
+
+  EXPECT_TRUE(zoneMRTCalculation.addMRTWeightingFactor(people, 0.5));
+  EXPECT_EQ(1u, zoneMRTCalculation.numberofMRTWeightingFactors());
+
+  boost::optional<MRTWeightingFactor> mrtWeightingFactor = zoneMRTCalculation.getMRTWeightingFactor(0);
+  ASSERT_TRUE(mrtWeightingFactor);
+  EXPECT_EQ(people.handle(), mrtWeightingFactor->people().handle());
+  EXPECT_DOUBLE_EQ(0.5, mrtWeightingFactor->mrtWeightingFactor());
+
+  EXPECT_TRUE(zoneMRTCalculation.addMRTWeightingFactor(people, 0.25));
+  EXPECT_EQ(1u, zoneMRTCalculation.numberofMRTWeightingFactors());
+  mrtWeightingFactor = zoneMRTCalculation.getMRTWeightingFactor(0);
+  ASSERT_TRUE(mrtWeightingFactor);
+  EXPECT_DOUBLE_EQ(0.25, mrtWeightingFactor->mrtWeightingFactor());
+
+  People people2(definition);
+  people2.setName("People 2");
+  EXPECT_TRUE(people2.setSpace(space));
+  EXPECT_TRUE(zoneMRTCalculation.addMRTWeightingFactors({MRTWeightingFactor(people2, 0.75)}));
+  EXPECT_EQ(2u, zoneMRTCalculation.numberofMRTWeightingFactors());
+
+  zoneMRTCalculation.removeMRTWeightingFactor(0);
+  EXPECT_EQ(1u, zoneMRTCalculation.numberofMRTWeightingFactors());
+
+  zoneMRTCalculation.removeAllMRTWeightingFactors();
+  EXPECT_EQ(0u, zoneMRTCalculation.numberofMRTWeightingFactors());
+}
