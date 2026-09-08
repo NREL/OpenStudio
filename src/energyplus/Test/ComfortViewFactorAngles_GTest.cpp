1c973026872706212a454783fe9b730e70f6fbd2
Joe Robertson <joseph.robertson@nrel.gov>
2026-09-08 10:29:14 -0700

    Test ComfortViewFactorAngles translation

---
+++ src/energyplus/Test/ComfortViewFactorAngles_GTest.cpp
@@ -0,0 +1,126 @@
+/***********************************************************************************************************************
+*  OpenStudio(R), Copyright (c) Alliance for Energy Innovation, LLC.
+*  See also https://openstudio.net/license
+***********************************************************************************************************************/
+
+#include <gtest/gtest.h>
+#include "EnergyPlusFixture.hpp"
+
+#include "../ForwardTranslator.hpp"
+#include "../ReverseTranslator.hpp"
+
+#include "../../model/ComfortViewFactorAngles.hpp"
+#include "../../model/Model.hpp"
+#include "../../model/Space.hpp"
+#include "../../model/Surface.hpp"
+#include "../../model/ThermalZone.hpp"
+
+#include <utilities/idd/BuildingSurface_Detailed_FieldEnums.hxx>
+#include <utilities/idd/IddEnums.hxx>
+
+using namespace openstudio::energyplus;
+using namespace openstudio::model;
+using namespace openstudio;
+
+TEST_F(EnergyPlusFixture, ForwardTranslator_ComfortViewFactorAngles) {
+  Model model;
+  ThermalZone zone(model);
+  Space space(model);
+  EXPECT_TRUE(space.setThermalZone(zone));
+
+  Point3dVector points{{0, 0, 0}, {1, 0, 0}, {1, 1, 0}};
+  Surface surface(points, model);
+  surface.setName("Radiant Surface");
+  EXPECT_TRUE(surface.setSpace(space));
+
+  ComfortViewFactorAngles comfortViewFactorAngles(model);
+  comfortViewFactorAngles.setName("Angle Factors");
+  EXPECT_TRUE(comfortViewFactorAngles.addComfortViewFactorAngle(surface, 1.0));
+
+  ForwardTranslator forwardTranslator;
+  Workspace workspace = forwardTranslator.translateModelObject(comfortViewFactorAngles);
+
+  const auto angleFactorObjects = workspace.getObjectsByType(IddObjectType::ComfortViewFactorAngles);
+  ASSERT_EQ(1u, angleFactorObjects.size());
+  const auto& angleFactorObject = angleFactorObjects.front();
+  EXPECT_EQ("Angle Factors", angleFactorObject.nameString());
+  ASSERT_EQ(1u, angleFactorObject.numExtensibleGroups());
+  const auto group = angleFactorObject.extensibleGroups().front();
+  EXPECT_EQ("Radiant Surface", group.getString(0).get());
+  EXPECT_DOUBLE_EQ(1.0, group.getDouble(1).get());
+}
+
+TEST_F(EnergyPlusFixture, ForwardTranslator_ComfortViewFactorAngles_InvalidSum) {
+  Model model;
+  ThermalZone zone(model);
+  Space space(model);
+  EXPECT_TRUE(space.setThermalZone(zone));
+
+  Point3dVector points{{0, 0, 0}, {1, 0, 0}, {1, 1, 0}};
+  Surface surface(points, model);
+  EXPECT_TRUE(surface.setSpace(space));
+
+  ComfortViewFactorAngles comfortViewFactorAngles(model);
+  EXPECT_TRUE(comfortViewFactorAngles.addComfortViewFactorAngle(surface, 0.5));
+
+  ForwardTranslator forwardTranslator;
+  Workspace workspace = forwardTranslator.translateModelObject(comfortViewFactorAngles);
+  EXPECT_TRUE(workspace.getObjectsByType(IddObjectType::ComfortViewFactorAngles).empty());
+}
+
+TEST_F(EnergyPlusFixture, ForwardTranslator_ComfortViewFactorAngles_UntranslatedSurface) {
+  Model model;
+  Point3dVector points{{0, 0, 0}, {1, 0, 0}, {1, 1, 0}};
+  Surface surface(points, model);
+
+  ComfortViewFactorAngles comfortViewFactorAngles(model);
+  EXPECT_TRUE(comfortViewFactorAngles.addComfortViewFactorAngle(surface, 1.0));
+
+  ForwardTranslator forwardTranslator;
+  Workspace workspace = forwardTranslator.translateModelObject(comfortViewFactorAngles);
+  EXPECT_TRUE(workspace.getObjectsByType(IddObjectType::ComfortViewFactorAngles).empty());
+}
+
+TEST_F(EnergyPlusFixture, ReverseTranslator_ComfortViewFactorAngles) {
+  Workspace workspace(StrictnessLevel::Minimal, IddFileType::EnergyPlus);
+
+  auto zone = workspace.addObject(IdfObject(IddObjectType::Zone)).get();
+  zone.setName("Thermal Zone 1");
+
+  auto surface = workspace.addObject(IdfObject(IddObjectType::BuildingSurface_Detailed)).get();
+  surface.setName("Surface 1");
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::SurfaceType, "Wall"));
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::ConstructionName, ""));
+  EXPECT_TRUE(surface.setPointer(BuildingSurface_DetailedFields::ZoneName, zone.handle()));
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::OutsideBoundaryCondition, "Outdoors"));
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::OutsideBoundaryConditionObject, ""));
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::SunExposure, "SunExposed"));
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::WindExposure, "WindExposed"));
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::ViewFactortoGround, ""));
+  EXPECT_TRUE(surface.setString(BuildingSurface_DetailedFields::NumberofVertices, ""));
+  for (const Point3d& point : Point3dVector{{0, 0, 0}, {1, 0, 0}, {1, 1, 0}}) {
+    auto group = surface.pushExtensibleGroup();
+    group.setDouble(0, point.x());
+    group.setDouble(1, point.y());
+    group.setDouble(2, point.z());
+  }
+
+  auto angleFactorObject = workspace.addObject(IdfObject(IddObjectType::ComfortViewFactorAngles)).get();
+  angleFactorObject.setName("Angle Factors");
+  auto group = angleFactorObject.pushExtensibleGroup();
+  EXPECT_TRUE(group.setPointer(0, surface.handle()));
+  EXPECT_TRUE(group.setDouble(1, 1.0));
+
+  ReverseTranslator reverseTranslator;
+  Model model = reverseTranslator.translateWorkspace(workspace);
+
+  const auto angleFactors = model.getConcreteModelObjects<ComfortViewFactorAngles>();
+  ASSERT_EQ(1u, angleFactors.size());
+  const auto& comfortViewFactorAngles = angleFactors.front();
+  EXPECT_EQ("Angle Factors", comfortViewFactorAngles.nameString());
+  ASSERT_EQ(1u, comfortViewFactorAngles.numberofComfortViewFactorAngles());
+  const auto comfortViewFactorAngle = comfortViewFactorAngles.getComfortViewFactorAngle(0);
+  ASSERT_TRUE(comfortViewFactorAngle);
+  EXPECT_EQ("Surface 1", comfortViewFactorAngle->surface().nameString());
+  EXPECT_DOUBLE_EQ(1.0, comfortViewFactorAngle->angleFactor());
+}
